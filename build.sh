c2e488c7fc425da6648d97c04b3c3f5353a247e9
John Morley <john.morley@bjss.com>
2018-10-25 14:49:31 +0100

    Remove unneeded options from build script

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@ ionic cordova build ios
 xcodebuild \
     -workspace "platforms/ios/Mobile Examiner.xcworkspace" \
     -scheme "Mobile Examiner" \
-    -configuration "Release" \
-    -sdk iphoneos \
     -archivePath "platforms/ios/Mobile Examiner.xcarchive" \
-    CODE_SIGN_IDENTITY="" \
     CODE_SIGNING_REQUIRED=NO \
+    CODE_SIGN_IDENTITY="" \
     clean archive
