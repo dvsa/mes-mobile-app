6c2c4d858b1245bf7323c8084967b77c69c0ae85
John Morley <john.morley@bjss.com>
2018-10-26 16:34:26 +0100

    Add cordova hook to create timestamp based patch version

---
+++ hooks/before_prepare/increment-build-number.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+// Save hook under `project-root/hooks/before_prepare/`
+//
+// Don't forget to install xml2js using npm
+// `$ npm install xml2js`
+
+var fs = require('fs');
+var xml2js = require('xml2js');
+
+const configFile = 'config.xml';
+
+// Read config.xml
+fs.readFile(configFile, 'utf8', (err, xml) => {
+  if (err) {
+    return console.console.error(err);
+  }
+
+  // Parse XML to JS Obj
+  xml2js.parseString(xml, (err, obj) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    const currentVersion = obj.widget['$'].version;
+    const newVersion = `${currentVersion.slice(0, currentVersion.lastIndexOf('.'))}.${Math.floor(
+      Date.now() / 1000
+    )}`;
+    obj.widget['$'].version = newVersion;
+
+    var builder = new xml2js.Builder();
+    var xml = builder.buildObject(obj);
+
+    fs.writeFile(configFile, xml, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+      console.log(`Build number successfully incremented to ${newVersion}`);
+    });
+  });
+});
