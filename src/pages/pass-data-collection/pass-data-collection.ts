e43f7340c5cddfd79ab4ca67d8d2fceebeb1e670
John Morley <john.morley@bjss.com>
2018-10-30 15:57:48 +0000

    Add validation to pass finalisation submission button
    * Only allow submission of pass finalisation form once a choice for
      provisional provided and certificate number have been input

--- src/pages/pass-data-collection/pass-data-collection.ts
+++ src/pages/pass-data-collection/pass-data-collection.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { HealthDeclarationPage } from '../health-declaration/health-declaration';
 import { Page } from 'ionic-angular/navigation/nav-util';
+import { isNil } from 'lodash';
 
 @Component({
   selector: 'page-pass-data-collection',
@@ -9,10 +10,21 @@ import { Page } from 'ionic-angular/navigation/nav-util';
 })
 export class PassDataCollectionPage {
   healthDeclarationPage: Page = HealthDeclarationPage;
+  passCertificateNumber: string;
+  provisionalProvidedSelection: boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   provisionalInputChanged(event, secondInput) {
     secondInput.checked = false;
+    this.provisionalProvidedSelection = event.target.checked;
+  }
+
+  validation() {
+    const passCertNumberValid = !isNil(this.passCertificateNumber) && this.passCertificateNumber.trim().length > 0;
+    return [
+      passCertNumberValid,
+      this.provisionalProvidedSelection
+    ].some((p) => p === false);
   }
 }
