cb618af8a7692f6f2a41ef8084b55368b5aae1a2
Andrew Lai <andrew.lai@bjss.com>
2018-10-31 14:18:19 +0000

    Removed commented out options as per review

--- generateTestReport.js
+++ generateTestReport.js
@@ -6,14 +6,6 @@ var options = {
   output: 'test-reports/cucumber-report.html',
   reportSuiteAsScenarios: true,
   launchReport: false
-  // metadata: {
-  //     "App Version":"0.3.2",
-  //     "Test Environment": "STAGING",
-  //     "Browser": "Chrome  54.0.2840.98",
-  //     "Platform": "Windows 10",
-  //     "Parallel": "Scenarios",
-  //     "Executed": "Remote"
-  // }
 };
 
 reporter.generate(options);
