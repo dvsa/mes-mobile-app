ee85aaf111376c3fc77463e7d2d27c3384838947
Robert Stepnowski <r.stepnowski@kainos.com>
2018-11-12 16:03:22 +0000

    MES-1036 initial structure and styling for VIEW CANDIDATE page MES-1036 additional styling + fixes to candidate address formatter MES-1036 improved address formatter and fixed failing unit tests MES-1036 added candidate's special needs to VIEW CANDIDATE DETAILS page MES-1036 removed old todo MES-1036 small fixes around spacing of the header MES-1036 fix for missing bolding of labels + typo fix + candidate details data fixes MES-1036 fixed typo in function name + removed unused param in function call MES-1036 removed unnecessary css from REQUEST SWAP button

--- src/shared/utils/formatters.ts
+++ src/shared/utils/formatters.ts
@@ -1,5 +1,5 @@
 import { IAddress, ICandidateName } from '../../providers/journal/journal-model';
-
+import { isNonBlankString } from './string-utils';
 /**
  * Returns concatenated Candidate name
  */
@@ -14,11 +14,11 @@ export function getFormattedCandidateName(candidateName: ICandidateName): string
  */
 export function getFormattedAddress(address?: IAddress): string {
   return `
-     ${address.line1}<br>
-     ${address.line2}<br>
-     ${address.line3}<br>
-     ${address.line4}<br>
-     ${address.line5}<br>
-     ${address.postcode}
+     ${isNonBlankString(address.line1) ? `${address.line1}<br />` : ''}
+     ${isNonBlankString(address.line2) ? `${address.line2}<br />` : ''}
+     ${isNonBlankString(address.line3) ? `${address.line3}<br />` : ''}
+     ${isNonBlankString(address.line4) ? `${address.line4}<br />` : ''}
+     ${isNonBlankString(address.line5) ? `${address.line5}<br />` : ''}
+     ${isNonBlankString(address.postcode) ? `${address.postcode}<br />` : ''}
   `;
 }
