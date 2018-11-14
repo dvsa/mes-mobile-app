a9397f2f4d2df530f88baa78a279f2d5748c9452
Ammar Haider <ammar.haider@bjss.com>
2018-11-14 11:55:06 +0000

    adds links to top-level help

--- src/help/pages/help-section/help-section.ts
+++ src/help/pages/help-section/help-section.ts
@@ -1,5 +1,11 @@
+import { HelpWaitingRoomToCarPage } from './../help-waiting-room-to-car/help-waiting-room-to-car';
+import { HelpTestReportPage } from './../help-test-report/help-test-report';
+import { HelpJournalPage } from './../help-journal/help-journal';
+import { HelpFinalisationSubmissionPage } from './../help-finalisation-submission/help-finalisation-submission';
+import { HelpDebriefPage } from './../help-debrief/help-debrief';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Page } from 'ionic-angular/navigation/nav-util';
 
 @Component({
   selector: 'page-help-section',
@@ -7,6 +13,11 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class HelpSectionPage {
   title: 'Help';
+  helpDebriefPage: Page = HelpDebriefPage;
+  helpFinalisationSubmissionPage: Page = HelpFinalisationSubmissionPage;
+  helpJournalPage: Page = HelpJournalPage;
+  helpTestReportPage: Page = HelpTestReportPage;
+  helpWaitingRoomToCarPage: Page = HelpWaitingRoomToCarPage;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 }
