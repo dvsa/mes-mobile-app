f5b300b9e2693cf01e9cf1745fad04498bd526d9
Szabolcs Keresztes <keresztes.szabolcs93@gmail.com>
2018-11-15 10:49:10 +0000

    Mes 958 faults on show me tell me question (#40)
    * MES - 958
    Implementing Show me button to record the tell me faults against it. Also to be able to complete the action.
    
    implementing press and tap for show me button
    
    refactoring the controlled stop press and tap so that it fits in with the show me button actions
    
    removing unnecessary comments
    
    Carrying over the faults from the Tell me question to the Show me button
    
    calling the right methods to persist the fault recorded on tell me question
    
    Fixing the drivers fault position on the Show me button when the fault comes from Tell me question
    
    Adjusting styling
    
    * Setting the driving fault back to the usual place but leaving on the show me button a bit indented
    
    * Introducing an object to set the right faults on Show me button
    
    * removing an unnecesarry loop

--- src/components/all-on-one-form-sub-element-hold-no-modal/all-on-one-form-sub-element-hold-no-modal.ts
+++ src/components/all-on-one-form-sub-element-hold-no-modal/all-on-one-form-sub-element-hold-no-modal.ts
@@ -55,6 +55,10 @@ export class AllOnOneFormSubElementHoldNoModalComponent {
     });
   }
 
+  isShowMeSection() {
+    return this.section === 'showMe';
+  }
+
   resetHammer(newDuration: number) {
     this.mc.remove('press');
     const hammerPress = new Hammer.Press({
@@ -75,7 +79,8 @@ export class AllOnOneFormSubElementHoldNoModalComponent {
       return;
     }
     // Prevent more than one fault being added to controlled stop
-    if (this.section === 'controlledStop' && this.faultCounter > 0) return;
+    if ((this.section === 'controlledStop' || this.section === 'showMe') && this.faultCounter > 0)
+      return;
     this.faultStore.addFault(this.section, 'fault');
   }
 
