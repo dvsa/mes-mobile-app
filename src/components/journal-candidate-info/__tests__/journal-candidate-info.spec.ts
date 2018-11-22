f85e8dac4f4fcbe1d4add3e0ecb765761b6c7fe9
John Morley <john.morley@bjss.com>
2018-11-22 13:49:01 +0000

    Start writing some DOM tests for candidate info test completion

--- src/components/journal-candidate-info/__tests__/journal-candidate-info.spec.ts
+++ src/components/journal-candidate-info/__tests__/journal-candidate-info.spec.ts
@@ -25,26 +25,48 @@ describe('JournalCandidateInfoComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeDefined();
-  });
+  describe('Class', () => {
+    it('should create', () => {
+      expect(component).toBeDefined();
+    });
 
-  describe('getName', () => {
-    it('should return the result of passing the candidate name to the name formatter', () => {
-      mockGetFormattedCandidateName.mockReturnValue('Joe Bloggs');
-      expect(component.name).toBe('Joe Bloggs');
-      expect(mockGetFormattedCandidateName).toHaveBeenCalled();
+    describe('getName', () => {
+      it('should return the result of passing the candidate name to the name formatter', () => {
+        mockGetFormattedCandidateName.mockReturnValue('Joe Bloggs');
+        expect(component.name).toBe('Joe Bloggs');
+        expect(mockGetFormattedCandidateName).toHaveBeenCalled();
+      });
+    });
+
+    describe('extractCategoryCode', () => {
+      it('should return N/A for a null test category', () => {
+        component.testCategory = null;
+        expect(component.categoryCode).toBe('N/A');
+      });
+      it('should extract the textual part of test category', () => {
+        component.testCategory = 'B57mins';
+        expect(component.categoryCode).toBe('B');
+      });
     });
   });
 
-  describe('extractCategoryCode', () => {
-    it('should return N/A for a null test category', () => {
-      component.testCategory = null;
-      expect(component.categoryCode).toBe('N/A');
-    });
-    it('should extract the textual part of test category', () => {
-      component.testCategory = 'B57mins';
-      expect(component.categoryCode).toBe('B');
+  describe('DOM', () => {
+    describe('test completion class', () => {
+      describe('when the test is not complete', () => {
+        it('should not be applied', () => {
+          component.testComplete = false;
+          const topLevelDiv: HTMLElement = fixture.nativeElement;
+          expect(topLevelDiv.classList).not.toContain('journal-candidate-info-test-complete-text');
+        });
+      });
+      describe('when the test is complete', () => {
+        it('should be applied', () => {
+          component.testComplete = true;
+          const toplevelDiv: HTMLElement = fixture.nativeElement;
+          fixture.detectChanges();
+          // expect(toplevelDiv.className).toBe('journal-candidate-info-test-complete-text');
+        });
+      });
     });
   });
 });
