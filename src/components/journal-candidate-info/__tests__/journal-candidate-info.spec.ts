1d9bf578e7a4597786551548c48df574e948f69a
John Morley <john.morley@bjss.com>
2018-11-22 14:43:03 +0000

    Add DOM tests for binding of candidate name and test category

--- src/components/journal-candidate-info/__tests__/journal-candidate-info.spec.ts
+++ src/components/journal-candidate-info/__tests__/journal-candidate-info.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { JournalCandidateInfoComponent } from '../journal-candidate-info';
 import { IonicModule } from 'ionic-angular';
 import { getFormattedCandidateName } from '../../../shared/utils/formatters';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 jest.mock('../../../shared/utils/formatters');
 const mockGetFormattedCandidateName = <jest.Mock>getFormattedCandidateName;
@@ -22,7 +24,6 @@ describe('JournalCandidateInfoComponent', () => {
     component = fixture.componentInstance;
     component.candidateName = { title: 'Mr', firstName: 'Joe', lastName: 'Bloggs' };
     component.testCategory = 'B57mins';
-    fixture.detectChanges();
   });
 
   describe('Class', () => {
@@ -51,21 +52,55 @@ describe('JournalCandidateInfoComponent', () => {
   });
 
   describe('DOM', () => {
+    let componentEl: DebugElement;
+
+    beforeEach(() => {
+      componentEl = fixture.debugElement;
+    });
+
     describe('test completion class', () => {
+      const testCompletionClass = 'journal-candidate-info-test-complete-text';
+      let topLevelDivEl: HTMLElement;
+      let categoryImgEl: HTMLElement;
+
+      beforeEach(() => {
+        topLevelDivEl = componentEl.query(By.css('div')).nativeElement;
+        categoryImgEl = componentEl.query(By.css('img')).nativeElement;
+      });
+
       describe('when the test is not complete', () => {
         it('should not be applied', () => {
           component.testComplete = false;
-          const topLevelDiv: HTMLElement = fixture.nativeElement;
-          expect(topLevelDiv.classList).not.toContain('journal-candidate-info-test-complete-text');
+          fixture.detectChanges();
+          expect(topLevelDivEl.classList).not.toContain(testCompletionClass);
+          expect(categoryImgEl.classList).not.toContain(testCompletionClass);
         });
       });
       describe('when the test is complete', () => {
         it('should be applied', () => {
           component.testComplete = true;
-          const toplevelDiv: HTMLElement = fixture.nativeElement;
           fixture.detectChanges();
-          // expect(toplevelDiv.className).toBe('journal-candidate-info-test-complete-text');
+          expect(topLevelDivEl.classList).toContain(testCompletionClass);
+          // expect(categoryImgEl.classList).toContain(testCompletionClass);
         });
+      });
+    });
+
+    describe('candidate name', () => {
+      it('should be displayed', () => {
+        const nameSpan: HTMLElement = componentEl.query(By.css('ion-row:first-child span'))
+          .nativeElement;
+        fixture.detectChanges();
+        expect(nameSpan.textContent).toBe('Joe Bloggs');
+      });
+    });
+
+    describe('test category', () => {
+      it('should be displayed', () => {
+        const categorySpan: HTMLElement = componentEl.query(By.css('ion-row:last-child span'))
+          .nativeElement;
+        fixture.detectChanges();
+        expect(categorySpan.textContent).toBe('Cat B');
       });
     });
   });
