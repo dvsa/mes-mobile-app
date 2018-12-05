5a13608c3f9d891f9b817df6e08e003b0fe52d3e
szabi.keresztes <szabi.keresztes@bjss.com>
2018-12-05 17:08:49 +0000

    Removing header and card from custom components

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
-import { IonicModule } from 'ionic-angular';
-import { MesHeaderComponent } from './mes-header/mes-header';
-import { MesCardComponent } from './mes-card/mes-card';
 @NgModule({
-	declarations: [MesHeaderComponent,
-    MesCardComponent],
-	imports: [ IonicModule ],
-	exports: [MesHeaderComponent,
-    MesCardComponent]
+	declarations: [],
+	imports: [],
+	exports: []
 })
 export class ComponentsModule {}
