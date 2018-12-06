16df652f663e8ccd08f38b3a88b75e2a2d712fd2
Ammar Haider <ammar.haider@bjss.com>
2018-12-06 15:23:46 +0000

    updates showcase video and analytics id

--- src/providers/app-config/app-config.ts
+++ src/providers/app-config/app-config.ts
@@ -15,8 +15,8 @@ export class AppConfigProvider {
   };
 
   // todo - needs to be commented in for deployment for familiarisation
-  // googleAnalyticsId: string = 'UA-129489007-1'; // Key for beta
-  googleAnalyticsId: string = 'UA-129814222-1'; // Brians test key for development until we deploy for familiarisation
+  googleAnalyticsId: string = 'UA-129489007-1'; // Key for beta
+  // googleAnalyticsId: string = 'UA-129814222-1'; // Brians test key for development until we deploy for familiarisation
   userIdDimensionIndex: number = 1; // This is the userId dimension index
 
   constructor() {
