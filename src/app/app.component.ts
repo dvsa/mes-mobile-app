f5bc361117edf10eeb36f9f38d81a01ded7e246d
Tom Billington <t.billington@kainos.com>
2018-12-11 15:25:49 +0000

    Added Authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component } from "@angular/core";
+import { Platform } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+
+import { AuthenticationServiceProvider } from "../providers/authentication-service/authentication-service";
 
 @Component({
   templateUrl: 'app.html'
@@ -12,7 +14,8 @@ export class App {
   constructor(
     platform: Platform,
     statusBar: StatusBar,
-    splashScreen: SplashScreen
+    splashScreen: SplashScreen,
+    authenticationService: AuthenticationServiceProvider
   ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -20,6 +23,11 @@ export class App {
       statusBar.styleDefault();
       statusBar.overlaysWebView(false);
       splashScreen.hide();
+
+      // Attempt to login if on an ios device
+      if (platform.is('ios')) {
+        authenticationService.login();
+      }
     });
   }
 }
