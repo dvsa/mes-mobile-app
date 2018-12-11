698d05a8e7c52c7afba39477e1145c533ed23d28
Ross Faulds <ross.faulds@gmailcom>
2018-12-11 10:06:27 +0000

    Added test to confirm ngFor works correctly

--- src/pages/journal/__tests__/journal.specs.ts
+++ src/pages/journal/__tests__/journal.specs.ts
@@ -1,9 +1,11 @@
 import { ComponentFixture, async, TestBed } from '@angular/core/testing';
 import { IonicModule, NavController, NavParams, Config } from 'ionic-angular';
 import { NavControllerMock, NavParamsMock, ConfigMock } from 'ionic-mocks-jest';
+import { By } from '@angular/platform-browser';
 
 import { AppModule } from '../../../app/app.module';
 import { JournalPage } from '../journal';
+import { DebugElement } from '@angular/core';
 
 describe('JournalPage', () => {
   let fixture: ComponentFixture<JournalPage>;
@@ -38,5 +40,18 @@ describe('JournalPage', () => {
 
   describe('DOM', () => {
     // Unit tests for the components template
+    let componentEl: DebugElement;
+
+    beforeEach(() => {
+      componentEl = fixture.debugElement;
+    });
+
+    it('there should be one card for every journal entry', () => {
+      const slotsList = componentEl.query(By.css('ion-list'));
+      expect(slotsList.children.length).toBe(0);
+      fixture.detectChanges();
+      expect(slotsList.children.length).toBe(7);
+      expect(slotsList.children.every((child) => child.name === 'ion-card')).toBeTruthy();
+    }); 
   });
 });
