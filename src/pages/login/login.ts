f0e0f3c12fdd04e4f9dbb84fcc37a6dfdf965524
Tom Billington <t.billington@kainos.com>
2018-12-13 12:47:01 +0000

    Now block access to pages if user is  not authorised

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,20 +1,31 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+
 import { AuthenticationServiceProvider } from '../../providers/authentication-service/authentication-service';
+import { BasePageComponenet } from '../../classes/base-page';
 
 @IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
-export class LoginPage {
+export class LoginPage extends BasePageComponenet {
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private authenticationService: AuthenticationServiceProvider) {}
+    public platform: Platform,
+    public authenticationService: AuthenticationServiceProvider,
+  ) {
+    super(platform, navCtrl, authenticationService, false);
+  }
 
 
-  logout() {
-    this.authenticationService.logout();
+  async login() {
+    await this.authenticationService.login();
+
+    if (this.authenticationService.isAuthenticated()) {
+      this.navController.push('JournalPage');
+    }
   }
 }
--- src/pages/test-report/test-report.ts
+++ src/pages/test-report/test-report.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { BasePageComponenet } from '../../classes/base-page';
+import { AuthenticationServiceProvider } from '../../providers/authentication-service/authentication-service';
 
 @IonicPage()
 @Component({
   selector: 'page-test-report',
   templateUrl: 'test-report.html'
 })
-export class TestReportPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+export class TestReportPage extends BasePageComponenet {
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public platform: Platform,
+    public authenticationService: AuthenticationServiceProvider
+  ) {
+    super(platform, navCtrl, authenticationService)
+  }
 }
