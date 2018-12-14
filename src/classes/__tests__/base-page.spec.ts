3aeebc5f34a38785bc6aeb577bb263cb99805f06
szabi.keresztes <szabi.keresztes@bjss.com>
2018-12-14 17:12:51 +0000

    Initial implementation of base-page unit tests

---
+++ src/classes/__tests__/base-page.spec.ts
@@ -0,0 +1,44 @@
+
+import { Platform, NavController } from 'ionic-angular';
+import { PlatformMock, NavControllerMock } from 'ionic-mocks-jest';
+import { TestBed } from '@angular/core/testing';
+
+import { AuthenticationServiceProvider } from '../../providers/authentication-service/authentication-service'; 
+import { AuthenticationServiceProviderMock } from '../../providers/authentication-service/authentication-service.mock';
+
+import { BasePageComponent } from '../base-page'
+
+
+describe('Base Page', () => {
+
+  let basePageComponent: BasePageComponent;
+
+  let platform: Platform;
+  let navController: NavController;
+  // let authenticationService: AuthenticationServiceProvider;
+  const loginRequired: boolean = true;
+
+  beforeAll(() => {
+    platform = PlatformMock.instance();
+    navController = NavControllerMock.instance();
+    // authenticationService = new AuthenticationServiceProvider();
+
+    class BasePageClass extends BasePageComponent {
+      constructor() {
+        super(platform, navController, null, loginRequired);
+      }
+    }
+
+    basePageComponent = new BasePageClass();
+  });
+
+  it('should be an instance of PageUtils', (done) => {
+    done();
+  });
+
+  it('isIos return true', () => {
+    expect(basePageComponent.isIos()).toBe(true);
+  });
+
+
+});
