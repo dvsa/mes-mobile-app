e917c95f4f74ef15b80293a42849ae69cd213421
Brian Simpson <brian@inhand.co.uk>
2018-12-17 14:44:40 +0000

    Revert error

--- src/providers/journal/journal.ts
+++ src/providers/journal/journal.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class JournalProvider {
 
-  url = 'https://vulv731rce.execute-api.eu-west-1.amazonaws.com/default';
+  url = 'https://vulv731rce.execute-api.eu-west-1.amazonaws.com/default'; // todo - pull this from config when added
 
   constructor(public http: HttpClient) {}
 
