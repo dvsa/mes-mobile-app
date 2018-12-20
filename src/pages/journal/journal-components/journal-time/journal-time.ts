7a679470ed1197703c2b5b955616fac16245b149
Ross Faulds <ross.faulds@gmailcom>
2018-12-20 13:18:47 +0000

    add styling to  journal page

--- src/pages/journal/journal-components/journal-time/journal-time.ts
+++ src/pages/journal/journal-components/journal-time/journal-time.ts
@@ -3,12 +3,19 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'journal-time',
   templateUrl: 'journal-time.html',
-  inputs: ['time']
+  inputs: ['time','testComplete']
 })
 export class JournalTimeComponent {
 
   @Input()
   time: string;
+
+  @Input()
+  testComplete: boolean;
+
+  ngOnInit() {
+    this.testComplete = false; // TODO pick up testComplete from JSON when available
+  }
 
   constructor() {
   }
