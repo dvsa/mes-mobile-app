16edd13472726cdc4fbc3b1346b055c306c89530
Tom Billington <t.billington@kainos.com>
2019-01-02 14:39:08 +0000

    updated journal endpoint config

--- src/environment/environment.production.ts
+++ src/environment/environment.production.ts
@@ -2,6 +2,5 @@ import { EnvironmentFile } from './models/environment.model';
 
 export const environment: EnvironmentFile = {
   isRemote: true,
-  remoteSettingsUrl: 'assets/config/environmentProduction.json',
-  journalEndPoint: 'https://vulv731rce.execute-api.eu-west-1.amazonaws.com/default'
+  remoteSettingsUrl: 'assets/config/environmentProduction.json'
 };
--- src/environment/environment.uat.ts
+++ src/environment/environment.uat.ts
@@ -2,6 +2,5 @@ import { EnvironmentFile } from './models/environment.model';
 
 export const environment: EnvironmentFile = {
   isRemote: true,
-  remoteSettingsUrl: 'assets/config/environmentUat.json',
-  journalEndPoint: 'https://vulv731rce.execute-api.eu-west-1.amazonaws.com/default'
+  remoteSettingsUrl: 'assets/config/environmentUat.json'
 };
