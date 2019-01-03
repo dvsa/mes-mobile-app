a2e1868705d582b7c5588fa007e8b8f9c78a74a8
Ammar Haider <ammar.haider@bjss.com>
2019-01-03 15:39:07 +0000

    fixes resourceUrl in AD config

--- src/environment/environment.dev.ts
+++ src/environment/environment.dev.ts
@@ -6,7 +6,7 @@ export const environment : EnvironmentFile = {
   userIdDimensionIndex: 1,
   authentication: {
     context: 'https://login.windows.net/common',
-    resourceUrl: 'https://graph.windows.net',
+    resourceUrl: '09fdd68c-4f2f-45c2-be55-dd98104d4f74',
     clientId: '09fdd68c-4f2f-45c2-be55-dd98104d4f74',
     redirectUrl: 'x-msauth-uk-gov-dvsa-mobile-examiner://uk.gov.dvsa.mobile-examiner',
     logoutUrl: 'ttps://login.windows.net/6c448d90-4ca1-4caf-ab59-0a2aa67d7801/oauth2/logout',
