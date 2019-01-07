4daf03b268062691f2ce871b52081d3b822d67f9
Ammar Haider <ammar.haider@bjss.com>
2019-01-07 11:11:24 +0000

    fixes setting of auth headers in incterceptor

--- src/providers/authentication/interceptor.ts
+++ src/providers/authentication/interceptor.ts
@@ -1,4 +1,5 @@
-import { map, switchMap } from 'rxjs/operators';
+import { CredentialsOptions } from 'aws-sdk/lib/credentials';
+import { switchMap, map } from 'rxjs/operators';
 import { Observable } from 'rxjs/Observable';
 import { from } from 'rxjs/Observable/from';
 import { Injectable } from '@angular/core';
@@ -27,6 +28,7 @@ export class AuthInterceptor implements HttpInterceptor {
     appConfig: AppConfigProvider) {
       this.authenticationSettings = appConfig.getAppConfig().authentication;
       this.region = appConfig.getAppConfig().aws.region;
+      config.region = this.region;
     }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -34,37 +36,40 @@ export class AuthInterceptor implements HttpInterceptor {
     if (!this.platform.is('ios')) return next.handle(request);
     const { host, pathname } = new URL(request.url);
 
-    return this.createCredentials().pipe(
-      map(creds => this.sign(creds, host, pathname)),
-      switchMap((signature) => {
-        const signedRequest = request.clone({
-          setHeaders: {
-            ...signature
-          }
-        });
-        return next.handle(signedRequest);  
-      })
-    );
+    return this.createCredentials()
+      .pipe(
+        map(() => this.sign(config.credentials, host, pathname)),
+        switchMap((signature: any) => {
+          const signedRequest = request.clone({
+            setHeaders: {
+              ...signature.headers
+            }
+          });
+          return next.handle(signedRequest);
+        })
+      );
   
   }
 
-  private sign(credentials: void, host: string, path: string) {
+  private sign(credentials: CredentialsOptions, host: string, path: string): any {
+    const { accessKeyId, secretAccessKey, sessionToken } = credentials;
     const options = {
       host,
       path,
       region: this.region,
     }
-    return sign(options, credentials);
+    sign(options, { accessKeyId, secretAccessKey, sessionToken });
+    return options;
   }
 
   private createCredentials(): Observable<void>  {
-    config.region = this.region;
     const cognitoIdentity = new CognitoIdentityCredentials({
       IdentityPoolId: this.authenticationSettings.identityPoolId,
       Logins: {
         [this.authenticationSettings.openIdConnectUrl]: this.auth.getAuthenticationToken()
       }
     });
+    config.credentials = cognitoIdentity;
     return from(cognitoIdentity.getPromise());
   }
 }
