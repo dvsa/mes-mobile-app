c83d2d40fcdcb763c25046948f6ddabf14f829dc
John Morley <john.morley@bjss.com>
2019-01-07 17:19:08 +0000

    Send If-Modified-Since header on subsequent journal requests

--- src/providers/journal/journal.ts
+++ src/providers/journal/journal.ts
@@ -1,14 +1,29 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AppConfigProvider } from '../app-config/app-config';
+import moment from 'moment';
 
 @Injectable()
 export class JournalProvider {
 
   constructor(public http: HttpClient, public appConfig: AppConfigProvider) {}
 
+  lastGet: Date;
+
   getJournal(){
-    return this.http.get(this.appConfig.getAppConfig().journal.journalUrl);
+    const prevGet = this.lastGet;
+    this.lastGet = new Date();
+
+    let get$;
+    if (prevGet === undefined) {
+      get$ = this.http.get(this.appConfig.getAppConfig().journal.journalUrl);
+    } else {
+      const options = {
+        headers: new HttpHeaders().set('If-Modified-Since', `${moment(prevGet).format('ddd[,] D MMM YYYY HH:mm:ss [GMT]')}`),
+      };
+      get$ = this.http.get(this.appConfig.getAppConfig().journal.journalUrl, options);
+    }
+    return get$;
   }
 
 }
