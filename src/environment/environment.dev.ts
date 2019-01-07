88ddc074d0b092eb35add1d0689fe3390385ace9
Ammar Haider <ammar.haider@bjss.com>
2019-01-07 11:10:55 +0000

    fixes typo in logoutUrl

--- src/environment/environment.dev.ts
+++ src/environment/environment.dev.ts
@@ -9,7 +9,7 @@ export const environment : EnvironmentFile = {
     resourceUrl: '09fdd68c-4f2f-45c2-be55-dd98104d4f74',
     clientId: '09fdd68c-4f2f-45c2-be55-dd98104d4f74',
     redirectUrl: 'x-msauth-uk-gov-dvsa-mobile-examiner://uk.gov.dvsa.mobile-examiner',
-    logoutUrl: 'ttps://login.windows.net/6c448d90-4ca1-4caf-ab59-0a2aa67d7801/oauth2/logout',
+    logoutUrl: 'https://login.windows.net/6c448d90-4ca1-4caf-ab59-0a2aa67d7801/oauth2/logout',
     openIdConnectUrl: 'sts.windows.net/6c448d90-4ca1-4caf-ab59-0a2aa67d7801',
     identityPoolId: 'eu-west-1:f5a0346e-9bbb-4153-affd-bbe59cd5b7a3',
   },
