50c7c9c11e277b71dc282a3aa90f6e42f2809973
John Morley <john.morley@bjss.com>
2019-01-08 21:42:08 +0000

    Working generation of new JournalSlots detecting updates via diff

--- src/pages/journal/utils/newSlotsDetectingChanges.ts
+++ src/pages/journal/utils/newSlotsDetectingChanges.ts
@@ -1,7 +1,21 @@
 import { JournalSlot } from '../domain/JournalSlot';
 import { ExaminerWorkSchedule } from '../../../common/domain/DJournal';
+import diff from 'deep-diff';
 
 export default function(oldJournalSlots: JournalSlot[], newJournal: ExaminerWorkSchedule): JournalSlot[] {
   const newSlots = newJournal.testSlot;
-  return newSlots.map(newSlot => (new JournalSlot(newSlot, true)));
+  return newSlots.map(newSlot => {
+    const newSlotId = newSlot.slotDetail.slotId;
+    const replacedJournalSlot = oldJournalSlots.find(oldSlot => oldSlot.slot.slotDetail.slotId === newSlotId);
+
+    let differenceFound = false;
+    if (replacedJournalSlot) {
+      const differenceToSlot = diff(replacedJournalSlot.slot, newSlot);
+      if (Array.isArray(differenceToSlot) && differenceToSlot.some(change => change.kind === 'E')) {
+        differenceFound = true;
+      }
+    }
+
+    return new JournalSlot(newSlot, differenceFound)
+  });
 }
