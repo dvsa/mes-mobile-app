2d88d6b2daaf00ea17ad2006df932d68f1cda43d
John Morley <john.morley@bjss.com>
2019-01-09 16:51:37 +0000

    Refactor journal provider last refresh check to eliminate else clause

--- src/providers/journal/journal.ts
+++ src/providers/journal/journal.ts
@@ -9,17 +9,14 @@ export class JournalProvider {
   constructor(public http: HttpClient, public appConfig: AppConfigProvider) {}
 
   getJournal(lastRefreshed: Date){
-    let get$;
     if (lastRefreshed === null) {
-      get$ = this.http.get(this.appConfig.getAppConfig().journal.journalUrl);
-    } else {
-      const modifiedSinceValue = moment(lastRefreshed).format('ddd[,] D MMM YYYY HH:mm:ss [GMT]');
-      const options = {
-        headers: new HttpHeaders().set('If-Modified-Since', modifiedSinceValue),
-      };
-      get$ = this.http.get(this.appConfig.getAppConfig().journal.journalUrl, options);
+      return this.http.get(this.appConfig.getAppConfig().journal.journalUrl);
     }
-    return get$;
+    const modifiedSinceValue = moment(lastRefreshed).format('ddd[,] D MMM YYYY HH:mm:ss [GMT]');
+    const options = {
+      headers: new HttpHeaders().set('If-Modified-Since', modifiedSinceValue),
+    };
+    return this.http.get(this.appConfig.getAppConfig().journal.journalUrl, options);
   }
 
 }
