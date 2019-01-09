eca2c622c3ec7cb5d7225896b32e1d2ac27a8266
John Morley <john.morley@bjss.com>
2019-01-09 09:59:41 +0000

    Add verification of change detection call to journal reducer spec

--- src/pages/journal/__tests__/journal.reducer.spec.ts
+++ src/pages/journal/__tests__/journal.reducer.spec.ts
@@ -1,7 +1,13 @@
 import { initialState, journalReducer } from '../journal.reducer';
 import { LoadJournal, LoadJournalSuccess } from '../journal.actions';
+import * as newSlotsDetectingChanges from '../utils/newSlotsDetectingChanges';
 
 describe('Journal Reducer', () => {
+
+  let changeDetectorSpy;
+  beforeEach(() => {
+    changeDetectorSpy = spyOn(newSlotsDetectingChanges, 'default').and.callFake(() => [{ newSlots: true }]);
+  });
 
   describe('undefined action', () => {
     it('should return the default state', () => {
@@ -25,17 +31,19 @@ describe('Journal Reducer', () => {
   });
 
   describe('[JournalPage] Load Journal Success', () => {
-    it('should toggle loading state', () => {
-
-      const action = new LoadJournalSuccess({ testJournal: true, testSlot: [] });
+    it('should toggle loading state and populate slots from change detection', () => {
+      const actionPayload = { testSlot: [] };
+      const action = new LoadJournalSuccess(actionPayload);
       const result = journalReducer(initialState, action);
 
       expect(result).toEqual({
         ...initialState,
         isLoading: false,
-        data: { 'testJournal': true, testSlot: [] },
-        lastRefreshed: expect.any(Date)
+        data: { testSlot: [] },
+        lastRefreshed: expect.any(Date),
+        slots: [{ newSlots: true }]
       });
+      expect(changeDetectorSpy).toHaveBeenCalledWith([], actionPayload);
     });
   });
 
