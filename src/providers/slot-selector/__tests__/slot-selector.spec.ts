f07ffb4d839bd7a400465fe088c4d106c8a3d28c
John Morley <john.morley@bjss.com>
2019-01-09 10:54:33 +0000

    Fix slot selector test

--- src/providers/slot-selector/__tests__/slot-selector.spec.ts
+++ src/providers/slot-selector/__tests__/slot-selector.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { SlotSelectorProvider } from '../slot-selector';
-// import { TestSlotComponent } from '../../../pages/journal/components/test-slot/test-slot';
+import { TestSlotComponent } from '../../../pages/journal/components/test-slot/test-slot';
+import { JournalSlot } from '../../../pages/journal/domain/JournalSlot';
 
 describe('Slot Selector', () => {
   let slotSelector: SlotSelectorProvider;
@@ -26,21 +27,21 @@ describe('Slot Selector', () => {
       expect(slotSelector.getSlotTypes([]).length).toBe(0);
       expect(slotSelector.getSlotTypes(null).length).toBe(0);
       expect(slotSelector.getSlotTypes(undefined).length).toBe(0);
-      // expect(slotSelector.getSlotTypes('not a slot').length).toBe(0);
     });
     it('should provide correct component when test type is B57mins', () => {
-      // const slots = [
-      //   {
-      //     vehicleSlotType:  'B57mins',
-      //     testData: 'test data for Cat B'
-      //   }
-      // ];
+      const testSlot = {
+        vehicleSlotType: 'B57Mins'
+      };
+      const journalSlots = [
+        new JournalSlot(testSlot, false)
+      ];
 
-      // const response = slotSelector.getSlotTypes(slots);
+      const response = slotSelector.getSlotTypes(journalSlots);
 
-      // expect(response.length).toBe(1);
-      // expect(response[0].component).toBe(TestSlotComponent);
-      // expect(response[0].slotData).toBe(slots[0]);
+      expect(response.length).toBe(1);
+      expect(response[0].component).toBe(TestSlotComponent);
+      expect(response[0].slotData).toBe(testSlot);
+      expect(response[0].slotChanged).toBe(false)
     })
   });
 });
