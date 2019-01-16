802dca49c837b010f62011e404ada1866634235e
szabi.keresztes <szabi.keresztes@bjss.com>
2019-01-16 15:17:04 +0000

    the color palette card has been added

--- src/pages/component-library/component-library.module.ts
+++ src/pages/component-library/component-library.module.ts
@@ -4,13 +4,15 @@ import { ComponentLibraryPage } from './component-library';
 import { TypographyComponent } from './components/typography/typography';
 import { ButtonsComponent } from './components/buttons/buttons';
 import { DataRowsComponent } from './components/data-rows/data-rows';
+import { ColorPaletteComponent } from './components/color-palette/color-palette';
 
 @NgModule({
   declarations: [
     ComponentLibraryPage,
     TypographyComponent,
     ButtonsComponent,
-    DataRowsComponent
+    DataRowsComponent,
+    ColorPaletteComponent,
   ],
   imports: [
     IonicPageModule.forChild(ComponentLibraryPage),
