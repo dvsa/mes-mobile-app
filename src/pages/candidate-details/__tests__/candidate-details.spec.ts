d8c856d7fd7381a4b9d0172d393de7013a6cc9e4
szabi.keresztes <szabi.keresztes@bjss.com>
2019-01-23 11:42:42 +0000

    initial setup

--- src/pages/candidate-details/__tests__/candidate-details.spec.ts
+++ src/pages/candidate-details/__tests__/candidate-details.spec.ts
@@ -1,20 +1,34 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, async, TestBed } from '@angular/core/testing';
 import { IonicModule, NavController, NavParams, Config, Platform } from 'ionic-angular';
 import { NavControllerMock, NavParamsMock, ConfigMock, PlatformMock } from 'ionic-mocks-jest';
+import { StoreModule, Store } from '@ngrx/store';
+import { By } from '@angular/platform-browser';
 
 import { CandidateDetailsPage } from '../candidate-details';
 import { AppModule } from '../../../app/app.module';
 import { AuthenticationProvider } from '../../../providers/authentication/authentication';
 import { AuthenticationProviderMock } from '../../../providers/authentication/__mocks__/authentication.mock';
+import { journalReducer } from '../../journal/journal.reducer';
+import { LoadJournalSuccess } from '../../journal/journal.actions';
+import journalSlotsDataMock from '../../journal/__mocks__/journal-slots-data.mock';
+import { StoreModel } from '../../../common/store.model';
 
 describe('CandidateDetailsPage', () => {
   let fixture: ComponentFixture<CandidateDetailsPage>;
   let component: CandidateDetailsPage;
+  let store$: Store<StoreModel>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [CandidateDetailsPage],
-      imports: [IonicModule, AppModule],
+      imports: [
+        IonicModule,
+        AppModule,
+        StoreModule.forRoot({
+          journal: journalReducer
+        }),
+      ],
       providers: [
         { provide: NavController, useFactory: () => NavControllerMock.instance() },
         { provide: NavParams, useFactory: () => NavParamsMock.instance() },
@@ -27,7 +41,12 @@ describe('CandidateDetailsPage', () => {
       .then(() => {
         fixture = TestBed.createComponent(CandidateDetailsPage);
         component = fixture.componentInstance;
+
+        // this slot id comes from the local-journal.json file
+        component.slotId = 8165;
       });
+
+      store$ = TestBed.get(Store);
   }));
 
   describe('Class', () => {
@@ -39,5 +58,34 @@ describe('CandidateDetailsPage', () => {
 
   describe('DOM', () => {
     // Unit tests for the components template
+    let componentEl: DebugElement;
+
+    beforeEach(() => {
+      componentEl = fixture.debugElement;
+
+      // Manually dispatching an action which loads slots to the store
+      store$.dispatch(new LoadJournalSuccess(journalSlotsDataMock));
+    });
+
+    describe('heading with name and time', () => {
+      it('should display candidate name', () => {
+        const name  = componentEl.query(By.css('#test-candidate-details-name'));
+
+        expect(name).toBe('Captain Sims Montgomery');
+      });
+
+      it('should diplay test activity start time', () => {
+
+      });
+      
+    });
+
+    describe('candidate details', () => {
+
+    });
+
+    describe('candidate contacts', () => {
+
+    });
   });
 });
