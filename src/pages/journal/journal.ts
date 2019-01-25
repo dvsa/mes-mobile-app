06b66f1625f9c1580e85ed4de642d755cc40814f
szabi.keresztes <szabi.keresztes@bjss.com>
2019-01-25 14:19:19 +0000

    Starting on the navigation design

--- src/pages/journal/journal.ts
+++ src/pages/journal/journal.ts
@@ -7,7 +7,7 @@ import { BasePageComponent } from '../../classes/base-page';
 import { AuthenticationProvider } from '../../providers/authentication/authentication';
 import * as journalActions from './journal.actions';
 import { StoreModel } from '../../common/store.model';
-import { getSlotsOnselectedDate, getError, getIsLoading, getLastRefreshed, getLastRefreshedTime, getSelectedDate } from './journal.selector';
+import { getSlotsOnselectedDate, getError, getIsLoading, getLastRefreshed, getLastRefreshedTime } from './journal.selector';
 import { getJournalState } from './journal.reducer';
 import { MesError } from '../../common/mes-error.model';
 import { map } from 'rxjs/operators';
