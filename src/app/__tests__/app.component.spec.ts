992a7e389052cd7582657344622429953b0cff5a
John Morley <john.morley@bjss.com>
2019-01-30 20:24:33 +0000

    Add ionic-mocks and integrate into a the app component Jasmine spec

--- src/app/__tests__/app.component.spec.ts
+++ src/app/__tests__/app.component.spec.ts
@@ -6,12 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { App } from '../app.component';
 import { AppConfigProvider } from '../../providers/app-config/app-config';
 import { AppConfigProviderMock } from '../../providers/app-config/__mocks__/app-config.mock';
-import { StatusBarMock, PlatformMock } from 'ionic-mocks-jest';
+import { StatusBarMock, PlatformMock } from 'ionic-mocks';
+import { Spied } from '../../../test/helpers/spy-generic';
 
 describe('App', () => {
   let fixture: ComponentFixture<App>;
   let component: App;
-  let statusBar: StatusBar
+  let statusBar: Spied<StatusBar>
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
@@ -41,11 +42,11 @@ describe('App', () => {
     });
 
     it('should configure the status bar', () => {
-      expect(statusBar.styleLightContent).toBeCalledTimes(1);
-      expect(statusBar.overlaysWebView).toBeCalledTimes(1);
-      expect(statusBar.overlaysWebView).toBeCalledWith(false);
-      expect(statusBar.backgroundColorByHexString).toBeCalledTimes(1);
-      expect(statusBar.backgroundColorByHexString).toBeCalledWith('#000000')
+      expect(statusBar.styleLightContent.calls.count()).toBe(1);
+      expect(statusBar.overlaysWebView.calls.count()).toBe(1);
+      expect(statusBar.overlaysWebView).toHaveBeenCalledWith(false);
+      expect(statusBar.backgroundColorByHexString.calls.count()).toBe(1);
+      expect(statusBar.backgroundColorByHexString).toHaveBeenCalledWith('#000000')
     });
   });
 
