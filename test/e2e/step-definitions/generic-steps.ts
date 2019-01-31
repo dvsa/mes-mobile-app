e8d1b1029be4534047a6772f57f7320f3a0bb17e
Andrew Lai <andrew.lai@bjss.com>
2019-01-31 10:49:16 +0000

    Fixing lint issues

--- test/e2e/step-definitions/generic-steps.ts
+++ test/e2e/step-definitions/generic-steps.ts
@@ -1,5 +1,5 @@
-import { browser, ExpectedConditions, element, by } from "protractor";
-import { TestConfig } from '../test.config';
+import { browser, ExpectedConditions, element, by } from 'protractor';
+import { TEST_CONFIG } from '../test.config';
 
 const {
   Given,
@@ -7,29 +7,29 @@ const {
   When,
   setDefaultTimeout,
   After
-} = require("cucumber");
-const chai = require("chai");
-const chaiAsPromised = require("chai-as-promised");
+} = require('cucumber');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 // We need this much timeout for the login process to complete
 setDefaultTimeout(30 * 1000);
 
-Given("I am not logged in", function() {
+Given('I am not logged in', () => {
   return logout();
 });
 
-When("I launch the mobile app", function() {
+When('I launch the mobile app', () => {
   // Application is already launched by framework
 });
 
-Then("I should see the Microsoft login page", function() {
+Then('I should see the Microsoft login page', () => {
   // To be able to fill in the Authenticator login we need to switch to NATIVE context but then switch back to WEBVIEW after
-  browser.driver.getCurrentContext().then(function (webviewContext) {
+  browser.driver.getCurrentContext().then((webviewContext) => {
 
     // Switch to NATIVE context
-    browser.driver.selectContext("NATIVE_APP");
+    browser.driver.selectContext('NATIVE_APP');
 
     // Disable angular mode
     browser.ignoreSynchronization = true;
@@ -38,7 +38,7 @@ Then("I should see the Microsoft login page", function() {
     browser.driver.sleep(2000);
 
     // Check for Microsoft login username field
-    var usernameElement = element(by.xpath('//XCUIElementTypeTextField[@label="Enter your email, phone, or Skype."]'));
+    const usernameElement = element(by.xpath('//XCUIElementTypeTextField[@label="Enter your email, phone, or Skype."]'));
     expect(usernameElement.isPresent()).to.eventually.be.true;
 
     // Enable angular mode
@@ -51,41 +51,40 @@ Then("I should see the Microsoft login page", function() {
   });
 });
 
-When("I log in to the application as {string}", function(username) {
-  return logInToApplication(TestConfig.users[username].username, TestConfig.users[username].password);
+When('I log in to the application as {string}', (username) => {
+  return logInToApplication(TEST_CONFIG.users[username].username, TEST_CONFIG.users[username].password);
 });
 
-Then("I should see the {string} page", function(pageTitle) {
+Then('I should see the {string} page', (pageTitle) => {
   return expect(element.all(by.className('toolbar-title')).last().getText()).to.eventually.equal(pageTitle);
 });
 
-Given("I am on the journal page as {string}", function(username) {
+Given('I am on the journal page as {string}', (username) => {
   // If we are not already logged in do so now and then load the application
-  loggedIn().then(function(response) {
+  loggedIn().then((response) => {
     if (!response) {
-      logInToApplication(TestConfig.users[username].username, TestConfig.users[username].password);
+      logInToApplication(TEST_CONFIG.users[username].username, TEST_CONFIG.users[username].password);
     }
   });
 
   return loadApplication();
 });
 
-When("I view candidate details for {string}", function(candidateName) {
-  var buttonElement = element(by.xpath('//h3[text() = "' + candidateName + '"]'));
+When('I view candidate details for {string}', (candidateName) => {
+  const buttonElement = element(by.xpath(`//h3[text() = "${candidateName}"]`));
   return clickElement(buttonElement);
 });
 
-When("I start the test for {string}", function(candidateName) {
-  var buttonElement = element(by.xpath('//button/span[text()[normalize-space(.) = "Start test"]][ancestor::ion-row/ion-col/candidate-link/div/ion-grid/ion-row/ion-col/h3[text() = "' + candidateName + '"]]'));
+When('I start the test for {string}', (candidateName) => {
+  const buttonElement = element(by.xpath(`//button/span[text()[normalize-space(.) = "Start test"]][ancestor::ion-row/ion-col/candidate-link/div/ion-grid/ion-row/ion-col/h3[text() = "${candidateName}"]]`));
   return clickElement(buttonElement);
 });
 
 // After hook to take screenshots of page
-After(function(testCase) {
-  var world = this;
-  return browser.takeScreenshot().then(function(screenShot) {
+After(function (testCase) {
+  return browser.driver.takeScreenshot().then((screenShot) => {
     // screenShot is a base-64 encoded PNG
-    world.attach(screenShot, "image/png");
+    this.attach(screenShot, 'image/png');
   });
 });
 
@@ -95,9 +94,9 @@ After(function(testCase) {
 // Logs into the application with the given username and password. Assumes we will be on the Microsoft login page.
 function logInToApplication(username, password) {
   // To be able to fill in the Authenticator login we need to switch to NATIVE context but then switch back to WEBVIEW after
-  browser.driver.getCurrentContext().then(function (webviewContext) {
+  browser.driver.getCurrentContext().then((webviewContext) => {
     // Switch to NATIVE context
-    browser.driver.selectContext("NATIVE_APP");
+    browser.driver.selectContext('NATIVE_APP');
 
     // Disable angular mode
     browser.ignoreSynchronization = true;
@@ -106,16 +105,16 @@ function logInToApplication(username, password) {
     browser.driver.sleep(2000);
 
     // Fill in username and click Next
-    var usernameElement = element(by.xpath('//XCUIElementTypeTextField[@label="Enter your email, phone, or Skype."]'));
+    const usernameElement = element(by.xpath('//XCUIElementTypeTextField[@label="Enter your email, phone, or Skype."]'));
     usernameElement.sendKeys(username);
-    var nextButtonElement = element(by.xpath('//XCUIElementTypeButton[@label="Next"]'));
+    const nextButtonElement = element(by.xpath('//XCUIElementTypeButton[@label="Next"]'));
     nextButtonElement.click();
     browser.driver.sleep(2000);
 
     // Fill in password and click Sign in
-    var passwordElement = element(by.xpath('//XCUIElementTypeSecureTextField[@label="Enter the password for ' + username + '"]'));
+    const passwordElement = element(by.xpath(`//XCUIElementTypeSecureTextField[@label="Enter the password for ${username}"]`));
     passwordElement.sendKeys(password);
-    var signInButtonElement = element(by.xpath('//XCUIElementTypeButton[@label="Sign in"]'));
+    const signInButtonElement = element(by.xpath('//XCUIElementTypeButton[@label="Sign in"]'));
     signInButtonElement.click();
 
     // Enable angular mode
@@ -131,19 +130,19 @@ function logInToApplication(username, password) {
 // Checks whether the user is logged in. This will need updating as it only checks for the existance of the logout button.
 function loggedIn() {
   browser.driver.sleep(3000);
-  var logout = element(by.xpath('//button/span[contains(text(), "Logout")]'));
+  const logout = element(by.xpath('//button/span[contains(text(), "Logout")]'));
   return logout.isPresent();
 }
 
 // Logs out of the application and takes them to the login page if they were logged in else returns (which should be the login page)
 function logout() {
     browser.sleep(500);
-    var logout = element(by.xpath('//button/span[contains(text(), "Logout")]'));
-    logout.isPresent().then(function(result) {
+    const logout = element(by.xpath('//button/span[contains(text(), "Logout")]'));
+    logout.isPresent().then((result) => {
       if ( result ) {
-        logout.click().then (function() {
+        logout.click().then (() => {
           // After logout click sign in to get us to the login screen
-          var signIn = element(by.xpath('//span[contains(text(), "Sign in")]'));
+          const signIn = element(by.xpath('//span[contains(text(), "Sign in")]'));
           return signIn.click();
         });
       } else {
@@ -155,14 +154,14 @@ function logout() {
 
 // A framework safe click method
 function clickElement(fieldElement) {
-  fieldElement.click().then(function(promise) {
+  fieldElement.click().then((promise) => {
     return isReady(promise);
   });
 }
 
 // Goes to the home page which will be the journal for logged in Examiners
 function loadApplication() {
-  browser.get("").then(function(promise) {
+  browser.get('').then((promise) => {
     browser.sleep(500);
     return isReady(promise);
   });
