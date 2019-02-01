7f29f2ed0907700f1b0509a71f19fec6eef3313b
Ross Faulds <ross.faulds@bjss.com>
2019-02-01 14:38:48 +0000

    fix test-outcome tests by adding analyticsprovider mock

---
+++ src/providers/analytics/__mocks__/analytics.mock.ts
@@ -0,0 +1,13 @@
+import { IAnalyticsProvider } from '../analytics.model';
+
+export class AnalyticsProviderMock implements IAnalyticsProvider {
+    googleAnalyticsKey: string = 'UA-12345678';
+
+    setCurrentPage(name: string):void {}
+
+  logEvent(category: string, event: string, params?: any) {}
+
+  logError(message: string) {}
+
+  setUserId(userId: string) {}
+}
