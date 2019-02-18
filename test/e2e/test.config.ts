16ecdf914e0c2239e80d2a8f117a0c7f13a4aad8
Andrew Lai <andrew.lai@bjss.com>
2019-02-18 13:31:08 +0000

    Updates to UI tests to reflect updates from mes-1566 (#157)

--- test/e2e/test.config.ts
+++ test/e2e/test.config.ts
@@ -1,16 +1,15 @@
 export const TEST_CONFIG = {
-    'users': {
-      'mobexaminer': {'username': '<USERNAME-HERE>', 'password': '<PASSWORD-HERE>', 'employeeId': '12345678'},
-      'mobexaminer1': {'username': '<USERNAME-HERE>', 'password': '<PASSWORD-HERE>', 'employeeId': '01234567'},
-      'mobexaminer2': {'username': '<USERNAME-HERE>', 'password': '<PASSWORD-HERE>', 'employeeId': '00123456'},
-      'mobexaminer3': {'username': '<USERNAME-HERE>', 'password': '<PASSWORD-HERE>', 'employeeId': '28371932'},
-      'mobexaminer4': {'username': '<USERNAME-HERE>', 'password': '<PASSWORD-HERE>', 'employeeId': '90128472'},
-      'mobexaminer5': {'username': '<USERNAME-HERE>', 'password': '<PASSWORD-HERE>', 'employeeId': '47182032'},
-      'mobexaminer6': {'username': '<USERNAME-HERE>', 'password': '<PASSWORD-HERE>', 'employeeId': '78471231'},
-      'mobexaminer7': {'username': '<USERNAME-HERE>', 'password': '<PASSWORD-HERE>', 'employeeId': '67128492'}
-    },
-    'PAGE_LOAD_WAIT': 2000,
-    'ACTION_WAIT': 300,
-    'DEFAULT_TIMEOUT': 30000 
-  };
-  
+  users: {
+    mobexaminer: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '12345678' },
+    mobexaminer1: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '01234567' },
+    mobexaminer2: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '00123456' },
+    mobexaminer3: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '28371932' },
+    mobexaminer4: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '90128472' },
+    mobexaminer5: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '47182032' },
+    mobexaminer6: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '78471231' },
+    mobexaminer7: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '67128492' },
+  },
+  PAGE_LOAD_WAIT: 2000,
+  ACTION_WAIT: 300,
+  DEFAULT_TIMEOUT: 30000,
+};
