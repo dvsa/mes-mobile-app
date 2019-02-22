3fdd908803e26e894155dd70c27d2bc824706648
Natalie Johnson <Natalie.Johnson@bjss.com>
2019-02-22 16:57:04 +0000

    Remove commented out code and fix tests for vehicle details component

--- src/pages/journal/components/vehicle-details/__tests__/vehicle-details.spec.ts
+++ src/pages/journal/components/vehicle-details/__tests__/vehicle-details.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { IonicModule } from 'ionic-angular';
 import { VehicleDetailsComponent } from '../vehicle-details';
-// import { By } from '@angular/platform-browser';
+import { By } from '@angular/platform-browser';
 
 describe('VehicleDetailsComponent', () => {
   let fixture: ComponentFixture<VehicleDetailsComponent>;
@@ -27,35 +27,19 @@ describe('VehicleDetailsComponent', () => {
   });
 
   describe('DOM', () => {
-    // it('should show vehicle details when showDimensions is true', () => {
-    //   component.showDimensions = true;
-    //   fixture.detectChanges();
-    //   const ionCols = fixture.debugElement.queryAll(By.css('ion-col'));
-    //   expect(ionCols.length).toBe(3);
-    // });
+    it('should show vehicle details when showDimensions is true', () => {
+      component.showDimensions = true;
+      fixture.detectChanges();
+      const ionCols = fixture.debugElement.queryAll(By.css('p'));
+      expect(ionCols.length).toBe(1);
+    });
 
-    // it('should not vehicle details when showDimensions is false', () => {
-    //   component.showDimensions = false;
-    //   fixture.detectChanges();
-    //   const ionCols = fixture.debugElement.queryAll(By.css('ion-col'));
-    //   expect(ionCols.length).toBe(0);
-    // });
-
-    // it('should show number of seats when showNumberOfSeats is true', () => {
-    //   component.showDimensions = false;
-    //   component.showNumberOfSeats = true;
-    //   fixture.detectChanges();
-    //   const ionCols = fixture.debugElement.queryAll(By.css('ion-col'));
-    //   expect(ionCols.length).toBe(1);
-    // });
-
-    // it('should not show number of seats when showNumberOfSeats is false', () => {
-    //   component.showDimensions = false;
-    //   component.showNumberOfSeats = false;
-    //   fixture.detectChanges();
-    //   const ionCols = fixture.debugElement.queryAll(By.css('ion-col'));
-    //   expect(ionCols.length).toBe(0);
-    // });
+    it('should not vehicle details when showDimensions is false', () => {
+      component.showDimensions = false;
+      fixture.detectChanges();
+      const ionCols = fixture.debugElement.queryAll(By.css('p'));
+      expect(ionCols.length).toBe(0);
+    });
   });
 
 });
