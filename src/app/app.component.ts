ef11edcee8225741781b05a5f7a2decef4119063
Tom Billington <t.billington@kainos.com>
2019-03-01 14:05:59 +0000

    switched orientation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,7 +48,7 @@ export class App {
       translate.setDefaultLang(DEFAULT_LANG);
 
       if (platform.is('cordova')) {
-        screenOrientation.lock(screenOrientation.ORIENTATIONS.PORTRAIT_SECONDARY);
+        screenOrientation.lock(screenOrientation.ORIENTATIONS.PORTRAIT_PRIMARY);
         insomnia.keepAwake();
         globalization.getPreferredLanguage().then((res) => {
           this.setDefaultLanguage(res.value);
