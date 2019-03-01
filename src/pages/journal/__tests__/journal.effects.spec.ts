89ac5c65d0de9e347c25930c264481ffbb9cc89f
Ammar Haider <ammar.haider@bjss.com>
2019-03-01 13:34:42 +0000

    tests effects using ReplaySubject + subscribe

--- src/pages/journal/__tests__/journal.effects.spec.ts
+++ src/pages/journal/__tests__/journal.effects.spec.ts
@@ -1,6 +1,8 @@
 
 import { TestBed } from '@angular/core/testing';
-
+// import { cold, hot } from 'jasmine-marbles';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { provideMockActions } from '@ngrx/effects/testing';
 import { JournalEffects } from '../journal.effects';
 import { Actions } from '@ngrx/effects';
 import { Observable } from 'rxjs/Observable';
@@ -10,12 +12,13 @@ import { JournalProviderMock } from '../../../providers/journal/__mocks__/journa
 import { AnalyticsProvider } from '../../../providers/analytics/analytics';
 import { AnalyticsProviderMock } from '../../../providers/analytics/__mocks__/analytics.mock';
 import { SlotProvider } from '../../../providers/slot/slot';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, Store } from '@ngrx/store';
 import { journalReducer } from '../journal.reducer';
 import { AppConfigProvider } from '../../../providers/app-config/app-config';
 import { AppConfigProviderMock } from '../../../providers/app-config/__mocks__/app-config.mock';
 import { NetworkStateProvider } from '../../../providers/network-state/network-state';
 import { NetworkStateProviderMock } from '../../../providers/network-state/__mocks__/network-state.mock';
+import { LoadJournal, JournalActionTypes, LoadJournalSuccess, LoadJournalFailure } from '../journal.actions';
 
 export class TestActions extends Actions {
   constructor() {
@@ -27,11 +30,14 @@ export class TestActions extends Actions {
   }
 }
 
-describe('Journal Effects', () => {
+fdescribe('Journal Effects', () => {
 
   let effects: JournalEffects;
+  let actions$: ReplaySubject<JournalActionTypes>;
+  let journalProvider: JournalProvider;
 
   beforeEach(() => {
+    actions$ = new ReplaySubject(1);
     TestBed.configureTestingModule({
       imports: [
         StoreModule.forRoot({
@@ -41,18 +47,16 @@ describe('Journal Effects', () => {
       providers: [
         { provide: AppConfigProvider, useClass: AppConfigProviderMock },
         JournalEffects,
-        {
-          provide: Actions,
-          useFactory: () => new TestActions(),
-        },
+        provideMockActions(() => actions$),
         { provide: JournalProvider, useClass: JournalProviderMock },
         { provide: AnalyticsProvider, useClass: AnalyticsProviderMock },
         { provide: AppConfigProvider, useClass: AppConfigProviderMock },
         { provide: NetworkStateProvider, useClass: NetworkStateProviderMock },
+        { provide: Store, useClass: Store },
         SlotProvider,
       ],
     });
-
+    journalProvider = TestBed.get(JournalProvider);
     effects = TestBed.get(JournalEffects);
   });
 
@@ -60,8 +64,24 @@ describe('Journal Effects', () => {
     expect(effects).toBeTruthy();
   });
 
-  it('should load journal data', () => {
-    // todo - scaffold the load journal test
-    expect(true).toBeTruthy();
+  it('should dispatch the success action when the journal loads successfully', (done) => {
+    actions$.next(new LoadJournal());
+    spyOn(journalProvider, 'getJournal').and.callThrough();
+    effects.loadJournal$.subscribe((result) => {
+      expect(journalProvider.getJournal).toHaveBeenCalled();
+      expect(result instanceof LoadJournalSuccess).toBe(true);
+      actions$.next(result);
+      done();
+    });
+  });
+
+  it('should dispatch the failure action when the journal fails to load', (done) => {
+    actions$.next(new LoadJournal());
+    spyOn(journalProvider, 'getJournal').and.throwError;
+    effects.loadJournal$.subscribe((result) => {
+      expect(journalProvider.getJournal).toHaveBeenCalled();
+      expect(result instanceof LoadJournalFailure).toBe(true);
+      done();
+    });
   });
 });
