301ad1fcc866da6b9bbba56771dedc81a9191b6e
roberthall12 <roberth@kainos.com>
2019-03-05 11:53:55 +0000

    Removed all traces of the category icon slot

--- src/pages/test-report/test-report.module.ts
+++ src/pages/test-report/test-report.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { ComponentsModule } from '../../components/components.module';
 import { TestReportPage } from './test-report';
 import { EffectsModule } from '@ngrx/effects';
 import { TestReportAnalyticsEffects } from './test-report.analytics.effects';
@@ -13,7 +12,6 @@ import { AnalyticsProvider } from '../../providers/analytics/analytics';
   imports: [
     IonicPageModule.forChild(TestReportPage),
     EffectsModule.forFeature([TestReportAnalyticsEffects]),
-    ComponentsModule,
   ],
   providers: [
     AnalyticsProvider,
