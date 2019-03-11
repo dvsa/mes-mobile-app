771d69f459f0a988b01e15f82c54f5d7ce268b9b
szabi.keresztes <szabi.keresztes@bjss.com>
2019-03-11 14:28:15 +0000

    minor correction

--- test/e2e/test.config.ts
+++ test/e2e/test.config.ts
@@ -1,14 +1,13 @@
 export const TEST_CONFIG = {
   users: {
-    mobexaminer: {
-      username: 'mobexaminer@dvsagov.onmicrosoft.com', password: 'Appytappers2018', employeeId: '12345678' },
-    mobexaminer1: { username: 'mobexaminer1@dvsagov.onmicrosoft.com', password: 'Mava82452', employeeId: '01234567' },
-    mobexaminer2: { username: 'mobexaminer2@dvsagov.onmicrosoft.com', password: 'Vahu54091', employeeId: '00123456' },
-    mobexaminer3: { username: 'mobexaminer3@dvsagov.onmicrosoft.com', password: 'Daso03563', employeeId: '28371932' },
-    mobexaminer4: { username: 'mobexaminer4@dvsagov.onmicrosoft.com', password: 'Polu01094', employeeId: '90128472' },
-    mobexaminer5: { username: 'mobexaminer5@dvsagov.onmicrosoft.com', password: 'Qosu45925', employeeId: '47182032' },
-    mobexaminer6: { username: 'mobexaminer6@dvsagov.onmicrosoft.com', password: 'Kola74156', employeeId: '78471231' },
-    mobexaminer7: { username: 'mobexaminer7@dvsagov.onmicrosoft.com', password: 'Mada22677', employeeId: '67128492' },
+    mobexaminer: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '12345678' },
+    mobexaminer1: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '01234567' },
+    mobexaminer2: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '00123456' },
+    mobexaminer3: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '28371932' },
+    mobexaminer4: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '90128472' },
+    mobexaminer5: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '47182032' },
+    mobexaminer6: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '78471231' },
+    mobexaminer7: { username: '<USERNAME-HERE>', password: '<PASSWORD-HERE>', employeeId: '67128492' },
   },
   PAGE_LOAD_WAIT: 2000,
   ACTION_WAIT: 300,
