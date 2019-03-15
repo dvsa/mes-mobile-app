d621205790752ad7c9e0fbebe6bf0c0e200ef2af
John Morley <john.morley@bjss.com>
2019-03-15 16:12:41 +0000

    Unit test emission of declaration toggle actions

--- src/pages/waiting-room/__tests__/waiting-room.spec.ts
+++ src/pages/waiting-room/__tests__/waiting-room.spec.ts
@@ -6,15 +6,27 @@ import { AppModule } from '../../../app/app.module';
 import { WaitingRoomPage } from '../waiting-room';
 import { AuthenticationProvider } from '../../../providers/authentication/authentication';
 import { AuthenticationProviderMock } from '../../../providers/authentication/__mocks__/authentication.mock';
+import { StoreModule, Store } from '@ngrx/store';
+import { waitingRoomReducer } from '../waiting-room.reducer';
+import { StoreModel } from '../../../shared/models/store.model';
+import { ToggleResidencyDeclaration, ToggleInsuranceDeclaration } from '../waiting-room.actions';
 
 describe('WaitingRoomPage', () => {
   let fixture: ComponentFixture<WaitingRoomPage>;
   let component: WaitingRoomPage;
+  let store$: Store<StoreModel>;
+  let storeDispatchSpy: jasmine.Spy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [WaitingRoomPage],
-      imports: [IonicModule, AppModule],
+      imports: [
+        IonicModule,
+        AppModule,
+        StoreModule.forRoot({
+          waitingRoom: waitingRoomReducer,
+        }),
+      ],
       providers: [
         { provide: NavController, useFactory: () => NavControllerMock.instance() },
         { provide: NavParams, useFactory: () => NavParamsMock.instance() },
@@ -28,12 +40,28 @@ describe('WaitingRoomPage', () => {
         fixture = TestBed.createComponent(WaitingRoomPage);
         component = fixture.componentInstance;
       });
+
+    store$ = TestBed.get(Store);
+    storeDispatchSpy = spyOn(store$, 'dispatch');
   }));
 
   describe('Class', () => {
     // Unit tests for the components TypeScript class
     it('should create', () => {
       expect(component).toBeDefined();
+    });
+
+    describe('declaration status', () => {
+      it('should emit a residency declaration toggle action when changed', () => {
+        component.residencyDeclarationChanged();
+
+        expect(storeDispatchSpy).toHaveBeenCalledWith(new ToggleResidencyDeclaration());
+      });
+      it('should emit an insurance declaration toggle action when changed', () => {
+        component.insuranceDeclarationChanged();
+
+        expect(storeDispatchSpy).toHaveBeenCalledWith(new ToggleInsuranceDeclaration());
+      });
     });
   });
 
