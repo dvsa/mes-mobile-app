f031774626b4edc9567cd507f9f5b20229d32200
Tom Billington <t.billington@kainos.com>
2019-03-19 11:51:22 +0000

    Added Styling

--- src/pages/test-report/components/competency/competency.ts
+++ src/pages/test-report/components/competency/competency.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'competency',
   templateUrl: 'competency.html',
 })
 export class CompetencyComponent {
+
+  @Input()
+  label: string;
 
   constructor() {}
 
