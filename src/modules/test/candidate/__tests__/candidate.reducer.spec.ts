3abb0c9d8c97cb8de6f722c2c4e5033fd29de9b8
John Morley <john.morley@bjss.com>
2019-03-19 08:59:13 +0000

    Candidate reducer test

--- src/modules/test/candidate/__tests__/candidate.reducer.spec.ts
+++ src/modules/test/candidate/__tests__/candidate.reducer.spec.ts
@@ -1,2 +1,18 @@
+import { candidateReducer } from '../candidate.reducer';
+import { TestOutcomeStartTest } from '../../../../pages/journal/components/test-outcome/test-outcome.actions';
+import { Mock } from 'typemoq';
+import { TestSlot, Candidate, Booking } from '../../../../shared/models/DJournal';
+
 describe('candidate reducer', () => {
+  it('should return the candidate from a start test action', () => {
+    const moqSlot = Mock.ofType<TestSlot>();
+    const moqBooking = Mock.ofType<Booking>();
+    const moqCandidate = Mock.ofType<Candidate>();
+    moqBooking.setup((x: Booking) => x.candidate).returns(() => moqCandidate.object);
+    moqSlot.setup((x: TestSlot) => x.booking).returns(() => moqBooking.object);
+
+    const result = candidateReducer(null, new TestOutcomeStartTest(moqSlot.object));
+
+    expect(result).toBe(moqCandidate.object);
+  });
 });
