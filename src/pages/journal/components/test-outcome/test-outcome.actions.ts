abc74fefc7d941da084658a25f22d0b797a90a8e
John Morley <john.morley@bjss.com>
2019-03-19 10:56:59 +0000

    Fix Start Test action description

--- src/pages/journal/components/test-outcome/test-outcome.actions.ts
+++ src/pages/journal/components/test-outcome/test-outcome.actions.ts
@@ -1,7 +1,7 @@
 import { Action } from '@ngrx/store';
 import { TestSlot } from '../../../../shared/models/DJournal';
 
-export const TEST_OUTCOME_START_TEST = '[TestOutcomePage] Test Outcome start test';
+export const TEST_OUTCOME_START_TEST = '[TestOutcome] Test Outcome start test';
 
 export class TestOutcomeStartTest implements Action {
   readonly type = TEST_OUTCOME_START_TEST;
