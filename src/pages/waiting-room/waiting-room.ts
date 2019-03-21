ed81d9526b191439ce9fbefee5119d65743d118c
roberthall12 <roberth@kainos.com>
2019-03-21 14:21:07 +0000

    Initial attempt to add locking funtionality to WR screen

--- src/pages/waiting-room/waiting-room.ts
+++ src/pages/waiting-room/waiting-room.ts
@@ -62,6 +62,24 @@ export class WaitingRoomPage extends BasePageComponent {
     }
   }
 
+  clickContinue = async (): Promise<boolean> => {
+
+    const returnVal =  await this.deviceProvider.triggerLockScreen();
+    console.log(returnVal);
+    return returnVal;
+
+    //   .then(() => {
+    //     console.log('click continue resolved');
+    //     this.navCtrl.push('WaitingRoomToCarPage');
+    //     return resolve(true);
+    //   })
+    //   .catch(() => {
+    //     console.log('catched ...');
+    //     return reject(false);
+    //   });
+    // });
+  }
+
   ngOnInit(): void {
     this.pageState = {
       insuranceDeclarationAccepted$: this.store$.pipe(
---
+++ src/providers/lock-screen/lock-screen.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+declare let cordova: any;
+
+@Injectable()
+export class LockScreenProvider {
+
+  constructor(
+    ) { }
+
+  triggerLockScreen = (): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+      if (cordova && cordova.plugins && cordova.plugins.DeviceAuthentication) {
+
+          console.log('start ');
+          cordova.plugins.DeviceAuthentication.runAuthentication('Please authenticate yourself to proceed')
+                .then((isAuthenticated: boolean) => {
+                  console.log(`${isAuthenticated} - is authenticated`);
+                  return resolve(isAuthenticated);
+                })
+                .catch((errorMsg: string) => {
+                  console.log('is not authenticated');
+                  return reject(false);
+                });
+        }else {
+          console.log('not cordova');
+          return reject(false);
+        }
+    });
+  }
+}
