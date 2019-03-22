4430ea79c4a6d11c5d9ec93e49827e57d54817f5
John Morley <john.morley@bjss.com>
2019-03-22 15:33:10 +0000

    Unit test CompetencyComponent setting up press+hold event

--- src/pages/test-report/components/competency/__tests__/competency.spec.ts
+++ src/pages/test-report/components/competency/__tests__/competency.spec.ts
@@ -4,10 +4,12 @@ import { CompetencyComponent } from '../competency';
 import { AppModule } from '../../../../../app/app.module';
 import { By } from '@angular/platform-browser';
 import { HammerProvider } from '../../../../../providers/hammer/hammer';
+import { MockElementRef } from '../../../../../shared/mocks/element-ref.mock';
 
 describe('CompetencyComponent', () => {
   let fixture: ComponentFixture<CompetencyComponent>;
   let component: CompetencyComponent;
+  let hammerProvider: HammerProvider;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,12 +23,25 @@ describe('CompetencyComponent', () => {
       .then(() => {
         fixture = TestBed.createComponent(CompetencyComponent);
         component = fixture.componentInstance;
+        hammerProvider = TestBed.get(HammerProvider);
+        spyOn(hammerProvider, 'addPressAndHoldEvent');
+        spyOn(hammerProvider, 'init');
       });
   }));
 
   describe('Class', () => {
     it('should create', () => {
       expect(component).toBeDefined();
+    });
+    describe('ngOnInit', () => {
+      it('should use HammerProvider to attach a press and hold event to the button which records the fault', () => {
+        component.button = new MockElementRef();
+
+        component.ngOnInit();
+
+        expect(hammerProvider.init).toHaveBeenCalledWith(component.button);
+        expect(hammerProvider.addPressAndHoldEvent).toHaveBeenCalledWith(component.recordFault);
+      });
     });
   });
 
@@ -36,7 +51,6 @@ describe('CompetencyComponent', () => {
       fixture.detectChanges();
       const label = fixture.debugElement.query(By.css('#competencyLabel'));
       expect(label.nativeElement.innerHTML).toBe('Gears');
-
     });
   });
 });
