f237541b72492349f85c521a4227b27673e464c6
John Morley <john.morley@bjss.com>
2019-03-22 09:18:20 +0000

    Improve tests for tests reducer

--- src/modules/tests/__tests__/tests.reducer.spec.ts
+++ src/modules/tests/__tests__/tests.reducer.spec.ts
@@ -2,18 +2,23 @@ import { testsReducer } from '../tests.reducer';
 import * as testOutcomeActions from '../../../pages/journal/components/test-outcome/test-outcome.actions';
 import { TestSlot } from '../../../shared/models/DJournal';
 import * as candidateReducer from '../candidate/candidate.reducer';
+import * as preTestDeclarationsReducer from '../pre-test-declarations/pre-test-declarations.reducer';
+import { PreTestDeclarations } from '@dvsa/mes-test-schema/categories/B';
 
 describe('testsReducer', () => {
+  const newCandidate = { candidate: { candidateId: 456 } };
+  const preTestDeclarations: PreTestDeclarations = preTestDeclarationsReducer.initialState;
+
+  beforeEach(() => {
+    spyOn(candidateReducer, 'candidateReducer').and.returnValue(newCandidate);
+    spyOn(preTestDeclarationsReducer, 'preTestDeclarationsReducer').and.returnValue(preTestDeclarations);
+  });
+
   it('use the payload of a test started action to setup state for a new test', () => {
     const state = {};
     const startTestPayload: TestSlot = {
       slotDetail: {
         slotId: 123,
-      },
-      booking: {
-        candidate: {
-          candidateId: 789,
-        },
       },
     };
     const action = new testOutcomeActions.TestOutcomeStartTest(startTestPayload);
@@ -22,13 +27,9 @@ describe('testsReducer', () => {
 
     // @ts-ignore
     expect(output.current.slotId).toBe('123');
-    expect(output['123'].candidate.candidateId).toBe(789);
-    expect(output['123'].preTestDeclarations.insuranceDeclarationAccepted).toBe(false);
   });
 
   it('should derive the sub-states from sub-reducers', () => {
-    const newCandidate = { candidate: { candidateId: 456 } };
-    spyOn(candidateReducer, 'candidateReducer').and.returnValue(newCandidate);
     const state = {
       candidate: {
         candidateId: 789,
@@ -38,7 +39,8 @@ describe('testsReducer', () => {
     const result = testsReducer(state, new testOutcomeActions.TestOutcomeStartTest({ slotDetail: { slotId: 123 } }));
 
     expect(candidateReducer.candidateReducer).toHaveBeenCalled();
-    // @ts-ignore
+    expect(preTestDeclarationsReducer.preTestDeclarationsReducer).toHaveBeenCalled();
     expect(result['123'].candidate).toBe(newCandidate);
+    expect(result['123'].preTestDeclarations).toBe(preTestDeclarations);
   });
 });
