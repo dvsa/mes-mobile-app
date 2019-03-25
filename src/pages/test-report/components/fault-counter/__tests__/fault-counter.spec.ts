32273c116817e4640f1242fe6ac8e30d2727f8be
John Morley <john.morley@bjss.com>
2019-03-25 15:31:02 +0000

    Render the fault count in the fault-counter component

---
+++ src/pages/test-report/components/fault-counter/__tests__/fault-counter.spec.ts
@@ -0,0 +1,38 @@
+import { FaultCounterComponent } from '../fault-counter';
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+describe('FaultCounterComponent', () => {
+  let fixture: ComponentFixture<FaultCounterComponent>;
+  let component: FaultCounterComponent;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        FaultCounterComponent,
+      ],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(FaultCounterComponent);
+        component = fixture.componentInstance;
+      });
+  }));
+
+  describe('Class', () => {
+    it('should create', () => {
+      expect(component).toBeDefined();
+    });
+  });
+
+  describe('DOM', () => {
+    it('should display the number of faults in the DOM', () => {
+      component.count = 5;
+
+      fixture.detectChanges();
+
+      const renderedCount = fixture.debugElement.query(By.css('#count')).nativeElement.innerHTML;
+      expect(renderedCount).toBe('5');
+    });
+  });
+});
