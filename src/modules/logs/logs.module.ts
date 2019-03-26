9c600d3c348854044f47bd011eed52be9444e96c
szabi.keresztes <szabi.keresztes@bjss.com>
2019-03-26 12:01:07 +0000

    providing date time to logs

--- src/modules/logs/logs.module.ts
+++ src/modules/logs/logs.module.ts
@@ -4,11 +4,15 @@ import { EffectsModule } from '@ngrx/effects';
 
 import { logsReducer } from './logs.reducer';
 import { LogsEffects } from './logs.effects';
+import { DateTimeProvider } from '../../providers/date-time/date-time';
 
 @NgModule({
   imports: [
     StoreModule.forFeature('logs', logsReducer),
     EffectsModule.forFeature([LogsEffects]),
+  ],
+  providers: [
+    DateTimeProvider,
   ],
 })
 export class LogsModule {}
