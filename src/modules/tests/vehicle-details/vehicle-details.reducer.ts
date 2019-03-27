a374164e1bd5ff940eac7ec939d60252b7528a9e
John Morley <john.morley@bjss.com>
2019-03-27 09:21:15 +0000

    Remove default values for VehicleDetails reducer initial state

--- src/modules/tests/vehicle-details/vehicle-details.reducer.ts
+++ src/modules/tests/vehicle-details/vehicle-details.reducer.ts
@@ -1,12 +1,7 @@
 import * as vehicleDetailsActions from './vehicle-details.actions';
 import { VehicleDetails } from '@dvsa/mes-test-schema/categories/B';
 
-const initialState: VehicleDetails = {
-  dualControls: false,
-  gearboxCategory: null,
-  registrationNumber: '',
-  schoolCar: false,
-};
+const initialState: VehicleDetails = {};
 
 export const vehicleDetailsReducer = (state = initialState, action: vehicleDetailsActions.Types) => {
   switch (action.type) {
