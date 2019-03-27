0223f470fd05228293b9a51e0d3b68aebc7bc8b8
Natalie Johnson <Natalie.Johnson@bjss.com>
2019-03-27 17:05:28 +0000

    Create basic page layout and state

--- src/pages/pass-finalisation/pass-finalisation.ts
+++ src/pages/pass-finalisation/pass-finalisation.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 import { BasePageComponent } from '../../shared/classes/base-page';
 import { AuthenticationProvider } from '../../providers/authentication/authentication';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { StoreModel } from '../../shared/models/store.model';
 import { PassFinalisationViewDidEnter } from './pass-finalisation.actions';
+import { Observable } from 'rxjs/Observable';
+import { getCandidate } from '../../modules/tests/candidate/candidate.reducer';
+import {
+  getCandidateName, getCandidateDriverNumber, formatDriverNumber, getUntitledCandidateName,
+} from '../../modules/tests/candidate/candidate.selector';
+import { map } from 'rxjs/operators';
+import { getCurrentTest } from '../../modules/tests/tests.selector';
+
+interface PassFinalisationPageState {
+  candidateName$: Observable<string>;
+  candidateUntitledName$: Observable<string>;
+  candidateDriverNumber$: Observable<string>;
+}
 
 @IonicPage()
 @Component({
@@ -12,6 +25,7 @@ import { PassFinalisationViewDidEnter } from './pass-finalisation.actions';
   templateUrl: 'pass-finalisation.html',
 })
 export class PassFinalisationPage extends BasePageComponent {
+  pageState: PassFinalisationPageState;
 
   constructor(
     private store$: Store<StoreModel>,
@@ -21,6 +35,27 @@ export class PassFinalisationPage extends BasePageComponent {
     public authenticationProvider: AuthenticationProvider,
   ) {
     super(platform, navCtrl, authenticationProvider);
+  }
+
+  ngOnInit(): void {
+    this.pageState = {
+      candidateName$: this.store$.pipe(
+        select(getCurrentTest),
+        select(getCandidate),
+        select(getCandidateName),
+      ),
+      candidateUntitledName$: this.store$.pipe(
+        select(getCurrentTest),
+        select(getCandidate),
+        select(getUntitledCandidateName),
+      ),
+      candidateDriverNumber$: this.store$.pipe(
+        select(getCurrentTest),
+        select(getCandidate),
+        select(getCandidateDriverNumber),
+        map(formatDriverNumber),
+      ),
+    };
   }
 
   ionViewDidEnter(): void {
