33604e8986e3980c7823931004e6506812a5d67c
Tom Billington <t.billington@kainos.com>
2019-03-28 11:31:38 +0000

    Test Data Tests

--- src/modules/tests/test_data/__tests__/test-data.reducer.spec.ts
+++ src/modules/tests/test_data/__tests__/test-data.reducer.spec.ts
@@ -1,42 +1,70 @@
 import { testDataReducer, initialState } from '../test-data.reducer';
-import { AddDrivingFault } from '../test-data.actions';
+import { AddDrivingFault, AddSeriousFault } from '../test-data.actions';
 import { Competencies } from '../test-data.constants';
 import { TestData } from '@dvsa/mes-test-schema/categories/B';
 
 describe('TestDataReducer reducer', () => {
-  it('should add a driving fault when no driving faults exist', () => {
-    const result = testDataReducer(initialState, new AddDrivingFault({
-      competency: Competencies.controlsGears,
-      newFaultCount: 1,
-    }));
-    expect(result.drivingFaults.controlsGears).toBe(1);
-  });
-  it('should update a driving fault when it already exists', () => {
-    const state: TestData = {
-      drivingFaults: {
-        controlsGears: 1,
-      },
-    };
+  describe('ADD_DRIVING_FAULT', () => {
+    it('should add a driving fault when no driving faults exist', () => {
+      const result = testDataReducer(initialState, new AddDrivingFault({
+        competency: Competencies.controlsGears,
+        newFaultCount: 1,
+      }));
+      expect(result.drivingFaults.controlsGears).toBe(1);
+    });
+    it('should update a driving fault when it already exists', () => {
+      const state: TestData = {
+        drivingFaults: {
+          controlsGears: 1,
+        },
+      };
 
-    const result = testDataReducer(state, new AddDrivingFault({
-      competency: Competencies.controlsGears,
-      newFaultCount: 2,
-    }));
-    expect(result.drivingFaults.controlsGears).toBe(2);
-  });
-  it('should not remove an exisitng driving fault when a new one is added', () => {
-    const state: TestData = {
-      drivingFaults: {
-        controlsParkingBrake: 1,
-      },
-    };
+      const result = testDataReducer(state, new AddDrivingFault({
+        competency: Competencies.controlsGears,
+        newFaultCount: 2,
+      }));
+      expect(result.drivingFaults.controlsGears).toBe(2);
+    });
+    it('should not remove an exisiting driving fault when a new one is added', () => {
+      const state: TestData = {
+        drivingFaults: {
+          controlsParkingBrake: 1,
+        },
+      };
 
-    const result = testDataReducer(state, new AddDrivingFault({
-      competency: Competencies.controlsGears,
-      newFaultCount: 1,
-    }));
-    expect(result.drivingFaults.controlsGears).toBe(1);
-    expect(result.drivingFaults.controlsParkingBrake).toBe(1);
+      const result = testDataReducer(state, new AddDrivingFault({
+        competency: Competencies.controlsGears,
+        newFaultCount: 1,
+      }));
+      expect(result.drivingFaults.controlsGears).toBe(1);
+      expect(result.drivingFaults.controlsParkingBrake).toBe(1);
+    });
   });
+  describe('ADD SERIOUS FAULT', () => {
+    it('should add a serious fault when none exist', () => {
+      const result = testDataReducer(initialState, new AddSeriousFault(Competencies.followingDistance));
+      expect(result.seriousFaults.followingDistance).toBeTruthy();
+    });
+    it('should update serious fault which already exists', () => {
+      const state: TestData = {
+        seriousFaults: {
+          followingDistance: true,
+        },
+      };
 
+      const result = testDataReducer(state, new AddSeriousFault(Competencies.followingDistance));
+      expect(result.seriousFaults.followingDistance).toBeTruthy();
+    });
+    it('should not remove an exisiting serious fault when a new one is added', () => {
+      const state: TestData = {
+        seriousFaults: {
+          followingDistance: true,
+        },
+      };
+
+      const result = testDataReducer(state, new AddSeriousFault(Competencies.judgementCrossing));
+      expect(result.seriousFaults.followingDistance).toBeTruthy();
+      expect(result.seriousFaults.judgementCrossing).toBeTruthy();
+    });
+  });
 });
