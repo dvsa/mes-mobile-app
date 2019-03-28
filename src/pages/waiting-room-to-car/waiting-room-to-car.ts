0b980cd1c6217c24cf241de8532e04f8143954ae
John Morley <41190821+johnmorleybjss@users.noreply.github.com>
2019-03-28 12:58:40 +0000

    MES-1639: Waiting Room To Car (#234)
    * Create the Waiting Room to Car screen and persist the form elements to state.
    * Updated Ionic grid to use 96 columns instead of 12 for more fine-grained control. Updated all usages of col-x directive to suit.
    * Add new directives for restriction of values typed into input fields.

--- src/pages/waiting-room-to-car/waiting-room-to-car.ts
+++ src/pages/waiting-room-to-car/waiting-room-to-car.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 import { BasePageComponent } from '../../shared/classes/base-page';
 import { AuthenticationProvider } from '../../providers/authentication/authentication';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { StoreModel } from '../../shared/models/store.model';
 import { WaitingRoomToCarViewDidEnter } from './waiting-room-to-car.actions';
+import { Observable } from 'rxjs/Observable';
+import { GearboxCategory } from '@dvsa/mes-test-schema/categories/B';
+import { getCurrentTest } from '../../modules/tests/tests.selector';
+import {
+  SchoolCarToggled,
+  DualControlsToggled,
+  GearboxCategoryChanged,
+  VehicleRegistrationChanged,
+} from '../../modules/tests/vehicle-details/vehicle-details.actions';
+import { fromEvent } from 'rxjs/Observable/fromEvent';
+import { map, distinctUntilChanged, debounceTime } from 'rxjs/operators';
+import { Subscription } from 'rxjs/Subscription';
+import {
+  InstructorAccompanimentToggled,
+  OtherAccompanimentToggled,
+  SupervisorAccompanimentToggled,
+} from '../../modules/tests/accompaniment/accompaniment.actions';
+import { getVehicleDetails } from '../../modules/tests/vehicle-details/vehicle-details.reducer';
+import { getAccompaniment } from '../../modules/tests/accompaniment/accompaniment.reducer';
+import { InstructorRegistrationNumberChanged } from '../../modules/tests/instructor-details/instructor-details.actions';
+import {
+  getRegistrationNumber,
+  getGearboxCategory,
+  getSchoolCar,
+  getDualControls,
+} from '../../modules/tests/vehicle-details/vehicle-details.selector';
+import {
+  getInstructorAccompaniment,
+  getSupervisorAccompaniment,
+  getOtherAccompaniment,
+} from '../../modules/tests/accompaniment/accompaniment.selector';
+import { getCandidate } from '../../modules/tests/candidate/candidate.reducer';
+import { getUntitledCandidateName } from '../../modules/tests/candidate/candidate.selector';
+
+interface WaitingRoomToCarPageState {
+  candidateName$: Observable<string>;
+  registrationNumber$: Observable<string>;
+  transmission$: Observable<GearboxCategory>;
+  schoolCar$: Observable<boolean>;
+  dualControls$: Observable<boolean>;
+  instructorAccompaniment$: Observable<boolean>;
+  supervisorAccompaniment$: Observable<boolean>;
+  otherAccompaniment$: Observable<boolean>;
+}
 
 @IonicPage()
 @Component({
@@ -12,6 +56,16 @@ import { WaitingRoomToCarViewDidEnter } from './waiting-room-to-car.actions';
   templateUrl: 'waiting-room-to-car.html',
 })
 export class WaitingRoomToCarPage extends BasePageComponent{
+  pageState: WaitingRoomToCarPageState;
+
+  @ViewChild('registrationInput')
+  regisrationInput: ElementRef;
+
+  @ViewChild('instructorRegistrationInput')
+  instructorRegisrationInput: ElementRef;
+
+  inputSubscriptions: Subscription[] = [];
+
   constructor(
     private store$: Store<StoreModel>,
     public navCtrl: NavController,
@@ -22,8 +76,108 @@ export class WaitingRoomToCarPage extends BasePageComponent{
     super(platform, navCtrl, authenticationProvider);
   }
 
+  ngOnInit(): void {
+    this.pageState = {
+      candidateName$: this.store$.pipe(
+        select(getCurrentTest),
+        select(getCandidate),
+        select(getUntitledCandidateName),
+      ),
+      registrationNumber$: this.store$.pipe(
+        select(getCurrentTest),
+        select(getVehicleDetails),
+        select(getRegistrationNumber),
+      ),
+      transmission$: this.store$.pipe(
+        select(getCurrentTest),
+        select(getVehicleDetails),
+        select(getGearboxCategory),
+      ),
+      schoolCar$: this.store$.pipe(
+        select(getCurrentTest),
+        select(getVehicleDetails),
+        select(getSchoolCar),
+      ),
+      dualControls$: this.store$.pipe(
+        select(getCurrentTest),
+        select(getVehicleDetails),
+        select(getDualControls),
+      ),
+      instructorAccompaniment$: this.store$.pipe(
+        select(getCurrentTest),
+        select(getAccompaniment),
+        select(getInstructorAccompaniment),
+      ),
+      supervisorAccompaniment$: this.store$.pipe(
+        select(getCurrentTest),
+        select(getAccompaniment),
+        select(getSupervisorAccompaniment),
+      ),
+      otherAccompaniment$: this.store$.pipe(
+        select(getCurrentTest),
+        select(getAccompaniment),
+        select(getOtherAccompaniment),
+      ),
+    };
+    this.inputSubscriptions = [
+      this.inputChangeSubscriptionDispatchingAction(this.regisrationInput, VehicleRegistrationChanged),
+      this.inputChangeSubscriptionDispatchingAction(
+        this.instructorRegisrationInput,
+        InstructorRegistrationNumberChanged,
+      ),
+    ];
+  }
+
+  ngOnDestroy(): void {
+    this.inputSubscriptions.forEach(sub => sub.unsubscribe());
+  }
+
   ionViewDidEnter(): void {
     this.store$.dispatch(new WaitingRoomToCarViewDidEnter());
   }
 
+  schoolCarToggled(): void {
+    this.store$.dispatch(new SchoolCarToggled());
+  }
+
+  dualControlsToggled(): void {
+    this.store$.dispatch(new DualControlsToggled());
+  }
+
+  manualTransmission(): void {
+    this.store$.dispatch(new GearboxCategoryChanged('Manual'));
+  }
+
+  automaticTransmission(): void {
+    this.store$.dispatch(new GearboxCategoryChanged('Automatic'));
+  }
+
+  instructorAccompanimentToggled(): void {
+    this.store$.dispatch(new InstructorAccompanimentToggled());
+  }
+
+  supervisorAccompanimentToggled(): void {
+    this.store$.dispatch(new SupervisorAccompanimentToggled());
+  }
+
+  otherAccompanimentToggled(): void {
+    this.store$.dispatch(new OtherAccompanimentToggled());
+  }
+
+  /**
+   * Returns a subscription to the debounced changes of a particular input fields.
+   * Dispatches the provided action type to the store when a new value is yielded.
+   * @param inputRef The input to listen for changes on.
+   * @param actionType The the type of action to dispatch, should accept an argument for the input value.
+   */
+  inputChangeSubscriptionDispatchingAction(inputRef: ElementRef, actionType: any): Subscription {
+    const changeStream$ = fromEvent(inputRef.nativeElement, 'keyup').pipe(
+        map((event: any) => event.target.value),
+        debounceTime(1000),
+        distinctUntilChanged(),
+      );
+    const subscription = changeStream$
+      .subscribe((newVal: string) => this.store$.dispatch(new actionType(newVal)));
+    return subscription;
+  }
 }
