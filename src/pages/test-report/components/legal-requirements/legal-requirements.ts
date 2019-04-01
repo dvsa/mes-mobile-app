1441f23e9b50ad0b8e77d53f9d9195f4f988cc99
szabi.keresztes <szabi.keresztes@bjss.com>
2019-04-01 11:57:05 +0100

    Selecting test requirements data from store

--- src/pages/test-report/components/legal-requirements/legal-requirements.ts
+++ src/pages/test-report/components/legal-requirements/legal-requirements.ts
@@ -1,8 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Store, select } from '@ngrx/store';
+import { StoreModel } from '../../../../shared/models/store.model';
+import { getCurrentTest } from '../../../../modules/tests/tests.selector';
+import { getTests } from '../../../../modules/tests/tests.reducer';
+import { getTestData } from '../../../../modules/tests/test_data/test-data.reducer';
+import { getTestRequirements } from '../../../../modules/tests/test_data/test-data.selector';
+import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs/Subscription';
+
+interface LegalRequirementsComponentState {
+  normalStart1$: Observable<boolean>;
+  normalStart2$: Observable<boolean>;
+  angledStart$: Observable<boolean>;
+  hillStart$: Observable<boolean>;
+}
 
 @Component({
   selector: 'legal-requirements',
   templateUrl: 'legal-requirements.html',
 })
-export class LegalRequirementsComponent {
+export class LegalRequirementsComponent implements OnInit {
+
+  componentState: LegalRequirementsComponentState;
+  subscription: Subscription;
+
+  constructor(private store$: Store<StoreModel>) {}
+
+  ngOnInit(): void {
+
+    const testRequirements$ = this.store$.pipe(
+      select(getTests),
+      select(getCurrentTest),
+      select(getTestData),
+      select(getTestRequirements),
+    );
+
+    this.componentState = {
+      normalStart1$: testRequirements$.pipe(
+        map(testRequirements => testRequirements.normalStart1),
+      ),
+      normalStart2$: testRequirements$.pipe(
+        map(testRequirements => testRequirements.normalStart2),
+      ),
+      angledStart$: testRequirements$.pipe(
+        map(testRequirements => testRequirements.angledStart),
+      ),
+      hillStart$: testRequirements$.pipe(
+        map(testRequirements => testRequirements.hillStart),
+      ),
+    };
+  }
+
 }
