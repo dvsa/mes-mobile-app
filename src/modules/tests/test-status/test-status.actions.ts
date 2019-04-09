58fb2797ce99a041ccca07da298c81f07adab971
John Morley <john.morley@bjss.com>
2019-04-09 17:16:22 +0100

    Mark test as decided once we reach the back-to-office page

--- src/modules/tests/test-status/test-status.actions.ts
+++ src/modules/tests/test-status/test-status.actions.ts
@@ -1,10 +1,16 @@
 import { Action } from '@ngrx/store';
 
 export const TEST_STATUS_STARTED = '[Test status] Started';
+export const TEST_STATUS_DECIDED = '[Test status] Decided';
 
 export class TestStatusStarted implements Action {
   readonly type = TEST_STATUS_STARTED;
 }
 
+export class TestStatusDecided implements Action {
+  readonly type = TEST_STATUS_DECIDED;
+}
+
 export type Types =
-  | TestStatusStarted;
+  | TestStatusStarted
+  | TestStatusDecided;
