4c0ab0680ae8fbc4d8594c9c1928bcc425723d62
roberthall12 <roberth@kainos.com>
2019-04-11 09:37:44 +0100

    Added ETA and ECO fault info

--- src/pages/office/office.ts
+++ src/pages/office/office.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 import { BasePageComponent } from '../../shared/classes/base-page';
 import { AuthenticationProvider } from '../../providers/authentication/authentication';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { StoreModel } from '../../shared/models/store.model';
 import { OfficeViewDidEnter } from './office.actions';
+import { Observable } from 'rxjs/Observable';
+import { getCurrentTest } from '../../modules/tests/tests.selector';
+import { getTests } from '../../modules/tests/tests.reducer';
+import { getETA, getETAFaultText, getEco, getEcoFaultText } from '../../modules/tests/test_data/test-data.selector';
+import { getTestData } from '../../modules/tests/test_data/test-data.reducer';
+
+interface OfficePageState {
+  etaFaults$: Observable<string>;
+  ecoFaults$: Observable<string>;
+}
 
 @IonicPage()
 @Component({
@@ -12,6 +22,7 @@ import { OfficeViewDidEnter } from './office.actions';
   templateUrl: 'office.html',
 })
 export class OfficePage extends BasePageComponent {
+  pageState: OfficePageState;
 
   constructor(
     private store$: Store<StoreModel>,
@@ -21,10 +32,31 @@ export class OfficePage extends BasePageComponent {
     public authenticationProvider: AuthenticationProvider,
   ) {
     super(platform, navCtrl, authenticationProvider);
+
   }
 
   ionViewDidEnter(): void {
     this.store$.dispatch(new OfficeViewDidEnter());
+  }
+
+  ngOnInit(): void {
+
+    this.pageState = {
+      etaFaults$: this.store$.pipe(
+        select(getTests),
+        select(getCurrentTest),
+        select(getTestData),
+        select(getETA),
+        select(getETAFaultText),
+      ),
+      ecoFaults$: this.store$.pipe(
+        select(getTests),
+        select(getCurrentTest),
+        select(getTestData),
+        select(getEco),
+        select(getEcoFaultText),
+      ),
+    };
   }
 
   popToRoot() {
