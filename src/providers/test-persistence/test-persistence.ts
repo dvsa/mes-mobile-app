13fe07d51a0ec1439637bcfded329961fab90ed4
John Morley <john.morley@bjss.com>
2019-04-11 11:33:34 +0100

    Persist all tests and read them back out to prove persistence is working

--- src/providers/test-persistence/test-persistence.ts
+++ src/providers/test-persistence/test-persistence.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
+import { Store, select } from '@ngrx/store';
+import { StoreModel } from '../../shared/models/store.model';
+import { getTests } from '../../modules/tests/tests.reducer';
+import { switchMap, tap } from 'rxjs/operators';
+import { DataStoreProvider } from '../data-store/data-store';
+import { from } from 'rxjs/observable/from';
 
 @Injectable()
 export class TestPersistenceProvider {
 
   constructor(
+    private store$: Store<StoreModel>,
+    private dataStoreProvider: DataStoreProvider,
   ) {}
 
   persistAllTests(): Observable<string> {
-    console.log('I will persist it for you...');
-    return of('');
+    return this.store$.pipe(
+      select(getTests),
+      tap(tests => console.log(`trying to persist tests: ${JSON.stringify(tests)}`)),
+      switchMap(tests => from(this.dataStoreProvider.setItem('TESTS', JSON.stringify(tests)).then(x => x))),
+      tap(tests => console.log(`PERSISTED: ${tests}`)),
+      switchMap(_ => from(this.dataStoreProvider.getItem('TESTS').then(x => x))),
+      tap(read => console.log(`READ BACK: ${read}`)),
+    );
   }
 
 }
