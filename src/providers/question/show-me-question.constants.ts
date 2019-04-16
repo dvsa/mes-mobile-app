b771b304b001beb26547da65946878b6bd48d2a3
Jason Pullen <jasonpu@kainos.com>
2019-04-16 13:07:27 +0100

    added all show me questions

--- src/providers/question/show-me-question.constants.ts
+++ src/providers/question/show-me-question.constants.ts
@@ -4,17 +4,43 @@ import { ShowMeQuestion } from './show-me-question.model';
 export default [
   {
     showMeQuestionCode: 'S1',
-    showMeQuestionDescription: 'Show me how you would clean the rear windscreen.',
-    showMeQuestionShortName: 'Clean rear windscreen',
+    showMeQuestionDescription:
+      'When it is safe to do so can you show me how you wash and clean the rear windscreen.',
+    showMeQuestionShortName: 'Rear windscreen',
   },
   {
     showMeQuestionCode: 'S2',
-    showMeQuestionDescription: 'Show me how you would clean the front windscreen.',
-    showMeQuestionShortName: 'Clean rear windscreen',
+    showMeQuestionDescription:
+      'When it is safe to do so can you show me how you wash and clean the front windscreen.',
+    showMeQuestionShortName: 'Front windscreen',
+
   },
   {
     showMeQuestionCode: 'S3',
-    showMeQuestionDescription: 'Show me how you would dip the headlights.',
+    showMeQuestionDescription:
+      'When it is safe to do so can you show me how you would switch on your dipped headlights',
     showMeQuestionShortName: 'Dipped headlights',
+
+  },
+  {
+    showMeQuestionCode: 'S4',
+    showMeQuestionDescription: 'When it is safe to do so can you show me how you would set the rear demister.',
+    showMeQuestionShortName: 'Rear demister',
+
+  },
+  {
+    showMeQuestionCode: 'S5',
+    showMeQuestionDescription: 'When it is safe to do so can you show me how you would operate the horn.',
+    showMeQuestionShortName: 'Horn',
+  },
+  {
+    showMeQuestionCode: 'S6',
+    showMeQuestionDescription: 'When it is safe to do so can you show me how you would demist the front windscreen.',
+    showMeQuestionShortName: 'Demist front windscreen',
+  },
+  {
+    showMeQuestionCode: 'S7',
+    showMeQuestionDescription: 'When it is safe can you show me how you would open and close the side window.',
+    showMeQuestionShortName: 'Side window',
   },
 ] as ShowMeQuestion[];
