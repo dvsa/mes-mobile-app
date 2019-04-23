cfb76a9a53f703bfbc2d26bc63a9a495da73096f
markpatt <m.patterson@kainos.com>
2019-04-23 10:59:39 +0100

    Moving input area subscription function to abstract class

--- src/pages/office/components/drivingFaultComment/driving-fault-comment.ts
+++ src/pages/office/components/drivingFaultComment/driving-fault-comment.ts
@@ -5,12 +5,11 @@ import { Observable } from 'rxjs/Observable';
 import { getTests } from '../../../../modules/tests/tests.reducer';
 import { getCurrentTest } from '../../../../modules/tests/tests.selector';
 import { getDrivingFaultComment } from '../../../debrief/debrief.selector';
-import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { FormGroup } from '@angular/forms';
 import { Subscription } from 'rxjs/Subscription';
-import { fromEvent } from 'rxjs/Observable/fromEvent';
 import { AddDrivingFaultComment } from '../../../../modules/tests/test_data/test-data.actions';
-import { extractPropertyNameFromId } from '../../../../shared/helpers/property-name-extraction';
+import { InputSubscriptionActionDispatcher } from '../../../../shared/classes/input-area-action-dispatcher';
 
 interface DrivingFaultCommentComponentState {
   competencyComment$: Observable<string>;
@@ -20,7 +19,7 @@ interface DrivingFaultCommentComponentState {
   selector: 'driving-fault-comment',
   templateUrl: 'driving-fault-comment.html',
 })
-export class DrivingFaultCommentComponent implements OnInit {
+export class DrivingFaultCommentComponent extends InputSubscriptionActionDispatcher implements OnInit {
   pageState: DrivingFaultCommentComponentState;
   @Input()
   parentForm: FormGroup;
@@ -44,51 +43,32 @@ export class DrivingFaultCommentComponent implements OnInit {
   inputSubscription$: Subscription;
 
   constructor(
-      private store$: Store<StoreModel>,
-    ) {}
+    public store$: Store<StoreModel>,
+  ) {
+    super(store$);
+  }
 
   ngOnInit(): void {
     const currentTest$ = this.store$.pipe(
       select(getTests),
       select(getCurrentTest),
-      );
+    );
 
     this.pageState = {
       competencyComment$: currentTest$.pipe(
-          map(data => getDrivingFaultComment(data.testData.drivingFaults, `${this.competency}Comments`)),
-        ),
+        map(data => getDrivingFaultComment(data.testData.drivingFaults, `${this.competency}Comments`)),
+      ),
     };
   }
 
   ngAfterViewInit(): void {
-    this.inputSubscription$ = this.inputAreaChangeSubscriptionDispatchingAction(
-        this.drivingFaultComment,
-        this.drivingFaultComment.nativeElement.id,
-        AddDrivingFaultComment);
+    this.inputSubscription$ = super.inputAreaChangeSubscriptionDispatchingAction(
+      this.drivingFaultComment,
+      this.drivingFaultComment.nativeElement.id,
+      AddDrivingFaultComment);
 
   }
   ngOnDestroy(): void {
     this.inputSubscription$.unsubscribe();
-  }
-
-    /**
-   * Returns a subscription to the debounced changes of a particular input fields.
-   * Dispatches the provided action type to the store when a new value is yielded.
-   * @param inputRef The input to listen for changes on.
-   * @param actionType The the type of action to dispatch, should accept an argument for the input value.
-   */
-  inputAreaChangeSubscriptionDispatchingAction(
-    inputRef: ElementRef,
-    id: string,
-    actionType: any): Subscription {
-    const changeStream$ = fromEvent(inputRef.nativeElement, 'keyup').pipe(
-      map((event: any) => event.target.value),
-      debounceTime(1000),
-      distinctUntilChanged(),
-    );
-    const subscription = changeStream$
-      .subscribe((newVal: string) => this.store$.dispatch(
-        new actionType(extractPropertyNameFromId(id), newVal)));
-    return subscription;
   }
 }
