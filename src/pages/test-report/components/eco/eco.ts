a528d8a4f18276213f32088973273cd0965abc4a
Natalie Johnson <Natalie.Johnson@bjss.com>
2019-04-26 11:45:15 +0100

    Add functionality to ECO buttons

--- src/pages/test-report/components/eco/eco.ts
+++ src/pages/test-report/components/eco/eco.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { StoreModel } from '../../../../shared/models/store.model';
+import { Store, select } from '@ngrx/store';
+import { getTests } from '../../../../modules/tests/tests.reducer';
+import { getCurrentTest } from '../../../../modules/tests/tests.selector';
+import { getTestData } from '../../../../modules/tests/test_data/test-data.reducer';
+import { getEco } from '../../../../modules/tests/test_data/test-data.selector';
+import {
+  ToggleEco,
+  ToggleControlEco,
+  TogglePlanningEco,
+} from '../../../../modules/tests/test_data/test-data.actions';
 
 @Component({
   selector: 'eco',
@@ -6,6 +19,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EcoComponent implements OnInit {
 
-  ngOnInit(): void {}
+  subscription: Subscription;
 
+  completed: boolean = false;
+  adviceGivenPlanning: boolean = false;
+  adviceGivenControl: boolean = false;
+
+  constructor(
+    private store$: Store<StoreModel>,
+  ) {}
+
+  ngOnInit(): void {
+
+    const eco$ = this.store$.pipe(
+      select(getTests),
+      select(getCurrentTest),
+      select(getTestData),
+      select(getEco),
+      );
+
+    this.subscription = eco$.subscribe((eco) => {
+      this.completed = eco.completed;
+      this.adviceGivenPlanning = eco.adviceGivenPlanning;
+      this.adviceGivenControl = eco.adviceGivenControl;
+    });
+
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  toggleEco = (): void => {
+    if (this.adviceGivenPlanning || this.adviceGivenControl) {
+      return;
+    }
+    this.store$.dispatch(new ToggleEco());
+  }
+
+  toggleEcoPlanning = (): void => {
+    this.store$.dispatch(new TogglePlanningEco());
+  }
+
+  toggleEcoControl = (): void => {
+    this.store$.dispatch(new ToggleControlEco());
+  }
 }
