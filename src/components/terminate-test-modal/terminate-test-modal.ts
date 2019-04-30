4f769d81deb63a011112e83491b7ba4f1e7e5dd3
John Morley <john.morley@bjss.com>
2019-04-30 16:40:26 +0100

    Implement lock screen wrapping of termination callback

--- src/components/terminate-test-modal/terminate-test-modal.ts
+++ src/components/terminate-test-modal/terminate-test-modal.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavParams } from 'ionic-angular';
+import { DeviceAuthenticationProvider } from '../../providers/device-authentication/device-authentication';
 
 @IonicPage()
 @Component({
@@ -10,10 +11,24 @@ export class TerminateTestModal {
 
   onCancel: Function;
 
+  onTerminate: Function;
+
   constructor(
     private navParams: NavParams,
+    private deviceAuthenticationProvider: DeviceAuthenticationProvider,
   ) {
     this.onCancel = this.navParams.get('onCancel');
+  }
+
+  /**
+   * Fired when the termination of the test is confirmed.
+   * Handles re-authentication and subsequent delegation to the onTerminate callback.
+   */
+  terminationWrapper(): Promise<void> {
+    return this.deviceAuthenticationProvider.triggerLockScreen()
+      .then(() => {
+        this.onTerminate();
+      });
   }
 
 }
