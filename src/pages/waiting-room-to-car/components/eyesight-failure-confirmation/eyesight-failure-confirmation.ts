e3b54608a7ace4961b275c4e8188b874958bee1b
John Morley <john.morley@bjss.com>
2019-05-01 11:12:16 +0100

    Fix eyesight failure debrief page

--- src/pages/waiting-room-to-car/components/eyesight-failure-confirmation/eyesight-failure-confirmation.ts
+++ src/pages/waiting-room-to-car/components/eyesight-failure-confirmation/eyesight-failure-confirmation.ts
@@ -1,16 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'eyesight-failure-confirmation',
   templateUrl: 'eyesight-failure-confirmation.html',
 })
 export class EyesightFailureConfirmationComponent {
-  constructor() {}
+  constructor(public navController: NavController) { }
 
   @Input()
   cancelFn: Function;
 
   onCancel(): void {
     this.cancelFn();
+  }
+
+  onContinue(): void {
+    this.navController.push('DebriefPage', { outcome: 'fail' });
   }
 }
