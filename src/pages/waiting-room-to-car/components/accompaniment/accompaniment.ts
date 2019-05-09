8fa6b90196b3c65866c3d2b79d44cbd0f5cfca21
Ross Faulds <ross.faulds@bjss.com>
2019-05-09 09:13:50 +0100

    temporary commit

---
+++ src/pages/waiting-room-to-car/components/accompaniment/accompaniment.ts
@@ -0,0 +1,45 @@
+import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { GearboxCategory } from '@dvsa/mes-test-schema/categories/B';
+
+@Component({
+  selector: 'accompaniment',
+  templateUrl: 'accompaniment.html',
+})
+export class TransmissionComponent implements OnChanges {
+
+  @Input()
+  instructorAccompaniment: boolean;
+  @Input()
+  superVisorAccompaniment: boolean;
+  @Input()
+  otherAccompaniment: boolean;
+
+  @Input()
+  formGroup: FormGroup;
+
+  @Output()
+  instructorAccompanimentChange = new EventEmitter<boolean>();
+  @Output()
+  superVisorAccompanimentChange = new EventEmitter<boolean>();
+  @Output()
+  otherAccompanimentChange = new EventEmitter<boolean>();
+
+  private formControl: FormControl;
+
+  ngOnChanges(): void {
+    if (!this.formControl) {
+      this.formControl = new FormControl('Transmission', [Validators.required]);
+      this.formGroup.addControl('accompaniment', this.formControl);
+    }
+    this.formControl.patchValue(this.accompaniment);
+  }
+
+  accompanimentChanged(accompaniment: GearboxCategory): void {
+    if (this.formControl.valid) {
+      this.accompanimentChange.emit(accompaniment);
+    }
+  }
+
+}
