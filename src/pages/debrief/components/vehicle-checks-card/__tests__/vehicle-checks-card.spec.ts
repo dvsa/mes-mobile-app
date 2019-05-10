940d6d9f3af2bdc4a10c108bf12d9a95ae546bad
szabi.keresztes <szabi.keresztes@bjss.com>
2019-05-10 15:51:33 +0100

    Implementing unit tests for vehicle-checks-card

--- src/pages/debrief/components/vehicle-checks-card/__tests__/vehicle-checks-card.spec.ts
+++ src/pages/debrief/components/vehicle-checks-card/__tests__/vehicle-checks-card.spec.ts
@@ -1,12 +1,20 @@
 import { ComponentFixture, async, TestBed } from '@angular/core/testing';
 import { VehicleChecksCardComponent } from '../vehicle-checks-card';
-import { IonicModule } from 'ionic-angular';
+import { IonicModule, Config } from 'ionic-angular';
 import { StoreModule, Store } from '@ngrx/store';
 import { testsReducer } from '../../../../../modules/tests/tests.reducer';
 import { StoreModel } from '../../../../../shared/models/store.model';
 import { StartTest } from '../../../../journal/journal.actions';
+import {
+  TellMeQuestionCorrect,
+  ShowMeQuestionPassed,
+  ShowMeQuestionDrivingFault,
+  TellMeQuestionDrivingFault,
+} from '../../../../../modules/tests/test-data/test-data.actions';
+import { By } from '@angular/platform-browser';
+import { ConfigMock } from 'ionic-mocks';
 
-describe('VehicleChecksCardComponent', () => {
+fdescribe('VehicleChecksCardComponent', () => {
   let fixture: ComponentFixture<VehicleChecksCardComponent>;
   let component: VehicleChecksCardComponent;
   let store$: Store<StoreModel>;
@@ -19,6 +27,9 @@ describe('VehicleChecksCardComponent', () => {
       imports: [
         IonicModule,
         StoreModule.forRoot({ tests: testsReducer }),
+      ],
+      providers: [
+        { provide: Config, useFactory: () => ConfigMock.instance() },
       ],
     })
       .compileComponents()
@@ -37,6 +48,45 @@ describe('VehicleChecksCardComponent', () => {
   });
 
   describe('DOM', () => {
+    it('should not display the card when no fault marked', () => {
+      store$.dispatch(new TellMeQuestionCorrect());
+      store$.dispatch(new ShowMeQuestionPassed());
+      fixture.detectChanges();
+      const vehicleChecksCard = fixture.debugElement.query(By.css('#vehicle-checks'));
+      expect(vehicleChecksCard).toBeNull();
+    });
+
+    it('should display the card when show me has a fault', () => {
+      store$.dispatch(new TellMeQuestionCorrect());
+      store$.dispatch(new ShowMeQuestionDrivingFault());
+      fixture.detectChanges();
+      const vehicleChecksCard = fixture.debugElement.query(By.css('#vehicle-checks'));
+      expect(vehicleChecksCard).not.toBeNull();
+    });
+
+    it('should display the card when tell me has a fault', () => {
+      store$.dispatch(new TellMeQuestionDrivingFault());
+      store$.dispatch(new ShowMeQuestionPassed());
+      fixture.detectChanges();
+      const vehicleChecksCard = fixture.debugElement.query(By.css('#vehicle-checks'));
+      expect(vehicleChecksCard).not.toBeNull();
+    });
+
+    it('should display tell me question div, when there is a tell me fault', () => {
+      store$.dispatch(new TellMeQuestionDrivingFault());
+      store$.dispatch(new ShowMeQuestionPassed());
+      fixture.detectChanges();
+      const vehicleChecksCard = fixture.debugElement.query(By.css('#tell-me-question'));
+      expect(vehicleChecksCard).not.toBeNull();
+    });
+
+    it('should display show me question div, when there is a show me fault', () => {
+      store$.dispatch(new TellMeQuestionCorrect());
+      store$.dispatch(new ShowMeQuestionDrivingFault());
+      fixture.detectChanges();
+      const vehicleChecksCard = fixture.debugElement.query(By.css('#show-me-question'));
+      expect(vehicleChecksCard).not.toBeNull();
+    });
   });
 
 });
