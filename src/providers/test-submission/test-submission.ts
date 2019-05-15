fa2e628c1f55da0df68bc8903b4023e7a4c2959a
Tom Billington <t.billington@kainos.com>
2019-05-15 13:11:53 +0100

    Compress Test Result data

--- src/providers/test-submission/test-submission.ts
+++ src/providers/test-submission/test-submission.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import { StandardCarTestCATBSchema } from '@dvsa/mes-test-schema/categories/B';
 import { UrlProvider } from '../url/url';
 import { HttpClient } from '@angular/common/http';
+import { gzipSync } from 'zlib';
 
 @Injectable()
 export class TestSubmissionProvider {
@@ -13,9 +14,12 @@ export class TestSubmissionProvider {
     ) {}
 
   submitTests = (data: StandardCarTestCATBSchema[]): Observable<any> => {
+
+    const compressedData = gzipSync(JSON.stringify(data));
+
     return this.httpClient.post(
       this.urlProvider.getTestResultServiceUrl(),
-      data,
+      compressedData,
     );
   }
 }
