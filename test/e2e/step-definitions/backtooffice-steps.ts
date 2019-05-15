dbc9349cdd86a2b07f3253d46ea6bcfe216aeb59
Andrew Lai <andrew.lai@bjss.com>
2019-05-15 14:43:50 +0100

    Updates as per code review comments

--- test/e2e/step-definitions/backtooffice-steps.ts
+++ test/e2e/step-definitions/backtooffice-steps.ts
@@ -4,8 +4,8 @@ import { browser, by, ExpectedConditions } from 'protractor';
 
 Then('I am on the back to office page', () => {
   // No page title so need to check something else exists that exists on the page
-  const continueToWriteUpPage = getElement(by.id('continue-to-write-up-page'));
-  return browser.wait(ExpectedConditions.presenceOf(continueToWriteUpPage));
+  const backToOfficePage = getElement(by.id('back-to-office-page'));
+  return browser.wait(ExpectedConditions.presenceOf(backToOfficePage));
 });
 
 When('I continue to the office write up', () => {
