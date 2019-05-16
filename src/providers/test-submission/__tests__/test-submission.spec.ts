f5fbec8164cd19fe1b9537871e211716e85050d2
Tom Billington <t.billington@kainos.com>
2019-05-16 10:13:54 +0100

    Compress Test Result Data as Base64

--- src/providers/test-submission/__tests__/test-submission.spec.ts
+++ src/providers/test-submission/__tests__/test-submission.spec.ts
@@ -3,6 +3,7 @@ import { TestSubmissionProvider } from '../test-submission';
 import { UrlProvider } from '../../url/url';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UrlProviderMock } from '../../url/__mocks__/url.mock';
+import { gunzipSync } from 'zlib';
 
 describe('TestResultCalculatorProvider', () => {
 
@@ -32,6 +33,39 @@ describe('TestResultCalculatorProvider', () => {
 
       httpMock.expectOne('https://www.example.com/api/v1/test-result');
       expect(urlProvider.getTestResultServiceUrl).toHaveBeenCalled();
+    });
+  });
+  describe('compressData', () => {
+    it('should successfully compress the provided data', () => {
+
+      // ARRANGE
+      const mockData = {
+        tests: {
+          startedTests: {
+            1003: {
+              candidate: {
+                age: 56,
+                candidateId: 103,
+                candidateName: {
+                  firstName: 'Jane',
+                  lastName: 'Doe',
+                  title: 'Mrs',
+                },
+                driverNumber: 'DOEXX625220A99HC',
+                gender: 'Female',
+                mobileTelephone: '07654 123456',
+              },
+            },
+          },
+        },
+      };
+      // ACT
+      const result = testSubmissionProvider.compressData(mockData);
+      // ASSERT
+      const gzippedBytes = Buffer.from(result, 'base64');
+      const unzippedJson = gunzipSync(gzippedBytes).toString();
+      const json = JSON.parse(unzippedJson);
+      expect(json).toEqual(mockData);
     });
   });
 });
