cd77060cb0ccfcd37ad4e66f2d1b359fb585b049
Tom Billington <t.billington@kainos.com>
2019-05-21 12:00:34 +0100

    Added End To End Practice Test Card

---
+++ src/pages/journal/components/practice-end-to-end-card/practice-end-to-end-card.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'practice-end-to-end-card',
+  templateUrl: 'practice-end-to-end-card.html',
+})
+
+export class PracticeEndToEndCardComponent {
+
+  constructor(public navController: NavController) { }
+
+}
