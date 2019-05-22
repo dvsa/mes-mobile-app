3d86c8092208ed02a3eba6ebf8ac5a4fd235eea1
Tom Billington <t.billington@kainos.com>
2019-05-22 10:10:04 +0100

    Added Tests

--- src/pages/journal/components/practice-end-to-end-card/practice-end-to-end-card.ts
+++ src/pages/journal/components/practice-end-to-end-card/practice-end-to-end-card.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'practice-end-to-end-card',
@@ -8,6 +7,6 @@ import { NavController } from 'ionic-angular';
 
 export class PracticeEndToEndCardComponent {
 
-  constructor(public navController: NavController) { }
+  constructor() { }
 
 }
--- src/pages/journal/components/practice-test-report-card/__tests__/practice-test-report-card.spec.ts
+++ src/pages/journal/components/practice-test-report-card/__tests__/practice-test-report-card.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from 'ionic-angular';
+import { IonicModule, NavController, AlertController, ModalController } from 'ionic-angular';
 import { PracticeTestReportCardComponent } from '../practice-test-report-card';
+import { NavControllerMock, AlertControllerMock, ModalControllerMock } from 'ionic-mocks';
+import { journalReducer } from '../../../journal.reducer';
+import { StoreModule } from '@ngrx/store';
 
 describe('PracticeTestReportCard ', () => {
   let component: PracticeTestReportCardComponent;
@@ -9,8 +12,17 @@ describe('PracticeTestReportCard ', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [PracticeTestReportCardComponent],
-      imports: [IonicModule.forRoot(PracticeTestReportCardComponent)],
-      providers: [],
+      imports: [
+        IonicModule.forRoot(PracticeTestReportCardComponent),
+        StoreModule.forRoot({
+          journal: journalReducer,
+        }),
+      ],
+      providers: [
+        { provide: NavController, useFactory: () => NavControllerMock.instance() },
+        { provide: AlertController, useFactory: () => AlertControllerMock.instance() },
+        { provide: ModalController, useFactory: () => ModalControllerMock.instance() },
+      ],
     }).compileComponents().then(() => {
       fixture = TestBed.createComponent(PracticeTestReportCardComponent);
       component = fixture.componentInstance;
