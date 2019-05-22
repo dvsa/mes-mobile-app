6f0f344dd11f7d4c9b4a143b12bd2ff433e0eba0
Tom Billington <t.billington@kainos.com>
2019-05-22 12:52:57 +0100

    Peer Review Changes

--- src/pages/journal/__tests__/journal.spec.ts
+++ src/pages/journal/__tests__/journal.spec.ts
@@ -134,7 +134,7 @@ describe('JournalPage', () => {
           jasmine.createSpy('showTestReportPracticeMode').and.returnValue(true);
 
         fixture.detectChanges();
-        expect(fixture.debugElement.query(By.css('#testReportPracticeMode'))).toBeDefined();
+        expect(fixture.debugElement.query(By.css('#testReportPracticeMode'))).not.toBeNull();
 
       });
       it('should not show test report practice mode banner when config is set to false', () => {
@@ -152,7 +152,7 @@ describe('JournalPage', () => {
           jasmine.createSpy('showEndToEndPracticeMode').and.returnValue(true);
 
         fixture.detectChanges();
-        expect(fixture.debugElement.query(By.css('#endToendPracticeMode'))).toBeDefined();
+        expect(fixture.debugElement.query(By.css('#endToendPracticeMode'))).not.toBeNull();
 
       });
       it('should not show the end to end practice mode banner when config is set to false', () => {
