6536c62d11278b1bfe4dbb80424b7e856af7ed86
Tom Billington <t.billington@kainos.com>
2019-05-29 14:24:38 +0100

    Bug fix

--- src/pages/office/office.ts
+++ src/pages/office/office.ts
@@ -434,6 +434,7 @@ export class OfficePage extends PracticeableBasePageComponent {
   popToRoot() {
     if (this.isPracticeMode) {
       this.navCtrl.popTo('FakeJournalPage');
+      return;
     }
     this.navCtrl.popToRoot();
   }
