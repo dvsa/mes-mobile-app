b08a89705a666a7188459a15f47b2f22145e335d
Tom Billington <t.billington@kainos.com>
2019-05-30 13:06:03 +0100

    Added Fake Candidate Details Popup

--- src/pages/journal/components/candidate-link/candidate-link.ts
+++ src/pages/journal/components/candidate-link/candidate-link.ts
@@ -2,6 +2,9 @@ import { Component, Input } from '@angular/core';
 import { Name } from '../../../../shared/models/DJournal';
 import { ModalController } from 'ionic-angular';
 import { App } from '../../../../app/app.component';
+import { startsWith } from 'lodash';
+import { end2endPracticeSlotId } from '../../../../shared/mocks/test-slot-ids.mock';
+
 @Component({
   selector: 'candidate-link',
   templateUrl: 'candidate-link.html',
@@ -26,11 +29,17 @@ export class CandidateLinkComponent {
   }
 
   openCandidateDetailsModal() {
+    let pageToOpen = 'CandidateDetailsPage';
+
+    if (startsWith(this.slotId.toString(), end2endPracticeSlotId)) {
+      pageToOpen = 'FakeCandidateDetailsPage';
+    }
+
     // Modals are at the same level as the ion-nav so are not getting the zoom level class,
     // this needs to be passed in the create options.
     const zoomClass = `modal-fullscreen ${this.app.getTextZoomClass()}`;
     const profileModal = this.modalController.create(
-      'CandidateDetailsPage',
+      pageToOpen,
       { slotId: this.slotId, slotChanged: this.slotChanged },
       { cssClass: zoomClass });
     profileModal.present();
