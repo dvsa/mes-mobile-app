ab83f323151c5193fea3336febd949dec08f559e
James Highfield <James.Highfield@bjss.com>
2019-06-06 15:52:52 +0100

    Move return button out of legal requirements modal

--- src/pages/test-report/components/legal-requirements-modal/legal-requirements-modal.module.ts
+++ src/pages/test-report/components/legal-requirements-modal/legal-requirements-modal.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LegalRequirementsModal } from './legal-requirements-modal';
 import { ModalAlertTitleComponent } from '../modal-alert-title/modal-alert-title';
+import { ModalReturnButtonComponent } from '../modal-return-button/modal-return-button';
 
 @NgModule({
   declarations: [
     LegalRequirementsModal,
     ModalAlertTitleComponent,
+    ModalReturnButtonComponent,
   ],
   imports: [
     IonicPageModule.forChild(LegalRequirementsModal),
