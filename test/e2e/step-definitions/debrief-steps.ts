ff1cbf9512c43b45751658551046da2084ee1846
Andrew Lai <andrew.lai@bjss.com>
2019-06-07 15:07:43 +0100

    Adding debrief UI tests and placeholders for test report and office tests (#489)

--- test/e2e/step-definitions/debrief-steps.ts
+++ test/e2e/step-definitions/debrief-steps.ts
@@ -17,11 +17,25 @@ When('I complete the pass details', () => {
   passCertificateNumberField.sendKeys('123456789');
   const licenceRecievedRadio = getElement(by.id('license-received'));
   clickElement(licenceRecievedRadio);
-  const continueButton = getElement(by.xpath('//page-pass-finalisation//button[span[h3[text() = "Continue"]]]'));
-  clickElement(continueButton);
+  continuePassFinalisation();
+});
+
+When('I try to confirm the pass certificate details', () => {
+  continuePassFinalisation();
+});
+
+Then('I should see {string} transmission is selected', (transmissionType) => {
+  const transmissionRadio = getElement(
+    by.xpath(`//page-pass-finalisation//input[@id='transmission-${transmissionType}']`));
+  return expect(transmissionRadio.isSelected()).to.eventually.be.true;
 });
 
 Then('I should see the Debrief page with outcome {string}', (outcome) => {
   const testOutcome = getElement(by.xpath('//page-debrief//div[@id = "test-outcome-background"]/div/h1'));
   return expect(testOutcome.getText()).to.eventually.equal(outcome);
 });
+
+const continuePassFinalisation = () => {
+  const continueButton = getElement(by.xpath('//page-pass-finalisation//button[span[h3[text() = "Continue"]]]'));
+  clickElement(continueButton);
+};
