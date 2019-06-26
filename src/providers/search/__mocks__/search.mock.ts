207a0c5c9d98133677be79648a23f440f715cfe4
Tom Billington <t.billington@kainos.com>
2019-06-26 09:58:15 +0100

    Now send back fake data from mock

--- src/providers/search/__mocks__/search.mock.ts
+++ src/providers/search/__mocks__/search.mock.ts
@@ -1,11 +1,42 @@
-import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { StandardCarTestCATBSchema } from '@dvsa/mes-test-schema/categories/B';
+
+const fakeTestResult: StandardCarTestCATBSchema = {
+  category: 'B',
+  activityCode: '1',
+  journalData: {
+    applicationReference: {
+      applicationId: 1,
+      bookingSequence: 1,
+      checkDigit: 1,
+    },
+    candidate: {
+
+    },
+    examiner: {
+      staffNumber: '12345',
+    },
+    testCentre: {
+      centreId: 1,
+      costCode: '1',
+    },
+    testSlotAttributes: {
+      slotId: 1,
+      welshTest: false,
+      start: '',
+      vehicleSlotType: 'B',
+      specialNeeds: false,
+      extendedTest: false,
+    },
+  },
+};
 
 export class SearchProviderMock {
 
   driverNumberSearch =
-    jasmine.createSpy('driverNumberSearch').and.returnValue(Observable.empty);
+    jasmine.createSpy('driverNumberSearch').and.returnValue(of([fakeTestResult]));
 
   applicationReferenceSearch =
-    jasmine.createSpy('applicationReferenceSearch').and.returnValue(Observable.empty);
+    jasmine.createSpy('applicationReferenceSearch').and.returnValue(of([fakeTestResult]));
 
 }
