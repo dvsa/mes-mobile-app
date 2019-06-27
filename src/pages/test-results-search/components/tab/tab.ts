1905386339603d595090c2614e85401414aaf45c
szabi.keresztes <szabi.keresztes@bjss.com>
2019-06-27 09:21:21 +0100

    renaming components

--- src/pages/test-results-search/components/tab/tab.ts
+++ src/pages/test-results-search/components/tab/tab.ts
@@ -1,12 +1,8 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'my-tab',
-  template: `
-    <div [hidden]="!active" class="pane">
-      <ng-content></ng-content>
-    </div>
-  `,
+  selector: 'tab',
+  templateUrl: 'tab.html',
 })
 export class TabComponent {
   @Input('tabTitle') title: string;
--- src/pages/test-results-search/components/tabs/tabs.ts
+++ src/pages/test-results-search/components/tabs/tabs.ts
@@ -8,28 +8,23 @@ import {
 import { TabComponent } from '../tab/tab';
 
 @Component({
-  selector: 'my-tabs',
+  selector: 'tabs',
   templateUrl: 'tabs.html',
 })
 export class TabsComponent implements AfterContentInit {
   @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
 
-  // contentChildren are set
   ngAfterContentInit() {
-    // get all active tabs
     const activeTabs = this.tabs.filter(tab => tab.active);
 
-    // if there is no active tab set, activate the first
     if (activeTabs.length === 0) {
       this.selectTab(this.tabs.first);
     }
   }
 
   selectTab(tab: TabComponent) {
-    // deactivate all tabs
     this.tabs.toArray().forEach(tab => tab.active = false);
 
-    // activate the tab the user has clicked on.
     tab.active = true;
   }
 }
