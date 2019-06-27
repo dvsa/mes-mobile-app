3b61fe6916490a52d6cc85c30abc9718f9dd50bd
Tom Billington <t.billington@kainos.com>
2019-06-27 11:26:15 +0100

    Updated Provider

--- src/providers/search/search.ts
+++ src/providers/search/search.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { UrlProvider } from '../url/url';
 import { Observable } from 'rxjs/Observable';
 
@@ -20,6 +20,44 @@ export class SearchProvider {
   applicationReferenceSearch(applicationReference: String): Observable<any> {
     return this.http.get(
       this.urlProvider.getTestResultServiceUrl().concat(`?applicationReference=${applicationReference}`),
+    );
+  }
+
+  advancedSearch(
+    isLDTM: boolean,
+    startDate?: string,
+    endDate?: string,
+    staffNumber?: string,
+    costCode?: string,
+    ) {
+
+    const httpParams: HttpParams = new HttpParams();
+
+    isLDTM ?
+    httpParams.set('isLDTM', 'true') :
+    httpParams.set('isLDTM', 'false') ;
+
+    if (startDate) {
+      httpParams.set('startDate', startDate);
+    }
+
+    if (endDate) {
+      httpParams.set('endDate', endDate);
+    }
+
+    if (staffNumber) {
+      httpParams.set('staffNumber', staffNumber);
+    }
+
+    if (costCode) {
+      httpParams.set('dtcCode', costCode);
+    }
+
+    return this.http.get(
+      this.urlProvider.getTestResultServiceUrl(),
+      {
+        params: httpParams,
+      },
     );
   }
 
