856dd41528b61504595b551fe911c47f4f86e098
Tom Billington <t.billington@kainos.com>
2019-07-02 11:16:58 +0100

    Added View Test Result Page

---
+++ src/pages/view-test-result/view-test-result.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ViewTestResultPage } from './view-test-result';
+
+@NgModule({
+  declarations: [
+    ViewTestResultPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ViewTestResultPage),
+  ],
+  providers: [
+  ],
+})
+export class ViewTestResultPageModule {}
