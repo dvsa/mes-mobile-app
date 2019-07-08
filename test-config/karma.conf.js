9231a889525b2c5f73c326fcd10443495a07423d
Andrew Lai <andrew.lai@bjss.com>
2019-07-08 10:33:05 +0100

    Reverting executors back to default value for comparison

--- test-config/karma.conf.js
+++ test-config/karma.conf.js
@@ -63,11 +63,6 @@ module.exports = function(config) {
     },
     browsers: ['ChromeHeadless'],
     singleRun: true,
-
-    parallelOptions: {
-      executors: 4
-    },
-
     customLaunchers: {
       ChromeHeadless: {
         base: 'Chrome',
