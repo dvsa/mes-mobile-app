208214b3d7f31ba090808c09c32b48c6fddaec2e
szabi.keresztes <szabi.keresztes@bjss.com>
2019-07-09 11:00:18 +0100

    Adding analytics effect to tests module

--- src/modules/tests/tests.module.ts
+++ src/modules/tests/tests.module.ts
@@ -4,14 +4,20 @@ import { testsReducer } from './tests.reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { TestsEffects } from './tests.effects';
 import { TestSubmissionProvider } from '../../providers/test-submission/test-submission';
+import { TestsAnalyticsEffects } from './tests.analytics.effects';
+import { AnalyticsProvider } from '../../providers/analytics/analytics';
 
 @NgModule({
   imports: [
     StoreModule.forFeature('tests', testsReducer),
-    EffectsModule.forFeature([TestsEffects]),
+    EffectsModule.forFeature([
+      TestsEffects,
+      TestsAnalyticsEffects,
+    ]),
   ],
   providers:[
     TestSubmissionProvider,
+    AnalyticsProvider,
   ],
 })
 export class TestsModule {}
