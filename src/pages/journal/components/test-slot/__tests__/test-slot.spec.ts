dvsa/mes-mobile-app

commit 8320789a587525bff27b338b97480638f578eee7

MES-2816: Use test permission periods to determine whether tests can be started (#566)
* Extend the configuration model with testPermissionPeriods obtained from config service
* Assess the testPermissionPeriods for whether a particular test category can be started

src/pages/journal/components/test-slot/__tests__/test-slot.spec.ts
@@ -17,7 +17,6 @@ import { LanguageComponent } from '../../language/language';
 import { LocationComponent } from '../../location/location';
 import { AppConfigProvider } from '../../../../../providers/app-config/app-config';
 import { AppConfigProviderMock } from '../../../../../providers/app-config/__mocks__/app-config.mock';
-import { DateTime, Duration } from '../../../../../shared/helpers/date-time';
 import { DateTimeProvider } from '../../../../../providers/date-time/date-time';
 import { DateTimeProviderMock } from '../../../../../providers/date-time/__mocks__/date-time.mock';
 import { StoreModule, Store } from '@ngrx/store';
@@ -225,29 +224,96 @@ describe('TestSlotComponent', () => {
     });
 
     describe('canStartTest', () => {
-      it('should not allow the starting of tests if allowTests is false', () => {
-        component.appConfig.getAppConfig =
-          jasmine.createSpy('getAppConfig').and.returnValue({ journal: { allowTests: false } });
-        expect(component.canStartTest()).toBeFalsy();
+      let getAppConfigSpy;
+      beforeEach(() => {
+        getAppConfigSpy = jasmine.createSpy('getAppConfig');
+        component.appConfig.getAppConfig = getAppConfigSpy;
       });
-      it('should not allow the starting of tests if the testCategory is not allowedTestCategories', () => {
-        component.appConfig.getAppConfig =
-          jasmine.createSpy('getAppConfig')
-          .and
-          .returnValue({ journal: { allowTests: true, allowedTestCategories: ['C'] } });
-        expect(component.canStartTest()).toBeFalsy();
+      it('should disallow the test when there are no permissions', () => {
+        getAppConfigSpy.and.returnValue({ journal: { testPermissionPeriods: [] } });
+        expect(component.canStartTest()).toBe(false);
       });
-      it('should allow the starting of tests if the testCategory is in allowedTestCategories', () => {
-        component.appConfig.getAppConfig =
-          jasmine.createSpy('getAppConfig')
-          .and
-          .returnValue({ journal: { allowTests: true, allowedTestCategories: ['B'] } });
-        expect(component.canStartTest()).toBeTruthy();
+      it('should disallow the test when there are only permissions for other test categories', () => {
+        getAppConfigSpy.and.returnValue({
+          journal: {
+            testPermissionPeriods: [
+              { testCategory: 'C', from: '2019-01-01', to: null },
+            ],
+          },
+        });
+        expect(component.canStartTest()).toBe(false);
       });
-      it('should disallow starting of tests that arent today', () => {
-        component.slot.slotDetail.start =
-          DateTime.at(startTime).add(1, Duration.DAY).format('YYYY-MM-DDTHH:mm:ss+00:00');
-        expect(component.canStartTest()).toBeFalsy();
+      it('should disallow the test when there are permissions for the category which have expired', () => {
+        getAppConfigSpy.and.returnValue({
+          journal: {
+            testPermissionPeriods: [
+              { testCategory: 'B', from: '2019-01-01', to: '2019-01-20' },
+            ],
+          },
+        });
+        expect(component.canStartTest()).toBe(false);
+      });
+      it('should allow the test when there are multiple expired ranges and a subsequent valid range', () => {
+        getAppConfigSpy.and.returnValue({
+          journal: {
+            testPermissionPeriods: [
+              { testCategory: 'B', from: '2018-01-01', to: '2018-03-01' },
+              { testCategory: 'B', from: '2018-05-01', to: '2018-07-01' },
+              { testCategory: 'B', from: '2019-01-01', to: '2019-02-01' },
+            ],
+          },
+        });
+        expect(component.canStartTest()).toBe(true);
+      });
+      it('should allow the test when there is a permission range including the slot date', () => {
+        getAppConfigSpy.and.returnValue({
+          journal: {
+            testPermissionPeriods: [
+              { testCategory: 'B', from: '2019-01-01', to: '2019-02-02' },
+            ],
+          },
+        });
+        expect(component.canStartTest()).toBe(true);
+      });
+      it('should allow the test when there is a permission range starting on the slot date', () => {
+        getAppConfigSpy.and.returnValue({
+          journal: {
+            testPermissionPeriods: [
+              { testCategory: 'B', from: '2019-02-01', to: '2019-02-20' },
+            ],
+          },
+        });
+        expect(component.canStartTest()).toBe(true);
+      });
+      it('should allow the test when there is a permission range ending on the slot date', () => {
+        getAppConfigSpy.and.returnValue({
+          journal: {
+            testPermissionPeriods: [
+              { testCategory: 'B', from: '2019-01-20', to: '2019-02-01' },
+            ],
+          },
+        });
+        expect(component.canStartTest()).toBe(true);
+      });
+      it('should allow the test when there is a permission range for the slot date only', () => {
+        getAppConfigSpy.and.returnValue({
+          journal: {
+            testPermissionPeriods: [
+              { testCategory: 'B', from: '2019-02-01', to: '2019-02-01' },
+            ],
+          },
+        });
+        expect(component.canStartTest()).toBe(true);
+      });
+      it('should allow the test when there is a non-date bounded permission range', () => {
+        getAppConfigSpy.and.returnValue({
+          journal: {
+            testPermissionPeriods: [
+              { testCategory: 'B', from: '2019-02-01', to: null },
+            ],
+          },
+        });
+        expect(component.canStartTest()).toBe(true);
       });
     });
 

src/pages/journal/components/test-slot/test-slot.ts
@@ -5,7 +5,6 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation';
 import { vehicleDetails } from './test-slot.constants';
 import { TestCategory } from '../../../../shared/models/test-category';
 import { AppConfigProvider } from '../../../../providers/app-config/app-config';
-import { DateTime } from '../../../../shared/helpers/date-time';
 import { DateTimeProvider } from '../../../../providers/date-time/date-time';
 import { TestStatus } from '../../../../modules/tests/test-status/test-status.model';
 import { Store, select } from '@ngrx/store';
@@ -45,7 +44,7 @@ export class TestSlotComponent implements SlotComponent, OnInit {
     public appConfig: AppConfigProvider,
     public dateTimeProvider: DateTimeProvider,
     public store$: Store<StoreModel>,
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     const { slotId } = this.slot.slotDetail;
@@ -64,7 +63,7 @@ export class TestSlotComponent implements SlotComponent, OnInit {
   isIndicatorNeededForSlot(): boolean {
     const specialNeeds: boolean = this.isSpecialNeedsSlot();
     const checkNeeded: boolean = this.slot.booking.application.entitlementCheck || false;
-    const nonStandardTest: boolean = getSlotType(this.slot) !==  SlotTypes.STANDARD_TEST;
+    const nonStandardTest: boolean = getSlotType(this.slot) !== SlotTypes.STANDARD_TEST;
 
     return specialNeeds || checkNeeded || nonStandardTest;
   }
@@ -74,7 +73,7 @@ export class TestSlotComponent implements SlotComponent, OnInit {
     return !isNil(specialNeeds) && specialNeeds.length > 0;
   }
 
-  isPortrait() : boolean {
+  isPortrait(): boolean {
     return this.screenOrientation.type === this.screenOrientation.ORIENTATIONS.PORTRAIT_PRIMARY ||
       this.screenOrientation.type === this.screenOrientation.ORIENTATIONS.PORTRAIT;
   }
@@ -84,22 +83,31 @@ export class TestSlotComponent implements SlotComponent, OnInit {
   }
 
   canStartTest(): boolean {
-    if (!this.appConfig.getAppConfig().journal.allowTests) {
-      return false;
-    }
+    const { testPermissionPeriods } = this.appConfig.getAppConfig().journal;
+    const { testCategory } = this.slot.booking.application;
+    const { start } = this.slot.slotDetail;
+    const startAsDate = new Date(start);
+    startAsDate.setHours(0, 0, 0, 0);
+    const periodsPermittingStart = testPermissionPeriods.filter((period) => {
+      const slotHasPeriodStartCriteria = this.hasPeriodStartCriteria(startAsDate, period.from);
+      const slotHasPeriodEndCriteria = this.hasPeriodEndCritiera(startAsDate, period.to);
+      return period.testCategory === testCategory && slotHasPeriodStartCriteria && slotHasPeriodEndCriteria;
+    });
+    return periodsPermittingStart.length > 0;
+  }
 
-    const startDate = new DateTime(this.slot.slotDetail.start);
-    if (startDate.daysDiff(this.dateTimeProvider.now()) < 0) {
-      return false;
-    }
+  private hasPeriodStartCriteria(slotDate: Date, periodFrom: string) {
+    const periodStartDate = new Date(periodFrom);
+    periodStartDate.setHours(0, 0, 0, 0);
+    return slotDate >= periodStartDate;
+  }
 
-    const testCategory = this.slot.booking.application.testCategory;
-    const allowedTestCategories = this.appConfig.getAppConfig().journal.allowedTestCategories;
-
-    if (allowedTestCategories.includes(testCategory)) {
+  private hasPeriodEndCritiera(slotDate: Date, periodTo: string) {
+    if (periodTo === null) {
       return true;
     }
-
-    return false;
+    const periodEndDate = new Date(periodTo);
+    periodEndDate.setHours(0, 0, 0, 0);
+    return slotDate <= periodEndDate;
   }
 }

src/environment/environment.local.ts
@@ -30,6 +30,13 @@ export const environment: LocalEnvironmentFile = {
     enableTestReportPracticeMode: true,
     enableEndToEndPracticeMode: true,
     enableLogoutButton: true,
+    testPermissionPeriods: [
+      {
+        testCategory: 'B',
+        from: '2019-01-01',
+        to: null,
+      },
+    ],
   },
   tests: {
     testSubmissionUrl: 'https://dev.mes.dev-dvsacloud.uk/v1/test-results',

src/environment/models/environment.model.ts
@@ -1,3 +1,5 @@
+import { TestPermissionPeriod } from '../../providers/app-config/app-config.model';
+
 export interface EnvironmentFile {
   isRemote: boolean;
   configUrl: string;
@@ -31,6 +33,7 @@ export interface LocalEnvironmentFile extends EnvironmentFile {
     enableTestReportPracticeMode: boolean;
     enableEndToEndPracticeMode: boolean;
     enableLogoutButton: boolean;
+    testPermissionPeriods: TestPermissionPeriod[]
   };
   tests: {
     testSubmissionUrl: string,

src/providers/app-config/__mocks__/app-config.mock.ts
@@ -41,6 +41,7 @@ export class AppConfigProviderMock {
         enableTestReportPracticeMode: localEnvironmentMock.journal.enableTestReportPracticeMode,
         enableEndToEndPracticeMode: localEnvironmentMock.journal.enableEndToEndPracticeMode,
         enableLogoutButton: localEnvironmentMock.journal.enableLogoutButton,
+        testPermissionPeriods: localEnvironmentMock.journal.testPermissionPeriods,
       },
       tests: {
         testSubmissionUrl: localEnvironmentMock.tests.testSubmissionUrl,

src/providers/app-config/__mocks__/environment.mock.ts
@@ -49,6 +49,13 @@ export const localEnvironmentMock: LocalEnvironmentFile = {
     enableTestReportPracticeMode: true,
     enableEndToEndPracticeMode: true,
     enableLogoutButton: true,
+    testPermissionPeriods: [
+      {
+        testCategory: 'B',
+        from: '2019-01-01',
+        to: null,
+      },
+    ],
   },
   tests: {
     testSubmissionUrl: 'https://www.example.com/api/v1/test-result',

src/providers/app-config/app-config.model.ts
@@ -26,6 +26,7 @@ export type AppConfig = {
     enableTestReportPracticeMode: boolean,
     enableEndToEndPracticeMode: boolean,
     enableLogoutButton: boolean,
+    testPermissionPeriods: TestPermissionPeriod[],
   },
   tests: {
     testSubmissionUrl: string,
@@ -36,3 +37,9 @@ export type AppConfig = {
     autoSendInterval: number,
   },
 };
+
+export interface TestPermissionPeriod {
+  testCategory: string;
+  from: string;
+  to: string | null;
+}

src/providers/app-config/app-config.ts
@@ -171,6 +171,7 @@ export class AppConfigProvider {
         enableTestReportPracticeMode: data.journal.enableTestReportPracticeMode,
         enableEndToEndPracticeMode: data.journal.enableEndToEndPracticeMode,
         enableLogoutButton: data.journal.enableLogoutButton,
+        testPermissionPeriods: data.journal.testPermissionPeriods,
       },
       tests: {
         testSubmissionUrl: data.tests.testSubmissionUrl,

commit 21c63fc8325c1783e58d97c30a966795864b76eb

MES-2269 bugfix: Use DateTime for startTest, disallow dates in the future (#571)
* Fixing regression from #566 which allowed tests on future days to be started
* Migrate canStartTest method to use DateTime helper

src/pages/journal/components/test-slot/__tests__/test-slot.spec.ts
@@ -31,6 +31,7 @@ import { ProgressiveAccessComponent } from '../../progressive-access/progressive
 import { SpecialNeedsCode } from '../../../../candidate-details/candidate-details.constants';
 import { ActivityCodes } from '../../../../../shared/models/activity-codes';
 import { TestSlot } from '@dvsa/mes-journal-schema';
+import { DateTime, Duration } from '../../../../../shared/helpers/date-time';
 
 describe('TestSlotComponent', () => {
   let fixture: ComponentFixture<TestSlotComponent>;
@@ -314,6 +315,18 @@ describe('TestSlotComponent', () => {
           },
         });
         expect(component.canStartTest()).toBe(true);
+      });
+      it('should disallow starting of tests that arent today', () => {
+        getAppConfigSpy.and.returnValue({
+          journal: {
+            testPermissionPeriods: [
+              { testCategory: 'B', from: '2019-01-01', to: null },
+            ],
+          },
+        });
+        component.slot.slotDetail.start =
+          DateTime.at(startTime).add(1, Duration.DAY).format('YYYY-MM-DDTHH:mm:ss+00:00');
+        expect(component.canStartTest()).toBeFalsy();
       });
     });
 

src/pages/journal/components/test-slot/test-slot.ts
@@ -17,6 +17,7 @@ import { SlotTypes } from '../../../../shared/models/slot-types';
 import { map } from 'rxjs/operators';
 import { TestSlot } from '@dvsa/mes-journal-schema';
 import { ActivityCode } from '@dvsa/mes-test-schema/categories/B';
+import { DateTime } from '../../../../shared/helpers/date-time';
 
 interface TestSlotComponentState {
   testStatus$: Observable<TestStatus>;
@@ -85,29 +86,30 @@ export class TestSlotComponent implements SlotComponent, OnInit {
   canStartTest(): boolean {
     const { testPermissionPeriods } = this.appConfig.getAppConfig().journal;
     const { testCategory } = this.slot.booking.application;
-    const { start } = this.slot.slotDetail;
-    const startAsDate = new Date(start);
-    startAsDate.setHours(0, 0, 0, 0);
+    const startDate = new DateTime(this.slot.slotDetail.start);
+
+    if (startDate.daysDiff(this.dateTimeProvider.now()) < 0) {
+      return false;
+    }
+
     const periodsPermittingStart = testPermissionPeriods.filter((period) => {
-      const slotHasPeriodStartCriteria = this.hasPeriodStartCriteria(startAsDate, period.from);
-      const slotHasPeriodEndCriteria = this.hasPeriodEndCritiera(startAsDate, period.to);
+      const slotHasPeriodStartCriteria = this.hasPeriodStartCriteria(startDate, period.from);
+      const slotHasPeriodEndCriteria = this.hasPeriodEndCritiera(startDate, period.to);
       return period.testCategory === testCategory && slotHasPeriodStartCriteria && slotHasPeriodEndCriteria;
     });
     return periodsPermittingStart.length > 0;
   }
 
-  private hasPeriodStartCriteria(slotDate: Date, periodFrom: string) {
-    const periodStartDate = new Date(periodFrom);
-    periodStartDate.setHours(0, 0, 0, 0);
-    return slotDate >= periodStartDate;
+  private hasPeriodStartCriteria(slotDate: DateTime, periodFrom: string) {
+    const periodStartDate = new DateTime(periodFrom);
+    return slotDate.daysDiff(periodStartDate) <= 0;
   }
 
-  private hasPeriodEndCritiera(slotDate: Date, periodTo: string) {
+  private hasPeriodEndCritiera(slotDate: DateTime, periodTo: string) {
     if (periodTo === null) {
       return true;
     }
-    const periodEndDate = new Date(periodTo);
-    periodEndDate.setHours(0, 0, 0, 0);
-    return slotDate <= periodEndDate;
+    const periodEndDate = new DateTime(periodTo);
+    return slotDate.daysDiff(periodEndDate) >= 0;
   }
 }