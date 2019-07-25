fd41f49ffb5618a87c892a771cec8a99e699e0f6
szabi.keresztes <szabi.keresztes@bjss.com>
2019-07-25 14:25:55 +0100

    Adding label to test outcome changed analytics event

--- src/modules/tests/tests.analytics.effects.ts
+++ src/modules/tests/tests.analytics.effects.ts
@@ -92,6 +92,7 @@ export class TestsAnalyticsEffects {
       this.analytics.logEvent(
         AnalyticsEventCategories.TEST_REPORT,
         AnalyticsEvents.TEST_OUTCOME_CHANGED,
+        action.payload,
       );
 
       this.analytics.addCustomDimension(
--- src/pages/test-report/test-report.effects.ts
+++ src/pages/test-report/test-report.effects.ts
@@ -105,7 +105,9 @@ export class TestReportEffects {
 
             const actions: Action[] = [new testsActions.SetActivityCode(result)];
             if (!isEmpty(currentTest.activityCode) && currentTest.activityCode !== result) {
-              actions.push(new testsActions.TestOutcomeChanged());
+              const label = result === '1' ? 'fail to pass' : 'pass to fail';
+              console.log('the label is', label);
+              actions.push(new testsActions.TestOutcomeChanged(label));
             }
 
             return actions;
