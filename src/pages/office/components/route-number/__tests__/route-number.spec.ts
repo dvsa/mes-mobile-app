8a0a85fe80337ad6002122998c042d5da1defa41
Natalie Johnson <Natalie.Johnson@bjss.com>
2019-07-31 09:35:54 +0100

    Add tests

---
+++ src/pages/office/components/route-number/__tests__/route-number.spec.ts
@@ -0,0 +1,56 @@
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+import { RouteNumberComponent } from '../route-number';
+import { IonicModule } from 'ionic-angular';
+import { AppModule } from '../../../../../app/app.module';
+import { OutcomeBehaviourMapProvider } from '../../../../../providers/outcome-behaviour-map/outcome-behaviour-map';
+import { behaviourMap } from '../../../../../pages/office/office-behaviour-map';
+
+describe('RouteNumberComponent', () => {
+  let fixture: ComponentFixture<RouteNumberComponent>;
+  let component: RouteNumberComponent;
+  let behaviourMapProvider: OutcomeBehaviourMapProvider;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        RouteNumberComponent,
+      ],
+      imports: [
+        IonicModule,
+        AppModule,
+      ],
+      providers: [
+        { provide: OutcomeBehaviourMapProvider, useClass: OutcomeBehaviourMapProvider },
+      ],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(RouteNumberComponent);
+        behaviourMapProvider = TestBed.get(OutcomeBehaviourMapProvider);
+        behaviourMapProvider.setBehaviourMap(behaviourMap);
+        component = fixture.componentInstance;
+      });
+  }));
+
+  describe('class', () => {
+    it('should emit route number if 1 character', () => {
+      spyOn(component.routeNumberChange, 'emit');
+      const routeNumber = '7';
+      component.routeNumberChanged(routeNumber);
+      expect(component.routeNumberChange.emit).toHaveBeenCalledWith(Number.parseInt(routeNumber, 10));
+    });
+
+    it('should emit route number if 2 characters', () => {
+      spyOn(component.routeNumberChange, 'emit');
+      const routeNumber = '44';
+      component.routeNumberChanged(routeNumber);
+      expect(component.routeNumberChange.emit).toHaveBeenCalledWith(Number.parseInt(routeNumber, 10));
+    });
+
+    it('should emit no route number if not numeric', () => {
+      spyOn(component.routeNumberChange, 'emit');
+      const routeNumber = 'ABC123';
+      component.routeNumberChanged(routeNumber);
+      expect(component.routeNumberChange.emit).toHaveBeenCalledWith(null);
+    });
+  });
+});
