1fa8f14c58f716af9eaf2d865c9246fe2f8ba84e
John Morley <john.morley@bjss.com>
2019-07-31 09:57:26 +0100

    Insert PostDebriefHoldingPage into non-pass journey

---
+++ src/pages/post-debrief-holding/post-debrief-holding.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, Platform } from 'ionic-angular';
+import { PracticeableBasePageComponent } from '../../shared/classes/practiceable-base-page';
+import { AuthenticationProvider } from '../../providers/authentication/authentication';
+import { Store } from '@ngrx/store';
+import { StoreModel } from '../../shared/models/store.model';
+
+@IonicPage()
+@Component({
+  selector: 'post-debrief-holding',
+  templateUrl: 'post-debrief-holding.html',
+})
+export class PostDebriefHoldingPage extends PracticeableBasePageComponent {
+
+  constructor(
+    store$: Store<StoreModel>,
+    navController: NavController,
+    public platform: Platform,
+    public authenticationProvider: AuthenticationProvider,
+  ) {
+    super(platform, navController, authenticationProvider, store$);
+  }
+
+}
