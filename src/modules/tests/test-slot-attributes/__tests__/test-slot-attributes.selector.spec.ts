6ff9c502201ccf567b838a23f6a74622bf878070
szabi.keresztes <szabi.keresztes@bjss.com>
2019-08-01 14:18:53 +0100

    Writing unit tests for test-slot-attributes.selector

--- src/modules/tests/test-slot-attributes/__tests__/test-slot-attributes.selector.spec.ts
+++ src/modules/tests/test-slot-attributes/__tests__/test-slot-attributes.selector.spec.ts
@@ -4,14 +4,16 @@ import {
   isWelshTest,
   isSpecialNeeds,
   isExtendedTest,
+  extractTestSlotAttributes,
 } from '../test-slot-attributes.selector';
 import { TestSlotAttributes } from '@dvsa/mes-test-schema/categories/B';
 import { DateTime } from '../../../../shared/helpers/date-time';
+import { SlotTypes } from '../../../../shared/models/slot-types';
 
 const testTime = new DateTime().toString();
 const formattedTime = DateTime.at(testTime).format('HH:mm');
 
-describe('testSlotAttributes selector', () => {
+fdescribe('testSlotAttributes selector', () => {
   const testSlotAttributes: TestSlotAttributes = {
     slotId: 1234,
     specialNeeds: true,
@@ -48,6 +50,53 @@ describe('testSlotAttributes selector', () => {
   describe('isWelshTest', () => {
     it('should return if the test is welsh', () => {
       expect(isWelshTest(testSlotAttributes)).toEqual(true);
+    });
+  });
+
+  describe('extractTestSlotAttributes', () => {
+    const slotData = {
+      booking: {
+        application: {
+          welshTest: true,
+          specialNeeds: 'special need 1;special need 2',
+          specialNeedsCode: 'YES',
+          extendedTest: true,
+          entitlementCheck: true,
+          specialNeedsExtendedTest: false,
+        },
+        previousCancellation: ['Act of nature'],
+      },
+      slotDetail: {
+        slotId: 12345,
+        start: 'now',
+      },
+      vehicleSlotTypeCode: 6,
+      vehicleTypeCode: 'B',
+      examinerVisiting: true,
+    };
+
+    it('should return a TestSlotAttributes object from slot data', () => {
+      const testSlotAttributes = extractTestSlotAttributes(slotData);
+
+      expect(testSlotAttributes.welshTest).toBe(slotData.booking.application.welshTest);
+      expect(testSlotAttributes.slotId).toBe(slotData.slotDetail.slotId);
+      expect(testSlotAttributes.start).toBe(slotData.slotDetail.start);
+      expect(testSlotAttributes.specialNeeds).toBe(true);
+      expect(testSlotAttributes.specialNeedsCode).toBe(slotData.booking.application.specialNeedsCode);
+      expect(testSlotAttributes.specialNeedsArray).toEqual(['special need 1', 'special need 2']);
+      expect(testSlotAttributes.vehicleTypeCode).toBe(slotData. vehicleTypeCode);
+      expect(testSlotAttributes.extendedTest).toBe(slotData.booking.application.extendedTest);
+      expect(testSlotAttributes.examinerVisiting).toBe(slotData.examinerVisiting);
+      expect(testSlotAttributes.previousCancellation).toBe(slotData.booking.previousCancellation);
+      expect(testSlotAttributes.entitlementCheck).toBe(slotData.booking.application.entitlementCheck);
+      expect(testSlotAttributes.slotType).toBe(SlotTypes.SINGLE_SLOT_SPECIAL_NEEDS);
+    });
+
+    it('should populate specialNeedsArray with only one element: None', () => {
+      const slotDataWithNoSpecialNeeds = slotData;
+      slotDataWithNoSpecialNeeds.booking.application.specialNeeds = null;
+      const testSlotAttributes = extractTestSlotAttributes(slotDataWithNoSpecialNeeds);
+      expect(testSlotAttributes.specialNeedsArray).toEqual(['None']);
     });
   });
 
