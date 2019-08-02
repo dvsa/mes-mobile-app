264c93ed77fc833cdd6cafc25703ff33115c6df7
roberthall12 <roberth@kainos.com>
2019-08-02 10:57:57 +0100

    Fixed module import

--- src/pages/office/components/office.components.module.ts
+++ src/pages/office/components/office.components.module.ts
@@ -6,15 +6,12 @@ import { FaultCommentComponent } from './fault-comment/fault-comment';
 import { FaultCommentCardComponent } from './fault-comment-card/fault-comment-card';
 import { RouteNumberComponent } from './route-number/route-number';
 import { CandidateDescriptionComponent } from './candidate-description/candidate-description';
-import { DebriefWitnessedComponent } from './debrief-witnessed/debrief-witnessed';
 import { IdentificationComponent } from './identification/identification';
 import { ShowMeQuestionComponent } from './show-me-question/show-me-question';
 import { WeatherConditionsComponent } from './weather-conditions/weather-conditions';
-import { D255Component } from './d255/d255';
 import { AdditionalInformationComponent } from './additional-information/additional-information';
 import { IndependentDrivingComponent } from './independent-driving/independent-driving';
 import { ActivityCodeComponent } from './activity-code/activity-code';
-import { LanguagePreferencesComponent } from './language-preference/language-preferences';
 import { DirectivesModule } from '../../../directives/directives.module';
 
 @NgModule({
@@ -23,15 +20,12 @@ import { DirectivesModule } from '../../../directives/directives.module';
     FaultCommentCardComponent,
     RouteNumberComponent,
     CandidateDescriptionComponent,
-    DebriefWitnessedComponent,
     IdentificationComponent,
     ShowMeQuestionComponent,
     WeatherConditionsComponent,
-    D255Component,
     AdditionalInformationComponent,
     IndependentDrivingComponent,
     ActivityCodeComponent,
-    LanguagePreferencesComponent,
   ],
   imports: [
     CommonModule,
@@ -44,15 +38,12 @@ import { DirectivesModule } from '../../../directives/directives.module';
     FaultCommentCardComponent,
     RouteNumberComponent,
     CandidateDescriptionComponent,
-    DebriefWitnessedComponent,
     IdentificationComponent,
     ShowMeQuestionComponent,
     WeatherConditionsComponent,
-    D255Component,
     AdditionalInformationComponent,
     IndependentDrivingComponent,
     ActivityCodeComponent,
-    LanguagePreferencesComponent,
   ],
 })
 export class OfficeComponentsModule { }
