b00bb1d35b1094b94218525ec62bb375b774f02a
Tom Billington <t.billington@kainos.com>
2019-08-02 10:10:23 +0100

    Remove persisted tests

--- src/providers/test-persistence/test-persistence.ts
+++ src/providers/test-persistence/test-persistence.ts
@@ -5,6 +5,8 @@ import { getTests } from '../../modules/tests/tests.reducer';
 import { switchMap, take, mapTo } from 'rxjs/operators';
 import { DataStoreProvider } from '../data-store/data-store';
 import { TestsModel } from '../../modules/tests/tests.model';
+import { DateTime } from '../../shared/helpers/date-time';
+import { AppConfigProvider } from '../app-config/app-config';
 
 @Injectable()
 export class TestPersistenceProvider {
@@ -12,6 +14,7 @@ export class TestPersistenceProvider {
   constructor(
     private store$: Store<StoreModel>,
     private dataStoreProvider: DataStoreProvider,
+    private appConfigProvider: AppConfigProvider,
   ) {}
 
   private testKeychainKey = 'TESTS';
@@ -35,11 +38,49 @@ export class TestPersistenceProvider {
         console.error(`Error loading persisted tests: ${err}`);
       }
     }
-    return testsModel;
+    return this.clearCachedTests(testsModel);
   }
 
   async clearPersistedTests(): Promise<void> {
     await this.dataStoreProvider.removeItem(this.testKeychainKey);
   }
 
+  clearCachedTests(tests: TestsModel): TestsModel {
+    if (tests === null) {
+      return null;
+    }
+
+    const testsToDelete = this.getTestsToDelete(tests);
+
+    return {
+      currentTest: {
+        slotId: this.shouldResetCurrentTest(tests.currentTest.slotId , testsToDelete) ? null : tests.currentTest.slotId,
+      },
+      startedTests: {
+        ...this.deleteTestsFromTestObject(tests.startedTests, testsToDelete),
+      },
+      testStatus: {
+        ...this.deleteTestsFromTestObject(tests.testStatus, testsToDelete),
+      },
+    };
+
+  }
+
+  getTestsToDelete(tests: TestsModel): string[] {
+    return  Object.keys(tests.startedTests).filter((key) => {
+      const startDate: DateTime = new DateTime(tests.startedTests[key].journalData.testSlotAttributes.start);
+      return startDate.daysDiff(new DateTime()) > this.appConfigProvider.getAppConfig().daysToCacheJournalData;
+    });
+  }
+
+  deleteTestsFromTestObject(testObject: any , keysToDelete: string[]): any {
+    keysToDelete.forEach((key) => {
+      delete testObject[key];
+    });
+    return testObject;
+  }
+
+  shouldResetCurrentTest(slotId: string, keysToDelete: string[]): boolean {
+    return keysToDelete.includes(slotId);
+  }
 }
