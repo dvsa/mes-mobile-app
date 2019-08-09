a6684f9980db5387c364cca624e944728e098902
Tom Billington <Cavalier07@users.noreply.github.com>
2019-08-09 09:39:29 +0100

    Added new page (#660)

---
+++ src/pages/rekey-reason/__tests__/rekey-reason.spec.ts
@@ -0,0 +1,44 @@
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+import { IonicModule, NavController, Platform, Config } from 'ionic-angular';
+import { NavControllerMock, PlatformMock, ConfigMock } from 'ionic-mocks';
+import { RekeyReasonPage } from '../rekey-reason';
+import { AuthenticationProvider } from '../../../providers/authentication/authentication';
+import { AuthenticationProviderMock } from '../../../providers/authentication/__mocks__/authentication.mock';
+import { Store, StoreModule } from '@ngrx/store';
+import { AppModule } from '../../../app/app.module';
+
+describe('RekeyReasonPage', () => {
+  let fixture: ComponentFixture<RekeyReasonPage>;
+  let component: RekeyReasonPage;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        RekeyReasonPage,
+      ],
+      imports: [
+        IonicModule,
+        StoreModule.forRoot({}),
+        AppModule,
+      ],
+      providers: [
+        { provide: NavController, useFactory: () => NavControllerMock.instance() },
+        { provide: Platform, useFactory: () => PlatformMock.instance() },
+        { provide: AuthenticationProvider, useClass: AuthenticationProviderMock },
+        { provide: Config, useFactory: () => ConfigMock.instance() },
+        Store,
+      ],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(RekeyReasonPage);
+        component = fixture.componentInstance;
+      });
+  }));
+
+  describe('Class', () => {
+    it('should create', () => {
+      expect(component).toBeDefined();
+    });
+  });
+});
---
+++ src/pages/rekey-reason/rekey-reason.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { RekeyReasonPage } from './rekey-reason';
+
+@NgModule({
+  declarations: [
+    RekeyReasonPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(RekeyReasonPage),
+  ],
+})
+export class RekeyReasonPageModule {}
