8dded35ea443df9ecb67a52959fbf8779f5766ea
Natalie Johnson <Natalie.Johnson@bjss.com>
2019-08-21 17:49:34 +0100

    Move analytics tests back

--- src/pages/rekey-reason/__tests__/rekey-reason.analytics.effects.spec.ts
+++ src/pages/rekey-reason/__tests__/rekey-reason.analytics.effects.spec.ts
@@ -1,20 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { AnalyticsProvider } from '../../../../providers/analytics/analytics';
-import { AnalyticsProviderMock } from '../../../../providers/analytics/__mocks__/analytics.mock';
+import { AnalyticsProvider } from '../../../providers/analytics/analytics';
+import { AnalyticsProviderMock } from '../../../providers/analytics/__mocks__/analytics.mock';
 import {
   AnalyticsScreenNames,
-} from '../../../../providers/analytics/analytics.model';
-import { AnalyticRecorded } from '../../../../providers/analytics/analytics.actions';
-import * as rekeyReasonActions from '../rekey-reason.actions';
-import { RekeyReasonAnalyticsEffects } from '../../../../pages/rekey-reason/rekey-reason.analytics.effects';
+} from '../../../providers/analytics/analytics.model';
+import { AnalyticRecorded } from '../../../providers/analytics/analytics.actions';
+import * as rekeyReasonActions from '../../../modules/tests/rekey-reason/rekey-reason.actions';
+import { RekeyReasonAnalyticsEffects } from '../rekey-reason.analytics.effects';
 import { Store, StoreModule } from '@ngrx/store';
-import { testsReducer } from '../../../../modules/tests/tests.reducer';
-import { StoreModel } from '../../../../shared/models/store.model';
-import * as testsActions from '../../../../modules/tests/tests.actions';
-import * as candidateActions from '../../../../modules/tests/candidate/candidate.actions';
-import { Candidate } from '../../../../shared/models/DJournal';
+import { testsReducer } from '../../../modules/tests/tests.reducer';
+import { StoreModel } from '../../../shared/models/store.model';
+import * as testsActions from '../../../modules/tests/tests.actions';
+import * as candidateActions from '../../../modules/tests/candidate/candidate.actions';
+import { Candidate } from '../../../shared/models/DJournal';
 
 describe('Rekey Reason Analytics Effects', () => {
 
