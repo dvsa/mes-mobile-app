d6a27fe44ed7b12fe00fee69ab0c093adec7615e
natalie johnson <natalie.johnson@bjss.com>
2019-09-04 11:42:01 +0100

    Remove unused files

--- src/app/__mocks__/app.component.mock.ts
+++ src/app/__mocks__/app.component.mock.ts
@@ -1,11 +1,7 @@
-import { NavControllerMock } from '../../providers/navigation-state/__mocks__/nav-controller.mock';
-
 export class MockAppComponent {
 
   getTextZoomClass() {
     return 'text-zoom-regular';
   }
-
-  getRootNavs = () => [new NavControllerMock()];
 
 }
--- src/providers/navigation-state/__mocks__/nav-controller.mock.ts
+++
@@ -1,7 +0,0 @@
-export class NavControllerMock {
-
-  constructor() {}
-
-  public getActive = (): any => {};
-
-}
