a99794bdcdc036fe942f1c444c6d9d2526c89fb6
natalie johnson <natalie.johnson@bjss.com>
2019-09-04 11:53:22 +0100

    Add comment about nav helper

--- src/providers/navigation-state/navigation-helper.ts
+++ src/providers/navigation-state/navigation-helper.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
 import { App, NavController } from 'ionic-angular';
 
+/**
+ * Ionic will not allow the navigation controller to be injected at the root
+ * level, so this alternative approach gets the nav from the app.
+ *
+ * This helper allows the navigation controller to be mocked during testing.
+ */
+
 @Injectable()
 export class NavigationHelper {
 
