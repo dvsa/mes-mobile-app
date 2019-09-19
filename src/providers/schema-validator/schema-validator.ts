d223b4daf077fa4091feed22a2646980f7edcfb6
Ammar <ammar.haider@bjss.com>
2019-09-19 17:57:14 +0100

    uses browser versions of joi and enjoi

--- src/providers/schema-validator/schema-validator.ts
+++ src/providers/schema-validator/schema-validator.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@angular/core';
 
 import * as remoteConfigSchema from '@dvsa/mes-config-schema/remote-config.json';
-// import joi, { ValidationResult } from '@hapi/joi';
-import * as enjoi from 'enjoi';
+import * as joi from 'joi-browser';
+import { ValidationResult, Schema } from '@hapi/joi';
+import * as enjoi from 'enjoi-browser';
 
 @Injectable()
 export class SchemaValidatorProvider {
 
-  validateRemoteConfig = (data: any): boolean => { // ValidationResult<any> => {
-    const joiSchema = enjoi.schema(remoteConfigSchema);
-
-    joiSchema + 1;
-    // return joi.validate(data, joiSchema);
-    return ;
+  validateRemoteConfig = (data: any): ValidationResult<any> => {
+    const joiSchema: Schema = enjoi.schema(remoteConfigSchema);
+    return joi.validate(data, joiSchema);
   }
 
 }
