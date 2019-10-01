dc7e2895751d64b0a7bd2f18428ed28a00230a91
Tom Billington <t.billington@kainos.com>
2019-10-01 13:43:37 +0100

    Folder Structure v3

--- src/pages/waiting-room/cat-b/waiting-room.cat-b.page.module.ts
+++ src/pages/waiting-room/cat-b/waiting-room.cat-b.page.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { ComponentsModule } from '../../../../components/common/common-components.module';
+import { ComponentsModule } from '../../../components/common/common-components.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from 'ng2-translate';
 import { EffectsModule } from '@ngrx/effects';
-import { WaitingRoomAnalyticsEffects } from '../../waiting-room.analytics.effects';
-import { WaitingRoomPageB } from './waiting-room-b';
+import { WaitingRoomAnalyticsEffects } from '../waiting-room.analytics.effects';
+import { WaitingRoomCatBPage } from './waiting-room.cat-b.page';
 
 @NgModule({
   declarations: [
-    WaitingRoomPageB,
+    WaitingRoomCatBPage,
   ],
   imports: [
-    IonicPageModule.forChild(WaitingRoomPageB),
+    IonicPageModule.forChild(WaitingRoomCatBPage),
     EffectsModule.forFeature([
       WaitingRoomAnalyticsEffects,
     ]),
@@ -21,4 +21,4 @@ import { WaitingRoomPageB } from './waiting-room-b';
     TranslateModule,
   ],
 })
-export class WaitingRoomPageBModule { }
+export class WaitingRoomCatBPageModule { }
