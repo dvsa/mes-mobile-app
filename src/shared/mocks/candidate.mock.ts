30bf31d49acd97136a97c878d80e8ac289520d77
Tom Billington <t.billington@kainos.com>
2019-10-01 23:12:33 +0100

    Massive Refactor

---
+++ src/shared/mocks/candidate.mock.ts
@@ -0,0 +1,9 @@
+import { Candidate } from '@dvsa/mes-test-schema/categories/B';
+
+export const candidateMock: Candidate = {
+  driverNumber: '123',
+  candidateName: {
+    firstName: 'Joe',
+    lastName: 'Bloggs',
+  },
+};
