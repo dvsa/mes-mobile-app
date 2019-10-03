5edcea9c0a7621fe44e04e089d3879514d717350
Tom Billington <t.billington@kainos.com>
2019-10-03 11:47:32 +0100

    Added CAT-BE-Page

--- src/pages/waiting-room/waiting-room.module.ts
+++ src/pages/waiting-room/waiting-room.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { WaitingRoomCatBPageModule } from './cat-b/waiting-room.cat-b.page.module';
+import { WaitingRoomCatBEPageModule } from './cat-be/waiting-room.cat-be.page.module';
 
 @NgModule({
   imports: [
     WaitingRoomCatBPageModule,
+    WaitingRoomCatBEPageModule,
   ],
 })
 export class WaitingRoomPageModule { }
