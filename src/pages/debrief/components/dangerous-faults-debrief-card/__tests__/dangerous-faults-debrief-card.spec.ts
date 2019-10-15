c8aef40c734ec617d4b54342c51c5c349014b147
Ahmed <a.mobarak92@gmail.com>
2019-10-15 10:00:43 +0100

    mes-3660-refactored debrief page into components, and unit tests (#845)

---
+++ src/pages/debrief/components/dangerous-faults-debrief-card/__tests__/dangerous-faults-debrief-card.spec.ts
@@ -0,0 +1,76 @@
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+import { IonicModule } from 'ionic-angular';
+
+import { StoreModule } from '@ngrx/store';
+import { TranslateModule, TranslateService } from 'ng2-translate';
+import { DangerousFaultsDebriefCardComponent } from '../dangerous-faults-debrief-card';
+import { AppModule } from '../../../../../app/app.module';
+import { By } from '@angular/platform-browser';
+import { ComponentsModule } from '../../../../../components/common/common-components.module';
+import { Language } from '../../../../../modules/tests/communication-preferences/communication-preferences.model';
+import { configureI18N } from '../../../../../shared/helpers/translation.helpers';
+import * as welshTranslations from '../../../../../assets/i18n/cy.json';
+import { Competencies } from '../../../../../modules/tests/test-data/test-data.constants';
+
+describe('DangerousFaultsDebriefCardComponent', () => {
+  let fixture: ComponentFixture<DangerousFaultsDebriefCardComponent>;
+  let component: DangerousFaultsDebriefCardComponent;
+  let translate: TranslateService;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [DangerousFaultsDebriefCardComponent],
+      imports: [
+        IonicModule,
+        AppModule,
+        ComponentsModule,
+        StoreModule.forRoot({
+        }),
+        TranslateModule,
+      ],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(DangerousFaultsDebriefCardComponent);
+        component = fixture.componentInstance;
+        translate = TestBed.get(TranslateService);
+        translate.setDefaultLang('en');
+      });
+  }));
+
+  describe('DOM', () => {
+    it('correct dangerous faults showing', () => {
+      const dangerousFaults = [Competencies.ancillaryControls, Competencies.clearance];
+      component.dangerousFaults = dangerousFaults;
+      fixture.detectChanges();
+      const dangerousLabels = fixture.debugElement.queryAll(By.css('#dangerous-fault .counter-label'));
+      const dangerousCounts = fixture.debugElement.query(By.css('h1.fault-heading')).nativeElement;
+      expect(dangerousLabels[0].nativeElement.innerHTML).toBe('Ancillary Controls');
+      expect(dangerousLabels[1].nativeElement.innerHTML).toBe('Clearance');
+      expect(dangerousCounts.innerHTML).toBe(dangerousFaults.length.toString());
+    });
+
+    it('correct dangerous faults showing in welsh', (done) => {
+      configureI18N(Language.CYMRAEG, translate);
+      const dangerousFaults = [Competencies.ancillaryControls];
+      component.dangerousFaults = dangerousFaults;
+      translate.onLangChange.subscribe(() => {
+        fixture.detectChanges();
+        const dangerousLabels = fixture.debugElement.queryAll(By.css('#dangerous-fault .counter-label'));
+        const dangerousCounts = fixture.debugElement.query(By.css('h1.fault-heading')).nativeElement;
+        expect(dangerousLabels[0].nativeElement.innerHTML)
+          .toBe((<any>welshTranslations).debrief.competencies.ancillaryControls);
+        expect(dangerousCounts.innerHTML).toBe(dangerousFaults.length.toString());
+        done();
+      });
+
+    });
+
+    it('no dangerous faults showing', () => {
+      const dangerousFaults = [];
+      component.dangerousFaults = dangerousFaults;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('#dangerous-fault'))).toBeNull();
+    });
+  });
+});
---
+++ src/pages/debrief/components/driving-faults-debrief-card/__tests__/driving-faults-debrief-card.spec.ts
@@ -0,0 +1,103 @@
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+import { IonicModule } from 'ionic-angular';
+
+import { StoreModule } from '@ngrx/store';
+import { TranslateModule, TranslateService } from 'ng2-translate';
+import { AppModule } from '../../../../../app/app.module';
+import { By } from '@angular/platform-browser';
+import { ComponentsModule } from '../../../../../components/common/common-components.module';
+import * as welshTranslations from '../../../../../assets/i18n/cy.json';
+import { DrivingFaultsDebriefCardComponent } from '../driving-faults-debrief-card';
+import { MultiFaultAssignableCompetency } from '../../../../../shared/models/fault-marking.model';
+import { configureI18N } from '../../../../../shared/helpers/translation.helpers';
+import { Language } from '../../../../../modules/tests/communication-preferences/communication-preferences.model';
+
+describe('DrivingFaultsDebriefCardComponent', () => {
+  let fixture: ComponentFixture<DrivingFaultsDebriefCardComponent>;
+  let component: DrivingFaultsDebriefCardComponent;
+  let translate: TranslateService;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [DrivingFaultsDebriefCardComponent],
+      imports: [
+        IonicModule,
+        AppModule,
+        ComponentsModule,
+        StoreModule.forRoot({
+        }),
+        TranslateModule,
+      ],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(DrivingFaultsDebriefCardComponent);
+        component = fixture.componentInstance;
+        translate = TestBed.get(TranslateService);
+        translate.setDefaultLang('en');
+      });
+  }));
+
+  describe('DOM', () => {
+    it('correct driving faults showing', () => {
+      const drivingFaults: MultiFaultAssignableCompetency[] = [
+        {
+          competencyIdentifier: 'useOfSpeed',
+          competencyDisplayName: 'Use of speed',
+          faultCount: 1,
+        },
+        {
+          competencyIdentifier: 'signalsTimed',
+          competencyDisplayName: 'Signals - Timed',
+          faultCount: 1,
+        },
+      ];
+
+      component.drivingFaults = drivingFaults;
+      component.drivingFaultCount = 2;
+      fixture.detectChanges();
+      const drivingFaultLabels = fixture.debugElement.queryAll(By.css('#driving-fault .counter-label'));
+      const drivingFaultCount = fixture.debugElement.query(By.css('h1.fault-heading')).nativeElement;
+      expect(drivingFaultLabels[0].nativeElement.innerHTML).toBe('Use of speed');
+      expect(drivingFaultLabels[1].nativeElement.innerHTML).toBe('Signals - Timed');
+      expect(drivingFaultCount.innerHTML).toBe(drivingFaults.length.toString());
+    });
+
+    it('correct driving faults showing in welsh', (done) => {
+      configureI18N(Language.CYMRAEG, translate);
+      const drivingFaults: MultiFaultAssignableCompetency[] = [
+        {
+          competencyIdentifier: 'useOfSpeed',
+          competencyDisplayName: 'Use of speed',
+          faultCount: 1,
+        },
+        {
+          competencyIdentifier: 'signalsTimed',
+          competencyDisplayName: 'Signals - Timed',
+          faultCount: 1,
+        },
+      ];
+      component.drivingFaults = drivingFaults;
+      component.drivingFaultCount = 2;
+      translate.onLangChange.subscribe(() => {
+        fixture.detectChanges();
+        const drivingFaultsLabels = fixture.debugElement.queryAll(By.css('#driving-fault .counter-label'));
+        const drivingFaultCount = fixture.debugElement.query(By.css('h1.fault-heading')).nativeElement;
+        expect(drivingFaultsLabels[0].nativeElement.innerHTML)
+          .toBe((<any>welshTranslations).debrief.competencies.useOfSpeed);
+        expect(drivingFaultsLabels[1].nativeElement.innerHTML)
+          .toBe((<any>welshTranslations).debrief.competencies.signalsTimed);
+        expect(drivingFaultCount.innerHTML).toBe(drivingFaults.length.toString());
+        done();
+      });
+    });
+
+    it('no driving faults showing', () => {
+      const drivingFaults = [];
+      component.drivingFaults = drivingFaults;
+      component.drivingFaultCount = 0;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('#driving-fault'))).toBeNull();
+    });
+  });
+});
