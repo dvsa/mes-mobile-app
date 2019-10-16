d88500ef04bb043b9df2b26f0b9ad67a0f1a40ec
Ahmed <a.mobarak92@gmail.com>
2019-10-16 09:56:56 +0100

    mes-3661-refactoredDebriefPageForCatBEPreparation (#846)
    * mes-3661-refactoredDebriefPageForCatBEPreparation
    
    * mes-3661-renamedModuleName

--- src/pages/debrief/debrief.module.ts
+++ src/pages/debrief/debrief.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { DebriefCatBPageModule } from './cat-b/debrief.cat-b.page.module';
+import { DebriefCatBePageModule } from './cat-be/debrief.cat-be.page.module';
 
 @NgModule({
   imports: [
     DebriefCatBPageModule,
+    DebriefCatBePageModule,
   ],
 })
 export class DebriefPageModule { }
