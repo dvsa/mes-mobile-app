9f2563f084880ebd9dce9e2d7e75079cfbbd3095
Tom Billington <Cavalier07@users.noreply.github.com>
2019-10-22 11:10:42 +0100

    MES-3653 - Moved files to a Cat B page (#859)
    * Moved files to a Cat B page
    
    * Extracted Overlay Callback Interface

---
+++ src/pages/test-report/cat-b/test-report.cat-b.page.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { TestReportAnalyticsEffects } from '../test-report.analytics.effects';
+import { testReportReducer } from '../test-report.reducer';
+import { ComponentsModule } from '../../../components/common/common-components.module';
+import { TestReportComponentsModule } from '../components/test-report-components.module';
+import { TestReportValidatorProvider } from '../../../providers/test-report-validator/test-report-validator';
+import { TestReportEffects } from '../test-report.effects';
+import { TestResultProvider } from '../../../providers/test-result/test-result';
+import { TestReportCatBPage } from './test-report.cat-b.page';
+
+@NgModule({
+  declarations: [
+    TestReportCatBPage,
+  ],
+  imports: [
+    TestReportComponentsModule,
+    IonicPageModule.forChild(TestReportCatBPage),
+    StoreModule.forFeature('testReport', testReportReducer),
+    EffectsModule.forFeature([
+      TestReportAnalyticsEffects,
+      TestReportEffects,
+    ]),
+    ComponentsModule,
+  ],
+  providers: [
+    TestReportValidatorProvider,
+    TestResultProvider,
+  ],
+})
+export class TestReportCatBPageModule {}
--- src/pages/test-report/test-report.module.ts
+++ src/pages/test-report/test-report.module.ts
@@ -1,34 +1,9 @@
 import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { TestReportPage } from './test-report';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-
-import { TestReportAnalyticsEffects } from './test-report.analytics.effects';
-import { testReportReducer } from './test-report.reducer';
-import { ComponentsModule } from '../../components/common/common-components.module';
-import { TestReportComponentsModule } from './components/test-report-components.module';
-import { TestReportValidatorProvider } from '../../providers/test-report-validator/test-report-validator';
-import { TestReportEffects } from './test-report.effects';
-import { TestResultProvider } from '../../providers/test-result/test-result';
+import { TestReportCatBPageModule } from './cat-b/test-report.cat-b.page.module';
 
 @NgModule({
-  declarations: [
-    TestReportPage,
-  ],
   imports: [
-    TestReportComponentsModule,
-    IonicPageModule.forChild(TestReportPage),
-    StoreModule.forFeature('testReport', testReportReducer),
-    EffectsModule.forFeature([
-      TestReportAnalyticsEffects,
-      TestReportEffects,
-    ]),
-    ComponentsModule,
-  ],
-  providers: [
-    TestReportValidatorProvider,
-    TestResultProvider,
+    TestReportCatBPageModule,
   ],
 })
 export class TestReportPageModule {}
