88013c4559602d3dbdb0039c0ef2fc690e996427
Carl Fagg <carlf@kainos.com>
2019-10-23 12:03:01 +0100

    MES-3773: Added BE Version Of Back To Office Page

--- src/pages/back-to-office/back-to-office.module.ts
+++ src/pages/back-to-office/back-to-office.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BackToOfficeCatBPageModule } from './cat-b/back-to-office.cat-b.page.module';
+import { BackToOfficeCatBEPageModule } from './cat-be/back-to-office.cat-be.page.module';
 
 @NgModule({
   imports: [
     BackToOfficeCatBPageModule,
+    BackToOfficeCatBEPageModule,
   ],
 })
 export class BackToOfficePageModule {}
