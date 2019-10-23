f4590b883681f49131df6a1ee2e0ba2e782e1b87
Carl Fagg <carlf@kainos.com>
2019-10-23 11:13:32 +0100

    MES-3773: Added Back To Office Page For BE

---
+++ src/pages/back-to-office/cat-be/back-to-office.cat-be.page.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ComponentsModule } from '../../../components/common/common-components.module';
+import { BackToOfficeCatBEPage } from './back-to-office.cat-be.page';
+import { TranslateModule } from 'ng2-translate';
+import { EffectsModule } from '@ngrx/effects';
+import { BackToOfficeAnalyticsEffects } from '../back-to-office.analytics.effects';
+
+@NgModule({
+  declarations: [
+    BackToOfficeCatBEPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(BackToOfficeCatBEPage),
+    EffectsModule.forFeature([
+      BackToOfficeAnalyticsEffects,
+    ]),
+    ComponentsModule,
+    TranslateModule,
+  ],
+})
+export class BackToOfficeCatBEPageModule { }
---
+++ src/pages/back-to-office/cat-be/back-to-office.cat-be.page.ts
@@ -0,0 +1,78 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { PracticeableBasePageComponent } from '../../../shared/classes/practiceable-base-page';
+import { AuthenticationProvider } from '../../../providers/authentication/authentication';
+import { Store, select } from '@ngrx/store';
+import { StoreModel } from '../../../shared/models/store.model';
+import { BackToOfficeViewDidEnter, DeferWriteUp } from '../back-to-office.actions';
+import { DeviceProvider } from '../../../providers/device/device';
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
+import { Insomnia } from '@ionic-native/insomnia';
+import { JOURNAL_PAGE } from '../../page-names.constants';
+import { Observable } from 'rxjs/Observable';
+import { getTests } from '../../../modules/tests/tests.reducer';
+import { getCurrentTest } from '../../../modules/tests/tests.selector';
+import { getRekeyIndicator } from '../../../modules/tests/rekey/rekey.reducer';
+import { isRekey } from '../../../modules/tests/rekey/rekey.selector';
+
+interface BackToOfficePageState {
+  isRekey$: Observable<boolean>;
+}
+
+@IonicPage()
+@Component({
+  selector: 'back-to-office-cat-be-page',
+  templateUrl: 'back-to-office.cat-be.page.html',
+})
+export class BackToOfficeCatBEPage extends PracticeableBasePageComponent {
+  pageState: BackToOfficePageState;
+
+  constructor(
+    store$: Store<StoreModel>,
+    private deviceProvider: DeviceProvider,
+    public navController: NavController,
+    public navParams: NavParams,
+    public platform: Platform,
+    public authenticationProvider: AuthenticationProvider,
+    public screenOrientation: ScreenOrientation,
+    public insomnia: Insomnia,
+  ) {
+    super(platform, navController, authenticationProvider, store$);
+  }
+
+  ngOnInit(): void {
+    super.ngOnInit();
+
+    this.pageState = {
+      isRekey$: this.store$.pipe(
+        select(getTests),
+        select(getCurrentTest),
+        select(getRekeyIndicator),
+        select(isRekey),
+      ),
+    };
+  }
+
+  ionViewDidEnter(): void {
+    if (super.isIos()) {
+      this.screenOrientation.unlock();
+      this.insomnia.allowSleepAgain();
+
+      if (!this.isPracticeMode) {
+        this.deviceProvider.disableSingleAppMode();
+      }
+    }
+
+    this.store$.dispatch(new BackToOfficeViewDidEnter());
+  }
+
+  goToJournal() {
+    if (this.isPracticeMode) {
+      this.exitPracticeMode();
+      return;
+    }
+    this.store$.dispatch(new DeferWriteUp());
+    const journalPage = this.navController.getViews().find(view => view.id === JOURNAL_PAGE);
+    this.navController.popTo(journalPage);
+  }
+}
