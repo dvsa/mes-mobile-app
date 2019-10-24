cb0186d6a117a2f37e5edf3e244fe5ff21119987
Tom Billington <Cavalier07@users.noreply.github.com>
2019-10-24 13:47:36 +0100

    MES-3653 - Test Report - Create Cat BE version of Page (#869)
    * Added Cat B+E Page
    
    * Fixed Broken Test

---
+++ src/pages/test-report/cat-be/components/test-report.cat-be.components.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { ControlledStopComponent } from './controlled-stop/controlled-stop';
+import { ManoeuvresComponent } from './manoeuvres/manoeuvres';
+import { VehicleCheckComponent } from './vehicle-check/vehicle-check';
+import { ManoeuvreCompetencyComponent } from './manoeuvre-competency/manoeuvre-competency';
+import { ManoeuvresPopoverComponent } from './manoeuvres-popover/manoeuvres-popover';
+import { IonicModule } from 'ionic-angular';
+import { CommonModule } from '@angular/common';
+import { ComponentsModule } from '../../../../components/common/common-components.module';
+import { TestReportComponentsModule } from '../../components/test-report-components.module';
+
+@NgModule({
+  declarations: [
+    ControlledStopComponent,
+    ManoeuvresComponent,
+    ManoeuvreCompetencyComponent,
+    ManoeuvresPopoverComponent,
+    VehicleCheckComponent,
+  ],
+  imports: [
+    IonicModule,
+    CommonModule,
+    ComponentsModule,
+    TestReportComponentsModule,
+  ],
+  exports:[
+    ControlledStopComponent,
+    ManoeuvresComponent,
+    ManoeuvreCompetencyComponent,
+    ManoeuvresPopoverComponent,
+    VehicleCheckComponent,
+  ],
+})
+export class TestReportCatBEComponentsModule {}
---
+++ src/pages/test-report/cat-be/test-report.cat-be.page.ts
@@ -0,0 +1,243 @@
+import { Component } from '@angular/core';
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  Platform,
+  ModalController,
+  Modal,
+} from 'ionic-angular';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { merge } from 'rxjs/observable/merge';
+import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs/Subscription';
+import { AuthenticationProvider } from '../../../providers/authentication/authentication';
+import { StoreModel } from '../../../shared/models/store.model';
+import { getUntitledCandidateName } from '../../../modules/tests/journal-data/candidate/candidate.selector';
+import { getCandidate } from '../../../modules/tests/journal-data/candidate/candidate.reducer';
+import {
+  TestReportViewDidEnter,
+  CalculateTestResult,
+  TerminateTestFromTestReport,
+} from '../test-report.actions';
+import {
+  getCurrentTest,
+  getJournalData,
+} from '../../../modules/tests/tests.selector';
+import {
+  Competencies,
+  LegalRequirements,
+  ExaminerActions,
+} from '../../../modules/tests/test-data/test-data.constants';
+import { getTestData } from '../../../modules/tests/test-data/test-data.reducer';
+import { getTests } from '../../../modules/tests/tests.reducer';
+import { getTestReportState } from '../test-report.reducer';
+import {
+  isRemoveFaultMode,
+  isSeriousMode,
+  isDangerousMode,
+  isLegalRequirementsValid,
+  isEtaValid,
+} from '../test-report.selector';
+import { TestReportValidatorProvider } from '../../../providers/test-report-validator/test-report-validator';
+import { CatBLegalRequirements } from '../../../modules/tests/test-data/test-data.models';
+import {
+  getCatBLegalRequirements,
+  hasManoeuvreBeenCompleted,
+} from '../../../modules/tests/test-data/test-data.selector';
+import { ModalEvent } from '../test-report.constants';
+import { CAT_BE } from '../../page-names.constants';
+import { OverlayCallback } from '../test-report.model';
+import { BasePageComponent } from '../../../shared/classes/base-page';
+
+interface TestReportPageState {
+  candidateUntitledName$: Observable<string>;
+  isRemoveFaultMode$: Observable<boolean>;
+  isSeriousMode$: Observable<boolean>;
+  isDangerousMode$: Observable<boolean>;
+  manoeuvres$: Observable<boolean>;
+  isLegalRequirementsValid$: Observable<boolean>;
+  isEtaValid$: Observable<boolean>;
+  catBLegalRequirements$: Observable<CatBLegalRequirements>;
+}
+
+@IonicPage()
+@Component({
+  selector: 'test-report-cat-be-page',
+  templateUrl: 'test-report.cat-be.page.html',
+})
+export class TestReportCatBEPage extends BasePageComponent {
+  pageState: TestReportPageState;
+  subscription: Subscription;
+  competencies = Competencies;
+  legalRequirements = LegalRequirements;
+  eta = ExaminerActions;
+  displayOverlay: boolean;
+
+  isRemoveFaultMode: boolean = false;
+  isSeriousMode: boolean = false;
+  isDangerousMode: boolean = false;
+  manoeuvresCompleted: boolean = false;
+  isLegalRequirementsValid: boolean = false;
+  isEtaValid: boolean = true;
+
+  modal: Modal;
+  catBLegalRequirements: CatBLegalRequirements;
+
+  constructor(
+    public store$: Store<StoreModel>,
+    public navController: NavController,
+    public navParams: NavParams,
+    public platform: Platform,
+    public authenticationProvider: AuthenticationProvider,
+    private modalController: ModalController,
+    public testReportValidatorProvider: TestReportValidatorProvider,
+  ) {
+    super(platform, navController, authenticationProvider);
+    this.displayOverlay = false;
+  }
+
+  getCallback(): OverlayCallback {
+    return {
+      callbackMethod: () => {
+        this.toggleReportOverlay();
+      },
+    };
+  }
+  ngOnInit(): void {
+    this.pageState = {
+      candidateUntitledName$: this.store$.pipe(
+        select(getTests),
+        select(getCurrentTest),
+        select(getJournalData),
+        select(getCandidate),
+        select(getUntitledCandidateName),
+      ),
+      isRemoveFaultMode$: this.store$.pipe(
+        select(getTestReportState),
+        select(isRemoveFaultMode),
+      ),
+      isSeriousMode$: this.store$.pipe(
+        select(getTestReportState),
+        select(isSeriousMode),
+      ),
+      isDangerousMode$: this.store$.pipe(
+        select(getTestReportState),
+        select(isDangerousMode),
+      ),
+      manoeuvres$: this.store$.pipe(
+        select(getTests),
+        select(getCurrentTest),
+        select(getTestData),
+        select(hasManoeuvreBeenCompleted),
+      ),
+      isLegalRequirementsValid$: this.store$.pipe(
+        select(getTestReportState),
+        select(isLegalRequirementsValid),
+      ),
+      isEtaValid$: this.store$.pipe(
+        select(getTestReportState),
+        select(isEtaValid),
+      ),
+      catBLegalRequirements$: this.store$.pipe(
+        select(getTests),
+        select(getCurrentTest),
+        select(getTestData),
+        select(getCatBLegalRequirements),
+      ),
+    };
+    this.setupSubscription();
+
+  }
+  ionViewDidEnter(): void {
+
+    // it is possible that we come back to the page from the terminate screen
+    // so need to re-establish the subscription if it doesn't exists or is closed
+    if (!this.subscription || this.subscription.closed) {
+      this.setupSubscription();
+    }
+    this.store$.dispatch(new TestReportViewDidEnter());
+  }
+
+  toggleReportOverlay(): void {
+    this.displayOverlay = !this.displayOverlay;
+  }
+
+  ionViewDidLeave(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+  setupSubscription() {
+    const {
+      candidateUntitledName$,
+      isRemoveFaultMode$,
+      isSeriousMode$,
+      isDangerousMode$,
+      manoeuvres$,
+      isLegalRequirementsValid$,
+      isEtaValid$,
+      catBLegalRequirements$,
+    } = this.pageState;
+
+    this.subscription = merge(
+      candidateUntitledName$,
+      isRemoveFaultMode$.pipe(map(result => (this.isRemoveFaultMode = result))),
+      isSeriousMode$.pipe(map(result => (this.isSeriousMode = result))),
+      isDangerousMode$.pipe(map(result => (this.isDangerousMode = result))),
+      manoeuvres$.pipe(map(result => (this.manoeuvresCompleted = result))),
+      isLegalRequirementsValid$.pipe(
+        map(result => (this.isLegalRequirementsValid = result)),
+      ),
+      isEtaValid$.pipe(map(result => (this.isEtaValid = result))),
+      catBLegalRequirements$.pipe(
+        map(result => (this.catBLegalRequirements = result)),
+      ),
+    ).subscribe();
+  }
+
+  onEndTestClick = (): void => {
+    const options = { cssClass: 'mes-modal-alert text-zoom-regular' };
+    if (!this.isLegalRequirementsValid) {
+      this.modal = this.modalController.create(
+        'LegalRequirementsModal',
+        {
+          legalRequirements: this.catBLegalRequirements,
+        },
+        options,
+      );
+    } else if (!this.isEtaValid) {
+      this.modal = this.modalController.create('EtaInvalidModal', {}, options);
+    } else {
+      this.modal = this.modalController.create('EndTestModal', {}, options);
+    }
+    this.modal.onDidDismiss(this.onModalDismiss);
+    this.modal.present();
+  }
+
+  onModalDismiss = (event: ModalEvent): void => {
+    switch (event) {
+      case ModalEvent.CONTINUE:
+        this.store$.dispatch(new CalculateTestResult());
+        this.navController.push(CAT_BE.DEBRIEF_PAGE);
+        break;
+      case ModalEvent.TERMINATE:
+        this.store$.dispatch(new TerminateTestFromTestReport());
+        this.navController.push(CAT_BE.DEBRIEF_PAGE);
+        break;
+    }
+  }
+
+  onCancel = (): void => {
+    this.modal.dismiss();
+  }
+
+  onContinue = (): void => {
+    this.modal.dismiss().then(() => this.navController.push(CAT_BE.DEBRIEF_PAGE));
+  }
+
+  onTerminate = (): void => {
+    this.modal.dismiss().then(() => this.navController.push(CAT_BE.DEBRIEF_PAGE));
+  }
+}
