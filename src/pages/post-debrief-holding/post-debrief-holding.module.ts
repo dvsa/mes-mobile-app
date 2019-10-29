9a400caa39f3337455b773ad12c3d6e57be129d1
Jennifer <jenniferm@kainos.com>
2019-10-29 16:35:56 +0000

    lint fixes

--- src/pages/post-debrief-holding/post-debrief-holding.module.ts
+++ src/pages/post-debrief-holding/post-debrief-holding.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { PostDebriefHoldingCatBPageModule } from './cat-b/post-debrief-holding.cat-b.page.module';
 import { PostDebriefHoldingCatBEPageModule } from './cat-be/post-debrief-holding.cat-be.page.module';
 
-
 @NgModule({
   imports: [
     PostDebriefHoldingCatBPageModule,
