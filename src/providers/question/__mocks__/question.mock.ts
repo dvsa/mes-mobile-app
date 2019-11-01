29b215bf1e9f15553790bc456dc51d83acf55e12
Tom Billington <t.billington@kainos.com>
2019-11-01 13:03:06 +0000

    Updated mock

--- src/providers/question/__mocks__/question.mock.ts
+++ src/providers/question/__mocks__/question.mock.ts
@@ -4,8 +4,15 @@ import { TestCategory } from '../../../shared/models/test-category';
 export class QuestionProviderMock {
   getTellMeQuestions(testCategory: TestCategory): VehicleChecksQuestion[] {
     return [
-      { code: 'T1', description: 'What time is it?',  shortName: 'Time' },
+      { code: 'T1', description: 'What time is it?',  shortName: 'What' },
       { code: 'T2', description: 'Where are we?', shortName: 'Where' },
+    ];
+  }
+
+  getShowMeQuestions(testCategory: TestCategory): VehicleChecksQuestion[] {
+    return [
+      { code: 'S1', description: 'Who are we?',  shortName: 'Who' },
+      { code: 'S2', description: 'Why are we here?', shortName: 'Why' },
     ];
   }
 }
