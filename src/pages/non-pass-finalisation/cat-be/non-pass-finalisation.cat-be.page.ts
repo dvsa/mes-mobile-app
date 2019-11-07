24219353a4a59f91f08571f41a8c9be56414d168
Jennifer <jenniferm@kainos.com>
2019-11-07 10:39:32 +0000

    tests created for cat-b and cat-be debrief

--- src/pages/non-pass-finalisation/cat-be/non-pass-finalisation.cat-be.page.ts
+++ src/pages/non-pass-finalisation/cat-be/non-pass-finalisation.cat-be.page.ts
@@ -49,7 +49,7 @@ import {
 import { SetTestStatusWriteUp } from '../../../modules/tests/test-status/test-status.actions';
 import { SetActivityCode } from '../../../modules/tests/activity-code/activity-code.actions';
 import { BasePageComponent } from '../../../shared/classes/base-page';
-import { getGearboxCategory, isAutomatic, isManual } from 
+import { getGearboxCategory, isAutomatic, isManual } from
 '../../../modules/tests/vehicle-details/vehicle-details.selector';
 import { getVehicleDetails } from '../../../modules/tests/vehicle-details/vehicle-details.reducer';
 import { GearboxCategory } from '@dvsa/mes-test-schema/categories/Common';
