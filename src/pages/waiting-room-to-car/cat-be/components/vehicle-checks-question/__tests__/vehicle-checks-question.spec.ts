ba57d8b893edcc591e97bdac0149db1c5b6ff813
Tom Billington <t.billington@kainos.com>
2019-11-07 10:06:41 +0000

    Unit Tests

--- src/pages/waiting-room-to-car/cat-be/components/vehicle-checks-question/__tests__/vehicle-checks-question.spec.ts
+++ src/pages/waiting-room-to-car/cat-be/components/vehicle-checks-question/__tests__/vehicle-checks-question.spec.ts
@@ -5,6 +5,7 @@ import { VehicleChecksQuestionComponent } from '../vehicle-checks-question';
 import { AppModule } from '../../../../../../app/app.module';
 import { VehicleChecksQuestion } from '../../../../../../providers/question/vehicle-checks-question.model';
 import { EventEmitter } from '@angular/core';
+import { QuestionResult } from '@dvsa/mes-test-schema/categories/BE/partial';
 
 const vehicleChecksQuestion: VehicleChecksQuestion = {
   code: 'S04',
@@ -59,6 +60,70 @@ describe('VehicleChecksQuestionComponent', () => {
           code: 'S04',
           description: 'Parking brake',
         });
+      });
+    });
+    describe('vehicleChecksPassSelected', () => {
+      it('should emit the correct event', () => {
+        component.vehicleChecksQuestionOutcomeChange = new EventEmitter();
+        spyOn(component.vehicleChecksQuestionOutcomeChange, 'emit');
+
+        component.vehicleChecksPassSelected();
+
+        expect(component.vehicleChecksQuestionOutcomeChange.emit).toHaveBeenCalledWith({
+          outcome: 'P',
+        });
+      });
+    });
+    describe('vehicleChecksDrivingFaultSelected', () => {
+      it('should emit the correct event', () => {
+        component.vehicleChecksQuestionOutcomeChange = new EventEmitter();
+        spyOn(component.vehicleChecksQuestionOutcomeChange, 'emit');
+
+        component.vehicleChecksDrivingFaultSelected();
+
+        expect(component.vehicleChecksQuestionOutcomeChange.emit).toHaveBeenCalledWith({
+          outcome: 'DF',
+        });
+      });
+    });
+    describe('findQuestion', () => {
+      it('should return the question if it is found', () => {
+        component.questions = [vehicleChecksQuestion];
+        component.questionResult = { code: 'S04' };
+        expect(component.findQuestion()).toEqual(vehicleChecksQuestion);
+      });
+      it('should return undefined if the question is not found', () => {
+        component.questions = [vehicleChecksQuestion];
+        component.questionResult = { code: 'B04' };
+        expect(component.findQuestion()).toEqual(undefined);
+      });
+    });
+    describe('shouldShowOutcomeFields' , () => {
+      it('should return true if all the required data is present', () => {
+        component.questionResult = {
+          code: 'S04',
+          description: 'Test',
+        } as QuestionResult;
+
+        expect(component.shouldShowOutcomeFields()).toEqual(true);
+      });
+      it('should return false if question result is not defined', () => {
+        expect(component.shouldShowOutcomeFields()).toEqual(false);
+      });
+      it('should return false if all the question code is missing from the result object', () => {
+        component.questionResult = {
+          description: 'Test',
+        } as QuestionResult;
+
+        expect(component.shouldShowOutcomeFields()).toEqual(false);
+      });
+      it('should return false if all the question description is missing from the result object', () => {
+        component.questionResult = {
+          code: 'S04',
+          description: 'Test',
+        } as QuestionResult;
+
+        expect(component.shouldShowOutcomeFields()).toEqual(false);
       });
     });
   });
