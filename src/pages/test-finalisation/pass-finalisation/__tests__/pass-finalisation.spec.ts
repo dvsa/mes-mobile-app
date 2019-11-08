e9f588dbf10b50d61d543eacc912d83c29cd5910
rossfauldsbjss <33695102+rossfauldsbjss@users.noreply.github.com>
2019-11-08 12:22:03 +0000

    Feature mes 3959 pass certificate validation improvements (#909)
    * added mod 37 validation on pass certificate
    
    * remove fdescribe so all tests run

--- src/pages/test-finalisation/pass-finalisation/__tests__/pass-finalisation.spec.ts
+++ src/pages/test-finalisation/pass-finalisation/__tests__/pass-finalisation.spec.ts
@@ -50,7 +50,8 @@ describe('PassFinalisationPage', () => {
     it('should dispatch the PersistTests action', () => {
       const form = component.form;
       form.get('provisionalLicenseProvidedCtrl').setValue(true);
-      form.get('passCertificateNumberCtrl').setValue('A123456*');
+      // note: needs to be a valid pass certificate
+      form.get('passCertificateNumberCtrl').setValue('C867544H');
       form.get('transmissionCtrl').setValue('Manual');
       component.onSubmit();
       expect(store$.dispatch).toHaveBeenCalledWith(new PersistTests());
@@ -80,4 +81,110 @@ describe('PassFinalisationPage', () => {
       expect(formCtrl.hasError('maxlength')).toBe(false);
     });
   });
+
+  describe('calculateMod37CheckDigit', () => {
+    it('should return invalid if second character is not a digit', () => {
+      const checkDigit = component.calculateMod37CheckDigit('CX23458');
+      expect(checkDigit).toBe('invalid');
+    });
+  });
+  describe('calculateMod37CheckDigit', () => {
+    it('should return invalid if third character is not a digit', () => {
+      const checkDigit = component.calculateMod37CheckDigit('C1X3458');
+      expect(checkDigit).toBe('invalid');
+    });
+  });
+  describe('calculateMod37CheckDigit', () => {
+    it('should return invalid if fourth character is not a digit', () => {
+      const checkDigit = component.calculateMod37CheckDigit('C12X458');
+      expect(checkDigit).toBe('invalid');
+    });
+  });
+  describe('calculateMod37CheckDigit', () => {
+    it('should return invalid if fifth character is not a digit', () => {
+      const checkDigit = component.calculateMod37CheckDigit('C123X58');
+      expect(checkDigit).toBe('invalid');
+    });
+  });
+  describe('calculateMod37CheckDigit', () => {
+    it('should return invalid if sixth character is not a digit', () => {
+      const checkDigit = component.calculateMod37CheckDigit('C1234X8');
+      expect(checkDigit).toBe('invalid');
+    });
+  });
+  describe('calculateMod37CheckDigit', () => {
+    it('should return invalid if seventh character is not a digit', () => {
+      const checkDigit = component.calculateMod37CheckDigit('C12345X');
+      expect(checkDigit).toBe('invalid');
+    });
+  });
+  describe('calculateMod37CheckDigit', () => {
+    it('should return a check digit of N if passed C758920', () => {
+      const checkDigit = component.calculateMod37CheckDigit('C758920');
+      expect(checkDigit).toBe('N');
+    });
+  });
+  describe('calculateMod37CheckDigit', () => {
+    it('should return a check digit of W if passed C839255', () => {
+      const checkDigit = component.calculateMod37CheckDigit('C839255');
+      expect(checkDigit).toBe('W');
+    });
+  });
+
+  describe('isLetter', () => {
+    it('should return true if passed a alpha character', () => {
+      const result = component.isLetter('C');
+      expect(result).toBeTruthy();
+    });
+  });
+  describe('isLetter', () => {
+    it('should return false if passed a numeric character', () => {
+      const result = component.isLetter('9');
+      expect(result).toBeFalsy();
+    });
+  });
+  describe('isLetter', () => {
+    it('should return false if passed a punctuation character', () => {
+      const result = component.isLetter('@');
+      expect(result).toBeFalsy();
+    });
+  });
+
+  describe('isPassCertificateValid', () => {
+    it('should return false if passed a short certificate', () => {
+      const result = component.isPassCertificateValid('C123');
+      expect(result).toBeFalsy();
+    });
+  });
+  describe('isPassCertificateValid', () => {
+    it('should return false if passed a certificate starting with a non alphabet character', () => {
+      const result = component.isPassCertificateValid('1123456X');
+      expect(result).toBeFalsy();
+    });
+  });
+  describe('isPassCertificateValid', () => {
+    it('should return false if passed a certificate with a letter in place of a number', () => {
+      const result = component.isPassCertificateValid('C12X456X');
+      expect(result).toBeFalsy();
+    });
+  });
+  describe('isPassCertificateValid', () => {
+    it('should return false if passed a certificate with an incorrect check digit', () => {
+      const result = component.isPassCertificateValid('C839255T');
+      expect(result).toBeFalsy();
+    });
+  });
+  describe('isPassCertificateValid', () => {
+    it('should return true if passed a valid certificate', () => {
+      const result = component.isPassCertificateValid('C839255W');
+      expect(result).toBeTruthy();
+    });
+  });
+  describe('isPassCertificateValid', () => {
+    it('should return true if passed a valid certificate with lowercase letters', () => {
+      const result = component.isPassCertificateValid('c839255w');
+      expect(result).toBeTruthy();
+    });
+  });
+
 });
