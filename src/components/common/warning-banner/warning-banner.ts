d6b5dbf1095878188bbf1d74d3b9d0ea38fc4e7a
Jennifer <jenniferm@kainos.com>
2019-11-12 16:18:17 +0000

    code78 button and banner componenet created, code78 button functionality implemented and transmission fixed

--- src/components/common/warning-banner/warning-banner.ts
+++ src/components/common/warning-banner/warning-banner.ts
@@ -6,6 +6,7 @@ import { Component, Input } from '@angular/core';
 })
 
 export class WarningBannerComponent {
+
   @Input()
   warningText: string;
 
--- src/pages/pass-finalisation/cat-be/components/pass-finalisation.cat-be.components.module.ts
+++ src/pages/pass-finalisation/cat-be/components/pass-finalisation.cat-be.components.module.ts
@@ -1,25 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ComponentsModule } from '../../../../components/common/common-components.module';
-import { Code78ProvidedComponent } from './code-78-provided/code-78-provided';
-import { LicenseIssuedBannerComponent } from './license-issued-banner/license-issued-banner';
 import { IonicModule } from 'ionic-angular';
-import { PassFinalisationComponentsModule } from '../../components/pass-finalisation-components.module';
+import { Code78Component } from './code-78/code-78';
+import { ComponentsModule } from '../../../../components/common/common-components.module';
 
 @NgModule({
   declarations: [
-    Code78ProvidedComponent,
-    LicenseIssuedBannerComponent,
+    Code78Component,
   ],
   imports: [
     IonicModule,
     CommonModule,
     ComponentsModule,
-    PassFinalisationComponentsModule,
   ],
   exports:[
-    Code78ProvidedComponent,
-    LicenseIssuedBannerComponent,
+    Code78Component,
   ],
 })
 export class PassFinalisationCatBEComponentsModule {}
