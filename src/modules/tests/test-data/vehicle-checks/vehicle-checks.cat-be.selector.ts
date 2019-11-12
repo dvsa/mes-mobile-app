b71e6d71a0b5eb52347462d1c773ba3247a42741
Matt <mculliford.dvsa@icloud.com>
2019-11-12 14:10:46 +0000

    formatted function as per PR comments

--- src/modules/tests/test-data/vehicle-checks/vehicle-checks.cat-be.selector.ts
+++ src/modules/tests/test-data/vehicle-checks/vehicle-checks.cat-be.selector.ts
@@ -2,8 +2,10 @@ import { CatBEUniqueTypes } from '@dvsa/mes-test-schema/categories/BE';
 import { QuestionResult } from '@dvsa/mes-test-schema/categories/Common';
 import { createFeatureSelector } from '@ngrx/store';
 
-export const getSelectedShowMeQuestions =
-(vehicleChecksCatBEReducer: CatBEUniqueTypes.VehicleChecks): QuestionResult[] =>
-vehicleChecksCatBEReducer.showMeQuestions;
+export const getSelectedShowMeQuestions = (
+  vehicleChecksCatBEReducer: CatBEUniqueTypes.VehicleChecks): QuestionResult[] => {
+  return vehicleChecksCatBEReducer.showMeQuestions
+};
+
 export const getVehicleChecksCatBe =
 createFeatureSelector<CatBEUniqueTypes.VehicleChecks>('vehicleChecks');
