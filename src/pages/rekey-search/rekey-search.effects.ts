47732b8dbe551d94cfbecb55d9b58ee518cb7fd9
Ammar <ammar.haider@bjss.com>
2019-11-22 15:18:57 +0000

    performs dependant requests checking if booking has been completed

--- src/pages/rekey-search/rekey-search.effects.ts
+++ src/pages/rekey-search/rekey-search.effects.ts
@@ -2,33 +2,52 @@ import { Injectable } from '@angular/core';
 import { Effect, Actions, ofType } from '@ngrx/effects';
 import { RekeySearchProvider } from '../../providers/rekey-search/rekey-search';
 import { switchMap, map, catchError } from 'rxjs/operators';
-import * as rekeySearchActions from './rekey-search.actions';
 import { of } from 'rxjs/observable/of';
 import { CompressionProvider } from '../../providers/compression/compression';
+import { SearchProvider } from '../../providers/search/search';
+import {
+  SEARCH_BOOKED_TEST, SearchBookedTest, SearchBookedTestSuccess, SearchBookedTestFailure,
+} from './rekey-search.actions';
+import { RekeySearchErrorMessages } from './rekey-search-error-model';
+import { HttpResponse } from '@angular/common/http';
+import { HttpStatusCodes } from '../../shared/models/http-status-codes';
 
 @Injectable()
 export class RekeySearchEffects {
 
   constructor(
     private actions$: Actions,
+    private testSearchProvider: SearchProvider,
     private rekeySearchProvider: RekeySearchProvider,
     private compressionProvider: CompressionProvider,
   )
   { }
 
   @Effect()
-  getTest$ = this.actions$.pipe(
-    ofType(rekeySearchActions.SEARCH_BOOKED_TEST),
-    switchMap((action: rekeySearchActions.SearchBookedTest) => {
-      const rekeySearchParams = {
-        applicationReference: action.appRef,
-        staffNumber: action.staffNumber,
-      };
-      return this.rekeySearchProvider.getTest(rekeySearchParams).pipe(
-        map(response => this.compressionProvider.extractTestSlotResult(response.toString())),
-        map((testSlot: any) => new rekeySearchActions.SearchBookedTestSuccess(testSlot, action.staffNumber)),
+  getBooking$ = this.actions$.pipe(
+    ofType(SEARCH_BOOKED_TEST),
+    switchMap((action: SearchBookedTest) => {
+      return this.testSearchProvider.getTestResult(action.appRef, action.staffNumber).pipe(
+        switchMap((response: HttpResponse<any>) => {
+          if (response.status === HttpStatusCodes.OK) {
+            return of(new SearchBookedTestFailure({
+              message: RekeySearchErrorMessages.BookingAlreadyCompleted,
+            }));
+          }
+          const rekeySearchParams = {
+            applicationReference: action.appRef,
+            staffNumber: action.staffNumber,
+          };
+          return this.rekeySearchProvider.getBooking(rekeySearchParams).pipe(
+            map(response => this.compressionProvider.extractTestSlotResult(response.toString())),
+            map((testSlot: any) => new SearchBookedTestSuccess(testSlot, action.staffNumber)),
+            catchError((err: any) => {
+              return of(new SearchBookedTestFailure(err));
+            }),
+          );
+        }),
         catchError((err: any) => {
-          return of(new rekeySearchActions.SearchBookedTestFailure(err));
+          return of(new SearchBookedTestFailure(err));
         }),
       );
     }),
