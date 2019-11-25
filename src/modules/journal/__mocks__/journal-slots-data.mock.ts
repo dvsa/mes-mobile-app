f535d3c3a2041577424bea1be3979e4a38af56e0
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2019-11-25 11:37:42 +0000

    Mes-3445 Bank holiday next working day navigation (#954)
    * update logic to use DateTime constructor
    
    * push test changes
    
    * update spec description and add parentheses to operation

--- src/modules/journal/__mocks__/journal-slots-data.mock.ts
+++ src/modules/journal/__mocks__/journal-slots-data.mock.ts
@@ -2,6 +2,7 @@ import { groupBy } from 'lodash';
 
 import { SlotItem } from '../../../providers/slot-selector/slot-item';
 import { DateTime } from '../../../shared/helpers/date-time';
+import { JournalModel } from '../journal.model';
 
 const localJournalJson = require('../../../../mock/local-journal.json');
 
@@ -11,6 +12,105 @@ const slotItems: SlotItem[] = localJournalJson.testSlots.map((testSlot) => {
     slotData: testSlot,
   };
 });
+
+export const baseJournalData: JournalModel = {
+  isLoading: true,
+  lastRefreshed: new Date(0),
+  slots: {
+    ['2019-01-01']: [
+      {
+        hasSlotChanged: false,
+        hasSeenCandidateDetails: false,
+        slotData: {
+          slotDetail: {
+            slotId: 1001,
+            start: '2019-01-12T09:14:00',
+          },
+          booking: {
+            application: {
+              applicationId: 1234561,
+              bookingSequence: 1,
+              checkDigit: 4,
+              welshTest: false,
+              extendedTest: false,
+              meetingPlace: '',
+              progressiveAccess: false,
+              specialNeeds: '',
+              entitlementCheck: false,
+              testCategory: 'B',
+              vehicleGearbox: 'Manual',
+            },
+            candidate: null,
+            previousCancellation: null,
+            business: null,
+          },
+        },
+      },
+    ],
+    ['2019-01-02']: [
+      {
+        hasSlotChanged: false,
+        hasSeenCandidateDetails: false,
+        slotData: {
+          slotDetail: {
+            slotId: 2001,
+            start: '2019-01-13T09:14:00',
+          },
+          booking: {
+            application: {
+              applicationId: 1234561,
+              bookingSequence: 1,
+              checkDigit: 4,
+              welshTest: false,
+              extendedTest: false,
+              meetingPlace: '',
+              progressiveAccess: false,
+              specialNeeds: '',
+              entitlementCheck: false,
+              testCategory: 'B',
+              vehicleGearbox: 'Manual',
+            },
+            candidate: null,
+            previousCancellation: null,
+            business: null,
+          },
+        },
+      },
+    ],
+    ['2019-01-03']: [
+      {
+        hasSlotChanged: false,
+        hasSeenCandidateDetails: false,
+        slotData: {
+          slotDetail: {
+            slotId: 3001,
+            start: '2019-01-14T09:14:00',
+          },
+          booking: {
+            application: {
+              applicationId: 1234561,
+              bookingSequence: 1,
+              checkDigit: 4,
+              welshTest: false,
+              extendedTest: false,
+              meetingPlace: '',
+              progressiveAccess: false,
+              specialNeeds: '',
+              entitlementCheck: false,
+              testCategory: 'B',
+              vehicleGearbox: 'Manual',
+            },
+            candidate: null,
+            previousCancellation: null,
+            business: null,
+          },
+        },
+      },
+    ],
+  },
+  selectedDate: '2019-01-02',
+  examiner: { staffNumber: '123', individualId: 456 },
+};
 
 const slots:{[k: string]: SlotItem[]} = groupBy(
   slotItems, (slot: SlotItem) => DateTime.at(slot.slotData.slotDetail.start).format('YYYY-MM-DD'));
