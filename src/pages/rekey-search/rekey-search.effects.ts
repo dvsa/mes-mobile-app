c043fa5dc4b9aa8b189133fd248cfcb611c7ff07
Ammar <ammar.haider@bjss.com>
2019-11-25 10:32:28 +0000

    removes logs

--- src/pages/rekey-search/rekey-search.effects.ts
+++ src/pages/rekey-search/rekey-search.effects.ts
@@ -29,8 +29,6 @@ export class RekeySearchEffects {
     switchMap((action: SearchBookedTest) => {
       return this.testSearchProvider.getTestResult(action.appRef, action.staffNumber).pipe(
         switchMap((response: HttpResponse<any>) => {
-          console.log('### getTestResult switchMap response');
-          console.log(JSON.stringify(response));
           if (response.status === HttpStatusCodes.OK) {
             return of(new SearchBookedTestFailure({
               message: RekeySearchErrorMessages.BookingAlreadyCompleted,
@@ -38,22 +36,14 @@ export class RekeySearchEffects {
           }
         }),
         catchError((err: any) => {
-          console.log('### getTestResult switchMap err');
-          console.log(JSON.stringify(err));
           const rekeySearchParams = {
             applicationReference: action.appRef,
             staffNumber: action.staffNumber,
           };
           return this.rekeySearchProvider.getBooking(rekeySearchParams).pipe(
-            map((response) => {
-              console.log('### getBooking switchMap response');
-              console.log(JSON.stringify(response));
-              return this.compressionProvider.extractTestSlotResult(response.toString());
-            }),
+            map(response => this.compressionProvider.extractTestSlotResult(response.toString())),
             map((testSlot: any) => new SearchBookedTestSuccess(testSlot, action.staffNumber)),
             catchError((err: any) => {
-              console.log('### getTestResult switchMap err');
-              console.log(JSON.stringify(err));
               return of(new SearchBookedTestFailure(err));
             }),
           );
