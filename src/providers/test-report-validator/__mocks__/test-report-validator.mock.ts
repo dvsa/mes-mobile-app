897ea402c4d4a7ae1ee779b1bd3f4c3db240c52e
Tom Billington <t.billington@kainos.com>
2019-11-27 16:09:56 +0000

    Fix mock

--- src/providers/test-report-validator/__mocks__/test-report-validator.mock.ts
+++ src/providers/test-report-validator/__mocks__/test-report-validator.mock.ts
@@ -1,8 +1,9 @@
-import { of } from 'rxjs/observable/of';
-
 export class TestReportValidatorProviderMock {
 
   isTestReportValid =
-    jasmine.createSpy('isTestReportValid').and.returnValue(of(true));
+    jasmine.createSpy('isTestReportValid').and.returnValue(true);
+
+  getMissingLegalRequirements =
+    jasmine.createSpy('getMissingLegalRequirements').and.returnValue([]);
 
 }
