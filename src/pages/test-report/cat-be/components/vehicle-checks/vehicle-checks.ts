13bf0a10ec26607b0101d983ec82f1fcc48a4808
Matt <mculliford.dvsa@icloud.com>
2019-11-27 13:29:30 +0000

    vehicle-checks cat be component

---
+++ src/pages/test-report/cat-be/components/vehicle-checks/vehicle-checks.ts
@@ -0,0 +1,17 @@
+import { Component, OnDestroy, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'vehicle-checks',
+  templateUrl: 'vehicle-checks.html'
+})
+export class VehicleChecksComponent implements OnInit, OnDestroy {
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+
+  }
+}
