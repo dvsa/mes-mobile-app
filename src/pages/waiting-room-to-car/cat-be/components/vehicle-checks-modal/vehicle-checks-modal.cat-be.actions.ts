cb8dc775e6847e8da05c5479f94cdb7cbe1c8817
Cullima <33055124+Cullima@users.noreply.github.com>
2019-11-27 11:28:53 +0000

    MES 4159 show me tell me analytics cat be (#967)
    * cat be vehicle checks analytics
    
    * added unit tests for analytics effect

---
+++ src/pages/waiting-room-to-car/cat-be/components/vehicle-checks-modal/vehicle-checks-modal.cat-be.actions.ts
@@ -0,0 +1,10 @@
+import { Action } from '@ngrx/store';
+
+export const VEHICLE_CHECKS_VIEW_DID_ENTER = '[VehicleChecks] Vehicle Checks Did Enter';
+
+export class VehicleChecksViewDidEnter implements Action {
+  readonly type = VEHICLE_CHECKS_VIEW_DID_ENTER;
+}
+
+export type Types =
+  | VehicleChecksViewDidEnter;
