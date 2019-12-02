efc59a7ae0e3c90cb0c5581134a69b77cd2d3722
Cullima <33055124+Cullima@users.noreply.github.com>
2019-12-02 11:44:44 +0000

    MES-4180 Add candidate name to cat B and B+E debrief pages (#982)
    * added candidate name to cat B and B+E ddebrief pages

--- src/pages/test-report/cat-be/__tests__/test-report.cat-be.page.spec.ts
+++ src/pages/test-report/cat-be/__tests__/test-report.cat-be.page.spec.ts
@@ -40,7 +40,7 @@ import { candidateMock } from '../../../../modules/tests/__mocks__/tests.mock';
 import { UncoupleRecoupleComponent } from '../components/uncouple-recouple/uncouple-recouple';
 import { ReverseLeftComponent } from '../components/reverse-left/reverse-left';
 import { ReverseLeftPopoverComponent } from '../components/reverse-left-popover/reverse-left-popover';
-import { VehicleChecksComponent } from '../components/vehicle-checks/vehicle-checks'
+import { VehicleChecksComponent } from '../components/vehicle-checks/vehicle-checks';
 
 describe('TestReportCatBEPage', () => {
   let fixture: ComponentFixture<TestReportCatBEPage>;
