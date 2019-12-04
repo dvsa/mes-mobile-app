d7e5ba2f33a6bafde578b4c471f54fbaaa9d979c
Tom Billington <t.billington@kainos.com>
2019-12-04 09:22:20 +0000

    Removed Page

--- src/pages/component-library/components/color-palette/color-palette.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'color-palette',
-  templateUrl: 'color-palette.html',
-})
-export class ColorPaletteComponent {
-  constructor() {}
-}
--- src/pages/component-library/components/data-rows/data-rows.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'data-rows',
-  templateUrl: 'data-rows.html',
-})
-export class DataRowsComponent {
-  constructor() {}
-}
