1b744df4bfe61820372c55299ae408b3f93838dd
Ammar <ammar.haider@bjss,.com>
2019-12-04 15:11:07 +0000

    reads version from release branch

--- buildScripts/increment-build-number.js
+++ buildScripts/increment-build-number.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 // Updates Cordova config.xml with a semver compatible version.
-// The new version takes the format <existing-major>.<existing-minor>.<unix-timestamp>
+// The new version takes whatever is in the release branch, e.g.
+// a branch named "release-3.1.2" will yield the version "3.1.2"
+
+const branchName = require('current-git-branch')();
 
 var fs = require('fs');
 var xml2js = require('xml2js');
@@ -9,8 +12,15 @@ const configFile = 'config.xml';
 
 // Read config.xml
 fs.readFile(configFile, 'utf8', (err, xml) => {
+
   if (err) {
-    return console.console.error(err);
+    return console.error(err);
+  }
+
+  if (!branchName) return console.error('Not a git repository');
+
+  if (branchName.indexOf('release') < 0) {
+    return;
   }
 
   // Parse XML to JS Obj
@@ -19,10 +29,8 @@ fs.readFile(configFile, 'utf8', (err, xml) => {
       return console.error(err);
     }
 
-    const currentVersion = obj.widget['$'].version;
-    const newVersion = `${currentVersion.slice(0, currentVersion.lastIndexOf('.'))}.${Math.floor(
-      Date.now() / 1000
-    )}`;
+    const newVersion = branchName.substring(branchName.length - 5, branchName.length);
+
     obj.widget['$'].version = newVersion;
 
     var builder = new xml2js.Builder();
