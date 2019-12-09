86e79cdcd5da0b55095e84c377bdba692cbddc23
Edward Wilson <edward.wilson@me.com>
2019-12-09 10:35:19 +0000

    Unit tests

--- src/pages/debrief/cat-b/components/vehicle-checks-card/__tests__/vehicle-checks-card.spec.ts
+++ src/pages/debrief/cat-b/components/vehicle-checks-card/__tests__/vehicle-checks-card.spec.ts
@@ -18,8 +18,8 @@ import { ConfigMock } from 'ionic-mocks';
 import { TranslateService, TranslateModule, TranslateLoader } from 'ng2-translate';
 import { createTranslateLoader } from '../../../../../../app/app.module';
 import { Http } from '@angular/http';
-import * as welshTranslations from '../../../../../assets/i18n/cy.json';
-import { TestCategory } from '@dvsa/mes-test-schema/categories/common/test-category';
+import * as welshTranslations from '../../../../../../assets/i18n/cy.json';
+import { TestCategory } from '../../../../../../shared/models/test-category';
 
 describe('VehicleChecksCardComponent', () => {
   let fixture: ComponentFixture<VehicleChecksCardComponent>;
