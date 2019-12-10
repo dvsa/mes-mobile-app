2db2e4ae7c1e5f86312c56cd02b0fc98af7d8e77
Tom Billington <Cavalier07@users.noreply.github.com>
2019-12-10 08:42:14 +0000

    MES-4211 - View Test Result Cat BE Part 1 (#1010)
    * Refactored Rekey Reason Card
    
    * Refactored & sorted out the debrief card
    
    * Fixed Broken Test
    
    * Unit Tests
    
    * Copy changes

---
+++ src/pages/view-test-result/components/vehicle-checks-data-row/__tests__/vehicle-checks-data-row.spec.ts
@@ -0,0 +1,42 @@
+
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+import { IonicModule, Config } from 'ionic-angular';
+import { ConfigMock } from 'ionic-mocks';
+import { VehicleChecksDataRowComponent } from '../vehicle-checks-data-row';
+
+describe('VehicleChecksDataRowComponent', () => {
+  let fixture: ComponentFixture<VehicleChecksDataRowComponent>;
+  let component: VehicleChecksDataRowComponent;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        VehicleChecksDataRowComponent,
+      ],
+      imports: [
+        IonicModule,
+      ],
+      providers: [
+        { provide: Config, useFactory: () => ConfigMock.instance() },
+      ],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(VehicleChecksDataRowComponent);
+        component = fixture.componentInstance;
+      });
+  }));
+
+  describe('Class', () => {
+    describe('shouldShowFault', () => {
+      it('should return true if outcome is a DF', () => {
+        expect(component.shouldShowFault('DF')).toEqual(true);
+      });
+      it('should return false if outcome is not a DF', () => {
+        expect(component.shouldShowFault('P')).toEqual(false);
+        expect(component.shouldShowFault('D')).toEqual(false);
+        expect(component.shouldShowFault('S')).toEqual(false);
+      });
+    });
+  });
+});
---
+++ src/pages/view-test-result/components/vehicle-checks-data-row/vehicle-checks-data-row.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { QuestionResult, QuestionOutcome } from '@dvsa/mes-test-schema/categories/Common';
+
+@Component({
+  selector: 'vehicle-checks-data-row',
+  templateUrl: 'vehicle-checks-data-row.html',
+})
+export class VehicleChecksDataRowComponent {
+
+  @Input()
+  label: string;
+
+  @Input()
+  data: QuestionResult[];
+
+  @Input()
+  shouldHaveSeperator: boolean = true;
+
+  public shouldShowFault(outcome: QuestionOutcome): boolean {
+    return outcome === 'DF';
+  }
+}
