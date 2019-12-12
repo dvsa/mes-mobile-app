54f55e353e760ad887f10e3e644ca275eb2e60b4
mgoldson <54100299+mgoldson@users.noreply.github.com>
2019-12-12 12:31:38 +0000

    mes-4254: Add test pass finalisation page (#1019)
    * mes-4254: Add test pass finalisation page
    
    * mes-4254: Use test category C

---
+++ src/pages/pass-finalisation/cat-c/pass-finalisation.cat-c.page.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PassFinalisationCatCPage } from './pass-finalisation.cat-c.page';
+import { EffectsModule } from '@ngrx/effects';
+import { PassFinalisationAnalyticsEffects } from '../pass-finalisation.analytics.effects';
+import { ComponentsModule } from '../../../components/common/common-components.module';
+import { TestFinalisationComponentsModule } from
+'../../../components/test-finalisation/test-finalisation-component.module';
+import { PassFinalisationComponentsModule } from '../components/pass-finalisation-components.module';
+import { PassFinalisationCatCComponentsModule } from './components/pass-finalisation.cat-c.components.module';
+
+@NgModule({
+  declarations: [
+    PassFinalisationCatCPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PassFinalisationCatCPage),
+    EffectsModule.forFeature([PassFinalisationAnalyticsEffects]),
+    ComponentsModule,
+    TestFinalisationComponentsModule,
+    PassFinalisationComponentsModule,
+    PassFinalisationCatCComponentsModule,
+  ],
+})
+export class PassFinalisationCatCPageModule {}
