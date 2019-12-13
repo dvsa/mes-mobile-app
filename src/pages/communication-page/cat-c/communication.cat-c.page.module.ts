30d2028d79f034ef55f18606d25ea794c04e18b1
Szabolcs Keresztes <keresztes.szabolcs93@gmail.com>
2019-12-13 16:17:02 +0000

    MES-4254: Copy Communication page to Cat C (#1034)
    * Copying Communication Page
    
    * Solving PR issues

---
+++ src/pages/communication-page/cat-c/communication.cat-c.page.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ComponentsModule } from '../../../components/common/common-components.module';
+import { CommunicationCatCPage } from './communication.cat-c.page';
+import { TranslateModule } from 'ng2-translate';
+import { EffectsModule } from '@ngrx/effects';
+import { CommunicationAnalyticsEffects } from '../communication.analytics.effects';
+import { CommunicationEffects } from '../communication.effects';
+import { CommunicationComponentsModule } from '../components/communication.components.module';
+
+@NgModule({
+  declarations: [
+    CommunicationCatCPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(CommunicationCatCPage),
+    EffectsModule.forFeature([
+      CommunicationEffects,
+      CommunicationAnalyticsEffects,
+    ]),
+    ComponentsModule,
+    TranslateModule,
+    CommunicationComponentsModule,
+  ],
+})
+export class CommunicationCatCPageModule { }
