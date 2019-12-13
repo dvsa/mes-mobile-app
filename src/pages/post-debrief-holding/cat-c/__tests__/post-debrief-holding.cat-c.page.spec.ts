52c8835d3e3eef3ff5d4143f748da35d8cd368bd
mgoldson <54100299+mgoldson@users.noreply.github.com>
2019-12-13 12:10:35 +0000

    mes-4254: Add  post debrief holding page (#1030)
    * mes-4254: Add  post debrief holding page
    
    * mes-4254: reference cat c pages

---
+++ src/pages/post-debrief-holding/cat-c/__tests__/post-debrief-holding.cat-c.page.spec.ts
@@ -0,0 +1,53 @@
+import { ComponentFixture, async, TestBed } from '@angular/core/testing';
+import { IonicModule, NavController, Platform } from 'ionic-angular';
+import { NavControllerMock, PlatformMock } from 'ionic-mocks';
+import { AppModule } from '../../../../app/app.module';
+import { AuthenticationProvider } from '../../../../providers/authentication/authentication';
+import { AuthenticationProviderMock } from '../../../../providers/authentication/__mocks__/authentication.mock';
+import { StoreModule, Store } from '@ngrx/store';
+import { StoreModel } from '../../../../shared/models/store.model';
+import { MockComponent } from 'ng-mocks';
+import { PracticeModeBanner } from '../../../../components/common/practice-mode-banner/practice-mode-banner';
+import { PostDebriefHoldingViewDidEnter } from '../../post-debrief-holding.actions';
+import { PostDebriefHoldingCatCPage } from '../post-debrief-holding.cat-c.page';
+
+describe('PostDebriefHoldingCatCPage', () => {
+  let fixture: ComponentFixture<PostDebriefHoldingCatCPage>;
+  let component: PostDebriefHoldingCatCPage;
+  let store$: Store<StoreModel>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        PostDebriefHoldingCatCPage,
+        MockComponent(PracticeModeBanner),
+      ],
+      imports: [
+        IonicModule,
+        AppModule,
+        StoreModule.forRoot({}),
+      ],
+      providers: [
+        { provide: NavController, useFactory: () => NavControllerMock.instance() },
+        { provide: Platform, useFactory: () => PlatformMock.instance() },
+        { provide: AuthenticationProvider, useClass: AuthenticationProviderMock },
+      ],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(PostDebriefHoldingCatCPage);
+        component = fixture.componentInstance;
+        store$ = TestBed.get(Store);
+        spyOn(store$, 'dispatch');
+      });
+  }));
+
+  describe('Class', () => {
+    describe('ionViewDidEnter', () => {
+      it('should dispatch a view did enter action', () => {
+        component.ionViewDidEnter();
+        expect(store$.dispatch).toHaveBeenCalledWith(new PostDebriefHoldingViewDidEnter());
+      });
+    });
+  });
+});
