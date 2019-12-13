468ffb2243da1c027ab592bd92414b2452761ac5
Ammar Haider <ammar.haider@bjss,.com>
2019-12-13 16:52:14 +0000

    uses cloneDeep to ensure initial state is fully copied over in test

--- src/modules/tests/test-data/cat-be/vehicle-checks/__tests__/vehicle-checks.cat-be.reducer.spec.ts
+++ src/modules/tests/test-data/cat-be/vehicle-checks/__tests__/vehicle-checks.cat-be.reducer.spec.ts
@@ -10,6 +10,7 @@ import {
 } from '../vehicle-checks.cat-be.action';
 import { QuestionResult } from '@dvsa/mes-test-schema/categories/common';
 import { CatBEUniqueTypes } from '@dvsa/mes-test-schema/categories/BE';
+import { cloneDeep } from 'lodash';
 
 describe('Vehicle Checks Cat B+E Reducer', () => {
 
@@ -60,9 +61,7 @@ describe('Vehicle Checks Cat B+E Reducer', () => {
 
   describe('TELL_ME_QUESTION_OUTCOME_CHANGED' , () => {
     it('should update the outcome property for the object at the specified index', () => {
-      const state: CatBEUniqueTypes.VehicleChecks = {
-        ...initialState,
-      };
+      const state: CatBEUniqueTypes.VehicleChecks = cloneDeep(initialState);
       state.tellMeQuestions[1] = {
         code: 'T01',
         description: 'desc',
