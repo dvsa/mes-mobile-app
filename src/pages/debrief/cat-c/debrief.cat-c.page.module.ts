11d26aeb48502be6efe64da7ce5497a5f0dc419f
mgoldson <54100299+mgoldson@users.noreply.github.com>
2019-12-13 12:09:56 +0000

    mes-4254: add cat c debrief page (#1024)

---
+++ src/pages/debrief/cat-c/debrief.cat-c.page.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DebriefCatCPage } from './debrief.cat-c.page';
+import { EffectsModule } from '@ngrx/effects';
+import { DebriefAnalyticsEffects } from '../debrief.analytics.effects';
+import { ComponentsModule } from '../../../components/common/common-components.module';
+import { DebriefComponentsModule } from '../components/debrief-components.module';
+import { TranslateModule } from 'ng2-translate';
+import { DebriefEffects } from '../debrief.effects';
+import { FaultSummaryProvider } from '../../../providers/fault-summary/fault-summary';
+
+@NgModule({
+  declarations: [
+    DebriefCatCPage,
+  ],
+  imports: [
+    DebriefComponentsModule,
+    IonicPageModule.forChild(DebriefCatCPage),
+    EffectsModule.forFeature([
+      DebriefEffects,
+      DebriefAnalyticsEffects,
+    ]),
+    ComponentsModule,
+    TranslateModule,
+  ],
+  providers: [
+    FaultSummaryProvider,
+  ],
+})
+export class DebriefCatCPageModule { }
