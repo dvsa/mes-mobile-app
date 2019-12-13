43582399d04a5265a3f7618f64b1c5d55e645541
Szabolcs Keresztes <keresztes.szabolcs93@gmail.com>
2019-12-13 15:59:30 +0000

    MES-4254: Copying Health Declaration to Cat C (#1033)

---
+++ src/pages/health-declaration/cat-c/health-declaration.cat-c.page.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { HealthDeclarationCatCPage } from './health-declaration.cat-c.page';
+import { EffectsModule } from '@ngrx/effects';
+import { HealthDeclarationAnalyticsEffects } from '../health-declaration.analytics.effects';
+import { ComponentsModule } from '../../../components/common/common-components.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from 'ng2-translate';
+import { HealthDeclarationEffects } from '../health-declaration.effects';
+import { HealthDeclarationComponentsModule } from '../components/health-declaration.components.module';
+
+@NgModule({
+  declarations: [
+    HealthDeclarationCatCPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(HealthDeclarationCatCPage),
+    EffectsModule.forFeature([
+      HealthDeclarationAnalyticsEffects,
+      HealthDeclarationEffects,
+    ]),
+    HealthDeclarationComponentsModule,
+    ComponentsModule,
+    ReactiveFormsModule,
+    TranslateModule,
+  ],
+})
+export class HealthDeclarationCatCPageModule { }
