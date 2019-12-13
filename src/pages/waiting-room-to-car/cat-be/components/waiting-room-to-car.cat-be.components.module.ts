630a8a15823df00363063feb9a2bcf0720987c21
Szabolcs Keresztes <keresztes.szabolcs93@gmail.com>
2019-12-13 15:01:50 +0000

    MES-4254: Copying Waiting Room To Car (#1027)
    * Copying waiting room to car page to cat c
    
    * Solving PR issues
    
    * Solving PR issues
    
    * Solving PR issues

--- src/pages/waiting-room-to-car/cat-be/components/waiting-room-to-car.cat-be.components.module.ts
+++ src/pages/waiting-room-to-car/cat-be/components/waiting-room-to-car.cat-be.components.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { VehicleChecksCatBEComponent } from './vehicle-checks/vehicle-checks';
 import { IonicModule } from 'ionic-angular';
 import { VehicleChecksModalCatBEModule } from './vehicle-checks-modal/vehicle-checks-modal.cat-be.page.module';
-import { VehicleChecksCatBEModule } from './vehicle-checks/vehicle-checks.module';
 
 @NgModule({
   imports: [
     IonicModule,
     VehicleChecksModalCatBEModule,
-    VehicleChecksCatBEModule,
   ],
   exports: [
     VehicleChecksCatBEComponent,
