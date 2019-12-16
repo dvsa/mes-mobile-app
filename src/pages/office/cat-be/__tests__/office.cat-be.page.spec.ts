566e1ec010a02d47ec9069f097d2f74c9a599f66
Tom Billington <t.billington@kainos.com>
2019-12-16 12:12:56 +0000

    Fix Unit Tests

--- src/pages/office/cat-be/__tests__/office.cat-be.page.spec.ts
+++ src/pages/office/cat-be/__tests__/office.cat-be.page.spec.ts
@@ -64,6 +64,7 @@ import { NavigationStateProviderMock } from '../../../../providers/navigation-st
 import { SetActivityCode } from '../../../../modules/tests/activity-code/activity-code.actions';
 import { TestCategory } from '@dvsa/mes-test-schema/categories/common/test-category';
 import { FaultSummaryProvider } from '../../../../providers/fault-summary/fault-summary';
+import { VehicleChecksComponent } from '../../components/vehicle-checks/vehicle-checks';
 
 describe('OfficePage', () => {
   let fixture: ComponentFixture<OfficeCatBEPage>;
@@ -83,6 +84,7 @@ describe('OfficePage', () => {
         MockComponent(AdditionalInformationComponent),
         MockComponent(IndependentDrivingComponent),
         MockComponent(FaultCommentCardComponent),
+        MockComponent(VehicleChecksComponent),
       ],
       imports: [
         IonicModule,
