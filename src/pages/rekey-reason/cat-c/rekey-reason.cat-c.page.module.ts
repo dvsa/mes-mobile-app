e4926b7bdd82a3603ca806c743c5c2492769434c
Tom Billington <Cavalier07@users.noreply.github.com>
2019-12-16 11:10:39 +0000

    MES-4255 - Cat C Reason for rekey page (#1037)
    * Cat C version of Reason for Rekey
    
    * code review comments

---
+++ src/pages/rekey-reason/cat-c/rekey-reason.cat-c.page.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EffectsModule } from '@ngrx/effects';
+import { ComponentsModule } from '../../../components/common/common-components.module';
+import { RekeyReasonCatCPage } from './rekey-reason.cat-c.page';
+import { RekeyReasonComponentsModule } from '../components/rekey-reason.components.module';
+import { RekeyReasonAnalyticsEffects } from '../rekey-reason.analytics.effects';
+import { StoreModule } from '@ngrx/store';
+import { RekeyReasonEffects } from '../rekey-reason.effects';
+import { DirectivesModule } from '../../../directives/directives.module';
+import { FindUserProvider } from '../../../providers/find-user/find-user';
+import { rekeyReasonReducer } from '../rekey-reason.reducer';
+
+@NgModule({
+  declarations: [
+    RekeyReasonCatCPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(RekeyReasonCatCPage),
+    StoreModule.forFeature('rekeyReason', rekeyReasonReducer),
+    EffectsModule.forFeature([
+      RekeyReasonAnalyticsEffects,
+      RekeyReasonEffects,
+    ]),
+    DirectivesModule,
+    ComponentsModule,
+    RekeyReasonComponentsModule,
+  ],
+  providers: [
+    FindUserProvider,
+  ],
+})
+
+export class RekeyReasonCatCPageModule { }
