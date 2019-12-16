2dd37aea8fc21d550c2d18de9dc0305988834358
szabi.keresztes <szabi.keresztes@bjss.com>
2019-12-16 14:35:52 +0000

    Fixing rebase issues and composing vehicle checks module correctly

---
+++ src/pages/office/cat-c/components/office.cat-c.components.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { IndependentDrivingComponent } from './independent-driving/independent-driving';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from 'ionic-angular';
+
+@NgModule({
+  declarations: [
+    IndependentDrivingComponent,
+  ],
+  imports: [
+    CommonModule,
+    IonicModule,
+  ],
+  exports: [
+    IndependentDrivingComponent,
+  ],
+})
+
+export class OfficeCatCComponentsModule { }
--- src/pages/waiting-room-to-car/cat-be/components/waiting-room-to-car.cat-be.components.module.ts
+++ src/pages/waiting-room-to-car/cat-be/components/waiting-room-to-car.cat-be.components.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { VehicleChecksCatBEComponent } from './vehicle-checks/vehicle-checks';
 import { IonicModule } from 'ionic-angular';
-import { VehicleChecksModalCatBEModule } from './vehicle-checks-modal/vehicle-checks-modal.cat-be.page.module';
+// import { VehicleChecksModalCatBEModule } from './vehicle-checks-modal/vehicle-checks-modal.cat-be.page.module';
+import { ComponentsModule } from '../../../../components/common/common-components.module';
 
 @NgModule({
+  declarations: [
+    VehicleChecksCatBEComponent,
+  ],
   imports: [
     IonicModule,
-    VehicleChecksModalCatBEModule,
+    ComponentsModule,
   ],
   exports: [
     VehicleChecksCatBEComponent,
