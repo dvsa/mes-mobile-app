039712cf395d1dd414386cd090d3e5a084354302
Edward Wilson <edward.wilson@me.com>
2020-01-09 16:12:12 +0000

    Added WRTC pages for mod1 (#1071)

---
+++ src/pages/waiting-room-to-car/cat-a-mod1/waiting-room-to-car.cat-a-mod1.page.ts
@@ -0,0 +1,257 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+import { AuthenticationProvider } from '../../../providers/authentication/authentication';
+import { Store, select } from '@ngrx/store';
+import { StoreModel } from '../../../shared/models/store.model';
+import * as waitingRoomToCarActions from '../waiting-room-to-car.actions';
+import { Observable } from 'rxjs/Observable';
+import { GearboxCategory } from '@dvsa/mes-test-schema/categories/common';
+import { getCurrentTest, getJournalData } from '../../../modules/tests/tests.selector';
+import {
+  SchoolCarToggled,
+  GearboxCategoryChanged,
+  VehicleRegistrationChanged,
+} from '../../../modules/tests/vehicle-details/common/vehicle-details.actions';
+import { map } from 'rxjs/operators';
+import {
+  InstructorAccompanimentToggled,
+  OtherAccompanimentToggled,
+  SupervisorAccompanimentToggled,
+  InterpreterAccompanimentToggled,
+} from '../../../modules/tests/accompaniment/accompaniment.actions';
+// TODO - PREP-AMOD1: update to cat A
+import { getVehicleDetails } from '../../../modules/tests/vehicle-details/cat-be/vehicle-details.cat-be.reducer';
+import { getAccompaniment } from '../../../modules/tests/accompaniment/accompaniment.reducer';
+import {
+  getRegistrationNumber,
+  getGearboxCategory,
+  isAutomatic,
+  isManual,
+} from '../../../modules/tests/vehicle-details/common/vehicle-details.selector';
+// TODO - PREP-AMOD1: update to cat A
+import {
+  getSchoolCar,
+} from '../../../modules/tests/vehicle-details/cat-be/vehicle-details.cat-be.selector';
+
+// TODO - PREP-AMOD1: update to cat A
+import {
+  getInstructorAccompaniment,
+  getSupervisorAccompaniment,
+  getOtherAccompaniment,
+  getInterpreterAccompaniment,
+} from '../../../modules/tests/accompaniment/accompaniment.selector';
+// TODO - PREP-AMOD1: update to cat A
+import { getCandidate } from '../../../modules/tests/journal-data/cat-be/candidate/candidate.cat-be.reducer';
+import { getUntitledCandidateName } from '../../../modules/tests/journal-data/common/candidate/candidate.selector';
+import { getTests } from '../../../modules/tests/tests.reducer';
+import { FormGroup } from '@angular/forms';
+import { QuestionProvider } from '../../../providers/question/question';
+import {
+  EyesightTestReset,
+  EyesightTestPassed,
+  EyesightTestFailed,
+} from '../../../modules/tests/test-data/common/eyesight-test/eyesight-test.actions';
+// TODO - PREP-AMOD1: update to cat A
+import {
+  hasEyesightTestGotSeriousFault, hasEyesightTestBeenCompleted,
+} from '../../../modules/tests/test-data/cat-be/test-data.cat-be.selector';
+// TODO - PREP-AMOD1: update to cat A
+import { getTestData } from '../../../modules/tests/test-data/cat-be/test-data.cat-be.reducer';
+import { PersistTests } from '../../../modules/tests/tests.actions';
+import { CAT_AMOD1 } from '../../page-names.constants';
+import { BasePageComponent } from '../../../shared/classes/base-page';
+import { VehicleChecksQuestion } from '../../../providers/question/vehicle-checks-question.model';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
+import { FaultCountProvider } from '../../../providers/fault-count/fault-count';
+
+interface WaitingRoomToCarPageState {
+  candidateName$: Observable<string>;
+  registrationNumber$: Observable<string>;
+  transmission$: Observable<GearboxCategory>;
+  schoolCar$: Observable<boolean>;
+  instructorAccompaniment$: Observable<boolean>;
+  supervisorAccompaniment$: Observable<boolean>;
+  otherAccompaniment$: Observable<boolean>;
+  interpreterAccompaniment$: Observable<boolean>;
+  eyesightTestComplete$: Observable<boolean>;
+  eyesightTestFailed$: Observable<boolean>;
+  gearboxAutomaticRadioChecked$: Observable<boolean>;
+  gearboxManualRadioChecked$: Observable<boolean>;
+}
+
+@IonicPage()
+@Component({
+  selector: '.waiting-room-to-car-cat-a-mod1-page',
+  templateUrl: 'waiting-room-to-car.cat-a-mod1.page.html',
+})
+export class WaitingRoomToCarCatAMod1Page extends BasePageComponent {
+  pageState: WaitingRoomToCarPageState;
+  form: FormGroup;
+
+  showEyesightFailureConfirmation: boolean = false;
+
+  tellMeQuestions: VehicleChecksQuestion[];
+
+  constructor(
+    public store$: Store<StoreModel>,
+    public navController: NavController,
+    public navParams: NavParams,
+    public platform: Platform,
+    public authenticationProvider: AuthenticationProvider,
+    public faultCountProvider: FaultCountProvider,
+    public questionProvider: QuestionProvider,
+  ) {
+    super(platform, navController, authenticationProvider);
+    this.tellMeQuestions = questionProvider.getTellMeQuestions(TestCategory.BE);
+    this.form = new FormGroup({});
+  }
+
+  ngOnInit(): void {
+
+    const currentTest$ = this.store$.pipe(
+      select(getTests),
+      select(getCurrentTest),
+    );
+
+    this.pageState = {
+      candidateName$: currentTest$.pipe(
+        select(getJournalData),
+        select(getCandidate),
+        select(getUntitledCandidateName),
+      ),
+      registrationNumber$: currentTest$.pipe(
+        select(getVehicleDetails),
+        select(getRegistrationNumber),
+      ),
+      transmission$: currentTest$.pipe(
+        select(getVehicleDetails),
+        select(getGearboxCategory),
+      ),
+      schoolCar$: currentTest$.pipe(
+        select(getVehicleDetails),
+        select(getSchoolCar),
+      ),
+      instructorAccompaniment$: currentTest$.pipe(
+        select(getAccompaniment),
+        select(getInstructorAccompaniment),
+      ),
+      supervisorAccompaniment$: currentTest$.pipe(
+        select(getAccompaniment),
+        select(getSupervisorAccompaniment),
+      ),
+      otherAccompaniment$: currentTest$.pipe(
+        select(getAccompaniment),
+        select(getOtherAccompaniment),
+      ),
+      interpreterAccompaniment$: currentTest$.pipe(
+        select(getAccompaniment),
+        select(getInterpreterAccompaniment),
+      ),
+      eyesightTestComplete$: currentTest$.pipe(
+        select(getTestData),
+        select(hasEyesightTestBeenCompleted),
+      ),
+      eyesightTestFailed$: currentTest$.pipe(
+        select(getTestData),
+        select(hasEyesightTestGotSeriousFault),
+      ),
+      gearboxAutomaticRadioChecked$: currentTest$.pipe(
+        select(getVehicleDetails),
+        map(isAutomatic),
+      ),
+      gearboxManualRadioChecked$: currentTest$.pipe(
+        select(getVehicleDetails),
+        map(isManual),
+      ),
+    };
+  }
+
+  ionViewDidEnter(): void {
+    this.store$.dispatch(new waitingRoomToCarActions.WaitingRoomToCarViewDidEnter());
+  }
+
+  ionViewWillLeave(): void {
+    this.store$.dispatch(new PersistTests());
+  }
+
+  schoolCarToggled(): void {
+    this.store$.dispatch(new SchoolCarToggled());
+  }
+
+  transmissionChanged(transmission: GearboxCategory): void {
+    this.store$.dispatch(new GearboxCategoryChanged(transmission));
+  }
+
+  instructorAccompanimentToggled(): void {
+    this.store$.dispatch(new InstructorAccompanimentToggled());
+  }
+
+  supervisorAccompanimentToggled(): void {
+    this.store$.dispatch(new SupervisorAccompanimentToggled());
+  }
+
+  interpreterAccompanimentToggled(): void {
+    this.store$.dispatch(new InterpreterAccompanimentToggled());
+  }
+
+  otherAccompanimentToggled(): void {
+    this.store$.dispatch(new OtherAccompanimentToggled());
+  }
+
+  vehicleRegistrationChanged(vehicleRegistration: string) {
+    this.store$.dispatch(new VehicleRegistrationChanged(vehicleRegistration));
+  }
+
+  closeVehicleChecksModal = () => {
+    this.store$.dispatch(new waitingRoomToCarActions.WaitingRoomToCarViewDidEnter());
+  }
+
+  onSubmit() {
+    Object.keys(this.form.controls).forEach(controlName => this.form.controls[controlName].markAsDirty());
+    if (this.form.valid) {
+      this.navController.push(CAT_AMOD1.TEST_REPORT_PAGE).then(() => {
+        // remove Waiting Room To Car Page
+        const view = this.navController.getViews().find(view => view.id === CAT_AMOD1.WAITING_ROOM_TO_CAR_PAGE);
+        if (view) {
+          this.navController.removeView(view);
+        }
+      });
+    } else {
+      Object.keys(this.form.controls).forEach((controlName) => {
+        if (this.form.controls[controlName].invalid) {
+          this.store$.dispatch(new waitingRoomToCarActions.WaitingRoomToCarValidationError(
+            `${controlName} is blank`,
+          ));
+        }
+      });
+    }
+  }
+  updateForm(ctrl: string, value: any) {
+    this.form.patchValue({
+      [ctrl]: value,
+    });
+  }
+
+  isCtrlDirtyAndInvalid(controlName: string): boolean {
+    return !this.form.value[controlName] && this.form.get(controlName).dirty;
+  }
+
+  setEyesightFailureVisibility(show: boolean) {
+    this.showEyesightFailureConfirmation = show;
+  }
+
+  eyesightFailCancelled = () => {
+    this.form.get('eyesightCtrl') && this.form.get('eyesightCtrl').reset();
+    this.store$.dispatch(new EyesightTestReset());
+  }
+
+  eyesightTestResultChanged(passed: boolean): void {
+    const action = passed ? new EyesightTestPassed() : new EyesightTestFailed();
+    this.store$.dispatch(action);
+  }
+
+  getDebriefPage() {
+    return CAT_AMOD1.DEBRIEF_PAGE;
+  }
+
+}
