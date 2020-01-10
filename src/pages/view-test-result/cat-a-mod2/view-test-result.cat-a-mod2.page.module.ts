974f6cd02f5e24fec0c525b3bc50d8433b6211b7
MarkPatt89 <m.patterson@kainos.com>
2020-01-10 10:23:58 +0000

    MES-4442: Adding components for Cat A Mod 2 Test Details Views

--- src/pages/view-test-result/cat-a-mod2/view-test-result.cat-a-mod2.page.module.ts
+++ src/pages/view-test-result/cat-a-mod2/view-test-result.cat-a-mod2.page.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { ViewTestResultCatAMod1Page } from './view-test-result.cat-a-mod1.page';
 import { SearchProvider } from '../../../providers/search/search';
 import { CompressionProvider } from '../../../providers/compression/compression';
 import { EffectsModule } from '@ngrx/effects';
@@ -8,17 +7,18 @@ import { ViewTestResultAnalyticsEffects } from '../view-test-result.analytics.ef
 import { ComponentsModule } from '../../../components/common/common-components.module';
 import { FaultSummaryProvider } from '../../../providers/fault-summary/fault-summary';
 import { ViewTestResultComponentsModule } from '../components/view-test-result.components.module';
-import { ViewTestResultCatAMod1ComponentsModule } from './components/view-test-result.cat-a-mod1.components.module';
+import { ViewTestResultCatAMod2ComponentsModule } from './components/view-test-result.cat-a-mod2.components.module';
+import { ViewTestResultCatAMod2Page } from './view-test-result.cat-a-mod2.page';
 
 @NgModule({
   declarations: [
-    ViewTestResultCatAMod1Page,
+    ViewTestResultCatAMod2Page,
   ],
   imports: [
     ComponentsModule,
-    IonicPageModule.forChild(ViewTestResultCatAMod1Page),
+    IonicPageModule.forChild(ViewTestResultCatAMod2Page),
     ViewTestResultComponentsModule,
-    ViewTestResultCatAMod1ComponentsModule,
+    ViewTestResultCatAMod2ComponentsModule,
     EffectsModule.forFeature([ViewTestResultAnalyticsEffects]),
   ],
   providers: [
@@ -27,4 +27,4 @@ import { ViewTestResultCatAMod1ComponentsModule } from './components/view-test-r
     FaultSummaryProvider,
   ],
 })
-export class ViewTestResultCatAMod1PageModule { }
+export class ViewTestResultCatAMod2PageModule { }
