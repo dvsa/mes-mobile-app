29e113056d0ee4ffce392d53a019d7838c6e1724
Mark Patterson <30832405+MarkPatt89@users.noreply.github.com>
2020-01-10 10:01:59 +0000

    MES-4401: Adding test details view for Cat A Mod 1 (#1075)

---
+++ src/pages/view-test-result/cat-a-mod1/components/business-details-card/business-details-card.ts
@@ -0,0 +1,39 @@
+import { Component, Input } from '@angular/core';
+import { get } from 'lodash';
+// todo: PREP-AMOD1 change to CatAMod1UniqueTypes when schema changes are ready
+import { CatBEUniqueTypes } from '@dvsa/mes-test-schema/categories/BE';
+import { Address } from '@dvsa/mes-test-schema/categories/common';
+
+@Component({
+  selector: 'business-details-card',
+  templateUrl: 'business-details-card.html',
+})
+export class BusinessDetailsCardComponent {
+
+  @Input()
+  // todo: PREP-AMOD1 change to CatAMod1UniqueTypes when schema changes are ready
+  data: CatBEUniqueTypes.Candidate;
+
+  constructor() {}
+
+  public shouldHideCard(): boolean {
+    return (
+      !get(this.data, 'businessName') &&
+      !get(this.data, 'businessTelephone') &&
+      !get(this.data, 'businessAddress')
+    );
+  }
+
+  public getBusinessName(): string {
+    return get(this.data, 'businessName', 'Not supplied');
+  }
+
+  public getPhoneNumber(): string {
+    return get(this.data, 'businessTelephone' , 'Not supplied');
+  }
+
+  public getAddress(): Address {
+    return get(this.data, 'businessAddress');
+  }
+
+}
