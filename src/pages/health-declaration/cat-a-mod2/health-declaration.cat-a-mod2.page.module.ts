de455de259d031df3b48c2d5ed54cd5bc9dd2634
Jennifer <jenniferm@kainos.com>
2020-01-10 16:07:12 +0000

    created health declaration page for category a mod2

---
+++ src/pages/health-declaration/cat-a-mod2/health-declaration.cat-a-mod2.page.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { HealthDeclarationCatAMod2Page } from './health-declaration.cat-a-mod2.page';
+import { EffectsModule } from '@ngrx/effects';
+import { HealthDeclarationAnalyticsEffects } from '../health-declaration.analytics.effects';
+import { ComponentsModule } from '../../../components/common/common-components.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from 'ng2-translate';
+import { HealthDeclarationEffects } from '../health-declaration.effects';
+import { HealthDeclarationComponentsModule } from '../components/health-declaration.components.module';
+
+@NgModule({
+  declarations: [
+    HealthDeclarationCatAMod2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(HealthDeclarationCatAMod2Page),
+    EffectsModule.forFeature([
+      HealthDeclarationAnalyticsEffects,
+      HealthDeclarationEffects,
+    ]),
+    HealthDeclarationComponentsModule,
+    ComponentsModule,
+    ReactiveFormsModule,
+    TranslateModule,
+  ],
+})
+export class HealthDeclarationCatAMod2PageModule { }
