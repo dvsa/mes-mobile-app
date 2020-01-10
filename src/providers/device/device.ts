29f5f662f2aa355eaad4804d8471e8ebe8c69d20
ammarhaiderbjss <33695049+ammarhaiderbjss@users.noreply.github.com>
2020-01-10 16:33:57 +0000

    MES-4515 - Retry enabling ASAM cherry pick (#1088)
    * retries enabling ASAM timing out eventually
    
    * uses defer to ensure promise func is re-called

--- src/providers/device/device.ts
+++ src/providers/device/device.ts
@@ -7,12 +7,18 @@ import { LogType } from '../../shared/models/log.model';
 import { Store } from '@ngrx/store';
 import { StoreModel } from '../../shared/models/store.model';
 import { LogHelper } from '../logs/logsHelper';
+import { timeout, retry, map } from 'rxjs/operators';
+import { defer } from 'rxjs/observable/defer';
+import { Observable } from 'rxjs/Observable';
 
 declare let cordova: any;
 
 @Injectable()
 export class DeviceProvider implements IDeviceProvider {
   private supportedDevices: string[] = [];
+  private enableASAMRetryLimit: number = 3;
+  private enableASAMTimeout: number = 10000;
+  private enableASAMRetryFailureMessage: string =  `All retries to enable ASAM failed`;
 
   constructor(
     public appConfig: AppConfigProvider,
@@ -39,8 +45,40 @@ export class DeviceProvider implements IDeviceProvider {
     return this.device.uuid;
   }
 
-  enableSingleAppMode = async (): Promise<boolean> => {
-    return await this.setSingleAppMode(true);
+  /**
+   * [enableSingleAppMode description]
+   *
+   * @returns Promise<any>
+   *
+   * Runs setSingleAppMode(true) and retries a number
+   * of times, eventually timing out after a specified
+   * duration.
+   *
+   * This method is designed to execute and complete
+   * in the background without blocking the user.
+   * If after retrying, ASAM still failed to enable,
+   * a unique log is sent.
+  */
+  enableSingleAppMode = async(): Promise<any> => {
+    const enableAsamWithRetriesAndTimeout$: Observable<boolean> = defer(() => this.setSingleAppMode(true)).pipe(
+        map((didSucceed: boolean): boolean => {
+          if (!didSucceed) throw new Error('Call to enable ASAM failed');
+          return didSucceed;
+        }),
+        retry(this.enableASAMRetryLimit),
+        timeout(this.enableASAMTimeout),
+      );
+
+    const promisifiedEnableAsamWithRetriesAndTimeout = enableAsamWithRetriesAndTimeout$.toPromise()
+      .catch(() => {
+        this.store$.dispatch(new SaveLog(this.logHelper.createLog(
+          LogType.ERROR,
+          null,
+          this.enableASAMRetryFailureMessage,
+        )));
+      });
+
+    return await promisifiedEnableAsamWithRetriesAndTimeout;
   }
 
   disableSingleAppMode = async (): Promise<boolean> => {
@@ -52,7 +90,6 @@ export class DeviceProvider implements IDeviceProvider {
       if (cordova && cordova.plugins && cordova.plugins.ASAM) {
         cordova.plugins.ASAM.toggle(enabled, (didSucceed: boolean) => {
           const logMessage = `Call to ${enabled ? 'enable' : 'disable'} ASAM ${didSucceed ? 'succeeded' : 'failed'}`;
-          console.log(logMessage);
           if (!didSucceed) {
             const logError = `${enabled ? 'Enabling' : 'Disabling'} ASAM`;
             this.store$.dispatch(new SaveLog(this.logHelper.createLog(LogType.ERROR, logError, logMessage)));
