a901ca50cad434bbdaa70e85c0940a6d93a5b202
Edward Wilson <edward.wilson@me.com>
2020-01-13 16:54:15 +0000

    MES-4437 back to office (#1098)
    * Adding back to office cat a mod2 page
    
    * Removing unneeded banner

---
+++ src/pages/back-to-office/cat-a-mod2/back-to-office.cat-a-mod2.page.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ComponentsModule } from '../../../components/common/common-components.module';
+import { BackToOfficeCatAMod2Page } from './back-to-office.cat-a-mod2.page';
+import { TranslateModule } from 'ng2-translate';
+import { EffectsModule } from '@ngrx/effects';
+import { BackToOfficeAnalyticsEffects } from '../back-to-office.analytics.effects';
+
+@NgModule({
+  declarations: [
+    BackToOfficeCatAMod2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(BackToOfficeCatAMod2Page),
+    EffectsModule.forFeature([
+      BackToOfficeAnalyticsEffects,
+    ]),
+    ComponentsModule,
+    TranslateModule,
+  ],
+})
+export class BackToOfficeCatAMod2PageModule { }
