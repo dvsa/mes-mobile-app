ca241a49e1b316d6a817500550b15677db844b78
Jennifer <jenniferm@kainos.com>
2020-01-13 12:49:20 +0000

    created debrief page for CatAMod2

---
+++ src/pages/debrief/cat-a-mod2/debrief.cat-a-mod2.page.ts
@@ -0,0 +1,192 @@
+import { NavController, NavParams, Platform, IonicPage } from 'ionic-angular';
+import { Store, select } from '@ngrx/store';
+import { StoreModel } from '../../../shared/models/store.model';
+import { AuthenticationProvider } from '../../../providers/authentication/authentication';
+import { getCurrentTest, getJournalData } from '../../../modules/tests/tests.selector';
+import { DebriefViewDidEnter, EndDebrief } from '../debrief.actions';
+import { Observable } from 'rxjs/Observable';
+import { getTests } from '../../../modules/tests/tests.reducer';
+
+// TODO - PREP-AMOD2 - Implement category specific reducer
+import { getTestData } from '../../../modules/tests/test-data/cat-be/test-data.cat-be.reducer';
+import { getETA, getEco } from '../../../modules/tests/test-data/common/test-data.selector';
+import { map, tap, withLatestFrom } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { merge } from 'rxjs/observable/merge';
+import { getTestOutcome } from '../debrief.selector';
+import { FaultSummary } from '../../../shared/models/fault-marking.model';
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
+import { Insomnia } from '@ionic-native/insomnia';
+import { TranslateService } from 'ng2-translate';
+import { ETA, Eco } from '@dvsa/mes-test-schema/categories/common';
+import {
+  getCommunicationPreference,
+} from '../../../modules/tests/communication-preferences/communication-preferences.reducer';
+import { getConductedLanguage } from
+  '../../../modules/tests/communication-preferences/communication-preferences.selector';
+import { CAT_A_MOD2 } from '../../page-names.constants';
+import { Language } from '../../../modules/tests/communication-preferences/communication-preferences.model';
+import { configureI18N } from '../../../shared/helpers/translation.helpers';
+import { BasePageComponent } from '../../../shared/classes/base-page';
+import { FaultCountProvider } from '../../../providers/fault-count/fault-count';
+import { getTestCategory } from '../../../modules/tests/category/category.reducer';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
+import { FaultSummaryProvider } from '../../../providers/fault-summary/fault-summary';
+
+// TODO - PREP-AMOD2 - Implement category specific reducer
+import { getCandidate } from '../../../modules/tests/journal-data/cat-b/candidate/candidate.reducer';
+import { getUntitledCandidateName } from '../../../modules/tests/journal-data/common/candidate/candidate.selector';
+
+interface DebriefPageState {
+  seriousFaults$: Observable<string[]>;
+  dangerousFaults$: Observable<string[]>;
+  drivingFaults$: Observable<FaultSummary[]>;
+  drivingFaultCount$: Observable<number>;
+  etaFaults$: Observable<ETA>;
+  ecoFaults$: Observable<Eco>;
+  testResult$: Observable<string>;
+  conductedLanguage$: Observable<string>;
+  candidateName$: Observable<string>;
+}
+
+@IonicPage()
+@Component({
+  selector: '.debrief-cat-a-mod2-page',
+  templateUrl: 'debrief.cat-a-mod2.page.html',
+})
+
+export class DebriefCatAMod2Page extends BasePageComponent {
+
+  pageState: DebriefPageState;
+  subscription: Subscription;
+  isPassed: boolean;
+
+  // Used for now to test displaying pass/fail/terminated messages
+  public outcome: string;
+
+  public hasPhysicalEta: boolean = false;
+  public hasVerbalEta: boolean = false;
+
+  public adviceGivenControl: boolean = false;
+  public adviceGivenPlanning: boolean = false;
+
+  constructor(
+    public store$: Store<StoreModel>,
+    public navController: NavController,
+    public navParams: NavParams,
+    public platform: Platform,
+    public authenticationProvider: AuthenticationProvider,
+    public screenOrientation: ScreenOrientation,
+    public insomnia: Insomnia,
+    private translate: TranslateService,
+    private faultCountProvider: FaultCountProvider,
+    private faultSummaryProvider: FaultSummaryProvider,
+  ) {
+    super(platform, navController, authenticationProvider);
+  }
+
+  ngOnInit(): void {
+    const currentTest$ = this.store$.pipe(
+      select(getTests),
+      select(getCurrentTest),
+    );
+    const category$ = currentTest$.pipe(
+      select(getTestCategory),
+    );
+    this.pageState = {
+      seriousFaults$: currentTest$.pipe(
+        select(getTestData),
+        map(data =>
+          this.faultSummaryProvider.getSeriousFaultsList(data, TestCategory.EUA1M2)
+          .map(fault => fault.competencyIdentifier)),
+      ),
+      dangerousFaults$: currentTest$.pipe(
+        select(getTestData),
+        map(data =>
+          this.faultSummaryProvider.getDangerousFaultsList(data, TestCategory.EUA1M2)
+          .map(fault => fault.competencyIdentifier)),
+      ),
+      drivingFaults$: currentTest$.pipe(
+        select(getTestData),
+        map(data => this.faultSummaryProvider.getDrivingFaultsList(data, TestCategory.EUA1M2)),
+      ),
+      drivingFaultCount$: currentTest$.pipe(
+        select(getTestData),
+        withLatestFrom(category$),
+        map(([testData, category]) => {
+          return this.faultCountProvider.getDrivingFaultSumCount(category as TestCategory, testData);
+        }),
+      ),
+      etaFaults$: currentTest$.pipe(
+        select(getTestData),
+        select(getETA),
+      ),
+      ecoFaults$: currentTest$.pipe(
+        select(getTestData),
+        select(getEco),
+      ),
+      testResult$: currentTest$.pipe(
+        select(getTestOutcome),
+      ),
+      conductedLanguage$: currentTest$.pipe(
+        select(getCommunicationPreference),
+        select(getConductedLanguage),
+      ),
+      candidateName$: currentTest$.pipe(
+        select(getJournalData),
+        select(getCandidate),
+        select(getUntitledCandidateName),
+      ),
+    };
+
+    const { testResult$, etaFaults$, ecoFaults$, conductedLanguage$ } = this.pageState;
+
+    this.subscription = merge(
+      testResult$.pipe(map(result => this.outcome = result)),
+      etaFaults$.pipe(
+        map((eta) => {
+          this.hasPhysicalEta = eta.physical;
+          this.hasVerbalEta = eta.verbal;
+        }),
+      ),
+      ecoFaults$.pipe(
+        map((eco) => {
+          this.adviceGivenControl = eco.adviceGivenControl;
+          this.adviceGivenPlanning = eco.adviceGivenPlanning;
+        }),
+      ),
+      conductedLanguage$.pipe(tap(value => configureI18N(value as Language, this.translate))),
+    ).subscribe();
+  }
+
+  ionViewDidEnter(): void {
+    this.store$.dispatch(new DebriefViewDidEnter());
+  }
+
+  ionViewDidLeave(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+
+    }
+  }
+
+  endDebrief(): void {
+    this.store$.dispatch(new EndDebrief());
+    if (this.outcome === 'Pass') {
+      this.navController.push(CAT_A_MOD2.PASS_FINALISATION_PAGE);
+      return;
+    }
+    this.navController.push(CAT_A_MOD2.POST_DEBRIEF_HOLDING_PAGE).then(() => {
+      const testReportPage = this.navController.getViews().find(view => view.id === CAT_A_MOD2.TEST_REPORT_PAGE);
+      if (testReportPage) {
+        this.navController.removeView(testReportPage);
+      }
+      const debriefPage = this.navController.getViews().find(view => view.id === CAT_A_MOD2.DEBRIEF_PAGE);
+      if (debriefPage) {
+        this.navController.removeView(debriefPage);
+      }
+    });
+  }
+
+}
