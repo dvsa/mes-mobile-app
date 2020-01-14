1e6a772d8279407368659129767d60cb4a5c8e0c
Tom Billington <t.billington@kainos.com>
2020-01-14 16:40:57 +0000

    Added Cat C Sub Category Top Level Reducers

--- src/modules/tests/tests-reducer-factory.ts
+++ src/modules/tests/tests-reducer-factory.ts
@@ -3,11 +3,17 @@ import { Action } from '@ngrx/store';
 import { CatBUniqueTypes } from '@dvsa/mes-test-schema/categories/B';
 import { CatBEUniqueTypes } from '@dvsa/mes-test-schema/categories/BE';
 import { CatCUniqueTypes } from '@dvsa/mes-test-schema/categories/C';
+import { CatCEUniqueTypes } from '@dvsa/mes-test-schema/categories/CE';
+import { CatC1UniqueTypes } from '@dvsa/mes-test-schema/categories/C1';
+import { CatC1EUniqueTypes } from '@dvsa/mes-test-schema/categories/C1E';
 import { TestResultSchemasUnion } from '@dvsa/mes-test-schema/categories/index';
 import { testsCatBReducer } from './tests.cat-b.reducer';
 import { testsCatBEReducer } from './tests.cat-be.reducer';
 import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
 import { testsCatCReducer } from './tests.cat-c.reducer';
+import { testsCatCeReducer } from './tests.cat-ce.reducer';
+import { testsCatC1Reducer } from './tests.cat-c1.reducer';
+import { testsCatC1eReducer } from './tests.cat-c1e.reducer';
 
 export function testsReducerFactory(
   category: TestCategory | null, action: Action, state: TestResultSchemasUnion,
@@ -19,6 +25,12 @@ export function testsReducerFactory(
       return testsCatBEReducer(action, state as Required<CatBEUniqueTypes.TestResult>);
     case TestCategory.C:
       return testsCatCReducer(action, state as Required<CatCUniqueTypes.TestResult>);
+    case TestCategory.CE:
+      return testsCatCeReducer(action, state as Required<CatCEUniqueTypes.TestResult>);
+    case TestCategory.C1:
+      return testsCatC1Reducer(action, state as Required<CatC1UniqueTypes.TestResult>);
+    case TestCategory.C1E:
+      return testsCatC1eReducer(action, state as Required<CatC1EUniqueTypes.TestResult>);
     default:
       // TODO (low priority): throw an exception here instead of using category b reducer
       return testsCatBReducer(action, state as Required<CatBUniqueTypes.TestResult>);
---
+++ src/modules/tests/tests.cat-c1.reducer.ts
@@ -0,0 +1,51 @@
+
+import { Action, combineReducers } from '@ngrx/store';
+import { CatC1UniqueTypes } from '@dvsa/mes-test-schema/categories/C1';
+import { schemaVersionReducer } from './schema-version/schema-version.reducer';
+import { categoryReducer } from './category/category.reducer';
+import { preTestDeclarationsReducer } from './pre-test-declarations/pre-test-declarations.reducer';
+import { accompanimentReducer } from './accompaniment/accompaniment.reducer';
+import { postTestDeclarationsReducer } from './post-test-declarations/post-test-declarations.reducer';
+import { testSummaryReducer } from './test-summary/test-summary.reducer';
+import { communicationPreferencesReducer } from './communication-preferences/communication-preferences.reducer';
+import { rekeyReducer } from './rekey/rekey.reducer';
+import { rekeyDateReducer } from './rekey-date/rekey-date.reducer';
+import { rekeyReasonReducer } from './rekey-reason/rekey-reason.reducer';
+import { examinerBookedReducer } from './examiner-booked/examiner-booked.reducer';
+import { examinerConductedReducer } from './examiner-conducted/examiner-conducted.reducer';
+import { examinerKeyedReducer } from './examiner-keyed/examiner-keyed.reducer';
+import { changeMarkerReducer } from './change-marker/change-marker';
+import { activityCodeReducer } from './activity-code/activity-code.reducer';
+import { journalDataCatCReducer } from './journal-data/cat-c/journal-data.cat-c.reducer';
+import { testDataCatCReducer } from './test-data/cat-c/test-data.cat-c.reducer';
+import { vehicleDetailsCatCReducer } from './vehicle-details/cat-c/vehicle-details.cat-c.reducer';
+import { passCompletionCatCReducer } from './pass-completion/cat-c/pass-completion.cat-c.reducer';
+
+export function testsCatC1Reducer(
+  action: Action, state: CatC1UniqueTypes.TestResult): Required<CatC1UniqueTypes.TestResult> {
+  return combineReducers(
+    {
+      version: schemaVersionReducer,
+      category: categoryReducer,
+      activityCode: activityCodeReducer,
+      journalData: journalDataCatCReducer,
+      preTestDeclarations: preTestDeclarationsReducer,
+      accompaniment: accompanimentReducer,
+      vehicleDetails: vehicleDetailsCatCReducer,
+      testData: testDataCatCReducer,
+      passCompletion: passCompletionCatCReducer,
+      postTestDeclarations: postTestDeclarationsReducer,
+      testSummary: testSummaryReducer,
+      communicationPreferences: communicationPreferencesReducer,
+      rekey: rekeyReducer,
+      rekeyDate: rekeyDateReducer,
+      rekeyReason: rekeyReasonReducer,
+      examinerBooked: examinerBookedReducer,
+      examinerConducted: examinerConductedReducer,
+      examinerKeyed: examinerKeyedReducer,
+      changeMarker: changeMarkerReducer,
+    })(
+      state as Required<CatC1UniqueTypes.TestResult>,
+      action,
+    );
+}
