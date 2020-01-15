4c7019054c0031991ac30644873f259b244d449e
Edward Wilson <edward.wilson@me.com>
2020-01-15 10:59:53 +0000

    Added pages for office CAT A Mod 2 MES-4439 (#1097)
    * cat-a-mod1 page constants created
    
    * Added pages for office CAT A Mod 2
    
    * Missed todo comments
    
    * Adding todo comment and fixed lint issue
    
    Co-authored-by: Jennifer <jenniferrosemyers@gmail.com>

---
+++ src/pages/office/cat-a-mod2/office.cat-a-mod2.page.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { OfficeCatAMod2Page } from './office.cat-a-mod2.page';
+import { EffectsModule } from '@ngrx/effects';
+import { OfficeAnalyticsEffects } from '../office.analytics.effects';
+import { ComponentsModule } from '../../../components/common/common-components.module';
+import { OfficeComponentsModule } from '../components/office.components.module';
+import { OfficeEffects } from '../office.effects';
+import { FaultSummaryProvider } from '../../../providers/fault-summary/fault-summary';
+import { OfficeCatAMod2ComponentsModule } from './components/office.cat-a-mod2.components.module';
+
+@NgModule({
+  declarations: [
+    OfficeCatAMod2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(OfficeCatAMod2Page),
+    EffectsModule.forFeature([
+      OfficeAnalyticsEffects,
+      OfficeEffects,
+    ]),
+    ComponentsModule,
+    OfficeCatAMod2ComponentsModule,
+    OfficeComponentsModule,
+  ],
+  providers: [
+    FaultSummaryProvider,
+  ],
+})
+export class OfficeCatAMod2PageModule { }
