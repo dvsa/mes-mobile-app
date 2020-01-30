22f5bb048daafbd4f28d6dea5c20ea1cc5dc0b4c
Tom Billington <t.billington@kainos.com>
2020-01-30 19:10:21 +0000

    fix reversing diagram effects not firing

--- src/pages/test-report/cat-be/components/reverse-diagram-modal/reverse-diagram-modal.module.ts
+++ src/pages/test-report/cat-be/components/reverse-diagram-modal/reverse-diagram-modal.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { EffectsModule } from '@ngrx/effects';
 import { ReverseDiagramCatBEPage } from './reverse-diagram-modal';
 import { ReversingDistancesProvider } from '../../../../../providers/reversing-distances/reversing-distances';
+import {
+  ReverseDiagramModalAnalyticsEffects,
+} from '../../../components/reverse-diagram-modal/reverse-diagram-modal.analytics.effects';
 
 @NgModule({
   declarations: [
@@ -9,6 +13,7 @@ import { ReversingDistancesProvider } from '../../../../../providers/reversing-d
   ],
   imports: [
     IonicPageModule.forChild(ReverseDiagramCatBEPage),
+    EffectsModule.forFeature([ReverseDiagramModalAnalyticsEffects]),
   ],
   providers: [
     ReversingDistancesProvider,
