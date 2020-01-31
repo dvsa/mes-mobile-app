07c187a4258a0489b843b76eaccd95e1e26f76c3
Tom Billington <t.billington@kainos.com>
2020-01-31 09:16:59 +0000

    Non-Pass Finalisation Analytics

--- src/pages/non-pass-finalisation/__tests__/non-pass-finalisation.analytics.effects.spec.ts
+++ src/pages/non-pass-finalisation/__tests__/non-pass-finalisation.analytics.effects.spec.ts
@@ -8,10 +8,14 @@ import {
   AnalyticsScreenNames,
   AnalyticsEventCategories,
   AnalyticsErrorTypes,
+  AnalyticsEvents,
 } from '../../../providers/analytics/analytics.model';
-import { AnalyticRecorded } from '../../../providers/analytics/analytics.actions';
+import { AnalyticRecorded, AnalyticNotRecorded } from '../../../providers/analytics/analytics.actions';
 import { StoreModel } from '../../../shared/models/store.model';
 import * as testsActions from '../../../modules/tests/tests.actions';
+import * as vehicleDetailsActions from '../../../modules/tests/vehicle-details/common/vehicle-details.actions';
+import * as testSummaryActions from '../../../modules/tests/test-summary/test-summary.actions';
+import * as commsActions from '../../../modules/tests/communication-preferences/communication-preferences.actions';
 import * as fakeJournalActions from '../../fake-journal/fake-journal.actions';
 import { testsReducer } from '../../../modules/tests/tests.reducer';
 import { PopulateCandidateDetails } from '../../../modules/tests/journal-data/common/candidate/candidate.actions';
@@ -21,16 +25,20 @@ import * as nonPassFinalisationActions from '../non-pass-finalisation.actions';
 import { candidateMock } from '../../../modules/tests/__mocks__/tests.mock';
 import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
 import { configureTestSuite } from 'ng-bullet';
+import { Language } from '../../../modules/tests/communication-preferences/communication-preferences.model';
+import { SetActivityCode } from '../../../modules/tests/activity-code/activity-code.actions';
+import { ActivityCodes } from '../../../shared/models/activity-codes';
+import { TransmissionType } from '../../../shared/models/transmission-type';
 
 describe('Non Pass Finalisation Analytics Effects', () => {
 
   let effects: NonPassFinalisationAnalyticsEffects;
-  let analyticsProviderMock;
+  let analyticsProviderMock: any;
   let actions$: any;
   let store$: Store<StoreModel>;
   const screenName = AnalyticsScreenNames.NON_PASS_FINALISATION;
-  // tslint:disable-next-line:max-line-length
-  const practiceScreenName = `${AnalyticsEventCategories.PRACTICE_MODE} - ${AnalyticsScreenNames.NON_PASS_FINALISATION}`;
+  const practiceScreenName =
+    `${AnalyticsEventCategories.PRACTICE_MODE} - ${AnalyticsScreenNames.NON_PASS_FINALISATION}`;
 
   configureTestSuite(() => {
     TestBed.configureTestingModule({
@@ -116,6 +124,167 @@ describe('Non Pass Finalisation Analytics Effects', () => {
         expect(analyticsProviderMock.logError)
           .toHaveBeenCalledWith(`${AnalyticsErrorTypes.VALIDATION_ERROR} (${practiceScreenName})`,
             'error message');
+        done();
+      });
+    });
+  });
+  describe('transmissionChanged', () => {
+    it('should call logEvent with Manual if Gearbox Category is Manual', (done) => {
+      // ARRANGE
+      store$.dispatch(new testsActions.StartTest(123, TestCategory.C));
+      store$.dispatch(new PopulateCandidateDetails(candidateMock));
+      store$.dispatch(new SetActivityCode(ActivityCodes.PASS));
+      // ACT
+      actions$.next(new vehicleDetailsActions.GearboxCategoryChanged(TransmissionType.Manual));
+      // ASSERT
+      effects.transmissionChanged$.subscribe((result) => {
+        expect(result instanceof AnalyticRecorded).toBe(true);
+        expect(analyticsProviderMock.logEvent)
+          .toHaveBeenCalledWith(
+            AnalyticsEventCategories.POST_TEST,
+            AnalyticsEvents.GEARBOX_CATEGORY_CHANGED,
+            TransmissionType.Manual,
+          );
+        done();
+      });
+    });
+    it('should call logEvent with Automatic if Gearbox Category is Automatic', (done) => {
+      // ARRANGE
+      store$.dispatch(new testsActions.StartTest(123, TestCategory.C));
+      store$.dispatch(new PopulateCandidateDetails(candidateMock));
+      store$.dispatch(new SetActivityCode(ActivityCodes.PASS));
+      // ACT
+      actions$.next(new vehicleDetailsActions.GearboxCategoryChanged(TransmissionType.Automatic));
+      // ASSERT
+      effects.transmissionChanged$.subscribe((result) => {
+        expect(result instanceof AnalyticRecorded).toBe(true);
+        expect(analyticsProviderMock.logEvent)
+          .toHaveBeenCalledWith(
+            AnalyticsEventCategories.POST_TEST,
+            AnalyticsEvents.GEARBOX_CATEGORY_CHANGED,
+            TransmissionType.Automatic,
+          );
+        done();
+      });
+    });
+    it('should call not call logEvent if there is no activity code', (done) => {
+      // ARRANGE
+      store$.dispatch(new testsActions.StartTest(123, TestCategory.C));
+      store$.dispatch(new PopulateCandidateDetails(candidateMock));
+      // ACT
+      actions$.next(new vehicleDetailsActions.GearboxCategoryChanged(TransmissionType.Manual));
+      // ASSERT
+      effects.transmissionChanged$.subscribe((result) => {
+        expect(result instanceof AnalyticNotRecorded).toBe(true);
+        expect(analyticsProviderMock.logEvent).not.toHaveBeenCalled();
+        done();
+      });
+    });
+  });
+  describe('d255Yes', () => {
+    it('should call logEvent', (done) => {
+      // ARRANGE
+      store$.dispatch(new testsActions.StartTest(123, TestCategory.C));
+      store$.dispatch(new PopulateCandidateDetails(candidateMock));
+      // ACT
+      actions$.next(new testSummaryActions.D255Yes());
+      // ASSERT
+      effects.d255Yes$.subscribe((result) => {
+        expect(result instanceof AnalyticRecorded).toBe(true);
+        expect(analyticsProviderMock.logEvent)
+          .toHaveBeenCalledWith(
+            AnalyticsEventCategories.POST_TEST,
+            AnalyticsEvents.D255,
+            'Yes',
+          );
+        done();
+      });
+    });
+  });
+  describe('d255No', () => {
+    it('should call logEvent', (done) => {
+      // ARRANGE
+      store$.dispatch(new testsActions.StartTest(123, TestCategory.C));
+      store$.dispatch(new PopulateCandidateDetails(candidateMock));
+      // ACT
+      actions$.next(new testSummaryActions.D255No());
+      // ASSERT
+      effects.d255No$.subscribe((result) => {
+        expect(result instanceof AnalyticRecorded).toBe(true);
+        expect(analyticsProviderMock.logEvent)
+          .toHaveBeenCalledWith(
+            AnalyticsEventCategories.POST_TEST,
+            AnalyticsEvents.D255,
+            'No',
+          );
+        done();
+      });
+    });
+  });
+  describe('candidateChoseToProceedWithTestInEnglish$', () => {
+    it('should call logEvent with the correct parameters', (done) => {
+      // ARRANGE
+      store$.dispatch(new testsActions.StartTest(123, TestCategory.C));
+      store$.dispatch(new PopulateCandidateDetails(candidateMock));
+      store$.dispatch(new SetActivityCode(ActivityCodes.PASS));
+      // ACT
+      actions$.next(new commsActions.CandidateChoseToProceedWithTestInEnglish(Language.ENGLISH));
+      // ASSERT
+      effects.candidateChoseToProccedWithTestInEnglish$.subscribe((result) => {
+        expect(result instanceof AnalyticRecorded).toBe(true);
+        expect(analyticsProviderMock.logEvent)
+          .toHaveBeenCalledWith(
+            AnalyticsEventCategories.POST_TEST,
+            AnalyticsEvents.LANGUAGE_CHANGED,
+            Language.ENGLISH,
+          );
+        done();
+      });
+    });
+    it('should call not call logEvent if there is no activity code', (done) => {
+      // ARRANGE
+      store$.dispatch(new testsActions.StartTest(123, TestCategory.C));
+      store$.dispatch(new PopulateCandidateDetails(candidateMock));
+      // ACT
+      actions$.next(new commsActions.CandidateChoseToProceedWithTestInEnglish(Language.ENGLISH));
+      // ASSERT
+      effects.candidateChoseToProccedWithTestInEnglish$.subscribe((result) => {
+        expect(result instanceof AnalyticNotRecorded).toBe(true);
+        expect(analyticsProviderMock.logEvent).not.toHaveBeenCalled();
+        done();
+      });
+    });
+  });
+  describe('candidateChoseToProceedWithTestInWelsh$', () => {
+    it('should call logEvent with the correct parameters', (done) => {
+      // ARRANGE
+      store$.dispatch(new testsActions.StartTest(123, TestCategory.C));
+      store$.dispatch(new PopulateCandidateDetails(candidateMock));
+      store$.dispatch(new SetActivityCode(ActivityCodes.PASS));
+      // ACT
+      actions$.next(new commsActions.CandidateChoseToProceedWithTestInWelsh(Language.CYMRAEG));
+      // ASSERT
+      effects.candidateChoseToProccedWithTestInWelsh$.subscribe((result) => {
+        expect(result instanceof AnalyticRecorded).toBe(true);
+        expect(analyticsProviderMock.logEvent)
+          .toHaveBeenCalledWith(
+            AnalyticsEventCategories.POST_TEST,
+            AnalyticsEvents.LANGUAGE_CHANGED,
+            Language.CYMRAEG,
+          );
+        done();
+      });
+    });
+    it('should call not call logEvent if there is no activity code', (done) => {
+      // ARRANGE
+      store$.dispatch(new testsActions.StartTest(123, TestCategory.C));
+      store$.dispatch(new PopulateCandidateDetails(candidateMock));
+      // ACT
+      actions$.next(new commsActions.CandidateChoseToProceedWithTestInWelsh(Language.CYMRAEG));
+      // ASSERT
+      effects.candidateChoseToProccedWithTestInWelsh$.subscribe((result) => {
+        expect(result instanceof AnalyticNotRecorded).toBe(true);
+        expect(analyticsProviderMock.logEvent).not.toHaveBeenCalled();
         done();
       });
     });
--- src/pages/non-pass-finalisation/non-pass-finalisation.analytics.effects.ts
+++ src/pages/non-pass-finalisation/non-pass-finalisation.analytics.effects.ts
@@ -4,14 +4,26 @@ import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Store, select } from '@ngrx/store';
 import { StoreModel } from '../../shared/models/store.model';
 import * as nonPassFinalisationActions from './non-pass-finalisation.actions';
+import * as vehicleDetailsActions from '../../modules/tests/vehicle-details/common/vehicle-details.actions';
+import * as testSummaryActions from '../../modules/tests/test-summary/test-summary.actions';
+import * as commsActions from '../../modules/tests/communication-preferences/communication-preferences.actions';
 import { of } from 'rxjs/observable/of';
 import { withLatestFrom } from 'rxjs/operators/withLatestFrom';
 import { getTests } from '../../modules/tests/tests.reducer';
-import { AnalyticRecorded } from '../../providers/analytics/analytics.actions';
+import { AnalyticRecorded, AnalyticNotRecorded } from '../../providers/analytics/analytics.actions';
 import { TestsModel } from '../../modules/tests/tests.model';
 import { formatAnalyticsText } from '../../shared/helpers/format-analytics-text';
-import { AnalyticsScreenNames, AnalyticsErrorTypes } from '../../providers/analytics/analytics.model';
+import {
+  AnalyticsScreenNames,
+  AnalyticsErrorTypes,
+  AnalyticsEventCategories,
+  AnalyticsEvents,
+} from '../../providers/analytics/analytics.model';
 import { switchMap, concatMap } from 'rxjs/operators';
+import { ActivityCode } from '@dvsa/mes-test-schema/categories/common';
+import { getCurrentTest } from '../../modules/tests/tests.selector';
+import { getActivityCode } from '../../modules/tests/activity-code/activity-code.reducer';
+import { Language } from '../../modules/tests/communication-preferences/communication-preferences.model';
 
 @Injectable()
 export class NonPassFinalisationAnalyticsEffects {
@@ -56,6 +68,133 @@ export class NonPassFinalisationAnalyticsEffects {
         action.errorMessage,
       );
       return of(new AnalyticRecorded());
+    }),
+  );
+
+  @Effect()
+  transmissionChanged$ = this.actions$.pipe(
+    ofType(vehicleDetailsActions.GEARBOX_CATEGORY_CHANGED),
+    concatMap(action => of(action).pipe(
+      withLatestFrom(
+        this.store$.pipe(
+          select(getTests),
+        ),
+        this.store$.pipe(
+          select(getTests),
+          select(getCurrentTest),
+          select(getActivityCode),
+        ),
+      ),
+    )),
+    concatMap(([action, tests, activityCode]:
+        [vehicleDetailsActions.GearboxCategoryChanged, TestsModel, ActivityCode]) => {
+      if (activityCode != null) {
+        this.analytics.logEvent(
+          formatAnalyticsText(AnalyticsEventCategories.POST_TEST, tests),
+          formatAnalyticsText(AnalyticsEvents.GEARBOX_CATEGORY_CHANGED, tests),
+          action.gearboxCategory,
+        );
+        return of(new AnalyticRecorded());
+      }
+      return of(new AnalyticNotRecorded());
+    }),
+  );
+
+  @Effect()
+  d255Yes$ = this.actions$.pipe(
+    ofType(testSummaryActions.D255_YES),
+    concatMap(action => of(action).pipe(
+      withLatestFrom(
+        this.store$.pipe(
+          select(getTests),
+        ),
+      ),
+    )),
+    concatMap(([action, tests]: [testSummaryActions.D255Yes, TestsModel]) => {
+      this.analytics.logEvent(
+        formatAnalyticsText(AnalyticsEventCategories.POST_TEST, tests),
+        formatAnalyticsText(AnalyticsEvents.D255, tests),
+        'Yes',
+      );
+      return of(new AnalyticRecorded());
+    }),
+  );
+
+  @Effect()
+  d255No$ = this.actions$.pipe(
+    ofType(testSummaryActions.D255_NO),
+    concatMap(action => of(action).pipe(
+      withLatestFrom(
+        this.store$.pipe(
+          select(getTests),
+        ),
+      ),
+    )),
+    concatMap(([action, tests]: [testSummaryActions.D255No, TestsModel]) => {
+      this.analytics.logEvent(
+        formatAnalyticsText(AnalyticsEventCategories.POST_TEST, tests),
+        formatAnalyticsText(AnalyticsEvents.D255, tests),
+        'No',
+      );
+      return of(new AnalyticRecorded());
+    }),
+  );
+
+  @Effect()
+  candidateChoseToProccedWithTestInEnglish$ = this.actions$.pipe(
+    ofType(commsActions.CANDIDATE_CHOSE_TO_PROCEED_WITH_TEST_IN_ENGLISH),
+    concatMap(action => of(action).pipe(
+      withLatestFrom(
+        this.store$.pipe(
+          select(getTests),
+        ),
+        this.store$.pipe(
+          select(getTests),
+          select(getCurrentTest),
+          select(getActivityCode),
+        ),
+      ),
+    )),
+    concatMap(([action, tests, activityCode]:
+      [commsActions.CandidateChoseToProceedWithTestInEnglish, TestsModel, ActivityCode]) => {
+      if (activityCode !== null) {
+        this.analytics.logEvent(
+          formatAnalyticsText(AnalyticsEventCategories.POST_TEST, tests),
+          formatAnalyticsText(AnalyticsEvents.LANGUAGE_CHANGED, tests),
+          Language.ENGLISH,
+        );
+        return of(new AnalyticRecorded());
+      }
+      return of(new AnalyticNotRecorded());
+    }),
+  );
+
+  @Effect()
+  candidateChoseToProccedWithTestInWelsh$ = this.actions$.pipe(
+    ofType(commsActions.CANDIDATE_CHOSE_TO_PROCEED_WITH_TEST_IN_WELSH),
+    concatMap(action => of(action).pipe(
+      withLatestFrom(
+        this.store$.pipe(
+          select(getTests),
+        ),
+        this.store$.pipe(
+          select(getTests),
+          select(getCurrentTest),
+          select(getActivityCode),
+        ),
+      ),
+    )),
+    concatMap(([action, tests, activityCode]:
+      [commsActions.CandidateChoseToProceedWithTestInWelsh, TestsModel, ActivityCode]) => {
+      if (activityCode !== null) {
+        this.analytics.logEvent(
+          formatAnalyticsText(AnalyticsEventCategories.POST_TEST, tests),
+          formatAnalyticsText(AnalyticsEvents.LANGUAGE_CHANGED, tests),
+          Language.CYMRAEG,
+        );
+        return of(new AnalyticRecorded());
+      }
+      return of(new AnalyticNotRecorded());
     }),
   );
 
