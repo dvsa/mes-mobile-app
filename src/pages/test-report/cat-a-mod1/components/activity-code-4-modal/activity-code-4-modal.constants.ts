9869de21ded3c259d43f7ad37da3adeafca10e21
Matt <mculliford.dvsa@icloud.com>
2020-02-03 13:43:37 +0000

    added modal reason

--- src/pages/test-report/cat-a-mod1/components/activity-code-4-modal/activity-code-4-modal.constants.ts
+++ src/pages/test-report/cat-a-mod1/components/activity-code-4-modal/activity-code-4-modal.constants.ts
@@ -1,4 +1,5 @@
 export enum ModalReason {
   SPEED_REQUIREMENTS = 'Speed requirements not met',
   EMERGENCY_STOP = 'Serious fault recorded on emergency stop',
+  EMERGENCY_STOP_DANGEROUS = 'Dangerous fault recorded on emergency stop',
 }
