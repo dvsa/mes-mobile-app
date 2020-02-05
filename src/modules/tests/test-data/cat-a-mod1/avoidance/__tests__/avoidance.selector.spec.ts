5f97dfbab97f42a6571f039d5c4c3cc8a09ca419
Edward Wilson <edward.wilson@me.com>
2020-02-05 11:39:21 +0000

    New selector to check avoidance was attempted

--- src/modules/tests/test-data/cat-a-mod1/avoidance/__tests__/avoidance.selector.spec.ts
+++ src/modules/tests/test-data/cat-a-mod1/avoidance/__tests__/avoidance.selector.spec.ts
@@ -1,5 +1,5 @@
 
-import { getAvoidance } from '../avoidance.selector';
+import { getAvoidance, getAvoidanceAttempted } from '../avoidance.selector';
 import { TestData, Avoidance } from '@dvsa/mes-test-schema/categories/AM1';
 import { CompetencyOutcome } from '../../../../../../shared/models/competency-outcome';
 
@@ -18,6 +18,47 @@ describe('emergency stop selector', () => {
       const result = getAvoidance(testData);
 
       expect(result).toBe(testData.avoidance);
+    });
+  });
+
+  describe('getAvoidanceAttempted', () => {
+    it('should return false when first and second attempted are not set', () => {
+      const testData: TestData = {
+        avoidance: {
+          speedNotMetSeriousFault: false,
+        } as Avoidance,
+      };
+
+      const result = getAvoidanceAttempted(testData.avoidance);
+
+      expect(result).toBe(false);
+    });
+
+    it('should return true when first attempt is set but second attempted is not', () => {
+      const testData: TestData = {
+        avoidance: {
+          firstAttempt: 54,
+          speedNotMetSeriousFault: false,
+        } as Avoidance,
+      };
+
+      const result = getAvoidanceAttempted(testData.avoidance);
+
+      expect(result).toBe(true);
+    });
+
+    it('should return true when first and second attempted is set', () => {
+      const testData: TestData = {
+        avoidance: {
+          firstAttempt: 45,
+          secondAttempt: 51,
+          speedNotMetSeriousFault: false,
+        } as Avoidance,
+      };
+
+      const result = getAvoidanceAttempted(testData.avoidance);
+
+      expect(result).toBe(true);
     });
   });
 });
--- src/modules/tests/test-data/cat-a-mod1/avoidance/avoidance.selector.ts
+++ src/modules/tests/test-data/cat-a-mod1/avoidance/avoidance.selector.ts
@@ -1,3 +1,6 @@
 import { TestData, Avoidance } from '@dvsa/mes-test-schema/categories/AM1';
 
 export const getAvoidance = (testData: TestData): Avoidance => testData.avoidance;
+
+export const getAvoidanceAttempted = (avoidance: Avoidance) : boolean =>
+    typeof avoidance.firstAttempt === 'number' || typeof avoidance.secondAttempt === 'number';
