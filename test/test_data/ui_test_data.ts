152c62744ad93584c4da7ca1b9fd8f988be2a79d
Jono Basford <jono.basford@bjss.com>
2020-02-07 15:37:57 +0000

    [JAB] Full cat c pass journey

--- test/test_data/ui_test_data.ts
+++ test/test_data/ui_test_data.ts
@@ -1,7 +1,6 @@
 export const UI_TEST_DATA = {
   testData: {
-    be: ['S01 - Direction indicators', 'S02 - Doors secure', 'S03 - Horn', 'T01 - Brakes',
-      'T02 - Safety factors while loading'],
+    be: ['S01 - Direction indicators', 'S02 - Doors secure', 'S03 - Horn', 'T01 - Brakes', 'T02 - Safety factors while loading'],
     c: ['S1 - All doors secure', 'S2 - Air leaks', 'S5 - Air pressure', 'T4 - Sufficient tread', 'T6 - Body safety'],
   },
 };
