25ec5252151e96cd3c562e9cb3d72fefe85a16e8
mgoldson <marlon.goldson@bjss.com>
2020-02-10 17:37:00 +0000

    mes-4503: bump to latest test schema and fix mock

--- src/providers/fault-summary/cat-a-mod1/__tests__/cat-AM1-test-data-mock.ts
+++ src/providers/fault-summary/cat-a-mod1/__tests__/cat-AM1-test-data-mock.ts
@@ -3,12 +3,14 @@ import { CompetencyOutcome } from '../../../../shared/models/competency-outcome'
 import { TestData } from '@dvsa/mes-test-schema/categories/AM1';
 
 export const catAM1TestDataStateObject: TestData = {
-  useOfStand: CompetencyOutcome.DF,
-  manualHandling: CompetencyOutcome.DF,
-  slalom: CompetencyOutcome.D,
-  slowControl: CompetencyOutcome.S,
-  uTurn: CompetencyOutcome.D,
-  controlledStop: CompetencyOutcome.S,
+  singleFaultCompetencies: {
+    useOfStand: CompetencyOutcome.D,
+    manualHandling: CompetencyOutcome.D,
+    slalom: CompetencyOutcome.D,
+    slowControl: CompetencyOutcome.S,
+    uTurn: CompetencyOutcome.D,
+    controlledStop: CompetencyOutcome.S,
+  },
   emergencyStop: {
     outcome: CompetencyOutcome.D,
   },
