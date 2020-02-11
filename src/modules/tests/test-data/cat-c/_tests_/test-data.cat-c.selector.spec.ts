c3fded548e7348e51e12f8d623466327a5b5a375
mgoldson <marlon.goldson@bjss.com>
2020-02-11 10:11:57 +0000

    mes-4503: Remove tests as not applicable

--- src/modules/tests/test-data/cat-c/_tests_/test-data.cat-c.selector.spec.ts
+++ src/modules/tests/test-data/cat-c/_tests_/test-data.cat-c.selector.spec.ts
@@ -4,7 +4,6 @@ import {
   hasDangerousFault,
   getETAFaultText,
   getEcoFaultText,
-  getShowMeQuestionOptions,
 } from '../../common/test-data.selector';
 import {
   getDrivingFaultCount,
@@ -16,9 +15,6 @@ import {
 } from '../test-data.cat-c.selector';
 import { Competencies } from '../../test-data.constants';
 import { CompetencyOutcome } from '../../../../../shared/models/competency-outcome';
-import { OutcomeBehaviourMapProvider } from '../../../../../providers/outcome-behaviour-map/outcome-behaviour-map';
-import { behaviourMap } from '../../../../../pages/office/office-behaviour-map';
-import { VehicleChecksQuestion } from '../../../../../providers/question/vehicle-checks-question.model';
 
 describe('TestDataSelectors', () => {
   const state: CatCUniqueTypes.TestData = {
@@ -67,41 +63,6 @@ describe('TestDataSelectors', () => {
       ],
     },
   };
-
-  describe('getShowMeQuestionOptions', () => {
-    const outcomeBehaviourMapProvider = new OutcomeBehaviourMapProvider();
-    outcomeBehaviourMapProvider.setBehaviourMap(behaviourMap);
-
-    const showMeQuestions: VehicleChecksQuestion[] = [
-      {
-        code: 'S1',
-        description: 'S1 Desc',
-        shortName: 'S1 short',
-      },
-      {
-        code: 'S2',
-        description: 'S2 Desc',
-        shortName: 'S2 short',
-      },
-      {
-        code: 'N/A',
-        description: 'Not applicable',
-        shortName: 'Not applicable',
-      },
-    ];
-    it('should return the list of questions without N/A if outcome field does not have showNotApplicable set', () => {
-      const result = getShowMeQuestionOptions(showMeQuestions, '1', outcomeBehaviourMapProvider);
-      expect(result.length).toBe(2);
-      expect(result[0].code).toBe('S1');
-      expect(result[1].code).toBe('S2');
-    });
-    it('should return extra question if outcome showNotApplicable set', () => {
-      const result = getShowMeQuestionOptions(showMeQuestions, '4', outcomeBehaviourMapProvider);
-      expect(result.length).toBe(3);
-      expect(result[2].code).toBe('N/A');
-    });
-
-  });
 
   describe('getDrivingFaultCount', () => {
     it('should return the driving fault count', () => {
