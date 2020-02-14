fb4d09fa6b4cfdae5e50c3c61c8f582dd57cee7e
Tom <thomascrawley99@gmail.com>
2020-02-14 14:39:43 +0000

    VehicleDetails provider implemented, refactored manoeuvre spec to resolve an issue where it wasn't performing the correct check

---
+++ src/providers/vehicle-details-by-category/vehicle-details-by-category.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { CategoryCode } from '@dvsa/mes-test-schema/categories/common';
+import { getVehicleDetails as getVehicleDetailsC }
+  from '../../modules/tests/vehicle-details/cat-c/vehicle-details.cat-c.reducer';
+import { getVehicleDetails as getVehicleDetailsD }
+  from '../../modules/tests/vehicle-details/cat-d/vehicle-details.cat-d.reducer';
+import { getVehicleDetails as getVehicleDetailsBE }
+  from '../../modules/tests/vehicle-details/cat-be/vehicle-details.cat-be.reducer';
+import { getVehicleWidth as getVehicleWidthBE, getVehicleLength as getVehicleLengthBE }
+  from '../../modules/tests/vehicle-details/cat-be/vehicle-details.cat-be.selector';
+import { getVehicleWidth as getVehicleWidthC, getVehicleLength as getVehicleLengthC }
+  from '../../modules/tests/vehicle-details/cat-c/vehicle-details.cat-c.selector';
+import { getVehicleWidth as getVehicleWidthD, getVehicleLength as getVehicleLengthD }
+  from '../../modules/tests/vehicle-details/cat-d/vehicle-details.cat-d.selector';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
+
+export interface CategorySpecificVehicleDetails {
+  vehicleDetails: any;
+  vehicleWidth: any;
+  vehicleLength: any;
+}
+
+@Injectable()
+export class VehicleDetailsByCategoryProvider {
+
+  static getVehicleDetailsByCategoryCodeErrMsg: string = 'Error getting test category vehicle details';
+
+  public getVehicleDetailsByCategoryCode(category: CategoryCode): CategorySpecificVehicleDetails {
+    switch (category) {
+      case TestCategory.BE:
+        return {
+          vehicleDetails: getVehicleDetailsBE,
+          vehicleWidth: getVehicleWidthBE,
+          vehicleLength: getVehicleLengthBE,
+        };
+      case TestCategory.C:
+      case TestCategory.C1:
+      case TestCategory.CE:
+      case TestCategory.C1E:
+        return {
+          vehicleDetails: getVehicleDetailsC,
+          vehicleWidth: getVehicleWidthC,
+          vehicleLength: getVehicleLengthC,
+        };
+      case TestCategory.D:
+      case TestCategory.D1:
+      case TestCategory.DE:
+      case TestCategory.D1E:
+        return {
+          vehicleDetails: getVehicleDetailsD,
+          vehicleWidth: getVehicleWidthD,
+          vehicleLength: getVehicleLengthD,
+        };
+      default:
+        throw new Error(VehicleDetailsByCategoryProvider.getVehicleDetailsByCategoryCodeErrMsg);
+    }
+  }
+}
