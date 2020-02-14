25e197749b3b1f394fbf1564cfe930af5cb77f18
Andy Perry <60922656+AndyPerryKainos@users.noreply.github.com>
2020-02-14 15:45:33 +0000

    fix(EndTest): add missing category cases to allow test termination (#1301)

--- src/components/common/end-test-link/end-test-link.ts
+++ src/components/common/end-test-link/end-test-link.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ModalController, Modal, NavController } from 'ionic-angular';
-import { CAT_BE, CAT_B, CAT_C } from '../../../pages/page-names.constants';
+import { CAT_BE, CAT_B, CAT_C, CAT_D, CAT_A_MOD1, CAT_A_MOD2 } from '../../../pages/page-names.constants';
 import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
 
 @Component({
@@ -34,14 +34,29 @@ export class EndTestLinkComponent {
   onTerminate = () => {
     this.terminateTestModal.dismiss();
     switch (this.category) {
-      case TestCategory.BE:
-        this.navController.push(CAT_BE.DEBRIEF_PAGE);
-        break;
       case TestCategory.B:
         this.navController.push(CAT_B.DEBRIEF_PAGE);
         break;
+      case TestCategory.BE:
+        this.navController.push(CAT_BE.DEBRIEF_PAGE);
+        break;
       case TestCategory.C:
         this.navController.push(CAT_C.DEBRIEF_PAGE);
+        break;
+      case TestCategory.D:
+        this.navController.push(CAT_D.DEBRIEF_PAGE);
+        break;
+      case TestCategory.EUA1M1:
+      case TestCategory.EUA2M1:
+      case TestCategory.EUAM1:
+      case TestCategory.EUAMM1:
+        this.navController.push(CAT_A_MOD1.DEBRIEF_PAGE);
+        break;
+      case TestCategory.EUA1M2:
+      case TestCategory.EUA2M2:
+      case TestCategory.EUAM2:
+      case TestCategory.EUAMM2:
+        this.navController.push(CAT_A_MOD2.DEBRIEF_PAGE);
         break;
     }
   }
