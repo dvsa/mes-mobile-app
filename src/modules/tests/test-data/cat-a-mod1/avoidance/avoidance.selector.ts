a43b77566c1662e6f150c898ac0d26fd67b247f7
Edward Wilson <edward.wilson@me.com>
2020-02-19 11:24:17 +0000

    Fixed the code checks that broke when the data changed from string to numbers

--- src/modules/tests/test-data/cat-a-mod1/avoidance/avoidance.selector.ts
+++ src/modules/tests/test-data/cat-a-mod1/avoidance/avoidance.selector.ts
@@ -3,6 +3,6 @@ import { TestData, Avoidance } from '@dvsa/mes-test-schema/categories/AM1';
 export const getAvoidance = (testData: TestData): Avoidance => testData.avoidance;
 
 export const getAvoidanceAttempted = (avoidance: Avoidance) : boolean => {
-  if (avoidance.firstAttempt || avoidance.secondAttempt) return true;
+  if (avoidance.firstAttempt !== undefined || avoidance.secondAttempt !== undefined) return true;
   return false;
 };
