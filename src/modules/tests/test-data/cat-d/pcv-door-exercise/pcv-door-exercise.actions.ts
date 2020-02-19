5119ceab8095616171758490a8740e4c5700385c
anthonyp@kainos.com <anthonyp@kainos.com@Portavogie.local>
2020-02-19 09:30:25 +0000

    wip

--- src/modules/tests/test-data/cat-d/pcv-door-exercise/pcv-door-exercise.actions.ts
+++ src/modules/tests/test-data/cat-d/pcv-door-exercise/pcv-door-exercise.actions.ts
@@ -1,6 +1,5 @@
 import { Action } from '@ngrx/store';
 
-// export const TOGGLE_PCV_DOOR_EXERCISE = '[PcvDoorExercise] Toggle Controlled Stop';
 export const PCV_DOOR_EXERCISE_ADD_DRIVING_FAULT = '[PcvDoorExercise] Add Driving Fault';
 export const PCV_DOOR_EXERCISE_ADD_SERIOUS_FAULT = '[PcvDoorExercise] Add Serious Fault';
 export const PCV_DOOR_EXERCISE_ADD_DANGEROUS_FAULT = '[PcvDoorExercise] Add Dangerous Fault';
@@ -9,12 +8,7 @@ export const PCV_DOOR_EXERCISE_REMOVE_DRIVING_FAULT = '[PcvDoorExercise] Remove 
 export const PCV_DOOR_EXERCISE_REMOVE_SERIOUS_FAULT = '[PcvDoorExercise] Remove Serious Fault';
 export const PCV_DOOR_EXERCISE_REMOVE_DANGEROUS_FAULT = '[PcvDoorExercise] Remove Dangerous Fault';
 
-// export const PCV_DOOR_EXERCISE_REMOVE_FAULT = '[PcvDoorExercise] Remove Fault';
 export const ADD_PCV_DOOR_EXERCISE_COMMENT = '[PcvDoorExercise] Add Comment';
-
-// export class TogglePcvDoorExercise implements Action {
-//   readonly type = TOGGLE_PCV_DOOR_EXERCISE;
-// }
 
 export class PcvDoorExerciseAddDrivingFault implements Action {
   readonly type = PCV_DOOR_EXERCISE_ADD_DRIVING_FAULT;
