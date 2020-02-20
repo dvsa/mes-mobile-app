aeb7c6dc75a102ddc60972233a5623b664ab5c4f
Edward Wilson <edward.wilson@me.com>
2020-02-20 14:14:59 +0000

    PR suggestion

--- src/modules/tests/test-data/cat-a-mod1/avoidance/avoidance.selector.ts
+++ src/modules/tests/test-data/cat-a-mod1/avoidance/avoidance.selector.ts
@@ -2,7 +2,5 @@ import { TestData, Avoidance } from '@dvsa/mes-test-schema/categories/AM1';
 
 export const getAvoidance = (testData: TestData): Avoidance => testData.avoidance;
 
-export const getAvoidanceAttempted = (avoidance: Avoidance) : boolean => {
-  if (avoidance.firstAttempt !== undefined || avoidance.secondAttempt !== undefined) return true;
-  return false;
-};
+export const getAvoidanceAttempted = (avoidance: Avoidance) : boolean =>
+  avoidance.firstAttempt !== undefined || avoidance.secondAttempt !== undefined;
