f28403f3108c792afe00e71f90d57c2fec3a136f
Andy Perry <60922656+AndyPerryKainos@users.noreply.github.com>
2020-02-24 16:07:20 +0000

    chore: bump version of mes-test-schema dependency (#1329)

--- src/modules/tests/test-data/cat-a-mod2/test-data.cat-a-mod2.reducer.ts
+++ src/modules/tests/test-data/cat-a-mod2/test-data.cat-a-mod2.reducer.ts
@@ -2,6 +2,7 @@ import { TestData } from '@dvsa/mes-test-schema/categories/AM2';
 import { createFeatureSelector, combineReducers, Action } from '@ngrx/store';
 import { seriousFaultsReducer } from '../common/serious-faults/serious-faults.reducer';
 import { safetyAndBalanceCatAMod2Reducer } from './safety-and-balance/safety-and-balance.cat-a-mod2.reducer';
+import { testRequirementsCatAMod2Reducer } from './test-requirements/test-requirements.cat-a-mod-2.reducer';
 import { drivingFaultsReducer } from '../common/driving-faults/driving-faults.reducer';
 import { dangerousFaultsReducer } from '../common/dangerous-faults/dangerous-faults.reducer';
 import { ecoReducer } from '../common/eco/eco.reducer';
@@ -26,6 +27,7 @@ export function testDataCatAMod2Reducer(
   action: Action,
 ): Required<TestData> {
   return combineReducers({
+    testRequirements: testRequirementsCatAMod2Reducer,
     drivingFaults: drivingFaultsReducer,
     dangerousFaults: dangerousFaultsReducer,
     seriousFaults: seriousFaultsReducer,
---
+++ src/modules/tests/test-data/cat-a-mod2/test-requirements/test-requirements.cat-a-mod-2.reducer.ts
@@ -0,0 +1,9 @@
+import { TestRequirements } from '@dvsa/mes-test-schema/categories/AM2';
+
+export const initialState: TestRequirements = {};
+
+export function testRequirementsCatAMod2Reducer(
+  state = initialState,
+): TestRequirements {
+  return state;
+}
