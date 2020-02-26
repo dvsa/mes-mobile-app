49deffbe8f48370e068efcff29e138778a72caae
Tom Billington <t.billington@kainos.com>
2020-02-26 11:22:56 +0000

    Updated Questions

--- src/shared/constants/show-me-questions/show-me-questions.vocational.constants.ts
+++ src/shared/constants/show-me-questions/show-me-questions.vocational.constants.ts
@@ -3,85 +3,85 @@ import { VehicleChecksQuestion } from '../../../providers/question/vehicle-check
 
 export default [
   {
-    code: 'S1',
+    code: 'CD1',
     description: 'Show me how you would check that all doors including cargo doors are secure.',
     shortName: 'All doors secure',
   },
   {
-    code: 'S2',
+    code: 'CD2',
     description: 'Show me how you would check for air leaks on this vehicle.',
     shortName: 'Air leaks',
 
   },
   {
-    code: 'S4',
+    code: 'CD6',
     description: 'Show me how you would check the condition of the mudguards on this vehicle.',
     shortName: 'Mudguards condition',
 
   },
   {
-    code: 'S5',
+    code: 'CD10',
     description: 'Show me how you would check for the correct air pressure on this vehicle.',
     shortName: 'Air pressure',
 
   },
   {
-    code: 'S6',
+    code: 'CD12',
     description: 'Show me how you would check the wheel nuts are secure on this vehicle.',
     shortName: 'Wheel nuts',
   },
   {
-    code: 'S7',
+    code: 'CD13',
     description: 'Show me how you would check the operation (specify horn, warning device for reversing) of the audible warning devices on this vehicle.',
     shortName: 'Check audible warnings',
   },
   {
-    code: 'S8',
+    code: 'CD15',
     description: 'Show me how you would check that the brake lights are working on this vehicle (I can assist you, if you need to switch the ignition on, please don’t start the engine).',
     shortName: 'Brake lights',
   },
   {
-    code: 'S9',
+    code: 'CD18',
     description: 'Show me how you would replace the tachograph disc on this vehicle.',
     shortName: 'Change tacho',
   },
   {
-    code: 'S10',
+    code: 'CD20',
     description: 'Show me/ explain how you would check that the power assisted steering is working.',
     shortName: 'Power assisted steering',
   },
   {
-    code: 'S11',
+    code: 'CD21',
     description: 'Show me how you would check that the direction indicators are working.',
     shortName: 'Direction indicators',
   },
   {
-    code: 'S13',
+    code: 'CD23',
     description: 'Show me what instrument checks you would make before and after starting the engine on this vehicle.',
     shortName: 'Instrument checks',
   },
   {
-    code: 'S14',
+    code: 'CD24',
     description: 'Show me where the first aid equipment is kept on this vehicle.',
     shortName: 'First aid equipment',
   },
   {
-    code: 'S15',
+    code: 'CD25',
     description: 'Show me how you would clean the windscreen using the windscreen washer and wipers.',
     shortName: 'Clean windscreen',
   },
   {
-    code: 'S16',
+    code: 'CD26',
     description: 'Show me how you would set the windscreen demister to clear the windows effectively.',
     shortName: 'Demist windscreen',
   },
   {
-    code: 'S17',
+    code: 'CD27',
     description: 'Show me how you would switch on the rear fog light(s) and explain when you would use it/them. (No need to exit vehicle)',
     shortName: 'Rear fog lights',
   },
   {
-    code: 'S18',
+    code: 'CD28',
     description: 'Show me how you switch your headlight from dipped to main beam',
     shortName: 'Dipped to main beam',
   },
