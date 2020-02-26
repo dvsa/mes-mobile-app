5d23e9acb1e6a5df035074e603756fcd87febe59
Matt <mculliford.dvsa@icloud.com>
2020-02-26 13:19:48 +0000

    update upper case directive

---
+++ src/directives/__tests__/input-restriction-uppercasealphanum.directive.spec.ts
@@ -0,0 +1,33 @@
+import { Component, DebugElement } from '@angular/core';
+import { InputRestrictionUppercaseAlphanumDirective } from '../input-restriction-uppercasealphanum.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `<input type="text" uppercaseAlphanumOnly>`,
+})
+class TestAlphaNumComponent {
+}
+
+fdescribe('Directive: InputRestrictionUppercaseAlphanumDirective', () => {
+  let fixture: ComponentFixture<TestAlphaNumComponent>;
+  let inputElement: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TestAlphaNumComponent,
+        InputRestrictionUppercaseAlphanumDirective,
+      ],
+    });
+    fixture = TestBed.createComponent(TestAlphaNumComponent);
+  });
+  it('should remove spaces, non alpha numeric chars and upper case input', () => {
+    fixture.detectChanges();
+    inputElement = fixture.debugElement.query(By.css('input'));
+
+    inputElement.nativeElement.value = 'aBc!@ £$%^&123';
+    inputElement.nativeElement.dispatchEvent(new Event('input'));
+    expect(inputElement.nativeElement.value).toEqual('ABC123')
+  });
+});
--- src/directives/input-restriction-uppercasealphanum.directive.ts
+++ src/directives/input-restriction-uppercasealphanum.directive.ts
@@ -1,48 +1,15 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
-import { includes } from 'lodash';
 
 @Directive({
   selector: '[uppercaseAlphanumOnly]',
 })
 export class InputRestrictionUppercaseAlphanumDirective {
-  constructor(public el: ElementRef) {}
+  constructor(public ref: ElementRef) { }
 
-  // Allow usage of control keys aswell as numbers, useful for the browser
-  controlKeys = ['ArrowRight', 'ArrowLeft', 'Backspace'];
-
-  @HostListener('keydown', ['$event'])
-  onKeyDown(e: KeyboardEvent) {
-    let { key } = e;
-
-    if (includes(this.controlKeys, key)) {
-      return;
-    }
-
-    // Transform lowercase alphas to uppercase, dispatch a replacement event
-    if (this.isLowerCase(key)) {
-      e.preventDefault();
-      key = key.toUpperCase();
-      e.target['value'] += key;
-      const evt = document.createEvent('HTMLEvents');
-      evt.initEvent('input', false, true);
-      e.target.dispatchEvent(evt);
-      return;
-    }
-
-    if (!this.isUppercaseAlphanum(key)) {
-      e.preventDefault();
-    }
-  }
-
-  isLowerCase(key: string): boolean {
-    return key >= 'a' && key <= 'z';
-  }
-
-  isNumeric(key: string): boolean {
-    return key >= '0' && key <= '9';
-  }
-
-  isUppercaseAlphanum(key: string): boolean {
-    return (key >= '0' && key <= '9') || (key >= 'A' && key <= 'Z');
+  @HostListener('input', ['$event']) onInput(event) {
+    this.ref.nativeElement.value = event.target.value
+      .replace(/[^\w\s]/gi, '')
+      .trim()
+      .toUpperCase();
   }
 }
