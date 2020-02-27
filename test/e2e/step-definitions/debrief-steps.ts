cdf4ceb29191b5e17b767dd4a501f796b5741535
Katie Cookson <katie.cookson@bjss.com>
2020-02-27 16:23:05 +0000

    adding DebriefPage.ts

--- test/e2e/step-definitions/debrief-steps.ts
+++ test/e2e/step-definitions/debrief-steps.ts
@@ -1,8 +1,5 @@
 import { Then, When, Before } from 'cucumber';
-import { getElement } from './generic-steps';
-import { by } from 'protractor';
-import { textFieldInputViaNativeMode } from '../../helpers/helpers';
-import TempPage from '../pages/tempPage';
+import DebriefPage from '../pages/debriefPage';
 
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
@@ -32,88 +29,56 @@ Before({ tags: '@catc1e' }, () => {
 });
 
 When('I end the debrief', () => {
-  const endDebriefButton = getElement(by.xpath('//button[span[h3[text()="End debrief"]]]'));
-  TempPage.clickElement(endDebriefButton);
+  DebriefPage.clickEndDebriefButton();
 });
 
 When('I end the welsh debrief', () => {
-  const endDebriefButton = getElement(by.xpath('//button[span[h3[text()="Diwedd ôl-drafodaeth"]]]'));
-  TempPage.clickElement(endDebriefButton);
+  DebriefPage.clickEndDebriefButtonWelsh();
 });
 
 When('I complete the pass details', () => {
-  completePassdetails();
-  selectTransmission('manual');
-  continuePassFinalisation();
+  DebriefPage.completePassdetails();
+  DebriefPage.selectTransmission('manual');
+  DebriefPage.continuePassFinalisation(this.testCategory);
 });
 
 When('I select the code 78 option', () => {
-  code78onLicence(false);
+  DebriefPage.clickCode78NotReceived();
 });
 
 When('I complete the pass details with an automatic transmission', () => {
-  completePassdetails();
-  selectTransmission('automatic');
-  continuePassFinalisation();
+  DebriefPage.completePassdetails();
+  DebriefPage.selectTransmission('automatic');
+  DebriefPage.continuePassFinalisation(this.testCategory);
 });
 
 When('I complete the fail details', () => {
   if (this.testCategory === 'c' || this.testCategory === 'c1') {
-    selectTransmission('manual');
+    DebriefPage.selectTransmission('manual');
   }
-  const d255NoRadio = getElement(by.id('d255-no'));
-  TempPage.clickElement(d255NoRadio);
-  const debriefWitnessedRadio = getElement(by.id('debrief-witnessed-yes'));
-  TempPage.clickElement(debriefWitnessedRadio);
-  const submitButton = getElement(by.id('continue-button'));
-  TempPage.clickElement(submitButton);
+  DebriefPage.clickD255No();
+  DebriefPage.clickDebriefWitnessedYes();
+  // TODO: There seem to be 2 continue buttons...are they on different pages?
+  DebriefPage.clickContinueButton2();
+  // const submitButton = TempPage.getElement(by.id('continue-button'));
+  // TempPage.clickElement(submitButton);
 });
 
 When('I try to confirm the pass certificate details', () => {
-  continuePassFinalisation();
+  DebriefPage.continuePassFinalisation(this.testCategory);
 });
 
 Then('I should see the Debrief page with outcome {string}', (outcome) => {
-  const testOutcome = getElement(by.xpath(
-    `//div[contains(@class, "debrief-cat-${this.testCategory}-page")]//div[@id = "test-outcome-background"]/div/h1`));
+  const testOutcome = DebriefPage.getTestOutcome(this.testCategory);
   return expect(testOutcome.getText()).to.eventually.equal(outcome);
 });
 
 Then('I see a {string} fault for {string}', (faultSeverity, faultDescription) => {
-  const faultElement = getElement(by.xpath(`//ion-card[@id = '${faultSeverity}-fault']
-  //div[text() = '${faultDescription}']`));
+  const faultElement = DebriefPage.getFaultElement(faultSeverity, faultDescription);
   return expect(faultElement.isPresent()).to.eventually.be.true;
 });
 
 Then('I should see the application reference {string}', (applicationRef) => {
-  const applicationRefField = getElement(by.xpath('//ion-row[@id="application-reference-card"]/ion-col/span'));
+  const applicationRefField = DebriefPage.getApplicationRefField();
   return expect(applicationRefField.getText()).to.eventually.equal(applicationRef);
 });
-
-const continuePassFinalisation = () => {
-  const continueButton = getElement(by.xpath(
-  `//div[contains(@class, "pass-finalisation-cat-${this.testCategory}-page")]//button[span[h3[text() = "Continue"]]]`));
-  TempPage.clickElement(continueButton);
-};
-
-const completePassdetails = () => {
-  textFieldInputViaNativeMode('//XCUIElementTypeOther[XCUIElementTypeOther[@name="Pass certificate number"]]/'
-  + 'following-sibling::XCUIElementTypeOther[1]/XCUIElementTypeTextField', 'A123456X');
-  const licenceRecievedRadio = getElement(by.id('license-received'));
-  TempPage.clickElement(licenceRecievedRadio);
-  const d255YesRadio = getElement(by.id('d255-yes'));
-  TempPage.clickElement(d255YesRadio);
-  const debreifWitnessedRadio = getElement(by.id('debrief-witnessed-yes'));
-  TempPage.clickElement(debreifWitnessedRadio);
-};
-
-const code78onLicence = (result: boolean) => {
-  const radio = result ? 'code-78-received' : 'code-78-not-received';
-  const code78Radio = getElement(by.id(`${radio}`));
-  TempPage.clickElement(code78Radio);
-};
-
-const selectTransmission = (transmissionType: string) => {
-  const transmissionRadio = getElement(by.id(`transmission-${transmissionType}`));
-  TempPage.clickElement(transmissionRadio);
-};
--- test/e2e/step-definitions/journal-steps.ts
+++ test/e2e/step-definitions/journal-steps.ts
@@ -1,5 +1,5 @@
 import { by, element } from 'protractor';
-import { getElement, onJournalPageAs } from './generic-steps';
+import { onJournalPageAs } from './generic-steps';
 import TempPage from '../pages/tempPage';
 
 const {
@@ -16,7 +16,7 @@ Given('I am on the journal page as {string}', (username) => {
   onJournalPageAs(username);
 
   // If the journal page is loaded we should have a refresh button
-  const refreshButton = getElement(by.xpath('//button/span/span/span[text() = "Refresh"]'));
+  const refreshButton = TempPage.getElement(by.xpath('//button/span/span/span[text() = "Refresh"]'));
   return expect(refreshButton.isPresent()).to.eventually.be.true;
 });
 
@@ -30,7 +30,7 @@ When('I check candidate details for {string}', (candidateName) => {
 });
 
 When('I start the test for {string}', (candidateName) => {
-  const buttonElement = getElement(by.xpath(`//button/span/h3[text()[normalize-space(.) = "Start test"]]
+  const buttonElement = TempPage.getElement(by.xpath(`//button/span/h3[text()[normalize-space(.) = "Start test"]]
     [ancestor::ion-row/ion-col/ion-grid/ion-row/ion-col/candidate-link/div/button/span/
     h3[text() = "${candidateName}"]]`));
   TempPage.clickElement(buttonElement);
@@ -53,49 +53,49 @@ When('I start the test for {string}', (candidateName) => {
 });
 
 When('I rekey a test for {string}', (candidateName) => {
-  const buttonElement = getElement(by.xpath(`//button/span/h3[text()[normalize-space(.) = "Rekey"]]
+  const buttonElement = TempPage.getElement(by.xpath(`//button/span/h3[text()[normalize-space(.) = "Rekey"]]
     [ancestor::ion-row/ion-col/ion-grid/ion-row/ion-col/candidate-link/div/button/span/
     h3[text() = "${candidateName}"]]`));
   TempPage.clickElement(buttonElement);
 });
 
 When('I navigate to next day', () => {
-  const nextDayButtonElement = getElement(by.id('next-day-container'));
+  const nextDayButtonElement = TempPage.getElement(by.id('next-day-container'));
   return TempPage.clickElement(nextDayButtonElement);
 });
 
 When('I navigate to previous day', () => {
-  const previousDayButtonElement = getElement(by.id('previous-day-container'));
+  const previousDayButtonElement = TempPage.getElement(by.id('previous-day-container'));
   return TempPage.clickElement(previousDayButtonElement);
 });
 
 Then('I have a special needs slot for {string}', (candidateName) => {
-  const exclamationIndicator = getElement(by.xpath(`//indicators/div/img[@class = "exclamation-indicator"]
+  const exclamationIndicator = TempPage.getElement(by.xpath(`//indicators/div/img[@class = "exclamation-indicator"]
     [ancestor::ion-row/ion-col/ion-grid/ion-row/ion-col/candidate-link/div/button/span/
     h3[text() = "${candidateName}"]]`));
   return expect(exclamationIndicator.isPresent()).to.eventually.be.true;
 });
 
 Then('I have a welsh slot for {string}', (candidateName) => {
-  const exclamationIndicator = getElement(by.xpath(`//ion-grid/ion-row/ion-col/language/
+  const exclamationIndicator = TempPage.getElement(by.xpath(`//ion-grid/ion-row/ion-col/language/
   div[@class = "welsh-language-indicator"][ancestor::ion-grid/ion-row/ion-col/ion-grid/ion-row/ion-col/candidate-link
     /div/button/span/h3[text() = "${candidateName}"]]`));
   return expect(exclamationIndicator.isPresent()).to.eventually.be.true;
 });
 
 When('I refresh the journal', () => {
-  const refreshButton = getElement(by.xpath('//button/span/span/span[text() = "Refresh"]'));
+  const refreshButton = TempPage.getElement(by.xpath('//button/span/span/span[text() = "Refresh"]'));
   return TempPage.clickElement(refreshButton);
 });
 
 Then('I have a non-test slot for {string} with code {string} at {string}', (description, code, time) => {
-  const slotLocator = getElement(by.xpath(`//ion-row[ion-col/div/time/div/h2[text() = '${time}']]
+  const slotLocator = TempPage.getElement(by.xpath(`//ion-row[ion-col/div/time/div/h2[text() = '${time}']]
   [ion-col/h3[normalize-space(text()) = '${description}']][ion-col[h2[text() = '${code}']]]`));
   return expect(slotLocator.isPresent()).to.eventually.be.true;
 });
 
 Then('the test result for {string} is {string}', (candidateName, testResult) => {
-  const testResultElement = getElement(by.xpath(`//test-outcome//span[@class='outcome']/h2
+  const testResultElement = TempPage.getElement(by.xpath(`//test-outcome//span[@class='outcome']/h2
     [ancestor::ion-row/ion-col/ion-grid/ion-row/ion-col/candidate-link/div/button/span/
     h3[text() = "${candidateName}"]]`));
 
@@ -103,32 +103,32 @@ Then('the test result for {string} is {string}', (candidateName, testResult) => 
 });
 
 Then('I should have a category {string} test for {string}', (category, candidateName) => {
-  const testCategory = getElement(by.xpath(`//test-category/h2[ancestor::ion-row/ion-col/ion-grid/ion-row/
+  const testCategory = TempPage.getElement(by.xpath(`//test-category/h2[ancestor::ion-row/ion-col/ion-grid/ion-row/
     ion-col/candidate-link/div/button/span/h3[text() = "${candidateName}"]]`));
   return expect(testCategory.getText()).to.eventually.equal(category);
 });
 
 Then('The vehicle for {string} has length {string}, width {string}, height {string} and seats {string}',
 (candidateName, length, width, height, seats) => {
-  const lengthValue = getElement(
+  const lengthValue = TempPage.getElement(
     by.xpath(`//vehicle-details/div/span/span[text()= 'L: ']/following-sibling::span
     [ancestor::ion-grid/ion-row/ion-col/ion-grid/ion-row/ion-col/candidate-link/div/button/span/h3
       [text() = "${candidateName}"]]`));
   expect(lengthValue.getText()).to.eventually.equal(length);
 
-  const widthValue = getElement(
+  const widthValue = TempPage.getElement(
     by.xpath(`//vehicle-details/div/span/span[text()= 'W: ']/following-sibling::span
     [ancestor::ion-grid/ion-row/ion-col/ion-grid/ion-row/ion-col/candidate-link/div/button/span/h3
       [text() = "${candidateName}"]]`));
   expect(widthValue.getText()).to.eventually.equal(width);
 
-  const heightValue = getElement(
+  const heightValue = TempPage.getElement(
     by.xpath(`//vehicle-details/div/span/span[text()= 'H: ']/following-sibling::span
     [ancestor::ion-grid/ion-row/ion-col/ion-grid/ion-row/ion-col/candidate-link/div/button/span/h3
       [text() = "${candidateName}"]]`));
   expect(heightValue.getText()).to.eventually.equal(height);
 
-  const seatValue = getElement(
+  const seatValue = TempPage.getElement(
     by.xpath(`//vehicle-details/div/span/span[text() = 'Seats: ']/following-sibling::span
     [ancestor::ion-grid/ion-row/ion-col/ion-grid/ion-row/ion-col/candidate-link/div/button/span/h3
       [text() = "${candidateName}"]]`));
@@ -136,14 +136,14 @@ Then('The vehicle for {string} has length {string}, width {string}, height {stri
 });
 
 Then('I continue the write up for {string}', (candidateName) => {
-  const continueWriteUp = getElement(by.xpath(`//button/span/h3[text()[normalize-space(.) = "Write-up"]]
+  const continueWriteUp = TempPage.getElement(by.xpath(`//button/span/h3[text()[normalize-space(.) = "Write-up"]]
     [ancestor::ion-row/ion-col/ion-grid/ion-row/ion-col/candidate-link/div/button/span/
     h3[text() = "${candidateName}"]]`));
   TempPage.clickElement(continueWriteUp);
 });
 
 const viewCandidateDetails = (candidateName) => {
-  const buttonElement = getElement(by.xpath(`//h3[text()[normalize-space(.) = "${candidateName}"]]`));
+  const buttonElement = TempPage.getElement(by.xpath(`//h3[text()[normalize-space(.) = "${candidateName}"]]`));
   return TempPage.clickElement(buttonElement);
 };
 
