e54334186649b1860ebc572cd2b764d2530188f3
Katie Cookson <katie.cookson@bjss.com>
2020-02-27 16:23:05 +0000

    adding DebriefPage.ts

--- test/e2e/pages/page.ts
+++ test/e2e/pages/page.ts
@@ -14,6 +14,29 @@ export default class Page {
     });
   };
 
+  clickElementById(elementId){
+    this.clickElement(this.getElement(by.id(elementId)));
+  }
+
+  clickElementByXPath(xpath){
+    //this.clickElement(this.getElement(by.xpath(xpath)));
+    this.clickElementByXPath(this.getElementByXPath(xpath));
+  }
+
+  getElementByXPath(xpath){
+    return this.getElement(by.xpath(xpath));
+  }
+
+/**
+ * Waits for the element to exist on the page before returning it.
+ * @param elementBy the element finder
+ */
+getElement(elementBy) {
+  const foundElement = element(elementBy);
+  browser.wait(ExpectedConditions.presenceOf(foundElement));
+  return foundElement;
+};
+
   /**
  * Checks whether the page is ready to be interacted with.
  * Ionic adds an overlay preventing clicking until the page is ready so we need to wait for that to disappear.
@@ -28,4 +51,26 @@ export default class Page {
     // Then return the original promise
     return promise;
   };
+
+getParentContext(currentContext: string) {
+  return `${currentContext.substring(0, currentContext.indexOf('.'))}.1`;
+};
+
+textFieldInputViaNativeMode(xpathString: string, searchTerm) {
+  browser.driver.getCurrentContext().then((webviewContext) => {
+    // Switch to NATIVE context
+    browser.driver.selectContext('NATIVE_APP').then(() => {
+      const nativeField = element(by.xpath(xpathString));
+      nativeField.sendKeys(searchTerm);
+      // Switch back to WEBVIEW context
+      browser.driver.selectContext(this.getParentContext(webviewContext)).then(() => {
+        browser.driver.sleep(TEST_CONFIG.PAGE_LOAD_WAIT);
+      });
+    });
+  });
+};
+
+getPageTitle(pageTitle){
+  return this.getElementByXPath(`//div[contains(@class, 'toolbar-title')][normalize-space(text()) = '${pageTitle}']`);
+}
 }
