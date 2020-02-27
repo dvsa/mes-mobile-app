ec0d916b262f0e36ec75ca7ab47528954e08053e
Katie Cookson <katie.cookson@bjss.com>
2020-02-27 16:23:05 +0000

    adding DebriefPage.ts

--- test/helpers/helpers.ts
+++ test/helpers/helpers.ts
@@ -10,24 +10,6 @@ export const waitForOverlay = (elementName: string) =>  {
   browser.wait(ExpectedConditions.stalenessOf(element(by.className(elementName))));
 };
 
-export const getParentContext = (currentContext: string) => {
-  return `${currentContext.substring(0, currentContext.indexOf('.'))}.1`;
-};
-
-export const textFieldInputViaNativeMode = (xpathString: string, searchTerm) => {
-  browser.driver.getCurrentContext().then((webviewContext) => {
-    // Switch to NATIVE context
-    browser.driver.selectContext('NATIVE_APP').then(() => {
-      const nativeField = element(by.xpath(xpathString));
-      nativeField.sendKeys(searchTerm);
-      // Switch back to WEBVIEW context
-      browser.driver.selectContext(getParentContext(webviewContext)).then(() => {
-        browser.driver.sleep(TEST_CONFIG.PAGE_LOAD_WAIT);
-      });
-    });
-  });
-};
-
 export const scrollToElement = (element) => {
   browser.executeScript('arguments[0].scrollIntoView(true);', element).then(() => {
     expect(element.isPresent()).to.eventually.be.true;
