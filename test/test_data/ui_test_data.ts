2be856b119750d914b50fb403262a0578c2575d0
Teodor Turonek <44288821+turonekt@users.noreply.github.com>
2020-03-02 16:14:17 +0100

    safety questions prefix updated  (#1361)

--- test/test_data/ui_test_data.ts
+++ test/test_data/ui_test_data.ts
@@ -2,7 +2,7 @@ export const UI_TEST_DATA = {
   testData: {
     be: ['S01 - Direction indicators', 'S02 - Doors secure', 'S03 - Horn', 'T01 - Brakes',
       'T02 - Safety factors while loading'],
-    c: ['S1 - All doors secure', 'S2 - Air leaks', 'S5 - Air pressure', 'T4 - Sufficient tread', 'T6 - Body safety'],
-    ce: ['S1 - Change tacho', 'T2 - Loading mechanism'],
+    c: ['CD1 - All doors secure', 'CD2 - Air leaks', 'CD10 - Air pressure', 'CD7 - Sufficient tread', 'CD9 - Body safety'],
+    ce: ['E2 - Change tacho', 'E3 - Loading mechanism'],
   },
 };
