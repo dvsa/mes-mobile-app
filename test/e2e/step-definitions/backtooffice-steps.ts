8e35be430c7f674862d4cf15e11a8a01440e0570
Katie Cookson <katie.cookson@bjss.com>
2020-03-03 11:22:53 +0000

    adding backToOfficePage.js

--- test/e2e/step-definitions/backtooffice-steps.ts
+++ test/e2e/step-definitions/backtooffice-steps.ts
@@ -1,15 +1,12 @@
 import { Then, When } from 'cucumber';
-import { browser, by, ExpectedConditions } from 'protractor';
-import { getElement, clickElement } from '../../helpers/interactionHelpers';
-import TempPage from '../pages/tempPage';
+import BackToOfficePage from '../pages/backToOfficePage';
 
 Then('I am on the back to office page', () => {
-  // No page title so need to check something else exists that exists on the page
-  const backToOfficePage = TempPage.getElement(by.id('back-to-office-page'));
-  return browser.wait(ExpectedConditions.presenceOf(backToOfficePage));
+  // todo:do we need to return this?  What is picking it up?
+  // todo what happens if it isn't the current page?
+  return BackToOfficePage.isCurrentPage();
 });
 
 When('I continue to the office write up', () => {
-  const continueToWriteUpButton = TempPage.getElement(by.id('continue-to-write-up'));
-  TempPage.clickElement(continueToWriteUpButton);
+  BackToOfficePage.clickContinueToWriteUpButton();
 });
