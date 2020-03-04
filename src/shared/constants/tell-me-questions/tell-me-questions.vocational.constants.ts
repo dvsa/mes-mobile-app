7aba06dc842d05f4a1e2f364cf79d486f121718d
Tom Billington <t.billington@kainos.com>
2020-03-04 09:45:14 +0000

    Fixed Codes

--- src/shared/constants/tell-me-questions/tell-me-questions.vocational.constants.ts
+++ src/shared/constants/tell-me-questions/tell-me-questions.vocational.constants.ts
@@ -3,62 +3,62 @@ import { VehicleChecksQuestion } from '../../../providers/question/vehicle-check
 
 export default [
   {
-    code: 'CD3',
+    code: 'Q3',
     description: 'Tell me the main safety factors involved in loading this vehicle.',
     shortName: 'Safety factors while loading'
   },
   {
-    code: 'CD4',
+    code: 'Q4',
     description: 'Tell me how you would check the condition of the reflectors on this vehicle.',
     shortName: 'Reflectors condition'
   },
   {
-    code: 'CD5',
+    code: 'Q5',
     description: 'Tell me how you would check the condition of the windscreen & windows on this vehicle.',
     shortName: 'Windscreen & windows condition'
   },
   {
-    code: 'CD7',
+    code: 'Q7',
     description: 'Tell me how you would check your tyres to ensure that they are correctly inflated, have sufficient tread depth and that their general condition is safe to use on the road.',
     shortName: 'Sufficient tread'
   },
   {
-    code: 'CD8',
+    code: 'Q8',
     description: 'Tell me how you would check the condition of the windscreen wipers on this vehicle.',
     shortName: 'Windscreen wipers condition'
   },
   {
-    code: 'CD9',
+    code: 'Q9',
     description: 'Tell me how you would check the condition of the body is safe on this vehicle.',
     shortName: 'Body safety'
   },
   {
-    code: 'CD11',
+    code: 'Q11',
     description: 'Identify where you would check the engine oil level and tell me how you would check that the engine has sufficient oil.',
     shortName: 'Engine has sufficient oil'
   },
   {
-    code: 'CD14',
+    code: 'Q14',
     description: 'Tell me how you would check the condition of the suspension on this vehicle.',
     shortName: 'Suspension condition'
   },
   {
-    code: 'CD16',
+    code: 'Q16',
     description: 'Identify where you would check the engine coolant level and tell me how you would check that the engine has the correct level.',
     shortName: 'Engine coolant'
   },
   {
-    code: 'CD17',
+    code: 'Q17',
     description: 'Tell me how you would check that the headlamps, sidelights & tail lights are working.',
     shortName: 'Headlamps, sidelights and tail lights'
   },
   {
-    code: 'CD19',
+    code: 'Q19',
     description: 'Tell me how you would operate the loading mechanism on this vehicle (vehicle specific ie tail lift).',
     shortName: 'Loading mechanism'
   },
   {
-    code: 'CD22',
+    code: 'Q22',
     description: 'Identify where the windscreen washer reservoir is and tell me how you would check the windscreen washer level.',
     shortName: 'Windscreen washer reservoir'
   },
