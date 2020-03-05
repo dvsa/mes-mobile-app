f48ae4e68004bb39dc3f4430aa452813cb46eae7
Edward Wilson <edward.wilson@me.com>
2020-03-05 15:17:27 +0000

    MES-4464 health declaration page placeholder for ADI Part2 (#1370)
    * Added page names for ADI-Part 2

---
+++ src/pages/health-declaration/cat-adi-part2/health-declaration.cat-adi-part2.page.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { HealthDeclarationCatADIPart2Page } from './health-declaration.cat-adi-part2.page';
+import { EffectsModule } from '@ngrx/effects';
+import { HealthDeclarationAnalyticsEffects } from '../health-declaration.analytics.effects';
+import { ComponentsModule } from '../../../components/common/common-components.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from 'ng2-translate';
+import { HealthDeclarationEffects } from '../health-declaration.effects';
+import { HealthDeclarationComponentsModule } from '../components/health-declaration.components.module';
+
+@NgModule({
+  declarations: [
+    HealthDeclarationCatADIPart2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(HealthDeclarationCatADIPart2Page),
+    EffectsModule.forFeature([
+      HealthDeclarationAnalyticsEffects,
+      HealthDeclarationEffects,
+    ]),
+    HealthDeclarationComponentsModule,
+    ComponentsModule,
+    ReactiveFormsModule,
+    TranslateModule,
+  ],
+})
+export class HealthDeclarationCatBEPageModule { }
