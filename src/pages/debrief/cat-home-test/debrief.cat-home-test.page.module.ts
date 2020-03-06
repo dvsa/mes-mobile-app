ae47ca8c18c3a36d9f840b4940dc1ae1d129abce
Tom Billington <t.billington@kainos.com>
2020-03-06 10:48:36 +0000

    Created Debrief Cat Home Page

---
+++ src/pages/debrief/cat-home-test/debrief.cat-home-test.page.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DebriefCatHomeTestPage } from './debrief.cat-home-test.page';
+import { EffectsModule } from '@ngrx/effects';
+import { DebriefAnalyticsEffects } from '../debrief.analytics.effects';
+import { ComponentsModule } from '../../../components/common/common-components.module';
+import { DebriefComponentsModule } from '../components/debrief-components.module';
+import { TranslateModule } from 'ng2-translate';
+import { DebriefEffects } from '../debrief.effects';
+import { FaultSummaryProvider } from '../../../providers/fault-summary/fault-summary';
+
+@NgModule({
+  declarations: [
+    DebriefCatHomeTestPage,
+  ],
+  imports: [
+    DebriefComponentsModule,
+    IonicPageModule.forChild(DebriefCatHomeTestPage),
+    EffectsModule.forFeature([
+      DebriefEffects,
+      DebriefAnalyticsEffects,
+    ]),
+    ComponentsModule,
+    TranslateModule,
+  ],
+  providers: [
+    FaultSummaryProvider,
+  ],
+})
+export class DebriefCatHomeTestPageModule { }
