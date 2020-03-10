3ddd69509e371c124e381f82a124666092ece07a
Katie Cookson <katie.cookson@bjss.com>
2020-03-10 12:20:30 +0000

    adding searchPage and wiring up to search-steps.ts

--- test/helpers/helpers.ts
+++ test/helpers/helpers.ts
@@ -3,14 +3,7 @@ import { browser, ExpectedConditions, element, by } from 'protractor';
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
-const expect = chai.expect;
 
 export const waitForOverlay = (elementName: string) =>  {
   browser.wait(ExpectedConditions.stalenessOf(element(by.className(elementName))));
-};
-
-export const scrollToElement = (element) => {
-  browser.executeScript('arguments[0].scrollIntoView(true);', element).then(() => {
-    expect(element.isPresent()).to.eventually.be.true;
-  });
 };
