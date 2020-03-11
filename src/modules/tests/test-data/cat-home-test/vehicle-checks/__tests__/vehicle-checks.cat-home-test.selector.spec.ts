80f16e20eab3284cdf8471794c5c98e585e4e44b
anthonyp@kainos.com <anthonyp@kainos.com@Portavogie.local>
2020-03-11 16:40:07 +0000

    updating category

--- src/modules/tests/test-data/cat-home-test/vehicle-checks/__tests__/vehicle-checks.cat-home-test.selector.spec.ts
+++ src/modules/tests/test-data/cat-home-test/vehicle-checks/__tests__/vehicle-checks.cat-home-test.selector.spec.ts
@@ -1,16 +1,16 @@
-import { CatBEUniqueTypes } from '@dvsa/mes-test-schema/categories/BE';
+import { CatFUniqueTypes } from '@dvsa/mes-test-schema/categories/F';
 import { vehicleChecksExist } from '../vehicle-checks.cat-home-test.selector';
 
 describe('Vehicle Checks Selector Cat HOME', () => {
 
   describe('vehicleChecksExist', () => {
     it('should return false if there are no vehicle checks entered', ()  => {
-      const emptyVehicleChecks: CatBEUniqueTypes.VehicleChecks = { showMeQuestions: [{}], tellMeQuestions: [{}] };
+      const emptyVehicleChecks: CatFUniqueTypes.VehicleChecks = { showMeQuestions: [{}], tellMeQuestions: [{}] };
       const result = vehicleChecksExist(emptyVehicleChecks);
       expect(result).toBeFalsy();
     });
     it('should return true if there are vehicle checks (showMeQuestions) entered', ()  => {
-      const populatedVehicleChecks: CatBEUniqueTypes.VehicleChecks =
+      const populatedVehicleChecks: CatFUniqueTypes.VehicleChecks =
         {
           showMeQuestions: [{ code: 'S01', description: 'test s01', outcome: 'P' }],
           tellMeQuestions: [{}],
@@ -19,7 +19,7 @@ describe('Vehicle Checks Selector Cat HOME', () => {
       expect(result).toBeTruthy();
     });
     it('should return true if there are vehicle checks (tellMeQuestions) entered', ()  => {
-      const populatedVehicleChecks: CatBEUniqueTypes.VehicleChecks =
+      const populatedVehicleChecks: CatFUniqueTypes.VehicleChecks =
         {
           showMeQuestions: [{}],
           tellMeQuestions: [{ code: 'T01', description: 'test T01', outcome: 'DF' }],
@@ -28,7 +28,7 @@ describe('Vehicle Checks Selector Cat HOME', () => {
       expect(result).toBeTruthy();
     });
     it('should return false if there are vehicle checks but no outcome selected', ()  => {
-      const populatedVehicleChecks: CatBEUniqueTypes.VehicleChecks =
+      const populatedVehicleChecks: CatFUniqueTypes.VehicleChecks =
         {
           showMeQuestions: [{ code: 'S01', description: 'test s01', outcome: null }],
           tellMeQuestions: [{ code: 'T01', description: 'test T01', outcome: null }],
