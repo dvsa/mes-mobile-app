ae3731d8417022394965eeffc7a65a35a133050f
turnerc1 <60773915+turnerc1@users.noreply.github.com>
2020-03-11 09:45:30 +0000

    Mes 4850 rekey late test (#1431)
    * started rekey of late test
    
    * finished testing

--- test/e2e/step-definitions/journal-steps.ts
+++ test/e2e/step-definitions/journal-steps.ts
@@ -79,6 +79,22 @@ When('I rekey a late test for {string}',(candidateName) => {
   });
 });
 
+When('I rekey a late test for {string}',(candidateName) => {
+  const buttonElement = getElement(by.xpath(`//button/span/h3[text()[normalize-space(.) = "Start test"]]
+    [ancestor::ion-row/ion-col/ion-grid/ion-row/ion-col/candidate-link/div/button/span/
+    h3[text() = "${candidateName}"]]`));
+  clickElement(buttonElement);
+
+  // If the rekey dialog is shown so just select start test normally
+  const rekeyStartTestButton = element(by.id('rekey-rekey-test-button'));
+  rekeyIsPresent();
+  rekeyStartTestButton.isPresent().then((result) => {
+    if (result) {
+      clickElement(rekeyStartTestButton);
+    }
+  });
+});
+
 When('I navigate to next day', () => {
   const nextDayButtonElement = getElement(by.id('next-day-container'));
   return clickElement(nextDayButtonElement);
