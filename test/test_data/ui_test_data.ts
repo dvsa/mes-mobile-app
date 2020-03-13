20f1cb27e392efc7cf059fc3c43ae0c8a32ac2bf
cai <cai.turner@dvsa.gov.uk>
2020-03-13 12:48:30 +0000

    started cat a1 test

--- test/test_data/ui_test_data.ts
+++ test/test_data/ui_test_data.ts
@@ -4,5 +4,6 @@ export const UI_TEST_DATA = {
       'T02 - Safety factors while loading'],
     c: ['Q1 - All doors secure', 'Q2 - Air leaks', 'Q10 - Air pressure', 'Q7 - Sufficient tread', 'Q9 - Body safety'],
     ce: ['E2 - Change tacho', 'E3 - Loading mechanism'],
+    a1: ['A1'],
   },
 };
