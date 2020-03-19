fc422dee7bfe6106e9565d77e86e41a8e291b1c0
mgoldson <marlon.goldson@bjss.com>
2020-03-19 16:09:13 +0000

    mes-4977: Update debrief page

--- src/modules/tests/test-data/cat-home-test/test-data.cat-home.selector.ts
+++ src/modules/tests/test-data/cat-home-test/test-data.cat-home.selector.ts
@@ -1,9 +1,13 @@
-
 import { CatFUniqueTypes } from '@dvsa/mes-test-schema/categories/F';
 import { CatGUniqueTypes } from '@dvsa/mes-test-schema/categories/G';
 import { CatHUniqueTypes } from '@dvsa/mes-test-schema/categories/H';
 import { CatKUniqueTypes } from '@dvsa/mes-test-schema/categories/K';
-
+import {
+  NUMBER_OF_SHOW_ME_QUESTIONS,
+} from '../../../../shared/constants/show-me-questions/show-me-questions.cat-home-test.constants';
+import {
+  NUMBER_OF_TELL_ME_QUESTIONS,
+} from '../../../../shared/constants/tell-me-questions/tell-me-questions.cat-home-test.constants';
 import { get } from 'lodash';
 
 export type CatHomeTestData =
@@ -17,4 +21,48 @@ export type CatHomeTestManoeuvres =
 | CatGUniqueTypes.Manoeuvres
 | CatHUniqueTypes.Manoeuvres;
 
+export type CatHomeTestVehicleChecks =
+  | CatFUniqueTypes.VehicleChecks
+  | CatGUniqueTypes.VehicleChecks
+  | CatHUniqueTypes.VehicleChecks
+  | CatKUniqueTypes.VehicleChecks;
+
+export const getVehicleChecks = (
+  state: CatHomeTestData): CatHomeTestVehicleChecks => state.vehicleChecks;
+
 export const getManoeuvres = (data: CatHomeTestData): CatHomeTestManoeuvres => get(data, 'manoeuvres');
+
+// TODO - We should really pass a Vehicle Checks object here and not Test Data
+// TODO - Also this has to go into a provider
+export const hasVehicleChecksBeenCompletedCatHomeTest = (data: CatHomeTestData): boolean => {
+  let showMeQuestionComplete = true;
+  let tellMeQuestionComplete = true;
+
+  if (
+    !(data.vehicleChecks && data.vehicleChecks.showMeQuestions instanceof Array) ||
+    data.vehicleChecks.showMeQuestions.length !== NUMBER_OF_SHOW_ME_QUESTIONS
+  ) {
+    showMeQuestionComplete = false;
+  } else {
+    data.vehicleChecks.showMeQuestions.forEach((element) => {
+      if (element.outcome == null) {
+        showMeQuestionComplete = false;
+      }
+    });
+  }
+
+  if (
+    !(data.vehicleChecks && data.vehicleChecks.tellMeQuestions instanceof Array) ||
+    data.vehicleChecks.tellMeQuestions.length !== NUMBER_OF_TELL_ME_QUESTIONS
+  ) {
+    tellMeQuestionComplete = false;
+  } else {
+    data.vehicleChecks.tellMeQuestions.forEach((element) => {
+      if (element.outcome == null) {
+        tellMeQuestionComplete = false;
+      }
+    });
+  }
+
+  return (showMeQuestionComplete && tellMeQuestionComplete);
+};
