686cb094b6d31457c86de4c05e5c25bf49d4ac8f
Ross Faulds <ross.faulds@bjss.com>
2020-03-19 15:25:48 +0000

    remove mock warning banner from test as per peer review

--- src/pages/pass-finalisation/cat-home-test/_tests_/pass-finalisation.cat-home-test.page.spec.ts
+++ src/pages/pass-finalisation/cat-home-test/_tests_/pass-finalisation.cat-home-test.page.spec.ts
@@ -30,7 +30,6 @@ import { D255Yes, D255No, DebriefWitnessed, DebriefUnwitnessed } from
 import { CandidateChoseToProceedWithTestInWelsh, CandidateChoseToProceedWithTestInEnglish } from
     '../../../../modules/tests/communication-preferences/communication-preferences.actions';
 import { PassFinalisationCatHomeTestPage } from '../pass-finalisation.cat-home-test.page';
-import { WarningBannerComponent } from '../../../../components/common/warning-banner/warning-banner';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PASS_CERTIFICATE_NUMBER_CTRL }
   from '../../components/pass-certificate-number/pass-certificate-number.constants';
@@ -52,7 +51,6 @@ describe('PassFinalisationCatHomeTestPage', () => {
         MockComponent(DebriefWitnessedComponent),
         MockComponent(FinalisationHeaderComponent),
         MockComponent(LanguagePreferencesComponent),
-        MockComponent(WarningBannerComponent),
       ],
       imports: [IonicModule, AppModule],
       providers: [
