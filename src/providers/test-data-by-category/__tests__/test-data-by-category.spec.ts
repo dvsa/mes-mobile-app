0142c30287148fecea8c508762758a1c789ce01a
mgoldson <marlon.goldson@bjss.com>
2020-03-23 17:06:59 +0000

    refactor tests to cover all categories

--- src/providers/test-data-by-category/__tests__/test-data-by-category.spec.ts
+++ src/providers/test-data-by-category/__tests__/test-data-by-category.spec.ts
@@ -10,102 +10,47 @@ describe('TestDataByCategoryProvider', () => {
   });
 
   describe('getTestDataByCategoryCode()', () => {
-    describe('CAT BE', () => {
-      it('should return CAT BE test data for a BE Catgeory Code', () => {
-        const testData = provider.getTestDataByCategoryCode(TestCategory.BE);
-        expect(testData).toEqual(jasmine.any(Function));
-        expect(testData).not.toThrow();
-      });
-    });
 
-    describe('CAT C', () => {
-      it('should return CAT C test data for a C Catgeory Code', () => {
-        expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.C);
-        }).toEqual(jasmine.any(Function));
-        expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.C);
-        }).not.toThrowError('Error getting test category');
-      });
+    const categories = [
+      { category: TestCategory.B },
+      { category: TestCategory.BE },
+      { category: TestCategory.C },
+      { category: TestCategory.C1 },
+      { category: TestCategory.CE },
+      { category: TestCategory.C1E },
+      { category: TestCategory.D },
+      { category: TestCategory.D1 },
+      { category: TestCategory.DE },
+      { category: TestCategory.D1E },
+      { category: TestCategory.F },
+      { category: TestCategory.G },
+      { category: TestCategory.H },
+      { category: TestCategory.K },
+      { category: TestCategory.ADI2 },
+    ];
 
-      it('should return CAT CE test data for a CE Catgeory Code', () => {
+    categories.forEach((cat) => {
+      it(`should return ${cat.category} test data for a ${cat.category} Category Code`, () => {
         expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.CE);
+          provider.getTestDataByCategoryCode(cat.category);
         }).toEqual(jasmine.any(Function));
-        expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.CE);
-        }).not.toThrowError('Error getting test category');
-      });
 
-      it('should return CAT C1 test data for a C1 Catgeory Code', () => {
         expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.C1);
-        }).toEqual(jasmine.any(Function));
-        expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.C1);
-        }).not.toThrowError('Error getting test category');
-      });
-
-      it('should return CAT C1E test data for a C1E Catgeory Code', () => {
-        expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.C1E);
-        }).toEqual(jasmine.any(Function));
-        expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.C1E);
+          provider.getTestDataByCategoryCode(cat.category);
         }).not.toThrowError('Error getting test category');
       });
     });
 
-    describe('CAT D', () => {
-      it('should return CAT D test data for a D Catgeory Code', () => {
-        expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.D);
-        }).toEqual(jasmine.any(Function));
-        expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.D);
-        }).not.toThrowError('Error getting test category');
-      });
-
-      it('should return CAT DE test data for a DE Catgeory Code', () => {
-        expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.DE);
-        }).toEqual(jasmine.any(Function));
-        expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.DE);
-        }).not.toThrowError('Error getting test category');
-      });
-
-      it('should return CAT D1 test data for a D1 Catgeory Code', () => {
-        expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.D1);
-        }).toEqual(jasmine.any(Function));
-        expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.D1);
-        }).not.toThrowError('Error getting test category');
-      });
-
-      it('should return CAT D1E test data for a D1E Catgeory Code', () => {
-        expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.D1E);
-        }).toEqual(jasmine.any(Function));
-        expect(() => {
-          provider.getTestDataByCategoryCode(TestCategory.D1E);
-        }).not.toThrowError('Error getting test category');
-      });
+    it('should throw an error when there is no matching test category', () => {
+      expect(() => {
+        provider.getTestDataByCategoryCode('z' as TestCategory);
+      }).toThrowError('Error getting test category');
     });
 
-    describe('When there is no matching test category', () => {
-      it('should throw an error when there is no matching test category', () => {
-        expect(() => {
-          provider.getTestDataByCategoryCode('z' as TestCategory);
-        }).toThrowError('Error getting test category');
-      });
-
-      it('should throw an error when test category is undefined', () => {
-        expect(() => {
-          provider.getTestDataByCategoryCode(undefined);
-        }).toThrowError('Error getting test category');
-      });
+    it('should throw an error when test category is undefined', () => {
+      expect(() => {
+        provider.getTestDataByCategoryCode(undefined);
+      }).toThrowError('Error getting test category');
     });
   });
 });
