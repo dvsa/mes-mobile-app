72f7abce49be01d2fe8ed40a4a869ac6d0070285
mgoldson <54100299+mgoldson@users.noreply.github.com>
2020-04-03 15:13:55 +0100

    mes-5057 add reducer tests (#1515)
    * add reducer tests
    
    * fix test description

---
+++ src/modules/tests/test-data/cat-home-test/test-requirements/__tests__/test-requirements.cat-home.reducer.spec.ts
@@ -0,0 +1,27 @@
+import { testRequirementsCatHomeReducer } from '../test-requirements.cat-home.reducer';
+import { LegalRequirements } from '../../../test-data.constants';
+import { ToggleLegalRequirement } from '../../../common/test-requirements/test-requirements.actions';
+import { TestRequirements } from '@dvsa/mes-test-schema/categories/common';
+
+describe('Test Requirements CAT Home Reducer', () => {
+
+  describe('TOGGLE_LEGAL_REQUIREMENT', () => {
+    it('should toggle angled start to complete(true) when dispatched first time', () => {
+      const state: TestRequirements = {};
+      const result = testRequirementsCatHomeReducer(
+        state,
+        new ToggleLegalRequirement(LegalRequirements.angledStart),
+      );
+      expect(result[LegalRequirements.angledStart]).toEqual(true);
+    });
+
+    it('should toggle angled start to incomplete(false) when dispatched second time', () => {
+      const state: TestRequirements = {};
+      const modifiedState = testRequirementsCatHomeReducer(
+        state, new ToggleLegalRequirement(LegalRequirements.angledStart));
+      const result = testRequirementsCatHomeReducer(
+        modifiedState, new ToggleLegalRequirement(LegalRequirements.angledStart));
+      expect(result[LegalRequirements.angledStart]).toEqual(false);
+    });
+  });
+});
