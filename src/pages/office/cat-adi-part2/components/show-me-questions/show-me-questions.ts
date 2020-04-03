53e830112a3d4ed16f0b55e4ddd62aaf7463e3da
matthew <m.bell2412@hotmail.com>
2020-04-03 12:59:36 +0100

    push

--- src/pages/office/cat-adi-part2/components/show-me-questions/show-me-questions.ts
+++ src/pages/office/cat-adi-part2/components/show-me-questions/show-me-questions.ts
@@ -50,6 +50,10 @@ export class ShowMeQuestionsCatADI2Component implements OnInit, OnChanges {
   // @TODO - rename and make type strict
   index: number[] = [];
 
+  clicked: number = 0;
+
+  optionValue: boolean = false;
+
   private formControl: FormControl;
   static readonly fieldName: string = 'showMeQuestions';
 
@@ -89,26 +93,94 @@ export class ShowMeQuestionsCatADI2Component implements OnInit, OnChanges {
     this.values.push(question);
     this.index.push(index);
 
-    console.log('question', question);
-
     this.values = this.uniqueArray(this.values, 'code');
     this.index = this.uniqueArray(this.index, null);
 
-    // @TODO - create array dynamically (Matt B)
-    const filteredIndices: number[] = [0, 1, 2, 3, 4, 5, 6, 7].filter(v => this.index.indexOf(v) === -1);
+    const filteredIndices: number[] =
+      this.fillArray(8).filter(v => this.index.indexOf(v) === -1);
 
-    filteredIndices.forEach((res: number) => {
-      if (this.values.length >= 2 && res !== undefined && document.querySelector(`#alert-input-0-${res}`)) {
-        document.querySelector(`#alert-input-0-${res}`).setAttribute('disabled', 'true');
+    filteredIndices.forEach((res: number): void => {
+      if (res !== undefined) {
+        // const endsIn: Element = document.querySelector(`[id$='${res}']`);
+        // const contains: Element = document.querySelector(`[id*='alert-input']`);
+
+        console.log(`#alert-input-${this.clicked}-${res}`);
+        const dom: Element = document.querySelector(`#alert-input-${this.clicked}-${res}`);
+
+        if (this.values.length >= 2 && dom) {
+          // endsIn.setAttribute('disabled', 'true');
+          dom.setAttribute('disabled', 'true');
+          this.showMeQuestionOptionsDisabled[res].disabled = true;
+        }
       }
     });
-  }
+  };
+
+  fillArray = (length: number): number[] => Array.from({ length }, (val, index) => index);
 
   uniqueArray = (array: any[], key: string): any[] => {
     if (!key) {
       return uniq(array);
     }
     return uniqBy(array, key);
+  };
+
+  openedSelect = () => {
+    console.log('clicked');
+    console.log(this.formControl);
+
+    const vals = this.uniqueArray(this.formControl.value, 'code');
+
+    console.log('vals', vals);
+
+    const filteredIndices: number[] = this.fillArray(8);
+
+    filteredIndices.forEach((res: number): void => {
+      if (res !== undefined) {
+
+        // const dom = document.querySelector(`#alert-input-${this.clicked}-${res}`);
+
+        if (vals.length < 2) {
+          const dom = document.getElementsByTagName(`button`);
+
+          if (dom) {
+            console.log(dom);
+
+            const h = <HTMLInputElement>document.querySelector(`#alert-input-0-0`);
+            if (h) {
+              // h.addEventListener('click', (event: MouseEvent) => {
+              //   console.log('clicked', res);
+              // });
+              h.onclick = () => {
+                console.info('res', res);
+              };
+              // h.disabled = false;
+              //
+              // h.setAttribute('disabled', 'false');
+            }
+            //
+            // Array.from(dom).forEach(elem => {
+            //   elem.setAttribute('disabled', 'false');
+            // });
+            // dom.setAttribute('disabled', 'true');
+            this.showMeQuestionOptionsDisabled[res].disabled = false;
+          }
+          // const h = <HTMLInputElement>document.getElementById("myBtn");
+          // h.disabled = false;
+
+        }
+      }
+    });
   }
+
+  onSelectClicked = () => {
+    // const d: Element = document.querySelector(`#alert-input-0-0`);
+    //
+    // let val: boolean = false;
+    // if (d) {
+    //   d.onclick = function() { alert('blah'); };
+    // }
+  }
+
 
 }
