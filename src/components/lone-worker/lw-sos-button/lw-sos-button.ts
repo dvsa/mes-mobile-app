623574b4e875a74d3e2d1a73b2888d1aedd6b54a
Ammar Haider <ammar.haider@bjss.com>
2020-05-07 19:37:00 +0100

    starts on surfacing alert statuses to lw-sos-button

--- src/components/lone-worker/lw-sos-button/lw-sos-button.ts
+++ src/components/lone-worker/lw-sos-button/lw-sos-button.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { ModalController } from 'ionic-angular';
+import { Store, select } from '@ngrx/store';
+import { StoreModel } from '../../../shared/models/store.model';
+import { Observable } from 'rxjs';
+import { AlertStatusModel } from '../lw-store/alert/alert.model';
+import { map } from 'rxjs/operators';
+import { getAlertStatus } from '../lw-store/alert/alert.selector';
+import { Severity } from '@dvsa/lw-incident-model';
+import { getAlertState } from '../lw-store/alert/alert.reducer';
 
 @Component({
   selector: 'lw-sos-button',
@@ -7,9 +15,27 @@ import { ModalController } from 'ionic-angular';
 })
 export class LWSosButtonComponent {
 
-  constructor(private modalController: ModalController) { }
+  redAlertStatus$: Observable<AlertStatusModel>;
+  amberAlertStatus$: Observable<AlertStatusModel>;
 
+  constructor(
+    private modalController: ModalController,
+    private store$: Store<StoreModel>) { }
+
+  // TODO: Retrieve statuses from store
   status = {};
+
+  ngOnInit(): void {
+    this.redAlertStatus$ = this.store$.pipe(
+      select(getAlertState),
+      map(state => getAlertStatus(state, Severity.Red)),
+    );
+
+    this.amberAlertStatus$ = this.store$.pipe(
+      select(getAlertState),
+      map(state => getAlertStatus(state, Severity.Amber)),
+    );
+  }
 
   raiseSos(): void {
     const modalName = 'LWSosAlertModal';
