6cc36b25edc38764c56031fef3ddbca917fa6313
andrew <andrew.setterfield@dvsa.gov.uk>
2020-06-09 09:13:39 +0100

    updated health declaration for CPC

---
+++ src/pages/health-declaration/cat-cpc/health-declaration.cat-cpc.page.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EffectsModule } from '@ngrx/effects';
+import { HealthDeclarationAnalyticsEffects } from '../health-declaration.analytics.effects';
+import { ComponentsModule } from '../../../components/common/common-components.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { HealthDeclarationEffects } from '../health-declaration.effects';
+import { HealthDeclarationComponentsModule } from '../components/health-declaration.components.module';
+import { HealthDeclarationCatCpcPage } from './health-declaration.cat-cpc.page';
+
+@NgModule({
+  declarations: [
+    HealthDeclarationCatCpcPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(HealthDeclarationCatCpcPage),
+    EffectsModule.forFeature([
+      HealthDeclarationAnalyticsEffects,
+      HealthDeclarationEffects,
+    ]),
+    HealthDeclarationComponentsModule,
+    ComponentsModule,
+    ReactiveFormsModule,
+    TranslateModule,
+  ],
+})
+export class HealthDeclarationCatCpcPageModule { }
