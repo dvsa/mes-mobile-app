2c2e5191184a169c6c3c95fd3e05b41d0d242a38
Matt <mculliford.dvsa@icloud.com>
2020-06-11 14:45:51 +0100

    unit testing

--- src/providers/authentication/__tests__/authentication.spec.ts
+++ src/providers/authentication/__tests__/authentication.spec.ts
@@ -72,6 +72,7 @@ describe('Authentication', () => {
 
     it('getAuthenticationToken() should return a token', async () => {
       spyOn(dataStoreProvider, 'getItem').and.returnValue(Promise.resolve('"U0lMRU5UIEFZU05DIFRFU1QgVE9LRU4"'));
+      spyOn(authenticationProvider.ionicAuth, 'isAuthenticated').and.returnValue(Promise.resolve(true));
       const token = await authenticationProvider.getAuthenticationToken();
 
       expect(token).toEqual('U0lMRU5UIEFZU05DIFRFU1QgVE9LRU4');
