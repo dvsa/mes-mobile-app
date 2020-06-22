a189003aaf23054cb83773af8708c89f80319a2e
andrew <andrew.setterfield@dvsa.gov.uk>
2020-06-22 10:15:07 +0100

    added rekey for CPC

---
+++ src/pages/rekey-reason/cat-cpc/rekey-reason.cat-cpc.page.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EffectsModule } from '@ngrx/effects';
+import { ComponentsModule } from '../../../components/common/common-components.module';
+import { RekeyReasonCatCPCPage } from './rekey-reason.cat-cpc.page';
+import { RekeyReasonComponentsModule } from '../components/rekey-reason.components.module';
+import { RekeyReasonAnalyticsEffects } from '../rekey-reason.analytics.effects';
+import { StoreModule } from '@ngrx/store';
+import { RekeyReasonEffects } from '../rekey-reason.effects';
+import { DirectivesModule } from '../../../directives/directives.module';
+import { FindUserProvider } from '../../../providers/find-user/find-user';
+import { rekeyReasonReducer } from '../rekey-reason.reducer';
+
+@NgModule({
+  declarations: [
+    RekeyReasonCatCPCPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(RekeyReasonCatCPCPage),
+    StoreModule.forFeature('rekeyReason', rekeyReasonReducer),
+    EffectsModule.forFeature([
+      RekeyReasonAnalyticsEffects,
+      RekeyReasonEffects,
+    ]),
+    DirectivesModule,
+    ComponentsModule,
+    RekeyReasonComponentsModule,
+  ],
+  providers: [
+    FindUserProvider,
+  ],
+})
+
+export class RekeyReasonCatCPCPageModule { }
