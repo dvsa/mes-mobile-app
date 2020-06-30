f8592a6f40daded9d2810eff933f8bb3048245ab
andrew <andrew.setterfield@dvsa.gov.uk>
2020-06-30 11:24:44 +0100

    always display DF comments for ADI2

--- src/pages/view-test-result/components/faults-data-row/faults-data-row.ts
+++ src/pages/view-test-result/components/faults-data-row/faults-data-row.ts
@@ -28,6 +28,9 @@ export class FaultsDataRowComponent {
   @Input()
   isRider?: boolean = false;
 
+  @Input()
+  displayDrivingFaults?: boolean = false;
+
   showNoFaultsMessage = (): boolean =>
     this.drivingFaultCount === 0 &&
     this.seriousFaults.length === 0 &&
