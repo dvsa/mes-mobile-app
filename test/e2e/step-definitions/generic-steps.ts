fea567e28ceae6ccb2d53320e623bedb8eacde58
naqvis5 <syed.naqvi@dvsa.gov.uk>
2020-06-30 13:30:12 +0100

    removed space

--- test/e2e/step-definitions/generic-steps.ts
+++ test/e2e/step-definitions/generic-steps.ts
@@ -199,13 +199,6 @@ Given('I am on the {string} page', (pageTitle) => {
     .to.eventually.equal(pageTitle);
 });
 
-
-
-
-
-
-
-
 Then('I should see the {string} contains {string}', (rowName, rowValue) => {
   JournalPage.rowContains(rowName, rowValue);
 });
