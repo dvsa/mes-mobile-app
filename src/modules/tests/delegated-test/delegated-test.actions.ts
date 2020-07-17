4881e746767f30e081acc241f41909847b8c0c13
matthew <m.bell2412@hotmail.com>
2020-07-17 09:03:56 +0100

    create delegated-test reducer and apply to all necessary test reducers

---
+++ src/modules/tests/delegated-test/delegated-test.actions.ts
@@ -0,0 +1,9 @@
+import { Action } from '@ngrx/store';
+
+export const START_DELEGATED_TEST = '[Delegated Test Actions] Start Delegeated test';
+
+export class StartDelegatedTest implements Action {
+  readonly type = START_DELEGATED_TEST;
+}
+
+export type Types = StartDelegatedTest;
