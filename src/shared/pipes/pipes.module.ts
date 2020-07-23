f7cb875db008b89ef6f5f899258de8bb15c0dd9b
andrew <andrew.setterfield@dvsa.gov.uk>
2020-07-23 08:27:37 +0100

    added pipe to change move off to move away for all CatA tests

---
+++ src/shared/pipes/pipes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { ModifyLabel } from './modifyLabel';
+
+@NgModule({
+  declarations: [
+    ModifyLabel,
+  ],
+  exports: [
+    ModifyLabel,
+  ],
+  imports: [],
+})
+export class PipesModule {
+}
