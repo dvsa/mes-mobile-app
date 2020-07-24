5da176b72ff18dcdb8d894cbdee6554faec8751b
andrew <andrew.setterfield@dvsa.gov.uk>
2020-07-24 15:25:54 +0100

    WIP

--- src/pages/office/components/activity-code/activity-code.ts
+++ src/pages/office/components/activity-code/activity-code.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { ActivityCodeModel } from './activity-code.constants';
+import { ActivityCodeModel, ActivityCodeModelDelegatedExaminer } from './activity-code.constants';
 import { ActivityCode } from '@dvsa/mes-test-schema/categories/common';
 
 @Component({
@@ -10,7 +10,7 @@ import { ActivityCode } from '@dvsa/mes-test-schema/categories/common';
 export class ActivityCodeComponent implements OnChanges {
 
   @Input()
-  activityCodeModel: ActivityCodeModel;
+  activityCodeModel: ActivityCodeModel | ActivityCodeModelDelegatedExaminer;
 
   @Input()
   activityCodeOptions: ActivityCodeModel[];
@@ -22,10 +22,15 @@ export class ActivityCodeComponent implements OnChanges {
   disabled: boolean;
 
   @Output()
-  activityCodeChange = new EventEmitter<ActivityCodeModel>();
+  activityCodeChange = new EventEmitter<ActivityCodeModel | ActivityCodeModelDelegatedExaminer>();
 
   private formControl: FormControl;
   static readonly fieldName: string = 'activityCode';
+
+  ngOnInit(): void {
+    console.log('activityCodeModel', this.activityCodeModel);
+    console.log('formGroup', this.formGroup);
+  }
 
   ngOnChanges(): void {
     if (!this.formControl) {
@@ -35,7 +40,8 @@ export class ActivityCodeComponent implements OnChanges {
     this.formControl.patchValue(this.activityCodeModel);
   }
 
-  activityCodeChanged(activityCode: ActivityCodeModel): void {
+  activityCodeChanged(activityCode: ActivityCodeModel | ActivityCodeModelDelegatedExaminer): void {
+    console.log('change', activityCode);
     if (this.formControl.valid) {
       this.activityCodeChange.emit(activityCode);
     }
@@ -46,7 +52,8 @@ export class ActivityCodeComponent implements OnChanges {
   }
 
   isSelectDisabled(): boolean {
-    return this.disabled || (this.activityCodeModel && parseInt(this.activityCodeModel.activityCode, 10) < 4);
+    // return this.disabled || (this.activityCodeModel && parseInt(this.activityCodeModel.activityCode, 10) < 4);
+    return false;
   }
 
   isOptionDisabled(activityCode: ActivityCode): boolean {
