60eb2d2d5ba2d57da0f3465320fbbe2a495df0a4
tomcrawleyy <thomascrawley99@gmail.com>
2020-07-29 12:32:36 +0100

    release/MES-5483 Remove welsh translations from Office page (#1702)
    * first draft
    
    * default isTranslatable to true if nothing is parsed
    
    * final draft
    
    * introduced unit tests
    
    * removed unnecessary store import

--- src/pages/debrief/cat-a-mod1/components/speed-check-debrief-card/speed-check-debrief-card.ts
+++ src/pages/debrief/cat-a-mod1/components/speed-check-debrief-card/speed-check-debrief-card.ts
@@ -1,11 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Avoidance, EmergencyStop } from '@dvsa/mes-test-schema/categories/AM1';
+import { configureI18N } from '../../../../../shared/helpers/translation.helpers';
+import { TranslateService } from '@ngx-translate/core';
+import { Language } from '../../../../../modules/tests/communication-preferences/communication-preferences.model';
 
 @Component({
   selector: 'speed-check-debrief-card',
   templateUrl: 'speed-check-debrief-card.html',
 })
 export class SpeedCheckDebriefCardComponent {
+
+  constructor(
+    private translate: TranslateService,
+    ) {}
 
   @Input()
   public emergencyStop: EmergencyStop;
@@ -15,4 +22,14 @@ export class SpeedCheckDebriefCardComponent {
 
   @Input()
   public avoidanceAttempted: boolean;
+
+  @Input()
+  public isTranslatable: boolean = true;
+
+  ngOnInit(): void {
+    if (!this.isTranslatable) {
+      configureI18N(Language.ENGLISH, this.translate);
+    }
+  }
+
 }
