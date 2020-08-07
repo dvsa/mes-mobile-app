f66c60f5b1dd69e1e5ffdb2ccbe925867cf4abd4
Tom <thomascrawley99@gmail.com>
2020-08-07 15:57:56 +0100

    added additional data to delegated mock provider and removed console log

--- src/providers/delegated-rekey-search/delegated-rekey-search.ts
+++ src/providers/delegated-rekey-search/delegated-rekey-search.ts
@@ -1,23 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
-import { delegatedBooking } from './mock-data/delegated-mock-data';
+import { getDelegatedBooking } from './mock-data/delegated-mock-data';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
 
 @Injectable()
 export class DelegatedRekeySearchProvider {
+
+  applicationReferences = [
+    { referenceNumber: '12345678910', testCategory: TestCategory.BE },
+    { referenceNumber: '12345678911', testCategory: TestCategory.C },
+    { referenceNumber: '12345678912', testCategory: TestCategory.C1 },
+    { referenceNumber: '12345678913', testCategory: TestCategory.C1E },
+    { referenceNumber: '12345678914', testCategory: TestCategory.CE },
+    { referenceNumber: '12345678915', testCategory: TestCategory.D },
+    { referenceNumber: '12345678916', testCategory: TestCategory.D1 },
+    { referenceNumber: '12345678917', testCategory: TestCategory.DE },
+    { referenceNumber: '12345678918', testCategory: TestCategory.D1E },
+  ];
   constructor() {
   }
 
   getDelegatedExaminerBookingByAppRef(applicationReference: string): Observable<Object> {
-    console.log(applicationReference);
     // @TODO - MES-5436 mock responses added. Implementation needed once delegated end point is available
-    if (applicationReference === '12345678910') {
-      return of(new HttpResponse({
-        status: 200,
-        body: delegatedBooking,
-      }));
-    }
-    if (applicationReference === '12345678911') {
+    this.applicationReferences.forEach((ref) => {
+      if (applicationReference === ref.referenceNumber) {
+        return of(new HttpResponse({
+          status: 200,
+          body: getDelegatedBooking(ref.testCategory),
+        }));
+      }
+    });
+    if (applicationReference === '12345678919') {
       return throwError(new HttpErrorResponse({
         error: 'Internal server error',
         status: 500,
--- src/providers/delegated-rekey-search/mock-data/delegated-mock-data.ts
+++ src/providers/delegated-rekey-search/mock-data/delegated-mock-data.ts
@@ -1,25 +1,28 @@
 import { TestSlot } from '@dvsa/mes-journal-schema';
+import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/test-category';
 
-export const delegatedBooking: TestSlot = {
-  booking: {
-    application: {
-      applicationId: 22123411,
-      bookingSequence: 3,
-      checkDigit: 1,
-      testCategory: 'C',
-    },
-    candidate: {
-      candidateName: {
-        firstName: 'A Delegated',
-        lastName: 'Candidate',
+export function getDelegatedBooking(category: TestCategory): TestSlot {
+  return {
+    booking: {
+      application: {
+        applicationId: 22123411,
+        bookingSequence: 3,
+        checkDigit: 1,
+        testCategory: category,
       },
-      driverNumber: 'DAVID015220A99HC',
-      dateOfBirth: '1980-01-01',
+      candidate: {
+        candidateName: {
+          firstName: 'A Delegated',
+          lastName: 'Candidate',
+        },
+        driverNumber: 'DAVID015220A99HC',
+        dateOfBirth: '1980-01-01',
+      },
     },
-  },
-  slotDetail: {
-    slotId: 1234567,
-    start: '2020-07-15T08:10:00',
-  },
-  vehicleTypeCode: 'C',
-};
+    slotDetail: {
+      slotId: 1234567,
+      start: '2020-07-15T08:10:00',
+    },
+    vehicleTypeCode: category,
+  };
+}
