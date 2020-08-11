686c967103a5c379f305951f13c0c57ed21f740f
Matt <mculliford.dvsa@icloud.com>
2020-08-11 11:32:53 +0100

    mes-5435 check for existing results on delegated search

--- src/pages/delegated-rekey-search/delegated-rekey-search.effects.ts
+++ src/pages/delegated-rekey-search/delegated-rekey-search.effects.ts
@@ -2,14 +2,17 @@ import { Injectable } from '@angular/core';
 import { Effect, Actions, ofType } from '@ngrx/effects';
 import { DelegatedRekeySearchProvider } from '../../providers/delegated-rekey-search/delegated-rekey-search';
 import { switchMap, map, catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import {
   SEARCH_BOOKED_DELEGATED_TEST,
   SearchBookedDelegatedTest,
   SearchBookedDelegatedTestSuccess,
-  SearchBookedDelegatedTestFailure,
+  SearchBookedDelegatedTestFailure, DelegatedRekeySearchActionTypes,
 } from './delegated-rekey-search.actions';
-import { HttpResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { HttpStatusCodes } from '../../shared/models/http-status-codes';
+import { SearchProvider } from '../../providers/search/search';
+import { DelegatedRekeySearchErrorMessages } from './delegated-rekey-search-error-model';
 
 @Injectable()
 export class DelegatedRekeySearchEffects {
@@ -17,21 +20,33 @@ export class DelegatedRekeySearchEffects {
   constructor(
     private actions$: Actions,
     private delegatedRekeySearchProvider: DelegatedRekeySearchProvider,
-  )
-  { }
+    private testSearchProvider: SearchProvider,
+  ) {
+  }
 
   @Effect()
   getBooking$ = this.actions$.pipe(
     ofType(SEARCH_BOOKED_DELEGATED_TEST),
     switchMap((action: SearchBookedDelegatedTest) => {
-      return this.delegatedRekeySearchProvider.getDelegatedExaminerBookingByAppRef(action.appRef).pipe(
-        // @TODO - MES-5436 - map real response once available
-        // @TODO - MES-5435 check for existing search results
-        map((response: HttpResponse<any>) => {
-          return response.body;
+      return this.testSearchProvider.getTestResult(action.appRef, undefined).pipe(
+        switchMap((response: HttpResponse<any>): Observable<DelegatedRekeySearchActionTypes> => {
+          return of(new SearchBookedDelegatedTestFailure({
+            message: DelegatedRekeySearchErrorMessages.BookingAlreadyCompleted,
+          }));
         }),
-        map((testSlot: any) => new SearchBookedDelegatedTestSuccess(testSlot)),
-        catchError((err: any) => {
+        catchError((err: HttpErrorResponse): Observable<DelegatedRekeySearchActionTypes> => {
+          if (err.status === HttpStatusCodes.BAD_REQUEST) {
+            return this.delegatedRekeySearchProvider.getDelegatedExaminerBookingByAppRef(action.appRef).pipe(
+              // @TODO - MES-5436 - map real response once available
+              map((response: HttpResponse<any>) => {
+                return response.body;
+              }),
+              map((testSlot: any) => new SearchBookedDelegatedTestSuccess(testSlot)),
+              catchError((err: any) => {
+                return of(new SearchBookedDelegatedTestFailure(err));
+              }),
+            );
+          }
           return of(new SearchBookedDelegatedTestFailure(err));
         }),
       );
