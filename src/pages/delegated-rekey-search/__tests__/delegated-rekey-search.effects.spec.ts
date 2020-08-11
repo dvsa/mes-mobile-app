25d2b4679d206cdee2458895426b6d74616a63b1
Cullima <33055124+Cullima@users.noreply.github.com>
2020-08-11 12:59:13 +0100

    Mes 5435 check existing results (#1722)
    * mes-5435 check for existing results on delegated search
    
    * add unit test
    
    Co-authored-by: Matt <mculliford.dvsa@icloud.com>

--- src/pages/delegated-rekey-search/__tests__/delegated-rekey-search.effects.spec.ts
+++ src/pages/delegated-rekey-search/__tests__/delegated-rekey-search.effects.spec.ts
@@ -1,19 +1,32 @@
 import { DelegatedRekeySearchEffects } from '../delegated-rekey-search.effects';
-import { ReplaySubject, defer } from 'rxjs';
+import { defer, ReplaySubject } from 'rxjs';
 import { async, TestBed } from '@angular/core/testing';
 import { HttpErrorResponse } from '@angular/common/http';
 import { delegatedSearchReducer } from '../delegated-rekey-search.reducer';
-import { StoreModule, Store } from '@ngrx/store';
+import { Store, StoreModule } from '@ngrx/store';
 import { provideMockActions } from '@ngrx/effects/testing';
 import * as delegatedRekeySearchActions from '../delegated-rekey-search.actions';
 import { configureTestSuite } from 'ng-bullet';
 import { DelegatedRekeySearchProvider } from '../../../providers/delegated-rekey-search/delegated-rekey-search';
+import { SearchProvider } from '../../../providers/search/search';
+import { HttpStatusCodes } from '../../../shared/models/http-status-codes';
+import { SearchProviderMock } from '../../../providers/search/__mocks__/search.mock';
+import { DelegatedRekeySearchErrorMessages } from '../delegated-rekey-search-error-model';
 
 describe('Delegated Rekey Search Effects', () => {
 
   let effects: DelegatedRekeySearchEffects;
   let actions$: any;
   let delegatedRekeySearchProvider: DelegatedRekeySearchProvider;
+  let searchProvider: SearchProvider;
+
+  const getTestResultHttpErrorResponse = (status: HttpStatusCodes = 400): HttpErrorResponse => {
+    return new HttpErrorResponse({
+      status,
+      error: 'Error message',
+      statusText: 'Bad request',
+    });
+  };
 
   const appRef = '123456';
 
@@ -29,6 +42,7 @@ describe('Delegated Rekey Search Effects', () => {
         provideMockActions(() => actions$),
         Store,
         DelegatedRekeySearchProvider,
+        { provide: SearchProvider, useClass: SearchProviderMock },
       ],
     });
   });
@@ -37,12 +51,14 @@ describe('Delegated Rekey Search Effects', () => {
     actions$ = new ReplaySubject(1);
     effects = TestBed.get(DelegatedRekeySearchEffects);
     delegatedRekeySearchProvider = TestBed.get(DelegatedRekeySearchProvider);
+    searchProvider = TestBed.get(SearchProvider);
   }));
 
   it('should dispatch the SearchBookedTestSuccess action when searched with success', (done) => {
 
     spyOn(delegatedRekeySearchProvider, 'getDelegatedExaminerBookingByAppRef').and.callThrough();
-
+    spyOn(searchProvider, 'getTestResult')
+      .and.returnValue(asyncError(getTestResultHttpErrorResponse(HttpStatusCodes.BAD_REQUEST)));
     actions$.next(new delegatedRekeySearchActions.SearchBookedDelegatedTest('12345678910'));
 
     effects.getBooking$.subscribe((result) => {
@@ -53,7 +69,8 @@ describe('Delegated Rekey Search Effects', () => {
   });
 
   it('should dispatch the SearchBookedTestFailure action when searched with failure', (done) => {
-
+    spyOn(searchProvider, 'getTestResult')
+      .and.returnValue(asyncError(getTestResultHttpErrorResponse(HttpStatusCodes.BAD_REQUEST)));
     spyOn(delegatedRekeySearchProvider, 'getDelegatedExaminerBookingByAppRef')
       .and.returnValue(asyncError(new HttpErrorResponse({
         error: 'Error message',
@@ -72,7 +89,8 @@ describe('Delegated Rekey Search Effects', () => {
   });
 
   it('should call getDelegatedExaminerBookingByAppRef on the test search provider', (done) => {
-
+    spyOn(searchProvider, 'getTestResult')
+      .and.returnValue(asyncError(getTestResultHttpErrorResponse(HttpStatusCodes.BAD_REQUEST)));
     spyOn(delegatedRekeySearchProvider, 'getDelegatedExaminerBookingByAppRef').and.callThrough();
 
     actions$.next(new delegatedRekeySearchActions.SearchBookedDelegatedTest(appRef));
@@ -83,6 +101,24 @@ describe('Delegated Rekey Search Effects', () => {
     });
 
   });
+
+  it('should not call getBooking if getTestResult succeeds', (done) => {
+
+    spyOn(delegatedRekeySearchProvider, 'getDelegatedExaminerBookingByAppRef').and.callThrough();
+
+    const expectedFailureAction = new delegatedRekeySearchActions.SearchBookedDelegatedTestFailure({
+      message: DelegatedRekeySearchErrorMessages.BookingAlreadyCompleted,
+    });
+    actions$.next(new delegatedRekeySearchActions.SearchBookedDelegatedTest(appRef));
+
+    effects.getBooking$.subscribe((result) => {
+      expect(delegatedRekeySearchProvider.getDelegatedExaminerBookingByAppRef).not.toHaveBeenCalled();
+      expect(result).toEqual(expectedFailureAction);
+      done();
+    });
+
+  });
+
 });
 
 function asyncError(errorObject: any) {
