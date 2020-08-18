10533ae0f9e44df9601c24b4c976ea1eadb9a897
Tom <thomascrawley99@gmail.com>
2020-08-18 15:53:35 +0100

    revert cat be office page stuff and introduce delegated non cpc page

--- src/pages/office/cat-be/office.cat-be.page.ts
+++ src/pages/office/cat-be/office.cat-be.page.ts
@@ -17,7 +17,7 @@ import {
   SavingWriteUpForLater,
   OfficeValidationError,
 } from '../office.actions';
-import { Observable, merge, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { FormGroup } from '@angular/forms';
 import {
   getCurrentTest,
@@ -108,8 +108,6 @@ import {
 import { getVehicleChecks } from '../../../modules/tests/test-data/cat-be/test-data.cat-be.selector';
 import { ExaminerRole } from '../../../providers/app-config/constants/examiner-role.constants';
 import { AppConfigProvider } from '../../../providers/app-config/app-config';
-import { getDelegatedTestIndicator } from '../../../modules/tests/delegated-test/delegated-test.reducer';
-import { isDelegatedTest } from '../../../modules/tests/delegated-test/delegated-test.selector';
 
 interface OfficePageState {
   activityCode$: Observable<ActivityCodeModel>;
@@ -147,7 +145,6 @@ interface OfficePageState {
   seriousFaults$: Observable<FaultSummary[]>;
   isRekey$: Observable<boolean>;
   vehicleChecks$: Observable<QuestionResult[]>;
-  delegatedTest$: Observable<boolean>;
 }
 
 @IonicPage()
@@ -162,12 +159,10 @@ export class OfficeCatBEPage extends BasePageComponent {
   drivingFaultCtrl: String = 'drivingFaultCtrl';
   seriousFaultCtrl: String = 'seriousFaultCtrl';
   dangerousFaultCtrl: String = 'dangerousFaultCtrl';
-  isDelegated: boolean = false;
   static readonly maxFaultCount = 15;
 
   weatherConditions: WeatherConditionSelection[];
   activityCodeOptions: ActivityCodeModel[];
-  subscription: Subscription;
 
   constructor(
     private store$: Store<StoreModel>,
@@ -341,13 +336,13 @@ export class OfficeCatBEPage extends BasePageComponent {
           )),
       ),
       displayVehicleChecks$: currentTest$.pipe(
-          select(getTestOutcome),
-          withLatestFrom(currentTest$.pipe(
-            select(getTestData))),
-            map(([outcome, data]) =>
-            this.outcomeBehaviourProvider.isVisible(outcome,
-              'vehicleChecks',
-              vehicleChecksExist(data.vehicleChecks))),
+        select(getTestOutcome),
+        withLatestFrom(currentTest$.pipe(
+          select(getTestData))),
+        map(([outcome, data]) =>
+          this.outcomeBehaviourProvider.isVisible(outcome,
+            'vehicleChecks',
+            vehicleChecksExist(data.vehicleChecks))),
       ),
       candidateDescription$: currentTest$.pipe(
         select(getTestSummary),
@@ -404,22 +399,7 @@ export class OfficeCatBEPage extends BasePageComponent {
         select(getVehicleChecks),
         map(checks => [...checks.tellMeQuestions, ...checks.showMeQuestions]),
       ),
-      delegatedTest$: currentTest$.pipe(
-        select(getDelegatedTestIndicator),
-        select(isDelegatedTest),
-      ),
     };
-    this.setupSubscription();
-  }
-
-  setupSubscription() {
-    this.subscription = merge(
-      this.pageState.delegatedTest$.pipe(map(value => this.isDelegated = value)),
-      ).subscribe();
-  }
-
-  ionViewDidLeave() {
-    this.subscription.unsubscribe();
   }
 
   popToRoot() {
