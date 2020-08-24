a34cb35ce953d12aa338616f09f8ae12997cc1cd
Tom <thomascrawley99@gmail.com>
2020-08-24 09:33:44 +0100

    amended all changes to cat c & reintroduced back button, started work on health dec toggle

--- src/pages/office/components/delegated-health-declaration/delegated-health-declaration.ts
+++ src/pages/office/components/delegated-health-declaration/delegated-health-declaration.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 export enum ValidHealthDeclarationValues {
   SIGNED = 'Signed',
@@ -13,10 +14,31 @@ export class DelegatedHealthDeclarationComponent implements OnChanges {
   @Input()
   healthDeclarationSigned: boolean;
 
+  @Input()
+  formGroup: FormGroup;
+
+  @Output()
+  healthDeclarationChange = new EventEmitter<boolean>();
+
+  static readonly fieldName: string = 'healthDeclaration';
+  private formControl: FormControl;
+
   ngOnChanges(): void {
+    if (!this.formControl) {
+      this.formControl = new FormControl(null);
+      this.formGroup.addControl(DelegatedHealthDeclarationComponent.fieldName, this.formControl);
+    }
+    if (this.healthDeclarationSigned) {
+      this.formControl.patchValue(ValidHealthDeclarationValues.SIGNED);
+    } else {
+      this.formControl.patchValue(ValidHealthDeclarationValues.NOT_SIGNED);
+    }
   }
 
   healthDeclarationChanged(formValue: string): void {
+    if (this.formControl.valid) {
+      this.healthDeclarationChange.emit(formValue === ValidHealthDeclarationValues.SIGNED);
+    }
   }
 
 }
