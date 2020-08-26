91e0ba51f6fe300ff8ebe3d75f072269bf62191c
andrew <andrew.setterfield@dvsa.gov.uk>
2020-08-26 09:57:08 +0100

    add slotid to mock details to allow unique tests

--- src/providers/delegated-rekey-search/delegated-rekey-search.ts
+++ src/providers/delegated-rekey-search/delegated-rekey-search.ts
@@ -8,28 +8,28 @@ import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/
 export class DelegatedRekeySearchProvider {
 
   applicationReferences = [
-    { referenceNumber: '12345678910', testCategory: TestCategory.BE, isWelshTest: false },
-    { referenceNumber: '12345678911', testCategory: TestCategory.BE, isWelshTest: true },
-    { referenceNumber: '12345678912', testCategory: TestCategory.C, isWelshTest: false },
-    { referenceNumber: '12345678913', testCategory: TestCategory.C, isWelshTest: true },
-    { referenceNumber: '12345678914', testCategory: TestCategory.C1, isWelshTest: false },
-    { referenceNumber: '12345678915', testCategory: TestCategory.C1, isWelshTest: true },
-    { referenceNumber: '12345678916', testCategory: TestCategory.C1E, isWelshTest: false },
-    { referenceNumber: '12345678917', testCategory: TestCategory.C1E, isWelshTest: true },
-    { referenceNumber: '12345678918', testCategory: TestCategory.CE, isWelshTest: false },
-    { referenceNumber: '12345678919', testCategory: TestCategory.CE, isWelshTest: true },
-    { referenceNumber: '12345678920', testCategory: TestCategory.D, isWelshTest: false },
-    { referenceNumber: '12345678921', testCategory: TestCategory.D, isWelshTest: true },
-    { referenceNumber: '12345678922', testCategory: TestCategory.D1, isWelshTest: false },
-    { referenceNumber: '12345678923', testCategory: TestCategory.D1, isWelshTest: true },
-    { referenceNumber: '12345678924', testCategory: TestCategory.DE, isWelshTest: false },
-    { referenceNumber: '12345678925', testCategory: TestCategory.DE, isWelshTest: true },
-    { referenceNumber: '12345678926', testCategory: TestCategory.D1E, isWelshTest: false },
-    { referenceNumber: '12345678927', testCategory: TestCategory.D1E, isWelshTest: true },
-    { referenceNumber: '12345678928', testCategory: TestCategory.CCPC, isWelshTest: false },
-    { referenceNumber: '12345678929', testCategory: TestCategory.CCPC, isWelshTest: true },
-    { referenceNumber: '12345678930', testCategory: TestCategory.DCPC, isWelshTest: false },
-    { referenceNumber: '12345678931', testCategory: TestCategory.DCPC, isWelshTest: true },
+    { referenceNumber: '12345678910', testCategory: TestCategory.BE, isWelshTest: false, slotId: 1234567 },
+    { referenceNumber: '12345678911', testCategory: TestCategory.BE, isWelshTest: true, slotId: 1234568 },
+    { referenceNumber: '12345678912', testCategory: TestCategory.C, isWelshTest: false, slotId: 1234569 },
+    { referenceNumber: '12345678913', testCategory: TestCategory.C, isWelshTest: true, slotId: 1234570 },
+    { referenceNumber: '12345678914', testCategory: TestCategory.C1, isWelshTest: false, slotId: 1234571 },
+    { referenceNumber: '12345678915', testCategory: TestCategory.C1, isWelshTest: true, slotId: 1234572 },
+    { referenceNumber: '12345678916', testCategory: TestCategory.C1E, isWelshTest: false, slotId: 1234573 },
+    { referenceNumber: '12345678917', testCategory: TestCategory.C1E, isWelshTest: true, slotId: 1234574 },
+    { referenceNumber: '12345678918', testCategory: TestCategory.CE, isWelshTest: false, slotId: 1234575 },
+    { referenceNumber: '12345678919', testCategory: TestCategory.CE, isWelshTest: true, slotId: 1234576 },
+    { referenceNumber: '12345678920', testCategory: TestCategory.D, isWelshTest: false, slotId: 1234577 },
+    { referenceNumber: '12345678921', testCategory: TestCategory.D, isWelshTest: true, slotId: 1234578 },
+    { referenceNumber: '12345678922', testCategory: TestCategory.D1, isWelshTest: false, slotId: 1234579 },
+    { referenceNumber: '12345678923', testCategory: TestCategory.D1, isWelshTest: true, slotId: 1234580 },
+    { referenceNumber: '12345678924', testCategory: TestCategory.DE, isWelshTest: false, slotId: 1234581 },
+    { referenceNumber: '12345678925', testCategory: TestCategory.DE, isWelshTest: true, slotId: 1234582 },
+    { referenceNumber: '12345678926', testCategory: TestCategory.D1E, isWelshTest: false, slotId: 1234583 },
+    { referenceNumber: '12345678927', testCategory: TestCategory.D1E, isWelshTest: true, slotId: 1234584 },
+    { referenceNumber: '12345678928', testCategory: TestCategory.CCPC, isWelshTest: false, slotId: 1234585 },
+    { referenceNumber: '12345678929', testCategory: TestCategory.CCPC, isWelshTest: true, slotId: 1234586 },
+    { referenceNumber: '12345678930', testCategory: TestCategory.DCPC, isWelshTest: false, slotId: 1234587 },
+    { referenceNumber: '12345678931', testCategory: TestCategory.DCPC, isWelshTest: true, slotId: 1234588 },
   ];
   constructor() {
   }
@@ -42,7 +42,7 @@ export class DelegatedRekeySearchProvider {
     if (foundReference) {
       return of(new HttpResponse({
         status: 200,
-        body: getDelegatedBooking(foundReference.testCategory, foundReference.isWelshTest),
+        body: getDelegatedBooking(foundReference.testCategory, foundReference.isWelshTest, foundReference.slotId),
       }));
     }
     if (applicationReference === '12345678950') {
