5a428a5865899ec539e7f530e70bbae977eb2af9
andrew <andrew.setterfield@dvsa.gov.uk>
2020-09-02 13:38:08 +0100

    updated regex and rebased

--- src/shared/constants/field-validators/field-validators.ts
+++ src/shared/constants/field-validators/field-validators.ts
@@ -53,7 +53,7 @@ export const getPassCertificateAMOD1Validator = (): FieldValidators => {
 
 export const getDL196CBTCertificateNumberValidator = (): FieldValidators => {
   return {
-    pattern: /^[1-9][0-9]{6,6}$/g,
+    pattern: /^[1-9][0-9]{6}$/g,
     maxLength: '7',
   };
 };
