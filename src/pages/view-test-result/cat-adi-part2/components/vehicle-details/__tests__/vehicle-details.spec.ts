632e1b9b1658d7a64b874ba90775e2b8b444e656
andrew <andrew.setterfield@dvsa.gov.uk>
2020-10-27 14:46:11 +0000

    added functions to display vehicle details section of Vehicle details for ADI2 if details were recorded

--- src/pages/view-test-result/cat-adi-part2/components/vehicle-details/__tests__/vehicle-details.spec.ts
+++ src/pages/view-test-result/cat-adi-part2/components/vehicle-details/__tests__/vehicle-details.spec.ts
@@ -13,6 +13,8 @@ describe('VehicleDetailsCatADIPt2Component', () => {
   let component: VehicleDetailsCatADIPt2Component;
 
   const data = { gearboxCategory: 'Manual', registrationNumber: 'ABC123' };
+  const schoolCarData = { schoolCar: true, gearboxCategory: 'Manual', registrationNumber: 'ABC123' };
+  const dualControlsData = { dualControls: true, gearboxCategory: 'Manual', registrationNumber: 'ABC123' };
   const trainerDetails = {
     trainerRegistrationNumber: 123456,
     trainingRecords: null,
@@ -40,37 +42,28 @@ describe('VehicleDetailsCatADIPt2Component', () => {
   }));
 
   describe('Class', () => {
+
     describe('getTransmission', () => {
       it('should return the value of the gearboxCategory', () => {
         component.data = data as CatADI2UniqueTypes.VehicleDetails;
         expect(component.getTransmission()).toEqual('Manual');
       });
     });
+
     describe('getRegistrationNumber', () => {
       it('should return the value of the registrationNumber', () => {
         component.data = data as CatADI2UniqueTypes.VehicleDetails;
         expect(component.getRegistrationNumber()).toEqual('ABC123');
       });
     });
+
     describe('getTrainerPRN', () => {
       it('should return the value of the trainerRegistrationNumber', () => {
         component.trainerData = trainerDetails;
         expect(component.getTrainerPRN()).toEqual(123456);
       });
     });
-    describe('getTrainingRecords', () => {
-      it('should return `No` as default when null or undefined', () => {
-        expect(component.getTrainingRecords()).toEqual('No');
-      });
-      it('should return `No` when trainingRecords is false', () => {
-        component.trainerData = { trainingRecords: false } as CatADI2UniqueTypes.TrainerDetails;
-        expect(component.getTrainingRecords()).toEqual('No');
-      });
-      it('should return `Yes` when trainingRecords is true', () => {
-        component.trainerData = { trainingRecords: true } as CatADI2UniqueTypes.TrainerDetails;
-        expect(component.getTrainingRecords()).toEqual('Yes');
-      });
-    });
+
     describe('getOrdit', () => {
       it('should return `No` as default when null or undefined', () => {
         expect(component.getOrdit()).toEqual('No');
@@ -84,5 +77,46 @@ describe('VehicleDetailsCatADIPt2Component', () => {
         expect(component.getOrdit()).toEqual('Yes');
       });
     });
+
+    describe('getTrainingRecords', () => {
+      it('should return `No` as default when null or undefined', () => {
+        expect(component.getTrainingRecords()).toEqual('No');
+      });
+      it('should return `No` when trainingRecords is false', () => {
+        component.trainerData = { trainingRecords: false } as CatADI2UniqueTypes.TrainerDetails;
+        expect(component.getTrainingRecords()).toEqual('No');
+      });
+      it('should return `Yes` when trainingRecords is true', () => {
+        component.trainerData = { trainingRecords: true } as CatADI2UniqueTypes.TrainerDetails;
+        expect(component.getTrainingRecords()).toEqual('Yes');
+      });
+    });
+
+    describe('displayVehicleDetails', () => {
+      it('should return false if school car or dual controls dont exist', () => {
+        component.data = data as CatADI2UniqueTypes.VehicleDetails;
+        expect(component.displayVehicleDetails()).toEqual(false);
+      });
+      it('should return true if school car present', () => {
+        component.data = schoolCarData as CatADI2UniqueTypes.VehicleDetails;
+        expect(component.displayVehicleDetails()).toEqual(true);
+      });
+      it('should return true if duelControls present', () => {
+        component.data = dualControlsData as CatADI2UniqueTypes.VehicleDetails;
+        expect(component.displayVehicleDetails()).toEqual(true);
+      });
+    });
+
+    describe('getVehicleDetails', () => {
+      it('should return School Car if school car exists', () => {
+        component.data = schoolCarData as CatADI2UniqueTypes.VehicleDetails;
+        expect(component.getVehicleDetails()).toEqual('School Car');
+      });
+      it('should return Dual Controls if school car doesnt exist', () => {
+        component.data = dualControlsData as CatADI2UniqueTypes.VehicleDetails;
+        expect(component.getVehicleDetails()).toEqual('Dual Controls');
+      });
+    });
+
   });
 });
