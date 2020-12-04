6995166878f69b66851563ef78570ba29571bb92
cai <cai.turner@dvsa.gov.uk>
2020-12-04 09:45:08 +0000

    fixed cat be

--- test/test_data/ui_test_data.ts
+++ test/test_data/ui_test_data.ts
@@ -1,7 +1,7 @@
 export const UI_TEST_DATA = {
   testData: {
-    be: ['S01 - Direction indicators', 'S02 - Doors secure', 'S03 - Horn', 'T01 - Brakes',
-      'T02 - Safety factors while loading'],
+    be: ['E01 - Engine fluid levels', 'E02 - Direction indicators', 'E03 - Trailer doors', 'E20 - Safely hitched',
+      'E21 - Max weight for towing'],
     c: ['V01 - Air leaks', 'V08 - Dipped to main beam', 'V09 - Direction indicators', 'V30 - Body condition', 'V38 - Tyres'],
     ce: ['V01 - Air leaks', 'V30 - Body condition'],
     a1: ['A1'],
