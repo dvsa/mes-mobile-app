e87b525f737d053ca44926279a2e4f5f2b2f9355
Matt <mculliford.dvsa@icloud.com>
2021-03-05 15:57:27 +0000

    add licence-provided-warning-banner component to pass finalisation pages

---
+++ src/pages/pass-finalisation/components/licence-provided-warning-banner/licence-provided-warning-banner.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'licence-provided-warning-banner',
+  templateUrl: 'licence-provided-warning-banner.html',
+})
+
+export class LicenceProvidedWarningBannerComponent {
+  @Input()
+  licenceProvided: boolean;
+
+  yesText: string = 'Please retain the candidate\'s licence';
+  noText: string = 'Please ensure the licence is kept by the candidate';
+
+  constructor() {
+
+  }
+
+}
