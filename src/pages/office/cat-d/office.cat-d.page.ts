d8c757868f7c9f1b0f872610431dd49e52ee0519
Cullima <33055124+Cullima@users.noreply.github.com>
2021-03-08 13:42:45 +0000

    mes-6604 changes to wording on office pages (#1905)
    Co-authored-by: Matt <mculliford.dvsa@icloud.com>

--- src/pages/office/cat-d/office.cat-d.page.ts
+++ src/pages/office/cat-d/office.cat-d.page.ts
@@ -727,15 +727,15 @@ export class OfficeCatDPage extends BasePageComponent {
 
   showFinishTestModal() {
     const alert = this.alertController.create({
-      title: 'Are you sure you want to upload this test?',
+      title: 'Are you sure you wish to mark the write up for this test as complete?',
       cssClass: 'finish-test-modal',
       buttons: [
         {
-          text: 'Cancel',
+          text: 'Back',
           handler: () => { },
         },
         {
-          text: 'Upload',
+          text: 'Continue',
           handler: () => this.completeTest(),
         },
       ],
