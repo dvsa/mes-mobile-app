fa2836a44892782d92109f6324b70ef5250f1008
syed muhammad naqvi <syedmuhammadnaqvi@c02yp5evlvdc.dsa.gsi.gov.uk>
2021-03-10 19:34:41 +0000

    saving login work

--- test/e2e/helper/LoginPage/loginPage.ts
+++ test/e2e/helper/LoginPage/loginPage.ts
@@ -26,21 +26,21 @@ export class LoginPage extends Page {
     const webviewContext = await browser.driver.getCurrentContext();
     // Switch to NATIVE context
     await browser.driver.selectContext('NATIVE_APP');
-    await browser.wait(ExpectedConditions.presenceOf(this.loginpageElement.microsoftOnlineContinue));
+    await browser.wait(ExpectedConditions.visibilityOf(this.loginpageElement.microsoftOnlineContinue), TEST_CONFIG.Element_Wait);
     await this.loginpageElement.microsoftOnlineContinue.click();
     const useAnotherAccountButton = element(by.xpath(`//XCUIElementTypeButton[@name="Use another account, Use another account"]`));
-    await browser.wait(ExpectedConditions.presenceOf(useAnotherAccountButton), TEST_CONFIG.Element_Wait);
+    await browser.wait(ExpectedConditions.visibilityOf(useAnotherAccountButton), TEST_CONFIG.Element_Wait);
     await useAnotherAccountButton.click();
 
     // Fill in username and click Next
 
-    await browser.wait(ExpectedConditions.presenceOf(getUsernameField));
+    await browser.wait(ExpectedConditions.visibilityOf(getUsernameField), TEST_CONFIG.Element_Wait);
     await getUsernameField.sendKeys(username);
     await this.loginpageElement.nextButtonElement.click();
-    await browser.wait(ExpectedConditions.presenceOf(this.loginpageElement.passwordForOrgAccount));
+    await browser.wait(ExpectedConditions.visibilityOf(this.loginpageElement.passwordForOrgAccount), TEST_CONFIG.Element_Wait);
     await this.loginpageElement.passwordForOrgAccount.sendKeys(password);
     await this.loginpageElement.signinForOrgAccount.click();
-    await browser.wait(ExpectedConditions.presenceOf(this.loginpageElement.authCContinue), TEST_CONFIG.Element_Wait);
+    await browser.wait(ExpectedConditions.visibilityOf(this.loginpageElement.authCContinue), TEST_CONFIG.Element_Wait);
     try {
       if (this.loginpageElement.authCContinue.isPresent()) {
         await this.loginpageElement.authCContinue.click();
@@ -52,14 +52,14 @@ export class LoginPage extends Page {
     await browser.driver.selectContext(this.getParentContext(webviewContext));
 
     // Wait for dashboard page to load
-    await browser.wait(ExpectedConditions.presenceOf(this.loginpageElement.employeeId));
+    await browser.wait(ExpectedConditions.visibilityOf(this.loginpageElement.employeeId), TEST_CONFIG.Element_Wait);
   }
 
   async clickOnContinue() {
     const webviewContext = await browser.driver.getCurrentContext();
     await browser.driver.selectContext('NATIVE_APP');
     // Wait until we are on the login page before proceeding
-    await browser.wait(ExpectedConditions.presenceOf(this.loginpageElement.microsoftOnlineContinue));
+    await browser.wait(ExpectedConditions.visibilityOf(this.loginpageElement.microsoftOnlineContinue), TEST_CONFIG.Element_Wait);
     await this.loginpageElement.microsoftOnlineContinue.click();
     // Switch back to WEBVIEW context
     await browser.driver.selectContext(this.getParentContext(webviewContext));
@@ -73,34 +73,33 @@ export class LoginPage extends Page {
   async logout() {
     const webviewContext = await browser.driver.getCurrentContext();
     await browser.driver.selectContext(this.getParentContext(webviewContext));
-    await browser.wait(ExpectedConditions.presenceOf(element(by.xpath('//ion-app'))));
-    await browser.wait(ExpectedConditions.stalenessOf(element(by.className('click-block-active'))));
+    await browser.wait(ExpectedConditions.visibilityOf(element(by.xpath('//ion-app'))), TEST_CONFIG.Element_Wait);
+    await browser.wait(ExpectedConditions.stalenessOf(element(by.className('click-block-active'))), TEST_CONFIG.Element_Wait);
     try {
       if (await this.loginpageElement.logout.isPresent()) {
-        await browser.wait(ExpectedConditions.elementToBeClickable(this.loginpageElement.logout));
+        await browser.wait(ExpectedConditions.elementToBeClickable(this.loginpageElement.logout), TEST_CONFIG.Element_Wait);
         await this.loginpageElement.logout.click();
         try {
-          await browser.sleep(TEST_CONFIG.ACTION_WAIT);
+          await browser.wait(ExpectedConditions.visibilityOf(this.loginpageElement.logoutPopup), TEST_CONFIG.Element_Wait);
           await this.loginpageElement.logoutPopup.click();
           // After logout click sign in to get us to the login screen
-          await browser.sleep(TEST_CONFIG.ACTION_WAIT);
         } catch {
         }
 
         await browser.driver.selectContext('NATIVE_APP');
         // Wait until we are on the login page before proceeding
-        await browser.wait(ExpectedConditions.presenceOf(this.loginpageElement.microsoftOnlineContinue));
+        await browser.wait(ExpectedConditions.visibilityOf(this.loginpageElement.microsoftOnlineContinue), TEST_CONFIG.Element_Wait);
         await this.loginpageElement.microsoftOnlineContinue.click();
         try {
           if (this.loginpageElement.selectUserToSignOut.isVisible()) {
-            await browser.wait(ExpectedConditions.presenceOf(this.loginpageElement.selectUserToSignOut));
+            await browser.wait(ExpectedConditions.visibilityOf(this.loginpageElement.selectUserToSignOut), TEST_CONFIG.Element_Wait);
             await this.loginpageElement.selectUserToSignOut.click();
           }
         } catch {
         }
-        await browser.wait(ExpectedConditions.presenceOf(this.loginpageElement.cancelButton));
+        await browser.wait(ExpectedConditions.visibilityOf(this.loginpageElement.cancelButton), TEST_CONFIG.Element_Wait);
         await this.loginpageElement.cancelButton.click();
-        await browser.wait(ExpectedConditions.presenceOf(this.loginpageElement.signInLink));
+        await browser.wait(ExpectedConditions.visibilityOf(this.loginpageElement.signInLink), TEST_CONFIG.Element_Wait);
         await this.loginpageElement.signInLink.click();
         // Switch back to WEBVIEW context
         await browser.driver.selectContext(this.getParentContext(webviewContext));
