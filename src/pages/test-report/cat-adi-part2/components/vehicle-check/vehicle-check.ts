21ae5bac80d898206bd1206714ee2380ddf5ee05
Tom <thomascrawley99@gmail.com>
2021-06-09 16:15:22 +0100

    initial commit

--- src/pages/test-report/cat-adi-part2/components/vehicle-check/vehicle-check.ts
+++ src/pages/test-report/cat-adi-part2/components/vehicle-check/vehicle-check.ts
@@ -30,7 +30,7 @@ import { TestCategory } from '@dvsa/mes-test-schema/category-definitions/common/
 })
 export class VehicleCheckComponent implements OnInit, OnDestroy {
 
-  selectedShowMeQuestion: boolean;
+  selectedShowMeQuestion: boolean = false;
   showMeQuestionFaultCount: number;
   tellMeQuestionFaultCount: number;
 
@@ -115,8 +115,8 @@ export class VehicleCheckComponent implements OnInit, OnDestroy {
   }
 
   toggleShowMeQuestion = (): void => {
-    this.store$.dispatch(new VehicleChecksCompletedToggle());
     this.selectedShowMeQuestion = !this.selectedShowMeQuestion;
+    this.store$.dispatch(new VehicleChecksCompletedToggle());
   }
 
   canButtonRipple = () => {
