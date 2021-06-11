d8d5f24d13e5ae933865156a9bb5bb193aa7bdb9
hiddlestonej <john.hiddlestone@dvsa.gov.uk>
2021-06-11 16:58:03 +0100

    Increased verbosity of karma logging

--- test-config/karma.conf.js
+++ test-config/karma.conf.js
@@ -56,7 +56,7 @@ module.exports = function(config) {
     },
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
     autoWatch: true,
     specReporter: {
       suppressSkipped: true,
