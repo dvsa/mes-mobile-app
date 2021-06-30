078a9e72b484e02eb241e7f44f3a6184afc6454a
andrewsetterfield <48550267+andrewsetterfield@users.noreply.github.com>
2021-06-30 14:37:46 +0100

    mes-6966: Disable inputs when eyesight failed (#1980)
    * add functionality to disable inputs when eyesight failed

--- src/components/test-finalisation/d255/d255.ts
+++ src/components/test-finalisation/d255/d255.ts
@@ -51,6 +51,7 @@ export class D255Component implements OnChanges {
     }
 
     this.formControl.patchValue(this.getD255OrDefault());
+    if (this.eyesightTestFailed) this.formGroup.get(D255Component.fieldName).disable();
   }
 
   d255Changed(d255FormValue: string): void {
